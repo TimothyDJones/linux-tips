9e89765a719771d448635e8cd0edfacd121ede01
Virtual Box User <Tim Jones>
2017-03-27 14:13:30 -0500

    Add 'ubuntu_setup.sh' shell script for setting up new Ubuntu installation like I want.

---
+++ ubuntu_setup.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+# Add some necessary non-default packages
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get autoremove -f -y
+sudo apt-get install -y build-essential dtrx curl wget
+
+# Allow current user to run 'sudo' without password
+# https://phpraxis.wordpress.com/2016/09/27/enable-sudo-without-password-in-ubuntudebian/
+# http://stackoverflow.com/a/28382838
+echo "${USER}" > /tmp/user.tmp
+sudo bash -c 'echo "`cat /tmp/user.tmp` ALL=(ALL) NOPASSWD:ALL" | (EDITOR="tee -a" visudo)'
+rm -f /tmp/user.tmp
+
+# Add official Git package repository
+sudo apt-add-repository -y ppa:git-core/ppa
+
+# Add PHP 5.6/7.0/7.1 package repository
+sudo apt-add-repository -y ppa:ondrej/php
+
+# Add Vim 8.x package repository
+# https://itsfoss.com/vim-8-release-install/
+sudo apt-add-repository -y ppa:jonathonf/vim
+
+# Add NodeJS package repository
+# https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
+# Install NodeJS, Vim 8, and Git
+sudo apt-get install -y vim vim-gtk3 vim-common \
+	git \
+	nodejs
+
+
+# Install PHP 5.6, Apache 2, and MySQL Server
+export DEBIAN_FRONTEND=noninteractive
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password mysql_password'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password mysql_password'
+sudo apt-get install -y php5.6-bcmath php5.6-bz2 php5.6-cli php5.6-common php5.6-curl php5.6-gd php5.6-json php5.6-mbstring php5.6-mcrypt php5.6-mysql php5.6-readline php5.6-sqlite3 php5.6-xml php5.6-xsl php5.6-zip php-xdebug \
+libapache2-mod-php5.6 libapache2-mod-xsendfile \
+mysql-server mysql-workbench
+
+# Add current user to 'www-data' group
+sudo usermod -a -G www-data ${USER}
+
+# Change owner of /var/www/html directory to www-data
+sudo chown -R www-data:www-data /var/www/html
+
+# Create simple 'phpinfo' script in main web server directory
+# Note: Must create file in /tmp and then move because 'sudo cat...' is allowed.
+sudo cat > /tmp/phpinfo.php << EOL
+<?php
+	phpinfo();
+?>
+EOL
+sudo mv /tmp/phpinfo.php /var/www/html
+sudo chown www-data:www-data /var/www/html/phpinfo.php
+
+# Install PHP Composer as global utility
+php -r "copy('https://getcomposer.org/installer', '/tmp/composer-setup.php');"
+sudo php /tmp/composer-setup.php --install-dir=/usr/local --filename=composer
+php -r "unlink('/tmp/composer-setup.php');"
+rm -f /tmp/composer-setup.php
+
+
