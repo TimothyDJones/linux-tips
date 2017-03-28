c972a05e5062c09ac2358b7f7e4ebf4ec863bf0c
Virtual Box User <Tim Jones>
2017-03-28 10:25:41 -0500

    Add commands to 'ubuntu_setup.sh' to install DBeaver Java database utility.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -108,3 +108,14 @@ curl -o stacer.deb -A "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20
 sudo gdebi -n stacer.deb   # '-n' is non-interactive mode for gdebi
 rm -f stacer.deb
 
+
+# Install DBeaver Java database utility
+cd $HOME/Downloads
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	curl -o dbeaver.deb -J -L http://dbeaver.jkiss.org/files/dbeaver-ce_latest_amd64.deb
+else    # Otherwise use version for 32-bit kernel
+	curl -o dbeaver.deb -J -L http://dbeaver.jkiss.org/files/dbeaver-ce_latest_i386.deb
+fi
+sudo gdebi -n dbeaver.deb
+rm -f dbeaver.deb
+cd $HOME
