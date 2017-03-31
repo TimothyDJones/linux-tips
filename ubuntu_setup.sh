bf06df6d8ae39351749ec6562fbb41711fc2e4d2
Virtual Box User <Tim Jones>
2017-03-31 09:31:28 -0500

    Update version of Cudatext editor to 1.7.9.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -167,7 +167,7 @@ sudo gdebi -n /tmp/vivaldi.deb
 rm -f /tmp/vivaldi.deb
 
 # Install Cudatext editor from Sourceforge
-curl -o /tmp/cudatext.deb -J -L https://cytranet.dl.sourceforge.net/project/cudatext/release/Linux/cudatext_1.7.8.0-1_gtk2_amd64.deb
+curl -o /tmp/cudatext.deb -J -L https://cytranet.dl.sourceforge.net/project/cudatext/release/Linux/cudatext_1.7.9.0-1_gtk2_amd64.deb
 sudo gdebi -n /tmp/cudatext.deb
 rm -f /tmp/cudatext.deb
 
