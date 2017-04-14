786edb14ad8317c887f861bdc9cd209db22a140a
Virtual Box User <tdjones74021@yahoo.com>
2017-04-14 13:21:16 -0500

    Correct name of 'checkinstall' package in ubuntu_setup.sh.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -12,7 +12,7 @@ fi
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get autoremove -f -y
-sudo apt-get install -y build-essential dtrx curl wget check-install gdebi \
+sudo apt-get install -y build-essential dtrx curl wget checkinstall gdebi \
 	openjdk-8-jre python-software-properties software-properties-common \
 	mc
 
