9710aa19d2c7ec8f23d26e55eb5c37f6c0d05ffd
Virtual Box User <tdjones74021@yahoo.com>
2017-08-02 09:11:21 -0500

    Add commands to install Super Productivity To Do List and task manager from package to ubuntu_setup.sh.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1363,4 +1363,18 @@ sudo ln -s /opt/${APP_NAME,,}/${APP_NAME} /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/${APP_NAME}*
 
+# Install GeoServer Java-based mapping/GIS server
+APP_NAME=geoserver
+APP_VERSION=2.11.2
+APP_EXT=zip
+curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME}/${APP_NAME}-${APP_VERSION}-bin.${APP_EXT}
+cd /tmp
 
+# Install Super Productivity To Do List and task manager from package
+APP_NAME=superProductivity
+APP_VERSION=latest
+APP_EXT=deb
+curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/super-productivity/${APP_NAME}_${APP_VERSION}_amd64.${APP_EXT}
+sudo gdebi -n /tmp/${APP_NAME}.${APP_EXT}
+cd $HOME
+rm -rf ${APP_NAME)*
