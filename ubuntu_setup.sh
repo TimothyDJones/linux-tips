97c04a2179996e7fa59374191e877871794eaf3d
Virtual Box User <tdjones74021@yahoo.com>
2017-09-06 10:38:03 -0500

    Update Brave browser to use standard Sourceforge download URL.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1293,7 +1293,7 @@ rm -rf /tmp/${APP_NAME}*
 APP_NAME=brave
 APP_VERSION=0.19.5
 APP_EXT=deb
-curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://iweb.dl.sourceforge.net/project/brave-browser.mirror/v${APP_VERSION}beta/${APP_NAME}_${APP_NAME}_${KERNEL_TYPE}.${APP_EXT}
+curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME}-browser.mirror/${APP_NAME}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME}*
