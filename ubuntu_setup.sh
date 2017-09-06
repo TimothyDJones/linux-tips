1b08d71a893c64423c581fd5d65632df5e7385df
Virtual Box User <tdjones74021@yahoo.com>
2017-09-06 10:31:13 -0500

    Update Brave web browser to version 0.19.5.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1291,7 +1291,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Brave web browser from package
 APP_NAME=brave
-APP_VERSION=0.18.13
+APP_VERSION=0.19.5
 APP_EXT=deb
 curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://iweb.dl.sourceforge.net/project/brave-browser.mirror/v${APP_VERSION}beta/${APP_NAME}_${APP_NAME}_${KERNEL_TYPE}.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME}.${APP_EXT}
