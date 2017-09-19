f7592781f8a901b7da46c5551b15f4fb274a04f5
Virtual Box User <tdjones74021@yahoo.com>
2017-09-19 09:18:14 -0500

    Add commands to install Task Coach to do list manager.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2289,3 +2289,13 @@ cd /tmp/${APP_NAME,,}/${APP_NAME}-${APP_VERSION}
 ./configure && make && sudo make install
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install Task Coach to do list manager from package
+APP_NAME=taskcoach
+APP_VERSION=1.4.3-1
+APP_EXT=deb
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME}_${APP_VERSION}.${APP_EXT}
+cd /tmp
+sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
