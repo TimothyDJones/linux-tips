ec2588a62eeb125da2eadf228204b444f24000fc
Virtual Box User <tdjones74021@yahoo.com>
2017-10-03 10:02:26 -0500

    Add commands to install Shotcut video editor.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2802,3 +2802,35 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install Shotcut video editor
+APP_NAME=Shotcut
+APP_VERSION=171002
+APP_EXT=tar.bz2
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	ARCH_TYPE=x86_64
+else    # Otherwise use version for 32-bit kernel
+	ARCH_TYPE=i386
+fi
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${APP_NAME,,}-linux-${ARCH_TYPE}-${APP_VERSION}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${APP_NAME,,}.${APP_EXT}
+sudo mv /tmp/${APP_NAME,,}/${APP_NAME} /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=Cross-platform Video Editor
+GenericName=Video Editor
+Exec=/opt/${APP_NAME,,}/${APP_NAME}.app/${APP_NAME,,}
+Icon=applications-multimedia
+Path=/opt/${APP_NAME,,}/${APP_NAME}.app
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Video;Multimedia;
+Keywords=Video;Editor;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+sudo ln -s /opt/${APP_NAME,,}/${APP_NAME}.app/${APP_NAME,,} /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
