2a98368322b6f3401063bf6965d72e01f7d52626
Tim Jones <tdjones74021@yahoo.com>
2017-10-08 17:36:31 -0500

    Add commands to install NixNote open-source Evernote client from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2931,3 +2931,12 @@ curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://dl.opendesktop.org/api/files
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install NixNote open source Evernote client from package
+APP_NAME=nixnote
+APP_VERSION=2.0.2
+APP_EXT=deb
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/nevernote/${APP_NAME}2-${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
+sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
