0d4112f2d13b8db415016c366ab572242301c94d
Tim Jones <tdjones74021@yahoo.com>
2017-10-15 19:17:58 -0500

    Update Freeplane mind mapping tool to version 1.6.9.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1860,7 +1860,7 @@ sudo mv /tmp/${APP_NAME} /usr/local/bin
 
 # Install Freeplane mind-mapping tool from package
 APP_NAME=freeplane
-APP_VERSION=1.6.6
+APP_VERSION=1.6.9
 APP_EXT=deb
 curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME}/${APP_NAME}_${APP_VERSION}~upstream-1_all.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME}.${APP_EXT}
