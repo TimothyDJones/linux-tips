e3b14ebcf241c44b777c493eeda472dd618c5d14
Virtual Box User <tdjones74021@yahoo.com>
2017-10-18 06:58:00 -0500

    Update Jailer Java database utility to version 7.5.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -698,7 +698,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Jailer Java database utility
 APP_NAME=jailer
-APP_VERSION=7.5.1
+APP_VERSION=7.5.2
 curl -o /tmp/${APP_NAME}.zip -J -L https://cytranet.dl.sourceforge.net/project/${APP_NAME}/v${APP_VERSION}/${APP_NAME}_${APP_VERSION}.zip
 cd /tmp
 dtrx -n ${APP_NAME}.zip
