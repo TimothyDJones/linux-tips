53cc2cf5e4643a5ad85d578254892e09306b0deb
Tim Jones <tdjones74021@yahoo.com>
2017-10-29 19:18:05 -0500

    Update ubunsys installer/tweaker to version 2017.10.29.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -968,7 +968,7 @@ curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://ayera.dl.sourceforge.net/proje
 
 # Install ubunsys installer/tweaker
 APP_NAME=ubunsys
-APP_VERSION=2017.09.24
+APP_VERSION=2017.10.29
 APP_EXT=deb
 source /etc/os-release   # This config file contains Ubuntu version details.
 curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://versaweb.dl.sourceforge.net/project/${APP_NAME}/v${APP_VERSION}/${APP_NAME}_${APP_VERSION}_${KERNEL_TYPE}_${VERSION_ID}.${APP_EXT}
