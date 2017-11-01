ea6f9f6509b011a45164daa890a9074d536d9a2a
Tim Jones <tdjones74021@yahoo.com>
2017-11-01 20:21:32 +0000

    Add commands to install SmallBASIC interpreter from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3352,3 +3352,12 @@ curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install SmallBASIC interpreter from package
+APP_NAME=smallbasic
+APP_VERSION=0.12.9
+APP_EXT=deb
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME}/${APP_NAME}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
+sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
