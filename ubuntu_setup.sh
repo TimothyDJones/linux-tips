b6e2f3a71ec457814d3bddb0ddf8c16980d18c0c
Tim Jones <tdjones74021@yahoo.com>
2017-11-10 19:40:16 +0000

    Add commands to install JSoko Java-based Sokoban puzzle game from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3778,3 +3778,12 @@ curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install JSoko Java-based Sokoban puzzle game from package
+APP_NAME=JSoko
+APP_VERSION=1.79
+APP_EXT=deb
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}applet/${APP_NAME}_${APP_VERSION}_linux.${APP_EXT}
+sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
