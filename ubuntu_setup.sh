6721c8b3e22a76a3852effb0842a328294e6835d
Tim Jones <tdjones74021@yahoo.com>
2017-11-13 22:14:06 +0000

    Update Otter Browser to version 0.9.92-dev200.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -334,13 +334,13 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Otter Browser from Sourceforge (from source)
 APP_NAME=otter-browser
-APP_VERSION=195
+APP_VERSION=0.9.92-dev200
 APP_EXT=tar.bz2
 sudo apt-get install -y qt5-default libqt5multimedia5 qtmultimedia5-dev libqt5xmlpatterns5-dev libqt5webkit5-dev   # Qt5 development packages needed to build from source
-curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME}/${APP_NAME}-0.9.91-dev${APP_VERSION}.${APP_EXT}
+curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME}/${APP_NAME}-${APP_VERSION}.${APP_EXT}
 cd /tmp
 dtrx -n /tmp/${APP_NAME}.${APP_EXT}
-cd /tmp/otter-browser/${APP_NAME}-0.9.91-dev${APP_VERSION}
+cd /tmp/otter-browser/${APP_NAME}-${APP_VERSION}
 mkdir build && cd build
 cmake .. && make && sudo make install
 cd $HOME
