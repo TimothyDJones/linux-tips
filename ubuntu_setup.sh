da23016bf02d9a408323d300c6a02f81db074e3c
Tim Jones <tdjones74021@yahoo.com>
2017-11-22 21:20:14 -0600

    Update Insomnia REST client to version 5.11.7.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3552,7 +3552,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Insomnia REST client from package
 APP_NAME=insomnia
-APP_VERSION=5.11.5
+APP_VERSION=5.11.7
 APP_EXT=deb
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://builds.insomnia.rest/downloads/ubuntu/latest
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
