1ed82839bdcb290b227bbafb04b75c252ddbb725
Tim Jones <tdjones74021@yahoo.com>
2017-11-27 16:56:05 +0000

    Add commands to install Modelio UML modeling tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4548,3 +4548,12 @@ curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://github.com/${APP_NAME,,}/${A
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install Modelio UML modeling tool from package
+APP_NAME=Modelio
+APP_VERSION=3.7_3.7.0-201711211349
+APP_EXT=deb
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}uml/${APP_NAME,,}-open-source${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
+sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
