ef72e14824679b5ac56de335eb69cc1944e5fc29
Tim Jones <tdjones74021@yahoo.com>
2017-11-27 17:29:38 +0000

    Add commands to install Dooble web browser from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4557,3 +4557,12 @@ curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install Dooble web browser from package
+APP_NAME=Dooble
+APP_VERSION=2.1.1
+APP_EXT=deb
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
