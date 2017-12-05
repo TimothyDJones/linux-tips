a70492886ae4d268de4e7661c0a933e72b129704
Tim Jones <tdjones74021@yahoo.com>
2017-12-05 11:39:33 -0600

    Update Hyper JS/HTML/CSS Terminal to version 2.0.0-canary.8.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1762,7 +1762,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Hyper JS/HTML/CSS Terminal 
 APP_NAME=hyper
-APP_VERSION=2.0.4
+APP_VERSION=2.0.0-canary.8
 APP_EXT=deb
 curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/hyper.mirror/${APP_NAME}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME}.${APP_EXT}
