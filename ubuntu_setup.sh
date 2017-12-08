73440bb126d15e5be59e33064bd3eabc2551e8ad
Tim Jones <tdjones74021@yahoo.com>
2017-12-08 12:54:01 -0600

    Update ProjeQtor web-based project management tool to version 6.5.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1696,7 +1696,7 @@ xdg-open http://localhost/${APP_NAME}/setup.php &
 
 # Install ProjeQtor web-based project management tool
 APP_NAME=projeqtor
-APP_VERSION=6.4.4
+APP_VERSION=6.5.0
 APP_EXT=zip
 DB_NAME=projeqtor
 DB_USER=projeqtor
