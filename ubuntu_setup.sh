9e154e6f92c904a3d8db87b16f8e9089be209dcb
Tim Jones <tdjones74021@yahoo.com>
2017-12-08 12:37:12 -0600

    Add commands to install pgweb cross-platform client for PostgreSQL databases.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4880,3 +4880,46 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install pgweb cross-platform client for PostgreSQL databases 
+APP_NAME=pgweb
+APP_GUI_NAME="Cross-platform client for PostgreSQL databases"
+APP_VERSION=0.9.11
+APP_EXT=zip
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	ARCH_TYPE=linux_amd64
+else    # Otherwise use version for 32-bit kernel
+	ARCH_TYPE=linux_386
+fi
+sudo apt-get install -y libasound2-dev wavpack
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${APP_NAME,,}_${ARCH_TYPE}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${APP_NAME,,}.${APP_EXT}
+sudo mv /tmp/${APP_NAME,,} /opt
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:$PATH; export PATH
+/opt/${APP_NAME,,}/${APP_NAME,,}_${ARCH_TYPE} & 
+xdg-open http://localhost:8081/ &
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+#Icon=/usr/local/share/snd/s.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Development;Programming;
+Keywords=Database;PostgreSQL;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
