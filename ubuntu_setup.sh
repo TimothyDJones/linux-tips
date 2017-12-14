77584c635dfe25e07fa442ad261484c6087b2858
Tim Jones <tdjones74021@yahoo.com>
2017-12-14 13:09:08 -0600

    Update Dooble web browser to version 2.1.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4572,9 +4572,9 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Dooble web browser from package
 APP_NAME=Dooble
-APP_VERSION=2.1.1
+APP_VERSION=2.1.2
 APP_EXT=deb
-curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME}-${APP_VERSION}.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
