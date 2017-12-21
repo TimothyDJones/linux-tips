a6eb79e3359db8a0e2c1897324cccd0d48b79919
Tim Jones <tdjones74021@yahoo.com>
2017-12-21 15:39:10 -0600

    Add commands to install Alpus cross-platform offline dictionary viewer from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5323,3 +5323,48 @@ sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 sudo ln -s /usr/bin/${APP_NAME} /usr/local/bin/${APP_NAME,,}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Alpus cross-platform offline dictionary viewer from package
+APP_NAME=Alpus
+APP_GUI_NAME="Freeware cross-platform offline dictionary viewer."
+APP_VERSION=7.0
+APP_EXT=tgz
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	ARCH_TYPE=-x64
+else    # Otherwise use version for 32-bit kernel
+	ARCH_TYPE=
+fi
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L http://ng-comp.com/${APP_NAME,,}/${APP_NAME}-linux${ARCH_TYPE}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${APP_NAME,,}.${APP_EXT}
+cd /tmp/${APP_NAME,,}/${APP_NAME}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${APP_NAME,,}/${APP_NAME}/* /opt/${APP_NAME,,}
+sudo mkdir -p /opt/${APP_NAME,,}/${APP_NAME}.Config
+sudo chmod 777 /opt/${APP_NAME,,}/${APP_NAME}.Config
+cat > /tmp/${APP_NAME,,}/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:$PATH; export PATH
+/opt/${APP_NAME,,}/${APP_NAME}.sh
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${APP_NAME}.sh
+#Icon=/usr/share/icons/hicolor/scalable/apps/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Accessories;Office;Education;
+Keywords=Dictionary;Reference;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
