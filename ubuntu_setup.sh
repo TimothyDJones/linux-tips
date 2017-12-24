d86a844b008e9b11c2c8fee0bc8147e72a06021a
Tim Jones <tdjones74021@yahoo.com>
2017-12-24 12:06:22 -0600

    Update Boostnote notepad/PIM to version 0.8.19.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1417,7 +1417,7 @@ rm -rf ${APP_NAME}*
 
 # Install BoostNote notepad/PIM from package
 APP_NAME=boostnote
-APP_VERSION=0.8.17
+APP_VERSION=0.8.19
 APP_EXT=deb
 curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://github.com/BoostIO/boost-releases/releases/download/v${APP_VERSION}/${APP_NAME}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME}.${APP_EXT}
