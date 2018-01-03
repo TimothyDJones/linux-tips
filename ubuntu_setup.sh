606d8efd445e5ee9f7a32acd771650964fc1f4e7
Tim Jones <tdjones74021@yahoo.com>
2018-01-03 07:21:59 -0600

    Update SmallBASIC interpreter to version 0.12.11.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3404,7 +3404,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install SmallBASIC interpreter from package
 APP_NAME=smallbasic
-APP_VERSION=0.12.10
+APP_VERSION=0.12.11
 APP_EXT=deb
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME}/${APP_NAME}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
