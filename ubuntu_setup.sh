795c9f2e241131d4e61a5776c5468e65daac4f3b
Tim Jones <tdjones74021@yahoo.com>
2018-01-04 11:47:19 -0600

    Update Shotcut video editor to version 180102.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2879,7 +2879,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Shotcut video editor
 APP_NAME=Shotcut
-APP_VERSION=171203
+APP_VERSION=180102
 APP_EXT=tar.bz2
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=x86_64
