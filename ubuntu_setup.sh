7bf22cf33d6b42fed872c9263d584d8db1d46bf3
Tim Jones <tdjones74021@yahoo.com>
2018-01-05 11:45:50 -0600

    Add commands to install Cumulonimbus Electron-based podcast player and organizer from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4225,7 +4225,7 @@ sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
 
-# Install Riot instant messenger from package
+# Install Riot instant messenger/collaboration platform from package
 APP_NAME=riot-web
 APP_VERSION=0.13.4
 APP_EXT=deb
@@ -5890,8 +5890,12 @@ sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
 
-
-
-
-
-libqt5svg5-dev qttools5-dev-tools
+# Install Cumulonimbus Electron-based podcast player and organizer from package
+APP_NAME=Cumulonimbus
+APP_GUI_NAME="Cross-platform Electron-based podcast player and organizer."
+APP_VERSION=1.9.3
+APP_EXT=deb
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://github.com/z-------------/${APP_NAME,,}/releases/download/v${APP_VERSION}/${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
+sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
