02c491e428ca419a9f30203f3da12892eecb63e0
Tim Jones <tdjones74021@yahoo.com>
2018-01-10 12:43:08 -0600

    Add commands to install CrococryptFile Java-based file archiving and encryption utility.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -6171,3 +6171,40 @@ cd /tmp/${APP_NAME,,}/${APP_NAME,,}*
 cmake -DCMAKE_BUILD_TYPE=Release . && make && sudo make install
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install CrococryptFile Java-based file archiving and encryption utility
+APP_NAME=CrococryptFile
+APP_GUI_NAME="Java-based file archiving and encryption utility."
+APP_VERSION=1.6
+APP_EXT=tar.gz
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${APP_NAME,,}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${APP_NAME,,}/${APP_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:$PATH; export PATH
+/opt/${APP_NAME,,}/croco
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/croco
+#Icon=/usr/local/${APP_NAME}/resources/icon.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Accessories;System;
+Keywords=Encrypt;Decrypt;Archive;Crypto;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
