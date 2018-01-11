179fa4e6c4eafad7875adbeb0d8b45d39739c775
Tim Jones <tdjones74021@yahoo.com>
2018-01-11 16:59:28 -0600

    Add commands to install FeedReader RSS news aggregator/reader from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -6218,3 +6218,13 @@ curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://github.com/BiglySoftware/${A
 sudo sh /tmp/${APP_NAME,,}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install FeedReader RSS news reader/aggregator from package
+APP_NAME=FeedReader
+APP_GUI_NAME="RSS news reader/aggregator."
+APP_VERSION=1.6.2~ubuntu0.4.1
+APP_EXT=deb
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://launchpad.net/~eviltwin1/+archive/ubuntu/${APP_NAME,,}-stable/+files/${APP_NAME,,}_${APP_VERSION}_${ARCH_TYPE}.${APP_EXT}
+sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
