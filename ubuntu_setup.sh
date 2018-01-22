0b4292b2fce3b2c27e36618d2fdb7913482f310c
Tim Jones <tdjones74021@yahoo.com>
2018-01-22 20:57:03 +0000

    Add commands to install CToy interactive C coding environment (REPL) from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -6601,3 +6601,39 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install CToy interactive C coding environment (REPL) from package
+APP_NAME=CToy
+APP_GUI_NAME="Interactive C coding environment (REPL) from package."
+APP_VERSION=1.01
+APP_EXT=zip
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L http://anael.maratis3d.com/${APP_NAME,,}/bin/${APP_NAME}-${APP_VERSION}-LINUX-x86_64.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${APP_NAME,,}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${APP_NAME,,}/${APP_NAME}-${APP_VERSION}-LINUX-x86_64/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}/${ARCH_TYPE}:\$PATH; export PATH
+/opt/${APP_NAME,,}/${APP_NAME}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${APP_NAME}
+Icon=/opt/${APP_NAME,,}/data/hello_world.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Programming;Development;
+Keywords=REPL;C;Programming;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
