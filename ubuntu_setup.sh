5ca26ed50c1ba43da678927f79f70f302a4ba16f
Tim Jones <tdjones74021@yahoo.com>
2018-02-13 07:57:21 -0600

    Add commands to install PyPref Python-based Russian card game Preferans.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7032,4 +7032,40 @@ Keywords=Games;Adventure;Text;
 EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
+
+# Install PyPref Python-based Russian card game Preferans
+APP_NAME=PyPref
+APP_GUI_NAME="Python-based Russian card game Preferans."
+APP_VERSION=2.1
+APP_EXT=zip
+sudo apt-get install -y python-tk
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/python-pref/${APP_NAME}-${APP_VERSION}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${APP_NAME,,}.${APP_EXT}
+sudo mv /tmp/${APP_NAME,,} /opt
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+python2 /opt/${APP_NAME,,}/${APP_NAME,,}.pyw
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=python2 /opt/${APP_NAME,,}/${APP_NAME,,}.pyw
+Icon=/opt/${APP_NAME,,}/big/back.gif
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Games;Entertainment;
+Keywords=Games;Cards;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
 rm -rf /tmp/${APP_NAME,,}
