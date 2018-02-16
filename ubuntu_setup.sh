7e93e1c53ca2dc0354926a4e26c14b6c386d2794
Tim Jones <tdjones74021@yahoo.com>
2018-02-16 16:34:06 -0600

    Add commands to install JSound Java-based audio player and editor.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7161,3 +7161,39 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install JSound Java-based audio player and editor
+APP_NAME=JSound
+APP_GUI_NAME="Java-based audio player and editor."
+APP_VERSION=4.0
+APP_EXT=zip
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/jortegasound/${APP_NAME,,}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${APP_NAME,,}.${APP_EXT}
+sudo mv /tmp/${APP_NAME,,} /opt
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}/bin
+PATH=/opt/${APP_NAME,,}/bin:\$PATH; export PATH
+/opt/${APP_NAME,,}/bin/${APP_NAME,,}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}/bin
+Exec=/opt/${APP_NAME,,}/bin/${APP_NAME,,}
+#Icon=/opt/${APP_NAME,,}/icon.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Multimedia;Audio;
+Keywords=Audio;Player;Editor;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
