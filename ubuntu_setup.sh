0b8bcce4e1dbcfad16a4522744f7c461d6b1e608
Tim Jones <tdjones74021@yahoo.com>
2018-03-02 10:03:26 -0600

    Add commands to install Bitwarden Electron-based desktop/online password manager from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7429,3 +7429,15 @@ curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://github.com/${APP_NAME}/${APP
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install Bitwarden Electron-based desktop/online password manager from package
+APP_NAME=Bitwarden
+APP_GUI_NAME="Electron-based desktop/online password manager."
+APP_VERSION=1.0.5
+APP_EXT=deb
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://github.com/${APP_NAME,,}/desktop/releases/download/v1.0.5/${APP_NAME}-${APP_VERSION}-amd64.${APP_EXT}
+sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
+
+
