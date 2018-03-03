798a56196c2a818ccfaa7a59fb8287869702a1c2
Tim Jones <tdjones74021@yahoo.com>
2018-03-03 09:29:19 -0600

    Add commands to install Standard Notes Electron-based secure notepad from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7459,3 +7459,13 @@ curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://github.com/zaps166/${APP_NAM
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install Standard Notes Electron-based secure notepad from package
+APP_NAME=Standard-Notes
+APP_GUI_NAME="Electron-based secure notepad."
+APP_VERSION=2.1.21
+APP_EXT=deb
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://github.com/standardnotes/desktop/releases/download/v${APP_VERSION}/${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
+sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
