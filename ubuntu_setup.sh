1492789ccd67cb740655c2f1573798377aa20a12
Tim Jones <tdjones74021@yahoo.com>
2018-03-05 10:22:05 -0600

    Update PySolFC Python-based Solitaire card game to version 2.1.3.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5034,7 +5034,7 @@ xdg-open http://localhost/${APP_NAME,,}/installation/setup.php &
 # Install PySolFC Python-based Solitaire card game
 APP_NAME=PySolFC
 APP_GUI_NAME="Python-based Solitaire card game"
-APP_VERSION=2.1.2
+APP_VERSION=2.1.3
 APP_EXT=tar.bz2
 sudo apt-get install -y python3-pip
 sudo pip3 install random2 sgmllib3k
