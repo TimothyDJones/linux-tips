f62418c90b223362f7f7c50c497a352646c708ea
Tim Jones <tdjones74021@yahoo.com>
2018-03-05 09:59:58 -0600

    Update x-whnb self-contained web-based hierarchical notebook (similar to Cherrytree) to version v0.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7099,7 +7099,7 @@ rm -rf /tmp/${APP_NAME,,}*
 # Install x-whnb self-contained web-based hierarchical notebook (similar to Cherrytree)
 APP_NAME=x-whnb
 APP_GUI_NAME="Self-contained web-based hierarchical notebook (similar to Cherrytree)."
-APP_VERSION=v0.2
+APP_VERSION=v0.4
 APP_EXT=tar.gz
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://sourceforge.net/projects/${APP_NAME,,}/files/${APP_VERSION}/${APP_NAME,,}.deploy.${APP_EXT}/download
 cd /tmp
