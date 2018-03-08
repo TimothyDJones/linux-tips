4e65a776516f39c50b5bf3f398ddacd0a9deb151
Tim Jones <tdjones74021@yahoo.com>
2018-03-08 10:55:47 -0600

    Update Red Notebook notepad to version 2.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1446,7 +1446,7 @@ sudo rm -rf ${APP_NAME}*
 
 # Install Red Notebook notepad from source
 APP_NAME=rednotebook
-APP_VERSION=2.3
+APP_VERSION=2.4
 APP_EXT=tar.gz
 sudo apt-get install -y python3-enchant gir1.2-webkit2-4.0 python3-pip python3-yaml  # Install dependencies
 curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME}/${APP_NAME}-${APP_VERSION}.${APP_EXT}
