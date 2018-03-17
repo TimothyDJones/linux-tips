1b59654a092e47372fc80b86c10c0bf05b1386df
Tim Jones <tdjones74021@yahoo.com>
2018-03-17 17:28:55 -0500

    Add commands to install Tixati cross-platform BitTorrent P2P file sharing client from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7836,3 +7836,13 @@ sudo cp -R /tmp/${APP_NAME,,}/${APP_NAME,,}-0/* /opt/${APP_NAME,,}
 sudo ln -s /opt/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install Tixati cross-platform BitTorrent P2P file sharing client from package
+APP_NAME=Tixati
+APP_GUI_NAME="Cross-platform BitTorrent P2P file sharing client."
+APP_VERSION=2.57-1
+APP_EXT=deb
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://download2.${APP_NAME,,}.com/download/${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
+sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
