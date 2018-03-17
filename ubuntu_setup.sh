168d0ed548e73b387a30ca280621043c1c558f8a
Tim Jones <tdjones74021@yahoo.com>
2018-03-17 17:23:33 -0500

    Update Shallot file manager to version 1.1.3239.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3653,7 +3653,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Shallot Python-based file manager from package
 APP_NAME=shallot
-APP_VERSION=1.0.3106
+APP_VERSION=1.1.3239
 APP_EXT=deb
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://pseudopolis.eu/wiki/pino/projs/${APP_NAME}/${APP_NAME}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
