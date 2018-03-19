6bfbf429298a1faba526d7347779d0137e676606
Tim Jones <tdjones74021@yahoo.com>
2018-03-18 20:03:09 -0500

    Update Sushi browser to version 0.14.5.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3770,7 +3770,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Sushi multi-panel web browser from package
 APP_NAME=sushi-browser
-APP_VERSION=0.13.5
+APP_VERSION=0.14.5
 APP_EXT=deb
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://sushib.me/dl/${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
