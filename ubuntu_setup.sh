4fc2f7ef49065a544d7b74fee8f642747e59bed7
Tim Jones <tdjones74021@yahoo.com>
2018-03-20 08:04:28 -0500

    Update PyPref Python-based Russian card game Preferans to version 2.33.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7036,7 +7036,7 @@ cd $HOME
 # Install PyPref Python-based Russian card game Preferans
 APP_NAME=PyPref
 APP_GUI_NAME="Python-based Russian card game Preferans."
-APP_VERSION=2.32
+APP_VERSION=2.33
 APP_EXT=zip
 sudo apt-get install -y python-tk
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/python-pref/${APP_NAME}-${APP_VERSION}.${APP_EXT}
