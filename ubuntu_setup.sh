652b1c457c17b2809cd069c532afa6336d0336b7
Tim Jones <tdjones74021@yahoo.com>
2018-03-24 20:52:52 -0500

    Add commands to install GoldenDict cross-platform Qt-based dictionary client from source.
    Add commands to install Flextype flat-file content management system
    (CMS).

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8176,3 +8176,34 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${A
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Flextype flat-file content management system (CMS)
+APP_NAME=Flextype
+APP_VERSION=0.2.0
+APP_EXT=zip
+DB_NAME=${APP_NAME,,}
+DB_USER=${APP_NAME,,}
+DB_PASSWORD=${APP_NAME,,}
+sudo apt-get install -y php5.6-soap graphviz
+sudo service apache2 restart
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME,,}-${APP_VERSION}.${APP_EXT}
+cd /tmp
+dtrx -n ${APP_NAME,,}.${APP_EXT}
+sudo mv /tmp/${APP_NAME,,} ${WWW_HOME}
+sudo chown -R www-data:www-data ${WWW_HOME}/${APP_NAME,,}
+sudo chmod -R 777 ${WWW_HOME}/${APP_NAME,,}/site
+xdg-open http://localhost/${APP_NAME,,}/index.php &
+
+# Install GoldenDict cross-platform Qt-based dictionary client from source
+APP_NAME=GoldenDict
+APP_GUI_NAME="Cross-platform Qt-based dictionary client."
+APP_VERSION=1.5.0-RC2
+APP_EXT=tar.gz
+sudo apt-get install -y git pkg-config build-essential qt5-qmake libvorbis-dev zlib1g-dev libhunspell-dev x11proto-record-dev qtdeclarative5-dev libqtwebkit-dev libxtst-dev liblzo2-dev libbz2-dev libao-dev libavutil-dev libavformat-dev libtiff5-dev libeb16-dev libqt5webkit5-dev libqt5svg5-dev libqt5x11extras5-dev qttools5-dev qttools5-dev-tools
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://github.com/${APP_NAME,,}/${APP_NAME,,}/archive/${APP_VERSION}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${APP_NAME,,}.${APP_EXT}
+cd /tmp/${APP_NAME,,}/${APP_NAME,,}-${APP_VERSION}
+qtchooser -run-tool=qmake -qt=5 ${APP_NAME,,}.pro && make && sudo make install
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
