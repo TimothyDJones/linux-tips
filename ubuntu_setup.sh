9d1926daea229fce63a85c1d2525f57e351f0c9a
Tim Jones <tdjones74021@yahoo.com>
2018-03-26 09:19:20 -0500

    Update ZenTao project management tool to version 9.8.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1420,7 +1420,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install ZenTao project management tool from package
 APP_NAME=ZenTaoPMS
-APP_VERSION=9.8.1
+APP_VERSION=9.8.2
 APP_EXT=deb
 curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/zentao/${APP_NAME}_${APP_VERSION}_1_all.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME}.${APP_EXT}
