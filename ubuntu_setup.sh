400b951027c47fd8eb55f26f4743032a55b32b94
Tim Jones <tdjones74021@yahoo.com>
2018-03-31 16:03:59 -0500

    Add commands to install SwitchHosts cross-platform, Electron-based hosts file manager from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8525,3 +8525,43 @@ perl set.pl "debian" && autoreconf --install --force
 make install && mkdir -p $HOME/.pinky
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install SwitchHosts cross-platform, Electron-based hosts file manager from package
+APP_NAME=SwitchHosts
+APP_GUI_NAME="Cross-platform, Electron-based hosts file manager."
+APP_MAJOR_VERSION=3.3.11
+APP_MINOR_VERSION=5347
+APP_EXT=zip
+FILE_NAME=${APP_NAME}-linux-x64_v${APP_MAJOR_VERSION}.${APP_MINOR_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/oldj/${APP_NAME}/releases/download/v${APP_MAJOR_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/${APP_NAME}\!-linux-x64/* /opt/${APP_NAME,,}
+sudo ln -s /opt/${APP_NAME,,}/${APP_NAME}\! /opt/${APP_NAME,,}/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+/opt/${APP_NAME,,}/${APP_NAME,,}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${APP_NAME,,}
+#Icon=/opt/${APP_NAME,,}/icons/yicon.ico
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Accessories;Network;Internet;
+Keywords=Hosts;Networking;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
