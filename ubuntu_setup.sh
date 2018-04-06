0d8f4ca79bb885f5fb718cdf98307b10e6919094
Tim Jones <tdjones74021@yahoo.com>
2018-04-06 10:24:11 -0500

    Update Jailer Java database utility to version 7.6.7.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -762,7 +762,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Jailer Java database utility
 APP_NAME=jailer
-APP_VERSION=7.6.6
+APP_VERSION=7.6.7
 curl -o /tmp/${APP_NAME}.zip -J -L https://cytranet.dl.sourceforge.net/project/${APP_NAME}/v${APP_VERSION}/${APP_NAME}_${APP_VERSION}.zip
 cd /tmp
 dtrx -n ${APP_NAME}.zip
