46176ed0d0aa4208427e51e9950870818beac80a
Tim Jones <tdjones74021@yahoo.com>
2018-04-08 17:47:46 -0500

    Add commands to install Opera web browser from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8792,3 +8792,14 @@ cd /tmp/${FILE_NAME}/${APP_NAME}
 sudo python3 ./install.py
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install Opera web browser from package
+APP_NAME=Opera
+APP_GUI_NAME=""
+APP_VERSION=52.0.2871.40
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}-stable_${APP_VERSION}_${KERNEL_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://download3.operacdn.com/pub/${APP_NAME,,}/desktop/${APP_VERSION}/linux/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
