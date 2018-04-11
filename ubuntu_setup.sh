9e81e4bb2f1e79c1bfd7e3865d24bdc4cece8ecc
Tim Jones <tdjones74021@yahoo.com>
2018-04-10 20:50:29 -0500

    Add commands to install Extraterm Electron-based, cross-platform terminal emulator from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8845,3 +8845,46 @@ cd /tmp/${FILE_NAME}
 qtchooser -run-tool=qmake -qt=5 ghostwriter.pro && make && sudo make install
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install Extraterm Electron-based, cross-platform terminal emulator from package
+APP_NAME=Extraterm
+APP_GUI_NAME="Electron-based, cross-platform terminal emulator."
+APP_VERSION=0.33.0
+APP_EXT=zip
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	ARCH_TYPE=x64
+else    # Otherwise use version for 32-bit kernel
+	ARCH_TYPE=ia32
+fi
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}-linux-${ARCH_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/sedwards2009/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+/opt/${APP_NAME,,}/${APP_NAME,,}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME}
+Exec=/opt/${APP_NAME,,}/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/resources/app/${APP_NAME,,}/resources/logo/${APP_NAME,,}_small_logo.ico
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=System;TerminalEmulator;
+Keywords=Terminal;Shell;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
