1f40d478669ed9d952ea2072eb68b6d3197ab3a9
Tim Jones <tdjones74021@yahoo.com>
2018-04-13 15:29:25 -0500

    Add 'htop' to non-default packages to install.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -43,7 +43,7 @@ sudo apt-get upgrade -y
 sudo apt-get autoremove -f -y
 sudo apt-get install -y build-essential dtrx curl wget checkinstall gdebi \
 	openjdk-8-jre python-software-properties software-properties-common \
-	mc python3-pip
+	mc python3-pip htop
 
 # Allow current user to run 'sudo' without password
 # https://phpraxis.wordpress.com/2016/09/27/enable-sudo-without-password-in-ubuntudebian/
