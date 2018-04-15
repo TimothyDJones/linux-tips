56090189070634f45bf5e615551ffb23b17a234b
Tim Jones <tdjones74021@yahoo.com>
2018-04-15 18:31:01 -0500

    Add commands to install 2D Java Chess Java chess game from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8999,3 +8999,39 @@ sudo mv /tmp/${FILE_NAME}/${APP_NAME,,}-electron-${APP_VERSION}*.AppImage /usr/l
 /usr/local/bin/${FILE_NAME}/${APP_NAME,,}-electron-${APP_VERSION}*.AppImage &
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install 2D Java Chess Java chess game from package
+APP_NAME=Java-Chess-2D
+APP_GUI_NAME="Java chess game."
+APP_VERSION=N/A
+APP_EXT=jar
+FILE_NAME=${APP_NAME}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/2D%20Java%20Chess.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Games;Entertainment;
+Keywords=Games;Chess;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
