7ed32aa9e54829a12bee9aa72bfbb0e86f04a760
Tim Jones <tdjones74021@yahoo.com>
2018-04-22 18:14:16 -0500

    Update Admidio organizational management tool to version 3.3.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3592,7 +3592,7 @@ xdg-open http://localhost/${APP_NAME,,}/dependent/admin/install/index.php &
 
 # Install Admidio organizational management tool
 APP_NAME=admidio
-APP_VERSION=3.3-Beta.4
+APP_VERSION=3.3.1
 APP_EXT=zip
 DB_NAME=admidio
 DB_USER=admidio
