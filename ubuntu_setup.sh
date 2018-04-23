9ec27eb01cf645ff3dad510d61cca949f1647389
Tim Jones <tdjones74021@yahoo.com>
2018-04-23 11:00:20 -0500

    Add commands to install DCEdit cross-platform, Java-based editor with built-in snippets for Java, SQL, and JavaFX from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -9133,3 +9133,41 @@ dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
 sudo mv /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
 cd $HOME
 rm -rf /tmp/${APP_NAME}*
+
+# Install DCEdit cross-platform, Java-based editor with built-in snippets for Java, SQL, and JavaFX from package
+APP_NAME=DCEdit
+APP_GUI_NAME="Cross-platform, Java-based editor with built-in snippets for Java, SQL, and JavaFX."
+APP_VERSION=2.4
+APP_EXT=zip
+FILE_NAME=${APP_NAME}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/${APP_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+sh /opt/${APP_NAME,,}/${APP_NAME}.sh "\$1"
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=sh /opt/${APP_NAME,,}/${APP_NAME}.sh
+Icon=/opt/${APP_NAME,,}/bin/img/splash.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Accessories;Development;Programming
+Keywords=Editor;Text;Java;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME}*
