83a85619722902774fc2385cb27df18c361cbe69
Tim Jones <tdjones74021@yahoo.com>
2018-04-30 16:08:14 -0500

    Update FeedTurtle Java-based RSS news aggregator client to version X.6.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3283,7 +3283,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install FeedTurtle Java-based RSS news aggregator client
 APP_NAME=FeedTurtle
-APP_VERSION=X.5
+APP_VERSION=X.6
 APP_EXT=zip
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME}${APP_VERSION}.${APP_EXT}
 cd /tmp
