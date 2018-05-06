c3fdf84f9854aa731341ea4ab857a63704b1c0c9
Tim Jones <tdjones74021@yahoo.com>
2018-05-06 17:59:08 -0500

    Update Super Productivity To Do List and task manager to version 1.10.24.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1545,7 +1545,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Super Productivity To Do List and task manager from package
 APP_NAME=superProductivity
-APP_VERSION=1.10.20
+APP_VERSION=1.10.24
 APP_EXT=deb
 curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/super-productivity/${APP_NAME}_${APP_VERSION}_amd64.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME}.${APP_EXT}
