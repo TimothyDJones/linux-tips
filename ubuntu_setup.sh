2dd2efa30f83f8dff1c9db4f31e27ae993772b7a
Tim Jones <tdjones74021@yahoo.com>
2018-05-22 16:34:02 -0500

    Update Golang installation to version 1.10.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -220,7 +220,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Google Go language
 APP_NAME=go
-APP_VERSION=1.10
+APP_VERSION=1.10.2
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=amd64
 else    # Otherwise use version for 32-bit kernel
