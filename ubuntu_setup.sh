092648761bfc3415cb61f0cd660eaef35a8edb4b
Tim Jones <tdjones74021@yahoo.com>
2018-05-23 14:12:51 -0500

    Add commands to install Nighthawk cross-platform, Electron-based minimalist music player from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -9655,3 +9655,14 @@ EOF
 sudo mv /tmp/${FILE_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${FILE_NAME}*
+
+# Install Nighthawk cross-platform, Electron-based minimalist music player from Debian package
+APP_NAME=Nighthawk
+APP_GUI_NAME="Cross-platform, Electron-based minimalist music player."
+APP_VERSION=v1.0.0
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}-linux-${APP_VERSION}-${KERNEL_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/quantumkv/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/*${APP_NAME},,*
