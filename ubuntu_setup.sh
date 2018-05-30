55226e31cbc7e6ed1783dd7a5b9a650b07f33181
Tim Jones <tdjones74021@yahoo.com>
2018-05-30 12:30:22 -0500

    Update Leo Editor editor/IDE, outliner, and PIM to version 5.7.3 and to install from Github instead of Sourceforge.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2138,36 +2138,39 @@ cd $HOME
 rm -rf /tmp/${APP_NAME}*
 
 # Install Leo editor/IDE/PIM
-APP_NAME=Leo
-APP_VERSION=5.7b2
-APP_EXT=zip
+APP_NAME=Leo-Editor
+APP_GUI_NAME="Cross-platform, Python-based editor/IDE, outliner, and PIM."
+APP_VERSION=5.7.3
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get install -y python3-pyqt5
-curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME}-${APP_VERSION}.${APP_EXT}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME,,}/${APP_NAME,,}/archive/${APP_VERSION}.${APP_EXT}
 cd /tmp
-dtrx -n /tmp/${APP_NAME,,}.${APP_EXT}
-cd /tmp/${APP_NAME,,}
-mv ${APP_NAME}-${APP_VERSION} ${APP_NAME,,}
-sudo mv ${APP_NAME,,} /opt
-cat > /tmp/${APP_NAME,,}/${APP_NAME,,} << EOF
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
 #! /bin/sh
 cd /opt/${APP_NAME,,}
-PATH=/opt/${APP_NAME,,}:$PATH; export PATH
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
 python3 /opt/${APP_NAME,,}/launchLeo.py
 cd $HOME
 EOF
-sudo mv /tmp/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cat > /tmp/${APP_NAME,,}.desktop << EOF
 [Desktop Entry]
-Name=Leo Editor
-Comment=Cross-platform text edtior/IDE/PIM
-GenericName=IDE
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
 Exec=python3 /opt/${APP_NAME,,}/launchLeo.py
-Icon=/opt/${APP_NAME,,}/${APP_NAME,,}/Icons/LeoApp.ico
+Icon=/opt/${APP_NAME,,}/leo/Icons/LeoApp.ico
 Type=Application
 StartupNotify=true
 Terminal=true
-Categories=Programming;Development;
+Categories=Programming;Development;Accessories;
 Keywords=Editor;IDE;Python;PIM
 EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
