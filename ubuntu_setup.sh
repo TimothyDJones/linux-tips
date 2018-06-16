17facae1d2c8062b0f67f05360d866fb0c7d65d0
Tim Jones <tdjones74021@yahoo.com>
2018-06-16 18:25:17 -0500

    Add commands to install Java Open Chess cross-platform, Java-based chess client/engine from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -10338,3 +10338,41 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${A
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME}*
+
+# Install Java Open Chess cross-platform, Java-based chess client/engine from package
+APP_NAME=jChess
+APP_GUI_NAME="Cross-platform, Java-based chess client/engine."
+APP_VERSION=1.5
+APP_EXT=zip
+FILE_NAME=joChess-${APP_VERSION}
+curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/javaopenchess/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${APP_NAME,,}.${APP_EXT}
+sudo mkdir /opt/${APP_NAME,,}
+sudo mv /tmp/${APP_NAME,,}/${APP_NAME}-${APP_VERSION}/${APP_NAME}-${APP_VERSION}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${APP_NAME}-${APP_VERSION}.jar
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=java -jar /opt/${APP_NAME,,}/${APP_NAME}-${APP_VERSION}.jar
+Icon=/opt/${APP_NAME,,}/theme/default/images/King-W100.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Entertainment;Games;
+Keywords=Chess;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
