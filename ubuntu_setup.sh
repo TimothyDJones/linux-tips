40bc42a3cdc1641715f5ea28e7e4b94f4172004e
Tim Jones <tdjones74021@yahoo.com>
2018-06-21 11:55:50 -0500

    Update LinSSID Qt-based GUI wireless network scanner to version 3.5-1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -9856,7 +9856,7 @@ rm -rf /tmp/*${APP_NAME,,}*
 # Install LinSSID Qt-based GUI wireless network scanner from Debian package
 APP_NAME=LinSSID
 APP_GUI_NAME="Qt-based GUI wireless network scanner."
-APP_VERSION=3.4-1
+APP_VERSION=3.5-1
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
