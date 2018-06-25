689226884c84f6a74500344f56c31bb3cf02667e
Tim Jones <tdjones74021@yahoo.com>
2018-06-25 08:03:27 -0500

    Update Oni cross-platform GUI text editor based on Neovim and React/Redux to version 0.3.6.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8216,7 +8216,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install Oni cross-platform GUI text editor based on Neovim and React/Redux from package
 APP_NAME=Oni
 APP_GUI_NAME="Cross-GUI text editor based on Neovim and React/Redux."
-APP_VERSION=0.3.1
+APP_VERSION=0.3.6
 APP_EXT=deb
 # Neovim must be installed to use Oni
 sudo apt-get install -y python-dev python-pip python3-dev python3-pip
