065222f31b252e806b43b9a3d584f6e0f8803c51
Tim Jones <tdjones74021@yahoo.com>
2018-07-02 20:23:04 -0500

    Add commands to install TreeSheets Free-Form Data Organizer (Hierarchical Spreadsheet) from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -11076,3 +11076,48 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/num
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME}*
+
+# Install TreeSheets Free-Form Data Organizer (Hierarchical Spreadsheet) from package
+APP_NAME=TreeSheets
+APP_GUI_NAME="Free-Form Data Organizer (Hierarchical Spreadsheet)."
+APP_VERSION=N/A
+APP_EXT=tar.gz
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	ARCH_TYPE=linux
+else    # Otherwise use version for 32-bit kernel
+	ARCH_TYPE=linux64
+fi
+FILE_NAME=${APP_NAME,,}_${ARCH_TYPE}
+curl -o /tmp/libpng12.deb -J -L http://ftp.osuosl.org/pub/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_${KERNEL_TYPE}.deb
+sudo gdebi -n /tmp/libpng12.deb
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L http://strlen.com/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/TS/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+/opt/${APP_NAME,,}/treesheets
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/treesheets "\$1"
+Icon=/opt/${APP_NAME,,}/images/icon32.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Office;
+Keywords=Spreadsheet;Notepad;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
