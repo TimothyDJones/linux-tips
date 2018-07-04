a6dcb195fa88745fe30e7a46a0b4192dba77dec7
Tim Jones <tdjones74021@yahoo.com>
2018-07-04 16:51:53 -0500

    Add commands to install Sielo Qt- and WebEngine-based web browser from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -11223,3 +11223,40 @@ cd /tmp/${FILE_NAME}/${APP_NAME,,}-qt-${APP_VERSION}
 qtchooser -run-tool=qmake -qt=5 src.pro && make && sudo make install
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Sielo Qt- and WebEngine-based web browser from AppImage
+APP_NAME=SieloBrowser
+APP_GUI_NAME="Qt- and WebEngine-based web browser."
+APP_VERSION=1.16.07
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME}/${APP_NAME}/releases/download/v${APP_VERSION}/sielo-browser.AppImage
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+sudo chmod a+x /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+/opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Internet;
+Keywords=Web;Browser;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
