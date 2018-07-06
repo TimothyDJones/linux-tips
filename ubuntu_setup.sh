4f732fe5d8b17bbc9863939617c5ddb61cd0a221
Tim Jones <tdjones74021@yahoo.com>
2018-07-06 10:50:20 -0500

    Add commands to install MyPGP Java Swing-based GUI for PGP encrypting/signing from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -11363,3 +11363,40 @@ sudo sed -i 's@;phar.readonly = On@phar.readonly = Off@g' /etc/php/7.1/cli/php.i
 sudo sed -i 's@;phar.readonly = On@phar.readonly = Off@g' /etc/php/7.2/cli/php.ini
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install MyPGP Java Swing-based GUI for PGP encrypting/signing from package
+APP_NAME=MyPGP
+APP_GUI_NAME="Java Swing-based GUI for PGP encrypting/signing."
+APP_VERSION=N/A
+APP_EXT=jar
+FILE_NAME=${APP_NAME}
+sudo apt-get install -y libbcprov-java libbcpg-java 
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/my-pgp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Accessories;System;
+Keywords=PGP;Security;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
