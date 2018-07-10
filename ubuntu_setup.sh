55bff22fad15bb2c0f0a89211d34711c98f1dea7
Tim Jones <tdjones74021@yahoo.com>
2018-07-10 13:54:50 -0500

    Add commands to install eLogSim digital circuit simulator from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -11518,3 +11518,38 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/*${APP_NAME}*
+
+# Install eLogSim digital circuit simulator from package
+APP_NAME=eLogSim
+APP_GUI_NAME="Digital circuit simulator."
+APP_VERSION=N/A
+APP_EXT=zip
+FILE_NAME=My${APP_NAME}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+sudo mv /opt/${APP_NAME,,}/${APP_NAME} /opt/${APP_NAME,,}/${APP_NAME}.app
+sudo mv /opt/${APP_NAME,,}/${APP_NAME}.app/* /opt/${APP_NAME,,}
+sudo rm -rf /opt/${APP_NAME,,}/${APP_NAME}.app /opt/${APP_NAME,,}/${APP_NAME}.exe
+sudo ln -f -s /opt/${APP_NAME,,}/${APP_NAME} /usr/local/bin/${APP_NAME,,}
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/${APP_NAME}.ico
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Education;Electronics;
+Keywords=Electronics;Digital;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
