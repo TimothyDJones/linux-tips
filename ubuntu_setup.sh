8ccf87c6e2ceda16ef12fe14558970e16b3ca964
Tim Jones <tdjones74021@yahoo.com>
2018-07-15 15:37:59 -0500

    Update QXmlEdit to version 0.9.11.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -827,7 +827,7 @@ sudo mv /tmp/${APP_NAME}.desktop /usr/share/applications/
 
 # Install QXmlEdit from source via Sourceforge
 APP_NAME=QXmlEdit
-APP_VERSION=0.9.10
+APP_VERSION=0.9.11
 APP_EXT=tgz
 sudo apt-get install -y libqt5xmlpatterns5-dev libqt5svg5-dev
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME,,}-${APP_VERSION}-src.${APP_EXT}
