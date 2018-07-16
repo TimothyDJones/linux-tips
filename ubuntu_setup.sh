52b282d08865ad7758d1081f9043580d5ca02aa9
Tim Jones <tdjones74021@yahoo.com>
2018-07-16 09:37:32 -0500

    Add commands to install Ngraph-GTK tool for creating 2D graphs with support for exporting to PostScript, SVG, PNG or PDF from source.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -11762,3 +11762,18 @@ cd /tmp/${FILE_NAME}
 ./configure && make && sudo make install
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Ngraph-GTK tool for creating 2D graphs with support for exporting to PostScript, SVG, PNG or PDF from source
+APP_NAME=Ngraph-GTK
+APP_GUI_NAME="Tool for creating 2D graphs with support for exporting to PostScript, SVG, PNG or PDF."
+APP_VERSION=6.07.07
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}
+sudo apt-get install -y libgtk-3-dev automake libzip-dev librsvg2-dev libxml2-dev libmagic-dev libgtksourceview-3.0-dev
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+cd /tmp/${FILE_NAME}
+./configure && make && sudo make install && sudo ldconfig
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
