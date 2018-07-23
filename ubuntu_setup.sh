ed2b38e21460bd85a4d6e4b1203dee244878c490
Tim Jones <tdjones74021@yahoo.com>
2018-07-23 08:43:43 -0500

    Add commands to install TabuVis Java-based interactive visualization of tabular data tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -11984,3 +11984,40 @@ EOF
 sudo mv /tmp/hhdb_csadmin.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/hhdb_csadmin*
+
+# Install TabuVis Java-based interactive visualization of tabular data tool from package
+APP_NAME=TabuVis
+APP_GUI_NAME="Java-based interactive visualization of tabular data tool."
+APP_VERSION=N/A
+APP_EXT=zip
+FILE_NAME=${APP_NAME}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mv /tmp/${FILE_NAME} /opt
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME}
+PATH=/opt/${APP_NAME}:\$PATH; export PATH
+java -jar /opt/${APP_NAME}/${APP_NAME}.jar
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=java -jar /opt/${APP_NAME}/${APP_NAME}.jar
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Programming;Development;Science;
+Keywords=Data;Science;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
