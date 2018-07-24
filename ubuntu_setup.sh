061ec75a2313df721d5c45dd5ab81f0690d07176
Tim Jones <tdjones74021@yahoo.com>
2018-07-24 13:30:29 -0500

    Add commands to install VNote Vim-inspired note-taking application with MarkDown support from App Image.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -12032,3 +12032,37 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/Kilian/${APP_NAME,
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME}*
+
+# Install VNote Vim-inspired note-taking application with MarkDown support from App Image
+APP_NAME=VNote
+APP_GUI_NAME="Vim-inspired note-taking application with MarkDown support."
+APP_VERSION=1.20
+APP_EXT=AppImage
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	ARCH_TYPE=i386
+else    # Otherwise use version for 32-bit kernel
+	ARCH_TYPE=x86_64
+fi
+FILE_NAME=${APP_NAME}-${APP_VERSION}-${ARCH_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/tamlok/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+sudo chmod +x /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Office;Accessories;
+Keywords=Notepad;MarkDown;Vim;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
