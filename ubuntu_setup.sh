69d00ce493901057f22ed757922ec11766025d7a
Tim Jones <tdjones74021@yahoo.com>
2018-07-26 16:42:10 -0500

    Update Lite IDE for Go language development to version x34.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -263,7 +263,7 @@ cd $HOME
 
 # Install Lite IDE for Go language development
 APP_NAME=LiteIDE
-APP_VERSION=x33.4
+APP_VERSION=x34
 QT_VERSION=qt5.5.1
 APP_EXT=tar.gz
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
