86bd594993a3b6e1481b5de833249a5d2c8f552f
Tim Jones <tdjones74021@yahoo.com>
2018-07-30 08:09:16 -0500

    Update Golden Scrabble multi-language GUI Scrabble crossword game to version 0.1.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -12091,7 +12091,7 @@ sudo rm -rf /tmp/${APP_NAME}*
 # Install Golden Scrabble multi-language GUI Scrabble crossword game from package
 APP_NAME=GScrabble
 APP_GUI_NAME="Multi-language GUI Scrabble crossword game."
-APP_VERSION=0.1.0
+APP_VERSION=0.1.1
 APP_EXT=tar.xz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
