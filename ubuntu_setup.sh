e13ee04a7267fd6e6e458b2ded590274e22c832e
Tim Jones <tdjones74021@yahoo.com>
2018-07-31 14:57:59 -0500

    Add commands to install meo geo-aware journal and personal information manager (PIM) from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -12199,3 +12199,32 @@ cd /tmp/${FILE_NAME}
 ./configure && make && sudo make install && sudo ldconfig
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install meo geo-aware journal and personal information manager (PIM) from AppImage
+APP_NAME=meo
+APP_GUI_NAME="Geo-aware journal and personal information manager (PIM)."
+APP_VERSION=N/A
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://rebrand.ly/meo-release-linux
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+sudo chmod a+x /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo ln -f -s /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT} /usr/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Accessories;Office;
+Keywords=PIM;Journal;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME}*
