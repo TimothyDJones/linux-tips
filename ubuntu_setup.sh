fb10d3fef27babe6378b91a2f98cc08926e68f92
Tim Jones <tdjones74021@yahoo.com>
2018-08-01 17:51:25 -0500

    Update Textadept minimalist cross-platform text editor to version 10.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2946,7 +2946,7 @@ sudo apt-get install -y smuxi
 
 # Install Textadept minimalist cross-platform text editor
 APP_NAME=textadept
-APP_VERSION=9.6
+APP_VERSION=10.0
 APP_EXT=tgz
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=x86_64
