90ce492a57d1f53e8ddb5dde3780538fd1e158dc
Tim Jones <tdjones74021@yahoo.com>
2018-08-05 17:31:21 -0500

    Update Qt-based Torrent File Editor to version 0.3.12.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4780,7 +4780,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Qt-based Torrent File Editor from source
 APP_NAME=torrent-file-editor
-APP_VERSION=0.3.11
+APP_VERSION=0.3.12
 APP_EXT=tar.gz
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME,,}-${APP_VERSION}.${APP_EXT}
 cd /tmp
