d9dd7fc08bd2e6bb612aa67c2de914e734bc6e4a
Tim Jones <tdjones74021@yahoo.com>
2018-08-07 07:52:35 -0500

    Add commands to install UMLet Java-based UML diagram tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -12509,3 +12509,42 @@ rm -rf /tmp/*${APP_NAME}*
 sudo add-apt-repository -y ppa:lazygit-team/daily
 sudo apt-get update -y
 sudo apt-get install -y lazygit
+
+# Install UMLet Java-based UML diagram tool from package
+APP_NAME=UMLet
+APP_GUI_NAME="Java-based UML diagram tool."
+APP_VERSION=14.3
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}-standalone-${APP_VERSION}.0
+sudo apt-get install -y idle3
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://www.umlet.com/${APP_NAME,,}_${APP_VERSION//./_}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/${APP_NAME//ML/ml}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:/opt/${APP_NAME,,}/lib:\$PATH; export PATH
+java -Dsun.java2d.xrender=f -jar /opt/${APP_NAME,,}/${APP_NAME,,}.jar
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}:/opt/${APP_NAME,,}/lib
+Exec=java -Dsun.java2d.xrender=f -jar /opt/${APP_NAME,,}/${APP_NAME,,}.jar
+Icon=/opt/${APP_NAME,,}/img/${APP_NAME,,}_logo.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Programming;Development;
+Keywords=UML;Diagramming;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
