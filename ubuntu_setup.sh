dbc9e326297e72a1eda896b587593842f79625e9
Tim Jones <tdjones74021@yahoo.com>
2018-08-11 16:31:04 -0500

    Update SysUsage Perl-based GUI system monitor to version 5.7.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -12610,7 +12610,7 @@ rm -rf /tmp/*${APP_NAME}*
 # Install SysUsage Perl-based GUI system monitor from package
 APP_NAME=SysUsage
 APP_GUI_NAME="Perl-based GUI system monitor."
-APP_VERSION=5.6
+APP_VERSION=5.7
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get install -y rrdtool librrds-perl sysstat
@@ -12934,35 +12934,13 @@ if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 else    # Otherwise use version for 32-bit kernel
 	ARCH_TYPE=linux32
 fi
-FILE_NAME=${APP_NAME}-${APP_VERSION}-${ARCH_TYPE}
-curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/pycsvdb/${FILE_NAME}.${APP_EXT}
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}-${ARCH_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
 cd /tmp
 dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
 sudo mkdir -p /opt/${APP_NAME,,}
-sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
-cat > /tmp/${APP_NAME,,} << EOF
-#! /bin/sh
-cd /opt/${APP_NAME,,}
-PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
-python3 /opt/${APP_NAME,,}/${APP_NAME}.pyw
-cd $HOME
-EOF
-sudo mv /tmp/${APP_NAME,,} /usr/local/bin
-sudo chmod a+x /usr/local/bin/${APP_NAME,,}
-cat > /tmp/${APP_NAME,,}.desktop << EOF
-[Desktop Entry]
-Name=${APP_NAME}
-Comment=${APP_GUI_NAME}
-GenericName=${APP_NAME}
-Path=/opt/${APP_NAME,,}
-Exec=python3 /opt/${APP_NAME,,}/${APP_NAME}.pyw
-Icon=
-Type=Application
-StartupNotify=true
-Terminal=false
-Categories=Programming;Development;
-Keywords=SQLite;CSV;Database;
-EOF
-sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+sudo mv /tmp/${FILE_NAME}/${APP_NAME,,}-${APP_VERSION}/bin/* /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/${APP_NAME,,}-${APP_VERSION}/doc /opt/${APP_NAME,,}
+sudo ln -f -s /opt/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME}*
