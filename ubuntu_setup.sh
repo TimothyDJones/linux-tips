ec8ab1d444c884966a2df7ac9143383a15260706
Tim Jones <tdjones74021@yahoo.com>
2018-08-16 08:02:30 -0500

    Add commands to install Manuskript editing tool for book writers from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -13196,3 +13196,14 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/*${APP_NAME}*
+
+# Install Manuskript editing tool for book writers from Debian package
+APP_NAME=Manuskript
+APP_GUI_NAME="Editing tool for book writers."
+APP_VERSION=0.7.0-1
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
