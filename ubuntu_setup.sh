84617a3b629eb5d2f42381055b4728f169041ae0
Tim Jones <tdjones74021@yahoo.com>
2018-08-16 07:51:37 -0500

    Add commands to install jRCalc Java-based resistor calculator from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -13165,3 +13165,34 @@ cd /tmp/${FILE_NAME}/src
 make && sudo make install
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install jRCalc Java-based resistor calculator from package
+APP_NAME=jRCalc
+APP_GUI_NAME="Java-based resistor calculator."
+APP_VERSION=0.3
+APP_EXT=tgz
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}
+sudo apt-get install -y openjfx
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+sudo ln -f -s /opt/${APP_NAME,,}/${APP_NAME,,}.sh /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/icons/icon-${APP_NAME,,}32.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Electronics;Engineering;
+Keywords=Resistor;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
