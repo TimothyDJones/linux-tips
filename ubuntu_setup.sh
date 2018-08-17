b4fa55af36a3513408de00593601b2b150ea25fa
Tim Jones <tdjones74021@yahoo.com>
2018-08-17 09:37:59 -0500

    Remove extraneous commands.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -13224,38 +13224,6 @@ APP_NAME=Pext
 APP_GUI_NAME="Python/Qt-based productivity tool."
 APP_VERSION=0.17
 APP_EXT=AppImage
-FILE_NAME=${APP_NAME,,}-${APP_VERSION}
-sudo apt-get install -y libnotify-bin python3-pip python3-dulwich python3-pyqt5.qtquick qml-module-qtquick-controls
-curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME}/${APP_NAME}/archive/v${APP_VERSION}.${APP_EXT}
-cd /tmp
-dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
-cd /tmp/${FILE_NAME}/${APP_NAME}-${APP_VERSION}
-sudo mkdir -p /opt/${APP_NAME,,}
-sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
-sudo ln -f -s /opt/${APP_NAME,,}/${APP_NAME,,}.sh /usr/local/bin/${APP_NAME,,}
-cat > /tmp/${APP_NAME,,}.desktop << EOF
-[Desktop Entry]
-Name=${APP_NAME}
-Comment=${APP_GUI_NAME}
-GenericName=${APP_NAME}
-Path=/opt/${APP_NAME,,}
-Exec=/usr/local/bin/${APP_NAME,,}
-Icon=/opt/${APP_NAME,,}/icons/icon-${APP_NAME,,}32.png
-Type=Application
-StartupNotify=true
-Terminal=false
-Categories=Electronics;Engineering;
-Keywords=Resistor;
-EOF
-sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
-cd $HOME
-rm -rf /tmp/*${APP_NAME}*
-
-# Install Pext Python/Qt-based productivity tool from AppImage
-APP_NAME=Pext
-APP_GUI_NAME="Python/Qt-based productivity tool."
-APP_VERSION=0.17
-APP_EXT=AppImage
 FILE_NAME=${APP_NAME}-x86_64
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME}/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
 sudo mkdir -p /opt/${APP_NAME,,}
