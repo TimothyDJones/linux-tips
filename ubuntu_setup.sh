003c09588c88d19e0c38f8bf6488a33f837bec39
Tim Jones <tdjones74021@yahoo.com>
2018-08-25 16:08:28 -0500

    Update QtFM lightweight desktop-independent GUI file manager to version 6.1.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -11826,7 +11826,7 @@ rm -rf /tmp/*${APP_NAME}*
 # Install QtFM lightweight desktop-independent GUI file manager from source
 APP_NAME=QtFM
 APP_GUI_NAME="Lightweight desktop-independent GUI file manager."
-APP_VERSION=6.1.0
+APP_VERSION=6.1.1
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/rodlie/${APP_NAME,,}/archive/${APP_VERSION}.${APP_EXT}
