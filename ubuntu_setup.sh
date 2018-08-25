0d81d5b691440e4f85e6cda237831b3ccbba8fd0
Tim Jones <tdjones74021@yahoo.com>
2018-08-25 16:02:44 -0500

    Update Kid3 audio tag editor to version 3.6.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3611,7 +3611,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Kid3 audio tag editor
 APP_NAME=Kid3
-APP_VERSION=3.6.1
+APP_VERSION=3.6.2
 APP_EXT=tgz
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -k -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME,,}-${APP_VERSION}-Linux.${APP_EXT}
 cd /tmp
