dd835ca07e64a35bcd6c85ef9e77e34a6d3199e6
Tim Jones <tdjones74021@yahoo.com>
2018-09-14 16:48:34 +0100

    Update Raccoon Java-based Google Play Store and APK downloader utility to version 4.2.6.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3536,7 +3536,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Raccoon Java-based Google Play Store and APK downloader utility
 APP_NAME=raccoon
-APP_VERSION=4.2.3
+APP_VERSION=4.2.6
 APP_EXT=jar
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -k -L http://${APP_NAME}.onyxbits.de/sites/${APP_NAME}.onyxbits.de/files/${APP_NAME}-${APP_VERSION}.${APP_EXT}
 sudo mkdir -p /opt/${APP_NAME}
