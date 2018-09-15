d52602c8b0d24256f04be5a7f92ca9039744e1a8
Tim Jones <tdjones74021@yahoo.com>
2018-09-15 16:38:15 -0500

    Add commands to install Markdown Explorer Electron-based markdown viewer/editor from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -13688,3 +13688,47 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install Markdown Explorer Electron-based markdown viewer/editor from package
+APP_NAME=Markdown-Explorer
+APP_GUI_NAME="Markdown Explorer Electron-based markdown viewer/editor."
+APP_VERSION=0.1.1
+APP_EXT=tar.gz
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	ARCH_TYPE=x64
+else    # Otherwise use version for 32-bit kernel
+	ARCH_TYPE=ia32
+fi
+FILE_NAME=${APP_NAME//-/}-linux-${ARCH_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/jersou/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+LD_LIBRARY_PATH=/opt/${APP_NAME,,}:\$LD_LIBRARY_PATH; export LD_LIBRARY_PATH
+/opt/${APP_NAME,,}/${APP_NAME//-/}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=opt/${APP_NAME,,}
+Exec=LD_LIBRARY_PATH=/opt/${APP_NAME,,}:\$LD_LIBRARY_PATH; /opt/${APP_NAME,,}/${APP_NAME//-/}
+Icon=/opt/${APP_NAME,,}/icon.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Accessories;Office;
+Keywords=Markdown;Editor;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
