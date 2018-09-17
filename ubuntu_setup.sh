1bddbbc2fba5d1d31898af22ef5bcbb0a70a98be
Tim Jones <tdjones74021@yahoo.com>
2018-09-17 15:08:59 +0100

    Update MultiTextEditor Java-based text editor/word processor to version 3.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -12750,7 +12750,7 @@ rm -rf /tmp/*${APP_NAME}*
 # Install MultiTextEditor Java-based text editor/word processor from package
 APP_NAME=MultiTextEditor
 APP_GUI_NAME="Java-based text editor/word processor."
-APP_VERSION=3.0
+APP_VERSION=3.1
 APP_EXT=zip
 FILE_NAME=${APP_NAME//M/m}-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
@@ -13753,6 +13753,46 @@ FILE_NAME=${APP_NAME}${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
 cd /tmp
 dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -cp derbyclient.jar -jar /opt/${APP_NAME,,}/${APP_NAME,,}.jar
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=java -cp derbyclient.jar -jar /opt/${APP_NAME,,}/${APP_NAME,,}.jar
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Entertainment;Games;
+Keywords=Adventure;Space;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
+
+# Install EmACT minimalist Emacs editor clone from source
+APP_NAME=EmACT
+APP_GUI_NAME="Minimalist Emacs editor clone."
+APP_VERSION=2.58.0
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+cd ${FILE_NAME}
+./configure && make && sudo make install
 sudo mkdir -p /opt/${APP_NAME,,}
 sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
 cat > /tmp/${APP_NAME,,} << EOF
