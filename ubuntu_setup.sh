d778a922cfd5ee06be75f3ced385e3d7f8bf6da3
Tim Jones <tdjones74021@yahoo.com>
2018-09-19 14:45:42 +0100

    Add commands to install Password Keeper-Generator (PKG) Java-based password generator and manager from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -13942,3 +13942,41 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/*${APP_NAME}*
+
+# Install Password Keeper-Generator (PKG) Java-based password generator and manager from package
+APP_NAME=PKG
+APP_GUI_NAME="Java-based password generator and manager."
+APP_VERSION=18-09-18
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_bin
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/j-${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:/opt/${APP_NAME,,}/lib:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${APP_NAME,,}.jar
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}:/opt/${APP_NAME,,}/lib
+Exec=java -jar /opt/${APP_NAME,,}/${APP_NAME,,}.jar
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Internet;Accessories;
+Keywords=Password;Generator;Manager;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
