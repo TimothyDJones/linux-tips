df2e54f5fa90adeba61799f840fc52e438a5c8fd
Tim Jones <tdjones74021@yahoo.com>
2018-09-21 15:09:00 +0100

    Update eLogSim digital circuit simulator to version 1.1.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -11524,7 +11524,7 @@ rm -rf /tmp/*${APP_NAME}*
 # Install eLogSim digital circuit simulator from package
 APP_NAME=eLogSim
 APP_GUI_NAME="Digital circuit simulator."
-APP_VERSION=1.0.0
+APP_VERSION=1.1.0
 APP_EXT=zip
 FILE_NAME=My${APP_NAME}_${APP_VERSION//./}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
