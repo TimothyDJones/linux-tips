86728d6f97d4822b32795b99e98d48e6e21270ba
Tim Jones <tdjones74021@yahoo.com>
2018-09-24 02:06:00 +0100

    Add commands to install Chronometer stopwatch and countdown timer from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -14033,3 +14033,32 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${A
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install Chronometer stopwatch and countdown timer from package
+APP_NAME=Chrono
+APP_GUI_NAME="Stopwatch and countdown timer."
+APP_VERSION=1.1.1
+APP_EXT=tar.xz
+FILE_NAME=${APP_NAME,,}-gui_${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}-gui/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp /tmp/${FILE_NAME}/${APP_NAME,,}-gui/${APP_NAME,,}-linux64 /usr/local/bin/${APP_NAME,,}
+sudo cp /tmp/${FILE_NAME}/${APP_NAME,,}-gui/source/main.ico /usr/share/icons/${APP_NAME,,}.ico
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}:/opt/${APP_NAME,,}/lib
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/share/icons/${APP_NAME,,}.ico
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Accessories;
+Keywords=Stopwatch;Timer;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
