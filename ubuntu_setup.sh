8697def26234bd76cf647ca9b94fc5aeb3399ff1
Tim Jones <tdjones74021@yahoo.com>
2018-09-25 19:33:12 +0100

    Update UNA social-media community management web-based tool to version 9.0.0-RC10.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7025,7 +7025,7 @@ rm -rf /tmp/${APP_NAME,,}*
 
 # Install UNA social-media community management web-based tool (PHP/MySQL)
 APP_NAME=UNA
-APP_VERSION=9.0.0-RC5
+APP_VERSION=9.0.0-RC10
 APP_EXT=zip
 DB_NAME=${APP_NAME,,}
 DB_USER=${APP_NAME,,}
