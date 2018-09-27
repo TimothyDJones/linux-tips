a71b791900b5d8ee9c99789e768be0de899a8718
Tim Jones <tdjones74021@yahoo.com>
2018-09-27 23:52:18 +0100

    Add commands to install TurboVNC remote desktop tool from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -14204,3 +14204,14 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/*${APP_NAME}*
+
+# Install TurboVNC remote desktop tool from Debian package
+APP_NAME=TurboVNC
+APP_GUI_NAME="remote desktop tool."
+APP_VERSION=2.2
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
