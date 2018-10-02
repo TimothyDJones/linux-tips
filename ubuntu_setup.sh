5be3b287e6b33cdc4a4da93b372b74640d3d208d
Tim Jones <tdjones74021@yahoo.com>
2018-10-02 22:13:38 +0100

    Update MindForger notepad and Markdown editor/IDE with built-in preview to version 1.47.0-1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -10002,7 +10002,7 @@ rm -rf /tmp/*${APP_NAME,,}*
 # Install MindForger notepad and Markdown editor/IDE with built-in preview from Debian package
 APP_NAME=MindForger
 APP_GUI_NAME="Notepad and Markdown editor/IDE with built-in preview."
-APP_VERSION=1.46.0-1
+APP_VERSION=1.47.0-1
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
