2c7dc00e946ec619609461121dfbb46360db8d89
Tim Jones <tdjones74021@yahoo.com>
2018-10-02 16:58:18 +0100

    Update Modelio UML modeling tool to version 3.8_3.8.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4729,7 +4729,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Modelio UML modeling tool from package
 APP_NAME=Modelio
-APP_VERSION=3.7_3.7.1-201803072330
+APP_VERSION=3.8_3.8.0
 APP_EXT=deb
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}uml/${APP_NAME,,}-open-source${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
