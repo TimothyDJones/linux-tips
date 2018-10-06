4c4812b912b23e7cfedd049123d101a5f0a4fb20
Tim Jones <tdjones74021@yahoo.com>
2018-10-06 19:52:09 +0100

    Add commands to install Jet File Transfer Java/JavaFX-based LAN file transfer tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -14379,3 +14379,41 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/*${APP_NAME}*
+
+# Install Jet File Transfer Java/JavaFX-based LAN file transfer tool from package
+APP_NAME=Jet-File-Transfer
+APP_GUI_NAME="Java/JavaFX-based LAN file transfer tool."
+APP_VERSION=1.0
+APP_EXT=rar
+FILE_NAME=JetF_leTransferAll
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/Linux/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${APP_NAME//-}-v${APP_VERSION}.jar
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=java -jar /opt/${APP_NAME,,}/${APP_NAME//-}-v${APP_VERSION}.jar
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Accessories;Network;
+Keywords=LAN;Transfer;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
