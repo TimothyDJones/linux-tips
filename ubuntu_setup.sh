2925da2698a2ac589fc384fe6c2c8e8779752033
Tim Jones <tdjones74021@yahoo.com>
2018-10-08 19:55:38 +0100

    Add commands to install Namely Java/JavaFX-based multi-file renamer from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -14465,3 +14465,39 @@ git clone --recursive https://github.com/antonvw/wxExtension.git
 cd wxExtension
 mkdir build && cd build   
 cmake .. && make && sudo make install
+
+# Install Namely Java/JavaFX-based multi-file renamer from package
+APP_NAME=Namely
+APP_GUI_NAME="Java/JavaFX-based multi-file renamer."
+APP_VERSION=1.0
+APP_EXT=jar
+FILE_NAME=${APP_NAME}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L http://download1495.mediafire.com/dp60t6hu96lg/8dpfz3x0b4ea2nd/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Accessories;System;
+Keywords=Renamer;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
