dbdab7889c001940c54a1319ec227cddb2484f87
Tim Jones <tdjones74021@yahoo.com>
2018-10-12 20:28:12 +0100

    Add commands to install Google-Drive-Electron cross-platform, Electron-based desktop tool for Google Drive from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -14527,3 +14527,41 @@ cd /tmp/${FILE_NAME}/*${APP_NAME}*/
 sudo python3 ./setup.py install
 cd $HOME
 rm -rf /tmp/*${APP_NAME}*
+
+# Install Google-Drive-Electron cross-platform, Electron-based desktop tool for Google Drive from package
+APP_NAME=Google-Drive-Electron
+APP_GUI_NAME="Cross-platform, Electron-based desktop tool for Google Drive."
+APP_VERSION=v.funky-duck/0.0.2
+APP_EXT=zip
+FILE_NAME=${APP_NAME//-/.}-linux-x64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/alexkim205/Google-Drive-Electron/releases/download/${APP_VERSION////%2F}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv "/tmp/${FILE_NAME}/${FILE_NAME//./\ }"/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+"/opt/${APP_NAME,,}/${APP_NAME//-/\ }"
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec="/opt/${APP_NAME,,}/${APP_NAME//-/\ }"
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Internet;Accessories;System;
+Keywords=Google;Storage;Drive;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
