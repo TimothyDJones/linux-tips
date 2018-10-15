7d5ca49b6b59ffa1bccd940cd0dd4d5c778ded39
Tim Jones <tdjones74021@yahoo.com>
2018-10-15 15:24:18 +0100

    Add commands to install Dark Chess (Banqi) Python-based chess variant game from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -14657,3 +14657,42 @@ sudo usermod -aG docker ${USER}  # Add user account to 'docker' group to run com
 su ${USER}
 id -nG  # Confirm user account added to 'docker' group.
 docker run hello-world  # Confirm Docker installation.
+
+# Install Dark Chess (Banqi) Python-based chess variant game from package
+APP_NAME=DarkChess
+APP_GUI_NAME="Python-based chess variant game."
+APP_VERSION=086
+APP_EXT=tar.gz
+FILE_NAME=p_${APP_NAME,,}_${APP_VERSION}
+sudo pip3 install pygame
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+python3 /opt/${APP_NAME,,}/${APP_NAME,,}.py
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=python3 /opt/${APP_NAME,,}/${APP_NAME,,}.py
+Icon=/opt/${APP_NAME,,}/Image/${APP_NAME,,}_default.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Entertainment;Games;
+Keywords=Chess;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
