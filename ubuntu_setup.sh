35cabf55807fed3638b9f57794d3b46ef524e87e
Tim Jones <tdjones74021@yahoo.com>
2018-10-17 18:42:45 +0100

    Update InfiniTex Electron-based LaTeX source and WYSIWYG editor to version 0.9.15 and to install via AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7580,14 +7580,42 @@ sudo sh /tmp/${APP_NAME,,}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
 
-# Install InfiniTex Electron-based LaTeX source and WYSIWYG editor from package
+# Install InfiniTex Electron-based LaTeX source and WYSIWYG editor from AppImage
 APP_NAME=InfiniTex
 APP_GUI_NAME="Electron-based LaTeX source and WYSIWYG editor."
-APP_VERSION=0.9.2
-APP_EXT=deb
+APP_VERSION=0.9.15
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-${APP_VERSION}-x86_64
 sudo apt-get install -y git-svn unixodbc unixodbc-dev
-curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://github.com/fetacore/${APP_NAME}/releases/download/v${APP_VERSION}/${APP_NAME}_${APP_VERSION}_amd64.${APP_EXT}
-sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/fetacore/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+sudo chmod +x /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo ln -f -s /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+python3 /opt/${APP_NAME,,}/${APP_NAME,,}.py
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Office;
+Keywords=LaTeX;Word;Editor;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
 
