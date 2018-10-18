279d8076f89b8bb6c3750093592d7bc41f9ab472
Tim Jones <tdjones74021@yahoo.com>
2018-10-18 16:14:13 -0500

    Add commands to install glparchis OpenGL-based Parchis (Parcheesi) game from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -14781,3 +14781,41 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/prisma/${APP_NAME,
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME}*
+
+# Install glparchis OpenGL-based Parchis (Parcheesi) game from package
+APP_NAME=glparchis
+APP_GUI_NAME="OpenGL-based Parchis (Parcheesi) game."
+APP_VERSION=20180510
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-linux-${APP_VERSION}.x86_64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+/opt/${APP_NAME,,}/${APP_NAME,,}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/ficharoja.ico
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Entertainment;Games;
+Keywords=Parcheesi;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
