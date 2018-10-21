36915f8395951a0924e597dfe99666a98e0d6b35
Tim Jones <tdjones74021@yahoo.com>
2018-10-21 18:55:41 -0500

    Update DB Tarzan Java-based database client to version 1.15.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -6453,7 +6453,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 # Install DB Tarzan Java-based database client from package
 APP_NAME=DBTarzan
 APP_GUI_NAME="Java-based database client."
-APP_VERSION=1.14
+APP_VERSION=1.15
 APP_EXT=deb
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME,,}_${APP_VERSION}_all.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
