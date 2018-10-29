0ad5718866da8f7a04fe62a81effb965b903a110
Tim Jones <tdjones74021@yahoo.com>
2018-10-29 13:23:51 -0500

    Add commands to install HTML Notepad minimalist WYSIWG HTML editor from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -15148,3 +15148,41 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/kamranahmedse/${AP
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME}*
+
+# Install HTML Notepad minimalist WYSIWG HTML editor from package
+APP_NAME=HTML-Notepad
+APP_GUI_NAME="Minimalist WYSIWG HTML editor."
+APP_VERSION=N/A
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-dist
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://${APP_NAME,,}.com/dist/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+/opt/${APP_NAME,,}/${APP_NAME,,}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${APP_NAME,,}
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Programming;Development;
+Keywords=HTML;Editor;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
