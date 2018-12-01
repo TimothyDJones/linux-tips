b9f128d2424a6da25a515349aa2de1fa335e01da
Tim Jones <tdjones74021@yahoo.com>
2018-12-01 13:30:45 -0600

    Add commands to install JPhotoTagger Java-based GUI photo tag/metadata editor from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -15536,3 +15536,41 @@ Keywords=Clock;
 EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
+
+# Install JPhotoTagger Java-based GUI photo tag/metadata editor from package
+APP_NAME=JPhotoTagger
+APP_GUI_NAME="Java-based GUI photo tag/metadata editor."
+APP_VERSION=0.41.1
+APP_EXT=zip
+FILE_NAME=${APP_NAME}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mv /tmp/${FILE_NAME}/${APP_NAME} /opt
+sudo chmod -R a+w /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME}
+PATH=/opt/${APP_NAME}:/opt/${APP_NAME}/lib:\$PATH; export PATH
+java -jar ${APP_NAME}.jar
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME}:/opt/${APP_NAME}/lib
+Exec=java -jar ${APP_NAME}.jar
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Office;Accessories;Graphics;
+Keywords=Photo;Tagger;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
