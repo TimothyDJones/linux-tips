e0f3eb29165713bf26f5290e2abdefc052004b8e
Tim Jones <tdjones74021@yahoo.com>
2018-12-01 18:08:05 -0600

    Add commands to install fre:ac audio converter and CD ripper from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -15587,3 +15587,49 @@ dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
 cd /tmp/${FILE_NAME}
 sudo pip3 install -r requirements.txt
 sudo python3 setup.py install
+
+# Install fre:ac audio converter and CD ripper from package
+APP_NAME="fre:ac"
+APP_GUI_NAME="Audio converter and CD ripper."
+APP_VERSION=1.1-alpha-20181201
+APP_EXT=tar.gz
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	ARCH_TYPE=linux-x64
+else    # Otherwise use version for 32-bit kernel
+	ARCH_TYPE=linux
+fi
+FILE_NAME=${APP_NAME//:/}-${APP_VERSION}-${ARCH_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/bonkenc/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME//:/}
+sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME//:/}
+sudo ln -f -s /opt/${APP_NAME//:/}/${APP_NAME//:/} /usr/local/bin/${APP_NAME//:/}
+sudo ln -f -s /opt/${APP_NAME//:/}/${APP_NAME//:/}cmd /usr/local/bin/${APP_NAME//:/}cmd
+cat > /tmp/${APP_NAME//:/} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME//:/}
+LD_LIBRARY_PATH=/opt/${APP_NAME//:/}:${LD_LIBRARY_PATH}; export LD_LIBRARY_PATH
+PATH=/opt/${APP_NAME//:/}:\$PATH; export PATH
+/opt/${APP_NAME//:/}/${APP_NAME//:/}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME//:/} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME//:/}
+cat > /tmp/${FILE_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME//:/}
+Exec=/opt/${APP_NAME//:/}/${APP_NAME//:/}
+Icon=/opt/${APP_NAME//:/}/icons/${APP_NAME//:/}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Audio;Multimedia;
+Keywords=Audio;Converter;
+EOF
+sudo mv /tmp/${FILE_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME}*
