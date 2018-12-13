041c10d3feb8d274c118ba1732cd05b7c89dd33a
Tim Jones <tdjones74021@yahoo.com>
2018-12-13 15:44:06 -0600

    Update Brave web browser to version 0.26.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1529,7 +1529,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Brave web browser from package
 APP_NAME=brave
-APP_VERSION=0.25.3
+APP_VERSION=0.26.0
 APP_EXT=deb
 curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME}-browser.mirror/${APP_NAME}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME}.${APP_EXT}
