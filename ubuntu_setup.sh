812ab038c37be05d8e582f7838f77520d15d1c2a
Tim Jones <tdjones74021@yahoo.com>
2018-12-16 17:00:53 -0600

    Add commands to install NextFractal Java-based fractal explorer from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -15788,3 +15788,32 @@ sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 sudo ln -f -s /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install NextFractal Java-based fractal explorer from package
+APP_NAME=NextFractal
+APP_GUI_NAME="Java-based fractal explorer."
+APP_VERSION=2.1.0
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}_debian_x86_64_${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mv /tmp/${FILE_NAME}/${APP_NAME} /opt
+sudo ln -s -f /opt/${APP_NAME}/${APP_NAME} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Education;Math;Science;
+Keywords=Fractal;Math
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
