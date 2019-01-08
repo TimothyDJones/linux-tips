e7b30b26e50bee60c48dedaa8b9ab2827ad776d4
Tim Jones <tdjones74021@yahoo.com>
2019-01-08 14:52:24 -0600

    Update QPDF PDF utility to version 8.3.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2411,7 +2411,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install QPDF PDF utility from source
 APP_NAME=qpdf
-APP_VERSION=8.2.1
+APP_VERSION=8.3.0
 APP_EXT=tar.gz
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME}-${APP_VERSION}.${APP_EXT}
 cd /tmp
