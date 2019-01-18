38fa5152d4f1ffe49945925bcf378401a4c6eb29
Tim Jones <tdjones74021@yahoo.com>
2019-01-18 15:19:39 -0600

    Add commands to install Arena UCI and Winboard-compatible chess GUI from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -16345,3 +16345,46 @@ cd /tmp/${APP_NAME,,}
 sudo ln -s /usr/bin/${APP_NAME} /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install Arena UCI and Winboard-compatible chess GUI from package
+APP_NAME=Arena
+APP_GUI_NAME="UCI and Winboard-compatible chess GUI."
+APP_VERSION=1.1
+APP_EXT=tar.gz
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	ARCH_TYPE=64bit
+else    # Otherwise use version for 32-bit kernel
+	ARCH_TYPE=32bit
+fi
+FILE_NAME=${APP_NAME,,}linux_${ARCH_TYPE}_${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L http://www.playwitharena.de/downloads/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+/opt/${APP_NAME,,}/Arena_x86_64_linux
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/Arena_x86_64_linux
+Icon=/opt/${APP_NAME,,}/${APP_NAME}.ico
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Games;Entertainment;
+Keywords=Chess;UCI;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
