005c71b59da271184dfcfc2ae283bcd0b29c856c
Tim Jones <tdjones74021@yahoo.com>
2019-01-18 20:45:46 -0600

    Add commands to install cfiles ncurses file manager with Vim keybindings from source.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -16251,3 +16251,57 @@ FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
 sudo apt-get install -y libgconf2-4
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/headsetapp/headset-electron/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+
+# Install orng Java Markdown-based journal editor from package
+APP_NAME=orng
+APP_GUI_NAME="Java Markdown-based journal editor."
+APP_VERSION=1.1.0
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}j_${APP_VERSION}_gnu_linux_64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+sudo chmod -R a+w /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar ${FILE_NAME}.${APP_EXT}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=java -jar ${FILE_NAME}.${APP_EXT}
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Programming;Development;
+Keywords=UML;Diagramming;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
+
+# Install cfiles ncurses file manager with Vim keybindings from source
+APP_NAME=cfiles
+APP_GUI_NAME="Ncurses file manager with Vim keybindings."
+APP_VERSION=N/A
+APP_EXT=N/A
+FILE_NAME=N/A
+sudo apt-get install -y libncurses5-dev cmake 
+cd /tmp
+git clone https://github.com/mananapr/${APP_NAME,,}
+cd /tmp/${APP_NAME,,}
+mkdir build && cd build
+cmake .. && make
+sudo mv ${APP_NAME,,} /usr/local/bin
+sudo rm -rf /tmp/${APP_NAME,,}*
