e768e5bceba9a4e04832493bffdff83b0a3d8318
Tim Jones <tdjones74021@yahoo.com>
2019-01-19 22:54:40 -0600

    Add commands to install JIBS (Java Image Browser Sorter) image viewer focused on image sorting from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -16483,3 +16483,40 @@ sudo gdebi -n libgranite5-common.deb
 sudo gdebi -n libgranite5.deb
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/spez-browser-mirrors/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+
+# Install JIBS (Java Image Browser Sorter) image viewer focused on image sorting from package
+APP_NAME=JIBS
+APP_GUI_NAME="Image viewer focused on image sorting."
+APP_VERSION=3.2.1
+APP_EXT=jar
+FILE_NAME=${APP_NAME}-legacy-Java8-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/img-browse-sort/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Graphics;
+Keywords=Image;Viewer;Java;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
+
