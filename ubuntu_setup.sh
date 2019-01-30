005e4fa4ca70dfb1c569cc58441055d66b1ec16b
Tim Jones <tdjones74021@yahoo.com>
2019-01-30 13:44:41 -0600

    Update ZenTao project management tool to version 11.2.stable.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1552,7 +1552,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install ZenTao project management tool from package
 APP_NAME=ZenTaoPMS
-APP_VERSION=11.1.stable
+APP_VERSION=11.2.stable
 APP_EXT=deb
 curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/zentao/${APP_NAME}_${APP_VERSION}_1_all.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME}.${APP_EXT}
