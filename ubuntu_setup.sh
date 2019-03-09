31a29075ab19d96bf970fa58ec3c4594a6202fc8
Tim Jones <tdjones74021@yahoo.com>
2019-03-09 09:47:42 -0600

    Update FeedTurtle Java-based RSS news aggregator client to version 11 (a.k.a. 'X1') and to standard installation process.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3393,18 +3393,30 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install FeedTurtle Java-based RSS news aggregator client
 APP_NAME=FeedTurtle
-APP_VERSION=X.6
+APP_GUI_NAME="Java-based RSS news aggregator client"
+APP_VERSION=11
 APP_EXT=zip
-curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME}${APP_VERSION}.${APP_EXT}
+FILE_NAME=${APP_NAME}%20${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
 cd /tmp
-dtrx -n /tmp/${APP_NAME,,}.${APP_EXT}
-sudo mv /tmp/${APP_NAME,,}/${APP_NAME}${APP_VERSION} /opt/${APP_NAME,,}
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${APP_NAME}${APP_VERSION}.jar
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cat > /tmp/${APP_NAME,,}.desktop << EOF
 [Desktop Entry]
 Name=${APP_NAME}
-Comment=Java-based RSS news aggregator
+Comment=${APP_GUI_NAME}
 GenericName=${APP_NAME}
-Exec=java -jar /opt/${APP_NAME,,}/${APP_NAME,,}${APP_VERSION}.jar
+Exec=java -jar /opt/${APP_NAME,,}/${APP_NAME}${APP_VERSION}.jar
 #Icon=
 Type=Application
 StartupNotify=true
