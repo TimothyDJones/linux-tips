871790cb4c4b656dd069f7a7065aece10de6c577
Tim Jones <tdjones74021@yahoo.com>
2019-03-23 13:12:34 -0500

    Update Extraterm Electron-based, cross-platform terminal emulator to version 0.41.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -9106,7 +9106,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install Extraterm Electron-based, cross-platform terminal emulator from package
 APP_NAME=Extraterm
 APP_GUI_NAME="Electron-based, cross-platform terminal emulator."
-APP_VERSION=0.37.3
+APP_VERSION=0.41.1
 APP_EXT=zip
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=x64
