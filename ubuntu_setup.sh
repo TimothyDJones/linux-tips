f4b45059516ae349a42980e8748fd45cd22f05a6
Tim Jones <tdjones74021@yahoo.com>
2019-03-30 10:35:42 -0500

    Add commands to install NoSQLBooster cross-platform GUI manager for MongoDB from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -17360,3 +17360,33 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/*${APP_NAME}*
+
+# Install NoSQLBooster cross-platform GUI manager for MongoDB from AppImage
+APP_NAME=NoSQLBooster
+APP_GUI_NAME="Cross-platform GUI manager for MongoDB."
+APP_VERSION=5.1.5
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME,,}4mongo-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://nosqlbooster.com/s3/download/releasesv5/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+sudo chmod +x /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo ln -f -s /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Development;Programming;
+Keywords=Database;NoSQL;MongoDB;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
+
