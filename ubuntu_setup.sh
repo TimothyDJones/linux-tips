77bc123ed801fe5363a3c2f7fc786bb3260288f2
Tim Jones <tdjones74021@yahoo.com>
2019-04-07 07:36:58 -0500

    Add commands to install SQLTabs cross-platform, multi-database (MySQL, PostgreSQL, SQL Server, etc.) GUI database management/client tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -17504,3 +17504,42 @@ rm -rf /tmp/*${APP_NAME}*
 sudo add-apt-repository -y ppa:scorpio/ppa
 sudo apt-get update -y
 sudo apt-get install -y qelectrotech
+
+# Install SQLTabs cross-platform, multi-database (MySQL, PostgreSQL, SQL Server, etc.) GUI database management/client tool from package
+APP_NAME=SQLTabs
+APP_GUI_NAME="Cross-platform, multi-database (MySQL, PostgreSQL, SQL Server, etc.) GUI database management/client tool."
+APP_VERSION=1.0.0
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}.linux
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/sasha-alias/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/${APP_NAME,,}*/* /opt/${APP_NAME,,}
+sudo chmod -R a+w /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+/opt/${APP_NAME,,}/${APP_NAME,,}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/logo.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Development;Programming;
+Keywords=Database;Editor;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}* /tmp/*${APP_NAME,,}*
