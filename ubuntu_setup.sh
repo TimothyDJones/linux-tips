df2d3d8f97cc2d4841966ff71b9b45793f7050de
Tim Jones <tdjones74021@yahoo.com>
2019-04-12 19:42:26 -0500

    Update Fractalscope Qt-based fractal explorer to version 1.3.5.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -6917,7 +6917,7 @@ cargo install --git https://github.com/jmacdonald/${APP_NAME,,}/ --tag ${APP_VER
 # Install Fractalscope Qt-based fractal explorer from source
 APP_NAME=Fractalscope
 APP_GUI_NAME="Cross-platform Qt-based fractal explorer."
-APP_VERSION=1.3.4
+APP_VERSION=1.3.5
 APP_EXT=tar.gz
 sudo apt-get install -y qt5-default yasm
 # Install MPIR (Multiple Precision Integers and Rationals) LGPL C library
