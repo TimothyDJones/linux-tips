4813697bd875941f4da7c838a69871562990bc18
Tim Jones <tdjones74021@yahoo.com>
2019-04-17 15:45:36 -0500

    Add commands to install Moeditor cross-platform, Electron-based Markdown editor from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -17544,3 +17544,12 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/*${APP_NAME}* /tmp/*${APP_NAME,,}*
+
+# Install Moeditor cross-platform, Electron-based Markdown editor from Debian package
+APP_NAME=Moeditor
+APP_GUI_NAME="Cross-platform, Electron-based Markdown editor."
+APP_VERSION=0.2.0-1
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${ARCH_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME}/${APP_NAME}/releases/download/v0.2.0-beta/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
