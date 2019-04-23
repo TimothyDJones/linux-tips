77df7558d6642702680ebd2ff6707bb31c94f7aa
Tim Jones <tdjones74021@yahoo.com>
2019-04-23 12:50:04 -0500

    Add commands to install Mattermost cross-platform, Electron-based workplace messaging alternative to Slack from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -17645,3 +17645,11 @@ FILE_NAME=com.github.nick92.${APP_NAME,,}_${APP_VERSION}~ubuntu${VERSION_NUMBER}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://launchpad.net/~coffee-team/+archive/ubuntu/${APP_NAME,,}/+files/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 
+# Install Mattermost cross-platform, Electron-based workplace messaging alternative to Slack from Debian package
+APP_NAME=Mattermost
+APP_GUI_NAME="Cross-platform, Electron-based workplace messaging alternative to Slack."
+APP_VERSION=4.2.1
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}-desktop-${APP_VERSION}-linux-${ARCH_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://releases.mattermost.com/desktop/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
