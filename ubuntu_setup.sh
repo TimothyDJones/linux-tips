fec8c3f14a638d933eee5cd6cfd54a0e35c1536a
Tim Jones <tdjones74021@yahoo.com>
2019-04-27 20:25:13 -0500

    Add commands to install CutePy Qt-based interactive Python console with with variable explorer, command history, and syntax highlighting from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -17778,5 +17778,41 @@ xdg-open http://localhost/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME}*
 
-
-
+# Install CutePy Qt-based interactive Python console with with variable explorer, command history, and syntax highlighting from package
+APP_NAME=CutePy
+APP_GUI_NAME="Qt-based interactive Python console with with variable explorer, command history, and syntax highlighting."
+APP_VERSION=1.1
+APP_EXT=zip
+FILE_NAME=${APP_NAME}_v${APP_VERSION}
+sudo apt-get install -y python3-pyqt5
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+sudo chmod a+w /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+python3 /opt/${APP_NAME,,}/${APP_NAME}.pyw
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=python3 /opt/${APP_NAME,,}/${APP_NAME}.py
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Development;Programming;
+Keywords=Editor;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}* /tmp/*${APP_NAME,,}*
