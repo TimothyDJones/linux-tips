8adfeed4c6d819fac27008de302ba3177f444620
Tim Jones <tdjones74021@yahoo.com>
2019-05-16 11:12:13 -0500

    Add commands to install Knobjex wxPython personal information manager (PIM) from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -18006,3 +18006,42 @@ APP_EXT=deb
 FILE_NAME=sofastats-${APP_VERSION}_all
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+
+# Install Knobjex wxPython personal information manager (PIM) from package
+APP_NAME=Knobjex
+APP_GUI_NAME="wxPython personal information manager (PIM)."
+APP_VERSION=4.07
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_source
+sudo apt-get install -y python3-wxgtk4.0
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}-info-manager/${FILE_NAME}.${APP_EXT}
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+sudo chmod -R a+w /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+python3 /opt/${APP_NAME,,}/main_startup.py
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=python3 /opt/${APP_NAME,,}/main_startup.py
+Icon=/opt/${APP_NAME,,}/icons/kjx_logo.ico
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Accessories;Office;
+Keywords=PIM;Calendar;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}* /tmp/*${APP_NAME,,}*
