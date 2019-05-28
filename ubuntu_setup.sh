27172a8f1812cf00671e6c82ba99788062ec2c40
Tim Jones <tdjones74021@yahoo.com>
2019-05-28 08:31:18 -0500

    Add commands to install mtCellEdit lightweight Qt-based spreadsheet program from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -18199,3 +18199,31 @@ APP_EXT=deb
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/DEgITx/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+
+# Install mtCellEdit lightweight Qt-based spreadsheet program from AppImage
+APP_NAME=mtCellEdit
+APP_GUI_NAME="Lightweight Qt-based spreadsheet program."
+APP_VERSION=3.3
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-${APP_VERSION}-qt4-x86_64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/matyler/${APP_NAME}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+sudo chmod +x /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo ln -s /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Exec=/opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Office;
+Keywords=Spreadsheet;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
