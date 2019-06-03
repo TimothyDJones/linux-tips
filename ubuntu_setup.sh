0ffa060d440b9f0d437808fcdecac69fd7a8fa75
Tim Jones <tdjones74021@yahoo.com>
2019-06-03 16:49:58 -0500

    Update Group-Office web-based office suite to version 4.1.1-3932.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1208,7 +1208,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Skychart planetarium package from Debian package
 APP_NAME=Skychart
-APP_VERSION=4.1.1-3930
+APP_VERSION=4.1.1-3932
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
 # libpasastro (Pascal astronomical library) is dependency for Skychart.
