da6c81fc4bc065aeb4916b0fdfa3dda97af25f33
Tim Jones <tdjones74021@yahoo.com>
2019-06-28 17:56:44 -0500

    Add commands to install IP Calculator Java-based IP subnet calculator from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -18459,3 +18459,41 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install IP Calculator Java-based IP subnet calculator from package
+APP_NAME=IP-Calculator
+APP_GUI_NAME="Java-based IP subnet calculator."
+APP_VERSION=28-06-2019
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_bin
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:/opt/${APP_NAME,,}/lib:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${APP_NAME,,}.jar
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}:/opt/${APP_NAME,,}/lib
+Exec=java -jar /opt/${APP_NAME,,}/${APP_NAME,,}.jar
+Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Network;System;Accessories;
+Keywords=Networking;Calculator;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
