7cae796a6df8873c43ebf45bd008dacf099e0243
Tim Jones <tdjones74021@yahoo.com>
2019-06-30 17:38:07 -0500

    Add commands to install GitApp cross-platform Electron-based desktop GitHub client from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -18459,3 +18459,31 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install GitApp cross-platform Electron-based desktop GitHub client from AppImage
+APP_NAME=GitApp
+APP_GUI_NAME="Cross-platform Electron-based desktop GitHub client."
+APP_VERSION=3.0.3
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-linux-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/dan-online/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+sudo chmod +x /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo ln -s /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Exec=/opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Development;Programming;
+Keywords=Git;GitHub;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
