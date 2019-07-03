e40848c442c83bce445ca8953ca5d98e53a84daf
Tim Jones <tdjones74021@yahoo.com>
2019-07-03 15:58:48 -0500

    Add commands to install Kiwix offline Wikipedia downloader/reader desktop client from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -18663,3 +18663,32 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/*${APP_NAME}*
+
+# Install Kiwix offline Wikipedia downloader/reader desktop client from AppImage
+APP_NAME=Kiwix
+APP_GUI_NAME="Offline Wikipedia downloader/reader desktop client."
+APP_VERSION=2.0-beta5
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME,,}-desktop_x86_64_${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT,,}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+sudo chmod +x /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo ln -s /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Education;Office;
+Keywords=Wikipedia;Reader;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
