8c65f119c9a109c1a61ee066a86761e253ef240d
Tim Jones <tdjones74021@yahoo.com>
2019-07-04 19:37:18 -0500

    Add commands to install ProtoGraphQL Electron-based cross-platform GraphQL schema prototyping tool from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -18692,3 +18692,14 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install ProtoGraphQL Electron-based cross-platform GraphQL schema prototyping tool from Debian package
+APP_NAME=ProtoGraphQL
+APP_GUI_NAME="Electron-based cross-platform GraphQL schema prototyping tool."
+APP_VERSION=1.0.0
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/oslabs-beta/${APP_NAME,,}/releases/download/v1.0.0-beta/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
