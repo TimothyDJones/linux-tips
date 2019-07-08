c64dbbea13a55d582a9509770da2eb8e89b36ad8
Tim Jones <tdjones74021@yahoo.com>
2019-07-08 02:48:13 +0200

    Add commands to install Easy Cloud Shell Java-based web file manager with built-in text editor, terminal, image viewer and video player from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -18741,3 +18741,42 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/*${APP_NAME}*
+
+# Install Easy Cloud Shell Java-based web file manager with built-in text editor, terminal, image viewer and video player from package
+APP_NAME=Easy-Cloud-Shell
+APP_GUI_NAME="Java-based web file manager with built-in text editor, terminal, image viewer and video player."
+APP_VERSION=0.5
+APP_EXT=tar.gz
+FILE_NAME=cloud-shell-bin
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/subhra74/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+sudo ln -s -f /opt/${APP_NAME,,}/start-cloud-shell.sh /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+/opt/${APP_NAME,,}/${APP_NAME,,}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/start-cloud-shell.sh && xdg-open https://localhost:8055/
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Internet;Networking;
+Keywords=File;Manager;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
