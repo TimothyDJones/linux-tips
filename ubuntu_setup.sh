e0cd4429c7fd3f44054ccfcc65c169dcb75911dd
Tim Jones <tdjones74021@yahoo.com>
2019-07-15 00:54:56 +0200

    Update PlantUML Java-based UML modeling tool to version 1.2019.8.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3147,7 +3147,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install PlantUML Java-based UML modeling tool
 APP_NAME=PlantUML
-APP_VERSION=1.2019.7
+APP_VERSION=1.2019.8
 APP_EXT=jar
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME,,}.${APP_VERSION}.${APP_EXT}
 sudo mkdir -p /opt/${APP_NAME,,}
