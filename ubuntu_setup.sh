d85f84ff3986f50d4f3cc15c5cb91ce953c0b2d2
Tim Jones <tdjones74021@yahoo.com>
2019-07-18 13:58:57 +0200

    Add command to install muCommander Java-based dual-pane file manager from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -18977,3 +18977,14 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/*${APP_NAME}* /tmp/*${APP_NAME,,}*
+
+# Install muCommander Java-based dual-pane file manager from Debian package
+APP_NAME=muCommander
+APP_GUI_NAME="Java-based dual-pane file manager"
+APP_VERSION=0.9.3-3
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION//-3/}_all
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME,,}/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
