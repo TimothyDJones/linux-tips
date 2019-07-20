7f3d7d0335cef8e69d4c99d7d021e7c356ab838f
Tim Jones <tdjones74021@yahoo.com>
2019-07-20 23:55:14 +0200

    Update Buttercup JavaScript/Electron desktop password manager to version 1.16.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3552,7 +3552,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Buttercup JavaScript/Electron desktop password manager from package
 APP_NAME=buttercup-desktop
-APP_VERSION=1.15.0
+APP_VERSION=1.16.2
 APP_EXT=deb
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://github.com/buttercup/${APP_NAME}/releases/download/v${APP_VERSION}/${APP_NAME}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
