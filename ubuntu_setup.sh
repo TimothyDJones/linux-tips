cd971bc34e3d65e6fa404d53816ccc0d8ca12ccb
Tim Jones <tdjones74021@yahoo.com>
2019-07-22 22:16:31 +0200

    Add commands to install VCT (Video Converter & Transcoder) .NET-based cross-platform GUI front-end for ffmpeg from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -18989,3 +18989,43 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME,,}/${AP
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install VCT (Video Converter & Transcoder) .NET-based cross-platform GUI front-end for ffmpeg from package
+# Must install Mono binaries (see above) before installing VCT.
+APP_NAME=VCT
+APP_GUI_NAME=".NET-based cross-platform GUI front-end for ffmpeg."
+APP_VERSION=N/A
+APP_EXT=zip
+FILE_NAME=${APP_NAME}_Linux_mono_binary
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/videoconvertertranscoder/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/VCT_mono/* /opt/${APP_NAME,,}
+sudo chmod 777 -R /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+/opt/${APP_NAME,,}/${APP_NAME}.exe
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${APP_NAME}.exe
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Multimedia;Audio;Video;
+Keywords=FFMpeg;Converter;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME}* /tmp/*${APP_NAME,,}*
