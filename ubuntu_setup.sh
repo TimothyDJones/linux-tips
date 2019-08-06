06dd9c111b57d02619ba6aae2d6fab16d64f935f
Tim Jones <tdjones74021@yahoo.com>
2019-08-06 11:05:33 -0500

    Update Apricot Java-based database ERD, design, and reverse-engineering tool to version 0.6.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -17670,9 +17670,9 @@ sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 # Install Apricot Java-based database ERD, design, and reverse-engineering tool from package
 APP_NAME=ApricotDB
 APP_GUI_NAME="Java-based database ERD, design, and reverse-engineering tool."
-APP_VERSION=0.4
-APP_EXT=zip
-FILE_NAME=${APP_NAME,,}-${APP_VERSION}-all-bin
+APP_VERSION=0.6
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}-linux-x64
 sudo apt-get install -y openjdk-11-jre openjfx
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/apricot-db/${FILE_NAME}.${APP_EXT}
 dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
