bbc15816d62b0bb93799d8b176162162654b073d
Tim Jones <tdjones74021@yahoo.com>
2019-08-20 15:26:41 -0500

    Add commands to install JDBCSQL Java-based command-line SQL query tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -19518,3 +19518,23 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/ubu
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install JDBCSQL Java-based command-line SQL query tool from package
+APP_NAME=JDBCSQL
+APP_GUI_NAME="Java-based command-line SQL query tool."
+APP_VERSION=1.0
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}/${FILE_NAME}.jar
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${FILE_NAME}.jar "$1"
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
