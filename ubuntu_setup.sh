16c797243823ddd216fbb6f208941e0d93694441
Tim Jones <tdjones74021@yahoo.com>
2019-09-11 07:45:18 -0500

    Add commands to install Crypter cross-platform, Electron-based password-less encryption tool from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -19829,3 +19829,37 @@ https://github.com/Ulauncher/Ulauncher/releases/download/5.3.0-beta1/
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,} /tmp/${APP_NAME}
+
+# Install Crypter cross-platform, Electron-based password-less encryption tool from AppImage
+APP_NAME=Crypter
+APP_GUI_NAME="Cross-platform, Electron-based password-less encryption tool."
+APP_VERSION=4.0.0
+APP_EXT=AppImage
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	ARCH_TYPE=
+else    # Otherwise use version for 32-bit kernel
+	ARCH_TYPE=-i386
+fi
+FILE_NAME=${APP_NAME}-${APP_VERSION}${ARCH_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/HR/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+sudo chmod +x /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Internet;Accessories;
+Keywords=Security;Encryption;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
