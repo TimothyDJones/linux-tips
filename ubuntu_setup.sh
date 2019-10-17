4ba94ef4447bf3d7eb4d783833b198e6748654c6
Tim Jones <tdjones74021@yahoo.com>
2019-10-17 10:20:34 -0500

    Add commands to install Liquid pure Clojure text/MarkDown editor with Emacs and Vim features from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -20218,3 +20218,38 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/hackmdio/${APP_NAM
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,} /tmp/${APP_NAME}
+
+# Install Liquid pure Clojure text/MarkDown editor with Emacs and Vim features from package
+APP_NAME=Liquid
+APP_GUI_NAME="Pure Clojure text/MarkDown editor with Emacs and Vim features."
+APP_VERSION=1.1.3
+APP_EXT=jar
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}-standalone
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/mogenslund/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${FILE_NAME}
+Exec=java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=true
+Categories=Programming;Development;
+Keywords=Editor;Markdown;Clojure;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
