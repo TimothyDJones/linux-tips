be13bb59a01ddfa146cbded3e0faea11f8359f19
Tim Jones <tdjones74021@yahoo.com>
2019-10-30 08:19:28 -0500

    Correct version number for Google Go language.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -274,11 +274,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Google Go language
 APP_NAME=go
-<<<<<<< HEAD
-APP_VERSION=1.13.1
-=======
 APP_VERSION=1.13.3
->>>>>>> db77cd329228a0efaef1cb5841b6373542e104e1
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=amd64
 else    # Otherwise use version for 32-bit kernel
