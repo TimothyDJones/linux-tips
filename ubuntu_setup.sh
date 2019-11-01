6d000b35dae080b480832a2a336a23b67c1e5505
Tim Jones <tdjones74021@yahoo.com>
2019-11-01 15:30:29 -0500

    Add commands to install Guitar Qt-based GUI Git client from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -20533,3 +20533,32 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/smallhadroncollide
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,} /tmp/${APP_NAME}
+
+# Install Guitar Qt-based GUI Git client from AppImage
+APP_NAME=Guitar
+APP_GUI_NAME="Qt-based GUI Git client."
+APP_VERSION=1.1.1
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-6003546-x86_64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/soramimi/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+sudo chmod +x /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Development;Programming;
+Keywords=Git;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
