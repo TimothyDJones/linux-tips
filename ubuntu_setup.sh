3821be51205587ffc3f25dfb7d7dcdea8a95180b
Tim Jones <tdjones74021@yahoo.com>
2019-11-24 01:00:09 -0600

    Update FreeDoko cross-platform version of Doppelkopf German card game to version 0.7.21.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -16172,7 +16172,7 @@ rm -rf /tmp/*${APP_NAME}*
 # Install FreeDoko cross-platform version of Doppelkopf German card game from package
 APP_NAME=FreeDoko
 APP_GUI_NAME="Cross-platform version of Doppelkopf German card game."
-APP_VERSION=0.7.20
+APP_VERSION=0.7.21
 APP_EXT=zip
 FILE_NAME=${APP_NAME}_${APP_VERSION}.Linux
 sudo apt-get install -y libalut0
