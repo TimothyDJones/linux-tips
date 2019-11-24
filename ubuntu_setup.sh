9b0a9c72c2696b46d8803033fc2165fa83416c0a
Tim Jones <tdjones74021@yahoo.com>
2019-11-24 00:53:15 -0600

    Update Rolisteam cross-platform Qt-based role-playing game (RPG) management tool to version 1.9.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -20132,7 +20132,7 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # Install Rolisteam cross-platform Qt-based role-playing game (RPG) management tool from AppImage
 APP_NAME=Rolisteam
 APP_GUI_NAME="Cross-platform Qt-based role-playing game (RPG) management tool."
-APP_VERSION=1.9.0
+APP_VERSION=1.9.1
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME}_v${APP_VERSION}-x86_64
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
