331a85e5ea211c96e95dec0a09ff01a2b7b45390
Tim Jones <tdjones74021@yahoo.com>
2019-12-18 10:32:57 -0600

    Add commands to install remindME cross-platform sticky note tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -20999,3 +20999,40 @@ echo 'fi' >> $HOME/.bashrc
 source $HOME/.bashrc
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,} /tmp/${APP_NAME}
+
+# Install remindME cross-platform sticky note tool from package
+APP_NAME=remindME
+APP_GUI_NAME="Cross-platform sticky note tool."
+APP_VERSION=beta1
+APP_EXT=N/A
+FILE_NAME=${APP_NAME}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME} -J -L https://downloads.sourceforge.net/remind-me/${FILE_NAME}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp /tmp/${FILE_NAME} /opt/${APP_NAME,,}
+sudo chmod +x /opt/${APP_NAME,,}/${FILE_NAME}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+/opt/${APP_NAME,,}/${FILE_NAME}
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=
+Exec=/opt/${APP_NAME,,}/${FILE_NAME}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Accessories;System;
+Keywords=Notepad;Sticky;Notes;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,} /tmp/${APP_NAME}
