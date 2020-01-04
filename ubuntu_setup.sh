7d1e7c4e3d9d97cb77dd0d1894071149da8641c7
Tim Jones <tdjones74021@yahoo.com>
2020-01-04 10:36:11 -0600

    Update Google Go language to version 1.13.5.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -277,7 +277,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Google Go language
 APP_NAME=go
-APP_VERSION=1.13.3
+APP_VERSION=1.13.5
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=amd64
 else    # Otherwise use version for 32-bit kernel
