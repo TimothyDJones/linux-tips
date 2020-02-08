e99a4c3b5615036d71520a8ab95f03a36dc062cd
Tim Jones <tdjones74021@yahoo.com>
2020-02-08 14:12:57 -0600

    Update Swiss File Knife (SFK) shell file utility to version 1.9.6.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2109,7 +2109,7 @@ xdg-open http://localhost/${APP_NAME,,}/index.php &
 
 # Install Swiss File Knife (SFK) shell file utility
 APP_NAME=sfk
-APP_VERSION=1.9.5
+APP_VERSION=1.9.6
 APP_EXT=exe
 	if [[ $(uname -m | grep '64') ]]; then  # Check for 64-bit Linux kernel
 		ARCH_TYPE=linux-64
