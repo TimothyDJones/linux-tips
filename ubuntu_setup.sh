0b17fdefd8e622090b25e5cedb24f26c3e6b0cfd
Tim Jones <tdjones74021@yahoo.com>
2020-02-15 16:07:27 -0600

    Add commands to install Repeat Java-based record/replay automation utility from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -21577,3 +21577,25 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install Repeat Java-based record/replay automation utility from package
+APP_NAME=Repeat
+APP_GUI_NAME="Java-based record/replay automation utility."
+APP_VERSION=5.4.1
+APP_EXT=jar
+FILE_NAME=${APP_NAME}_${APP_VERSION//./_}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/repeat1/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+sudo chown -R ${USER}:${USER} /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
