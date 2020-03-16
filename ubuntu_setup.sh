4e9340eb0ed2ab5a1c81af8fba8c9e07e01ffa25
Tim Jones <tdjones74021@yahoo.com>
2020-03-16 09:46:20 -0500

    Add commands to install jSQL Injection Java-based SQL injection and database security testing tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -21684,3 +21684,40 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install jSQL Injection Java-based SQL injection and database security testing tool from package
+APP_NAME=jSQL-Injection
+APP_GUI_NAME="Java-based SQL injection and database security testing tool."
+APP_VERSION=0.82
+APP_EXT=jar
+FILE_NAME=${APP_NAME,,}-v${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/ron190/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT} &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=java -jar /opt/${APP_NAME,,}/${FILE_NAME//-/}.${APP_EXT} &
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Programming;Networking;
+Keywords=SQL;Security;Database;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
