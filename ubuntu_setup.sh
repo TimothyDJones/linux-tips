fc0b8785b6df4ee8b9fa3b5ccec526992407c10b
Tim Jones <tdjones74021@yahoo.com>
2020-03-25 07:32:04 -0500

    Add commands to install VNQ cross-platform Java-based encrypted console messenger/chat utility from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -21985,3 +21985,42 @@ cd /tmp/${FILE_NAME}
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install VNQ cross-platform Java-based encrypted console messenger/chat utility from package
+APP_NAME=VNQ
+APP_GUI_NAME="Cross-platform Java-based encrypted console messenger/chat utility."
+APP_VERSION=N/A
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}11
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+CLASSPATH=.:${CLASSPATH}; export CLASSPATH
+java -jar /opt/${APP_NAME,,}/${APP_NAME,,}.jar &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Internet;Networking;Accessories;
+Keywords=Messenger;Chat;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
