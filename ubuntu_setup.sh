eae0c6176cb96abc627422b9198654bbee400b22
Tim Jones <tdjones74021@yahoo.com>
2020-03-28 08:35:47 -0500

    Update Broot Rust-based command-line directory tree navigation tool with fuzzy-search capability to version 0.13.5.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -21206,19 +21206,23 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # https://dystroy.org/broot/
 APP_NAME=Broot
 APP_GUI_NAME="Rust-based command-line directory tree navigation tool with fuzzy-search capability."
-APP_VERSION=0.11.3
-APP_EXT=N/A
-FILE_NAME=${APP_NAME,,}
-curl -o /tmp/${FILE_NAME} -J -L https://github.com/Canop/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}
-sudo cp /tmp/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
+APP_VERSION=0.13.5
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/Canop/${APP_NAME,,}/releases/download/v${APP_VERSION}b/${FILE_NAME}.${APP_EXT}
+https://github.com/Canop/broot/releases/download/v0.13.5b/broot_0.13.5.zip
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${FILE_EXT}
+sudo cp /tmp/${FILE_NAME}/build/x86_64-linux/${APP_NAME,,} /usr/local/bin/
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 /usr/local/bin/${APP_NAME,,} --install
+echo ${HOME}'/.config/'${APP_NAME,,}'/launcher/bash/br' >> ${HOME}/.bashrc
 # Reload Bash configuration
 source $HOME/.bashrc
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 
-# Install massCode Electron-based, cross-platform codes snippet manager/editor from AppImage
+# Install massCode Electron-based, crosks-platform codes snippet manager/editor from AppImage
 APP_NAME=massCode
 APP_GUI_NAME="Electron-based, cross-platform codes snippet manager/editor."
 APP_VERSION=1.0.2
