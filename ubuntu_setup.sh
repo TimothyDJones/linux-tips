7786a4a93acc0fadb52927bb5d80010bdc098ff8
Tim Jones <tdjones74021@yahoo.com>
2020-03-28 05:46:44 -0500

    Update MarkText Electron-based Markdown editor to version 0.16.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8177,7 +8177,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install MarkText Electron-based Markdown editor from package
 APP_NAME=MarkText
 APP_GUI_NAME="Cross-platform, Electron-based Markdown editor."
-APP_VERSION=0.16.0-rc.2
+APP_VERSION=0.16.1
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-x64
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME,,}/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
