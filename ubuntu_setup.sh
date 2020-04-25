8d197c3bf92a830f4cd7df27899ebd4f5a4462b7
Tim Jones <tdjones74021@yahoo.com>
2020-04-25 13:49:59 -0500

    Update OpenJDK to version 11.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -52,7 +52,7 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get autoremove -f -y
 sudo apt-get install -y build-essential dtrx curl wget checkinstall gdebi \
-	openjdk-8-jre software-properties-common \
+	openjdk-11-jre software-properties-common \
 	mc python3-pip htop synaptic
 
 # Allow current user to run 'sudo' without password
