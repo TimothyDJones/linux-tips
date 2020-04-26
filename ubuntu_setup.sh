df24eaa5cba9fac465f7fd6e49fe0d2ae32d4ecb
Tim Jones <tdjones74021@yahoo.com>
2020-04-26 14:39:44 -0500

    Update PlantUML Java-based UML modeling tool to version 1.2020.8 and standard installation process.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3179,19 +3179,30 @@ sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
 
-# Install PlantUML Java-based UML modeling tool
+# Install PlantUML Java-based UML modeling tool from package
 APP_NAME=PlantUML
-APP_VERSION=1.2019.12
+APP_GUI_NAME="Java-based UML modeling tool"
+APP_VERSION=1.2020.8
 APP_EXT=jar
-curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME,,}.${APP_VERSION}.${APP_EXT}
+FILE_NAME=${APP_NAME,,}.${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
 sudo mkdir -p /opt/${APP_NAME,,}
-sudo mv /tmp/${APP_NAME,,}.${APP_EXT} /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT} &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cat > /tmp/${APP_NAME,,}.desktop << EOF
 [Desktop Entry]
 Name=${APP_NAME}
-Comment=Java-based UML modeling tool
+Comment=${APP_GUI_NAME}
 GenericName=${APP_NAME}
-Exec=java -jar /opt/${APP_NAME,,}/${APP_NAME,,}.${APP_EXT}
+Exec=/usr/local/bin/${APP_NAME,,}
 #Icon=
 Type=Application
 StartupNotify=true
