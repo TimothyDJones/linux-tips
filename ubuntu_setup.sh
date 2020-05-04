36f61bbc419a76b04b873409c4a6dab9ff0079ed
Tim Jones <tdjones74021@yahoo.com>
2020-05-03 19:51:55 -0500

    Add commands to install Viter Python-based terminal emulator with Vim keybindings from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -22675,4 +22675,41 @@ sudo chmod +x /usr/local/bin/${FILE_NAME}
 cd ${HOME}
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 
-
+# Install Viter Python-based terminal emulator with Vim keybindings from package
+APP_NAME=Viter
+APP_GUI_NAME="Python-based terminal emulator with Vim keybindings."
+APP_VERSION=master
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}
+sudo apt-get install -y python-gobject libvte-dev
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/Kharacternyk/${APP_NAME,,}/archive/${APP_VERSION}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+python3 /opt/${APP_NAME,,}/${APP_NAME,,}.py &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Accessories;System;
+Keywords=Terminal;Console;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd ${HOME}
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
