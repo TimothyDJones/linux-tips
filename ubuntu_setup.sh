dc7585cf97d6f71715f33c1d46e9c0c9f9a005ad
Tim Jones <tdjones74021@yahoo.com>
2020-05-10 10:38:42 -0500

    Add commands to install Xi Electron cross-platform GUI for Xi Editor from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -23009,3 +23009,14 @@ cd /tmp/${FILE_NAME}
 sudo python3 /tmp/${FILE_NAME}/setup.py install
 cd $HOME
 rm -rf /tmp/${APP_NAME}*
+
+# Install Xi Electron cross-platform GUI for Xi Editor from Debian package
+APP_NAME=Xi-Electron
+APP_GUI_NAME="Cross-platform GUI for Xi Editor."
+APP_VERSION=0.1.3
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/acheronfail/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
