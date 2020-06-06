c29fb6f2d9adad66c06f785a3a36eff122c3c926
Tim Jones <tdjones74021@yahoo.com>
2020-06-06 13:39:13 -0500

    Add commands to install ijq interactive 'jq' JSON query tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -23611,3 +23611,16 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install ijq interactive 'jq' JSON query tool from package
+# https://gpanders.com/blog/introducing-ijq/
+APP_NAME=ijq
+APP_VERSION=0.1.0
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-x86_64-linux-gnu
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://git.sr.ht/~gpanders/${APP_NAME,,}/refs/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+cd $HOME
+rm -rf /tmp/${APP_NAME}*
