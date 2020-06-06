6e052e6c3352d045e276d49f1eabb1571ee74ece
Tim Jones <tdjones74021@yahoo.com>
2020-06-06 09:43:37 -0500

    Add commands to install HSKinter Python GUI Mandarin Chinese flashcard tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -22758,7 +22758,7 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # Install Clementine cross-platform music player/organizer from Debian package
 APP_NAME=Clementine
 APP_GUI_NAME="Cross-platform music player/organizer."
-APP_VERSION=1.4.0rc1-231-g60a46d193
+APP_VERSION=1.4.0rc1-263-g4c9241db1
 APP_EXT=deb
 source /etc/lsb-release
 if [[ ! "${DISTRIB_CODENAME:0:2}" =~ (fo)$ ]]; then  # 20.04
@@ -23525,4 +23525,43 @@ FILE_NAME=${APP_NAME,,}-${APP_VERSION}-${ARCH_TYPE}-linux-musl
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/angt/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
 sudo cp -R /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install HSKinter Python GUI Mandarin Chinese flashcard tool from package
+APP_NAME=HSKinter
+APP_GUI_NAME="Python GUI Mandarin Chinese flashcard tool."
+APP_VERSION=0.4.1
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}
+sudo pip3 install gTTS playsound
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+python3 /opt/${APP_NAME,,}/${APP_NAME,,}.py &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=Education;
+Keywords=Chinese;Language;Flashcards;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd ${HOME}
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
