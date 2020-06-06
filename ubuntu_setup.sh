b37be9d7d63fbbff4cf3f7afb8fb00c33ed22a97
Tim Jones <tdjones74021@yahoo.com>
2020-06-06 10:34:06 -0500

    Add commands to install GRIEF cross-platform console text editor from source.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -23564,3 +23564,17 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd ${HOME}
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install GRIEF cross-platform console text editor from source
+APP_NAME=GRIEF
+APP_VERSION=master
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}
+sudo apt-get install -y build-essential
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/adamyg/${APP_NAME,,}/archive/${APP_VERSION}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+cd /tmp/${FILE_NAME}
+./configure_new && make && sudo make install
+cd $HOME
+rm -rf /tmp/${APP_NAME}*
