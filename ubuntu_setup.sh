a111c0268266593a1504aff0c695f1108c682890
Tim Jones <tdjones74021@yahoo.com>
2020-06-23 13:21:15 -0500

    Add commands to install DailyDesktopWallpaperPlus utility to set Bing or Windows Spotlight daily photo as wallpaper from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -23945,7 +23945,24 @@ APP_VERSION=1.1.1
 BUILD_NBR=220
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}-${BUILD_NBR}_${KERNEL_TYPE}
-curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/microsoft/ProcDump-for-Linux/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/microsoft/"ProcDump-for-Linux"/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install DailyDesktopWallpaperPlus utility to set Bing or Windows Spotlight daily photo as wallpaper from package
+APP_NAME=DailyDesktopWallpaperPlus
+APP_GUI_NAME="Utility to set Bing or Windows Spotlight daily photo as wallpaper."
+APP_GUI_CATEGORIES="System;Accessories;"
+APP_GUI_KEYWORDS="Wallpaper;"
+APP_VERSION=1.1
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME}-v${APP_VERSION}-build-x86_64-linux
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/${APP_NAME}*/* /opt/${APP_NAME,,}
+sudo ln -s -f /opt/${APP_NAME,,}/${APP_NAME}.sh /usr/local/bin/${APP_NAME,,}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
