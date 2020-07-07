438ebec188787aa2a5687b1cc27a926e2ee02083
Tim Jones <tdjones74021@yahoo.com>
2020-07-07 09:11:54 -0500

    Update ZenTao project management tool from Debian package to version 12.3.3.int.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1747,7 +1747,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install ZenTao project management tool from Debian package
 APP_NAME=ZenTaoPMS
-APP_VERSION=12.0stable
+APP_VERSION=12.3.3.int
 APP_EXT=deb
 FILE_NAME=${APP_NAME}_${APP_VERSION}_1_all
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/zentao/${FILE_NAME}.${APP_EXT}
