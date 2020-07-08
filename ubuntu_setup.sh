f554dce733461b2e9589adf7ca494e2074a1eb10
Tim Jones <tdjones74021@yahoo.com>
2020-07-08 12:35:42 -0500

    Add commands to install Meditor Java-based symbolic computing library and math editor from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -24134,3 +24134,45 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/grdl/${APP_NAME,,}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install Meditor Java-based symbolic computing library and math editor from package
+APP_NAME=Meditor
+APP_GUI_NAME="Java-based symbolic computing library and math editor."
+APP_GUI_CATEGORIES="Development;Programming;Education;Science;Math;"
+APP_GUI_KEYWORDS="Math;Visualization;"
+APP_VERSION=5.4.2
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/jscl-${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+sh /opt/${APP_NAME,,}/bin/linoleum &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+Categories=Programming;Development;
+Keywords=REST;API;Testing;
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd ${HOME}
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
