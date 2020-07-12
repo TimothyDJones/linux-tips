aec6edf8b622b8787fd66decef7927f841b91415
Tim Jones <tdjones74021@yahoo.com>
2020-07-12 14:16:42 -0500

    Update CopyQ clipboard manager to version 3.12.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -547,7 +547,7 @@ rm -rf /tmp/${APP_NAME,,}*
 
 # Install CopyQ clipboard manager from Debian package
 APP_NAME=CopyQ
-APP_VERSION=3.10.0
+APP_VERSION=3.12.0
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_Debian_9.0-1_${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
