4211e70212bcc2e10b5b829a5c6834c1bf86577e
Tim Jones <tdjones74021@yahoo.com>
2020-07-16 15:11:07 -0500

    Update Shotcut video editor to version 20.07.11.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3227,7 +3227,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Shotcut video editor
 APP_NAME=Shotcut
-APP_VERSION=20.06.28
+APP_VERSION=20.07.11
 APP_EXT=txz
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=x86_64
