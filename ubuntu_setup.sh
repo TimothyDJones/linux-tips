afd554e057b2f785cad0dcd9a78caf8ec4e28e51
Tim Jones <tdjones74021@yahoo.com>
2020-07-18 13:06:43 -0500

    Update Joy of Text (JOT) minimalist text editor to version 3.0.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8289,7 +8289,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install Joy of Text (JOT) minimalist text editor from package
 APP_NAME=JOT
 APP_GUI_NAME="Cross-platform minimalist text editor."
-APP_VERSION=3.0.0
+APP_VERSION=3.0.1
 APP_EXT=tz
 sudo ln -s /lib/x86_64-linux-gnu/libncurses.so.5 /lib/x86_64-linux-gnu/libncurses.so.6
 sudo ln -s /lib/x86_64-linux-gnu/libtinfo.so.5 /lib/x86_64-linux-gnu/libtinfo.so.6
