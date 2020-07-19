dc32b40679d19f0a830f67ce9b32f0ba493865b8
Tim Jones <tdjones74021@yahoo.com>
2020-07-19 16:23:46 -0500

    Add commands to install PyCheck minimalist GUI for checksum calculation/comparison from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -24329,3 +24329,43 @@ autoreconf --force --install && ./configure --with-edit=readline
 make && sudo make install
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}
+
+# Install PyCheck minimalist GUI for checksum calculation/comparison from package
+APP_NAME=PyCheck
+APP_GUI_NAME="Minimalist GUI for checksum calculation/comparison."
+APP_GUI_CATEGORIES="System;Accessories;"
+APP_GUI_KEYWORDS="Checksum;"
+APP_VERSION=20.7.18
+APP_EXT=tar.xz
+FILE_NAME=${APP_NAME}_${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/checkmd5/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -Rf /tmp/${FILE_NAME}/${APP_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+python3 /opt/${APP_NAME,,}/${APP_NAME,,}.py &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=python3 /opt/${APP_NAME,,}/${APP_NAME,,}.py &
+Icon=/opt/${APP_NAME,,}/${APP_NAME,,}_icon.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd ${HOME}
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
