f4512d5d7216a8490efa5b294cebd251a3064c8d
Tim Jones <tdjones74021@yahoo.com>
2020-07-20 10:57:47 -0500

    Update Clementine cross-platform music player/organizer to version 1.4.0rc1-296-g68d375c43.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -22781,7 +22781,7 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # Install Clementine cross-platform music player/organizer from Debian package
 APP_NAME=Clementine
 APP_GUI_NAME="Cross-platform music player/organizer."
-APP_VERSION=1.4.0rc1-284-g7d28e8700
+APP_VERSION=1.4.0rc1-296-g68d375c43
 source /etc/lsb-release
 if [[ ! "${DISTRIB_CODENAME:0:2}" =~ (fo)$ ]]; then  # 20.04
 	DISTRIB_VERSION=focal
