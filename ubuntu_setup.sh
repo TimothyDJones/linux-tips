f99e9fa1a28fb19d3adf5f1337da43010f463405
Tim Jones <tdjones74021@yahoo.com>
2020-07-31 11:20:29 -0500

    Update Scintilla/SciTE GTK text editor to version 4.4.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7856,7 +7856,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install Scintilla/SciTE GTK text editor from source
 APP_NAME=SciTE
 APP_GUI_NAME="GTK text editor."
-APP_VERSION=4.4.3
+APP_VERSION=4.4.4
 APP_EXT=tgz
 FILE_NAME=${APP_NAME,,}${APP_VERSION//./}
 sudo apt-get install -y pkg-config libglib2.0-dev libgtk2.0-dev
