3e89293e84f536303a354a8fc2c10757f001a5e4
Tim Jones <tdjones74021@yahoo.com>
2020-08-02 19:45:36 -0500

    Update VNote Vim-inspired note-taking application with MarkDown support to version 2.9.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -12394,7 +12394,7 @@ sudo rm -rf /tmp/${APP_NAME}*
 # Install VNote Vim-inspired note-taking application with MarkDown support from App Image
 APP_NAME=VNote
 APP_GUI_NAME="Vim-inspired note-taking application with MarkDown support."
-APP_VERSION=2.9
+APP_VERSION=2.9.1
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME}-${APP_VERSION}-x86_64
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/tamlok/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
