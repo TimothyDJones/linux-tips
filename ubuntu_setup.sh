ac546a39133e2430a0bdb7d4c0a2fabd8bf58070
Tim Jones <tdjones74021@yahoo.com>
2020-08-09 18:33:10 -0500

    Add commands to install Espanso Cross-platform Text Expander written in Rust from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -24653,3 +24653,14 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd ${HOME}
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install Espanso Cross-platform Text Expander written in Rust from Debian package
+APP_NAME=Espanso
+APP_GUI_NAME="Cross-platform Text Expander written in Rust."
+APP_VERSION=0.6.3
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}-debian-${KERNEL_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/federico-terzi/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
