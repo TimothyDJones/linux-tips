84bde5726a9d92918d4616fb4129760910f650f6
Tim Jones <tdjones74021@yahoo.com>
2020-09-03 08:14:11 -0500

    Correct download URL for jEdit Java text editor.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2343,7 +2343,7 @@ APP_NAME=jEdit
 APP_VERSION=5.6.0
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_all
-curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
