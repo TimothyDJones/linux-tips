d2803df08e539d6fb10916d45871de74afa3ca79
Tim Jones <tdjones74021@yahoo.com>
2020-09-12 16:16:31 -0500

    Add commands to install PdfJumbler Java-based tool to rearrange/merge/delete pages from PDF files from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -24955,3 +24955,43 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/pgollangi/${APP_NA
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install PdfJumbler Java-based tool to rearrange/merge/delete pages from PDF files from package
+APP_NAME=PdfJumbler
+APP_GUI_NAME="Java-based tool to rearrange/merge/delete pages from PDF files."
+APP_GUI_CATEGORIES="Accessories;Office;"
+APP_GUI_KEYWORDS="PDF;Editor;"
+APP_VERSION=0.2.0
+APP_EXT=jar
+FILE_NAME=${APP_NAME,,}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/mgropp/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT} &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+curl -o /tmp/${APP_NAME,,}.svg -J -L https://raw.githubusercontent.com/mgropp/pdfjumbler/master/src/main/resources/pdfjumbler.svg
+sudo mv /tmp/${APP_NAME,,}.svg /usr/share/pixmaps/${APP_NAME,,}.svg
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/share/pixmaps/${APP_NAME,,}.svg
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
