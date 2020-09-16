fb01b6c24ed81aceee9644a18f963fec0cc2fcfd
Tim Jones <tdjones74021@yahoo.com>
2020-09-16 15:00:11 -0500

    Add commands to install PatternGenerator Java GUI algorithmic graphical pattern generator utility from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25043,3 +25043,43 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 sudo apt-add-repository -y ppa:linuxgndu/sqlitebrowser
 sudo apt-get update
 sudo apt-get install -y sqlitebrowser
+
+# Install PatternGenerator Java GUI algorithmic graphical pattern generator utility from package
+APP_NAME=patternGenerator
+APP_GUI_NAME="Java GUI algorithmic graphical pattern generator utility."
+APP_GUI_CATEGORIES="Graphics;"
+APP_GUI_KEYWORDS="Pattern;Generator;"
+APP_VERSION=2.2
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME}-${APP_VERSION//./_}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/pattgenerator/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${APP_NAME}.jar &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/images/${APP_NAME}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
