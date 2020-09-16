da58b0b341e7160f5a22285837bd4f76d325d5dd
Tim Jones <tdjones74021@yahoo.com>
2020-09-16 16:56:56 -0500

    Add commands to install NoteMaster cross-platform, Electron-based pinnable desktop notepad from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25083,3 +25083,13 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install NoteMaster cross-platform, Electron-based pinnable desktop notepad from Debian package
+APP_NAME=NoteMaster
+APP_VERSION=0.2.3
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/LiamRiddell/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
