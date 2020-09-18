bdf9999990b0cba3de4f93e1f3b40dc59c6fd4c9
Tim Jones <tdjones74021@yahoo.com>
2020-09-18 13:53:52 -0500

    Add commands to install Pure Java Net Tools (pj-net-tools) Java GUI networking utilities with tabbed interface from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25124,3 +25124,44 @@ dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
 sudo /tmp/${FILE_NAME}/install.sh
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Pure Java Net Tools (pj-net-tools) Java GUI networking utilities with tabbed interface from package
+# https://github.com/harp077/pj-net-tools
+APP_NAME=pj-net-tools
+APP_GUI_NAME="Java GUI networking utilities with tabbed interface."
+APP_GUI_CATEGORIES="Internet;System;Accessories;"
+APP_GUI_KEYWORDS="Networking;"
+APP_VERSION=1.0.13
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}_v${APP_VERSION}_bin
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${APP_NAME}.jar &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
