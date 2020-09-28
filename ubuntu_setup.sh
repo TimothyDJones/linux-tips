3fce1d2c4ba7c5c94533880eb8dd39852cc4ccd4
Tim Jones <tdjones74021@yahoo.com>
2020-09-28 08:51:23 -0500

    Add commands to install Pure Java FTP Server cross-platform, portable Java-based FTP server tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25357,3 +25357,43 @@ sudo alien --to-deb --verbose /tmp/${FILE_NAME}.${APP_EXT}  # Convert RPM packag
 sudo gdebi -n ${APP_NAME,,}_${APP_VERSION}*.deb
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Pure Java FTP Server cross-platform, portable Java-based FTP server tool from package
+APP_NAME=pj-ftp-server
+APP_GUI_NAME="Cross-platform, portable Java-based FTP server tool."
+APP_GUI_CATEGORIES="Networking;Internet"
+APP_GUI_KEYWORDS="FTP;"
+APP_VERSION=1.0.4
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}-v.${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${APP_NAME}.jar &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
