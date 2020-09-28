61431567a2083064a2bb791b5a50f5ebe1a004fa
Tim Jones <tdjones74021@yahoo.com>
2020-09-28 11:27:47 -0500

    Update SimulIDE electronic circuit simulator to version 0.4.13-SR5.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4712,28 +4712,27 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install SimulIDE electronic circuit simulator
 APP_NAME=SimulIDE
-APP_VERSION=0.3.10-SR2
-#APP_MINOR_VERSION=SR1
+APP_VERSION=0.4.13-SR5
 APP_EXT=tar.gz
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=Lin64
 else    # Otherwise use version for 32-bit kernel
 	ARCH_TYPE=Lin32
 fi
-curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME}_${APP_VERSION}-${ARCH_TYPE}.${APP_EXT}
+FILE_NAME=${APP_NAME}_${APP_VERSION}_${ARCH_TYPE}
+sudo apt-get install -y libqt5serialport5
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://mailfence.com/pub/docs/santigoro/web/${APP_NAME}_${APP_VERSION//-SR5/}/${FILE_NAME}.${APP_EXT}
 cd /tmp
-dtrx -n /tmp/${APP_NAME,,}.${APP_EXT}
-cd /tmp
-mv ${APP_NAME}_${APP_VERSION}-${ARCH_TYPE} ${APP_NAME,,}
-sudo mv ${APP_NAME,,} /opt
-cat > /tmp/${APP_NAME,,}/${APP_NAME,,} << EOF
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
 # /bin/sh
-cd /opt/${APP_NAME,,}/bin
-PATH=/opt/${APP_NAME,,}/bin:$PATH; export PATH
-./${APP_NAME}_${APP_VERSION}
+PATH=/opt/${APP_NAME,,}/bin:/opt/${APP_NAME,,}/share:$PATH; export PATH
+/opt/${APP_NAME,,}/bin/${APP_NAME,,}
 cd $HOME
 EOF
-sudo mv /tmp/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cat > /tmp/${APP_NAME,,}.desktop << EOF
 [Desktop Entry]
@@ -4741,8 +4740,8 @@ Name=${APP_NAME}
 Comment=Electronic circuit emulator
 GenericName=${APP_NAME}
 Path=/opt/${APP_NAME,,}/bin
-Exec=/opt/${APP_NAME,,}/bin/${APP_NAME}_${APP_VERSION}
-#Icon=/opt/${APP_NAME,,}/lib/ico-gvSIG.png
+Exec=/opt/${APP_NAME,,}/bin/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/share/icons/hicolor/256x256/${APP_NAME,,}.png
 Type=Application
 StartupNotify=true
 Terminal=false
