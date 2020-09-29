b4de3cf0a15417dd76e395567b539ab63655c24b
Tim Jones <tdjones74021@yahoo.com>
2020-09-29 08:04:57 -0500

    Update VeroRoute Qt-based PCB layout and routing tool to version V2.00.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7390,7 +7390,7 @@ xdg-open http://localhost/${APP_NAME,,}/install &
 # Install VeroRoute Qt-based PCB layout and routing tool from source
 APP_NAME=VeroRoute
 APP_GUI_NAME="Qt-based PCB layout and routing tool."
-APP_VERSION=V1.99
+APP_VERSION=V2.00
 APP_EXT=zip
 sudo apt-get install -y qt5-default
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME}_${APP_VERSION//./}_Src.${APP_EXT}
