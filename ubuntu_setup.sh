4558522030d8c2f7c1b59b69a31f3ebab2ee75f2
Tim Jones <tdjones74021@yahoo.com>
2020-10-01 10:44:21 -0500

    Add commands to install Float2Binary cross-platform, portable Java-based GUI tool to display IEEE 754 representation of floating point numbers from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25397,3 +25397,43 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install Float2Binary cross-platform, portable Java-based GUI tool to display IEEE 754 representation of floating point numbers from package
+APP_NAME=Float2Binary
+APP_GUI_NAME="Cross-platform, portable Java-based GUI tool to display IEEE 754 representation of floating point numbers."
+APP_GUI_CATEGORIES="Programming;Development"
+APP_GUI_KEYWORDS="Math;"
+APP_VERSION=N/A
+APP_EXT=zip
+FILE_NAME=${APP_NAME}_Executable
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/Executable*/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${APP_NAME}.jar &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
