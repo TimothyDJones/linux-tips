7f60d40bc6e48f9d62a56a097324159cc6b658f6
Tim Jones <tdjones74021@yahoo.com>
2020-10-04 13:46:31 -0500

    Add commands to install FreeFileSync cross-platform, folder comparison and synchronization software from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25507,3 +25507,43 @@ cd /tmp/${FILE_NAME}/${APP_NAME}-${APP_VERSION}
 ./configure --enable-ssl --prefix=/usr/local && make && sudo make install_global
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install FreeFileSync cross-platform, folder comparison and synchronization software from package
+APP_NAME=FreeFileSync
+APP_GUI_NAME="Cross-platform, folder comparison and synchronization software."
+APP_GUI_CATEGORIES="Accessories;System"
+APP_GUI_KEYWORDS="Backup;File Management;"
+APP_VERSION=11.2
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME}_${APP_VERSION}_Linux
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://freefilesync.org/download/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/${APP_NAME}/* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+/opt/${APP_NAME,,}/${APP_NAME} &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/Resources/${APP_NAME}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
