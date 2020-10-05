e07e8b94d0ae8b5eccf219668c5239263fb1bb07
Tim Jones <tdjones74021@yahoo.com>
2020-10-05 09:07:04 -0500

    Update Full Circle - Issue 161 September 2020 to version r6.0.12.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5884,7 +5884,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 
 # Install WackoWiki PHP-based lightweight wiki tool
 APP_NAME=wacko
-APP_VERSION=r6.0.11
+APP_VERSION=r6.0.12
 APP_EXT=zip
 DB_NAME=${APP_NAME,,}
 DB_USER=${APP_NAME,,}
