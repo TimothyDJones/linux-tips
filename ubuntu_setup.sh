4b67a0718866d80e7c0bce2db3204bf95384f689
Tim Jones <tdjones74021@yahoo.com>
2020-10-06 12:56:55 -0500

    Update Small Text Pad minimalist Java-based notepad to version 1.4.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -11120,7 +11120,7 @@ sudo rm -rf /tmp/${APP_NAME}*
 # http://www.wagemaker.co.uk/
 APP_NAME=SmallTextPad
 APP_GUI_NAME="Minimalist Java-based notepad."
-APP_VERSION=1.3.5
+APP_VERSION=1.4.1
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_all
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
