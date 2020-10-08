92d5a32551817ec318f8aa09f5c19ebf205d2d77
Tim Jones <tdjones74021@yahoo.com>
2020-10-08 13:40:03 -0500

    Update Micro terminal-based text editor to version 2.0.8.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3121,7 +3121,7 @@ rm -rf /tmp/${APP_NAME,,}*
 
 # Install Micro terminal-based text editor from package
 APP_NAME=Micro
-APP_VERSION=2.0.6
+APP_VERSION=2.0.8
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=linux64
 else    # Otherwise use version for 32-bit kernel
