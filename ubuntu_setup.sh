64728d94176ff2313ccf5f970b682cada952ef21
Tim Jones <tdjones74021@yahoo.com>
2020-10-23 12:29:10 -0500

    Update KeePassXC password manager to version 2.6.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -746,7 +746,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install KeePassXC password manager from source
 APP_NAME=KeePassXC
-APP_VERSION=2.6.1
+APP_VERSION=2.6.2
 APP_EXT=tar.xz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-src
 LIBCRYPT20_VERSION=1.8.5-5ubuntu2
