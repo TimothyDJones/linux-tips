837d582a8924dc356f62ee04d337a592f615179b
Tim Jones <tdjones74021@yahoo.com>
2020-10-26 18:52:16 -0500

    Update Broot Rust-based command-line directory tree navigation tool with fuzzy-search capability to version 1.0.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -21381,7 +21381,7 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # https://dystroy.org/broot/
 APP_NAME=Broot
 APP_GUI_NAME="Rust-based command-line directory tree navigation tool with fuzzy-search capability."
-APP_VERSION=1.0.0
+APP_VERSION=1.0.4
 APP_EXT=zip
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/Canop/${APP_NAME,,}/releases/download/v${APP_VERSION}b/${FILE_NAME}.${APP_EXT}
