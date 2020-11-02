aa5bdf7c6009dd563bf9501f5af907dee0c7735d
Tim Jones <tdjones74021@yahoo.com>
2020-11-02 07:44:14 -0600

    Update DocSearcher cross-platform indexed search tool for documents to version 3.95.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -14541,7 +14541,7 @@ rm -rf /tmp/*${APP_NAME}*
 # Install DocSearcher cross-platform indexed search tool for documents from package
 APP_NAME=DocSearcher
 APP_GUI_NAME="Cross-platform indexed search tool for documents."
-APP_VERSION=3.94.0-rc1
+APP_VERSION=3.95.1
 APP_EXT=zip
 FILE_NAME=${APP_NAME}-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
