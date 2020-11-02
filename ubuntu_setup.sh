5e4880c7f09ce13b7d63cea0e602dac02f1787c7
Tim Jones <tdjones74021@yahoo.com>
2020-11-02 12:15:41 -0600

    Add commands to install Bit Golang-based modern command-line Git client from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25882,3 +25882,21 @@ cp Makefile.gcc Makefile
 make && sudo make install
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Bit Golang-based modern command-line Git client from package
+APP_NAME=Bit
+APP_VERSION=0.9.10
+APP_EXT=tar.gz
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	ARCH_TYPE=amd64
+else    # Otherwise use version for 32-bit kernel
+	ARCH_TYPE=386
+fi
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_linux_${ARCH_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/chriswalz/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin/${APP_NAME,,}
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
