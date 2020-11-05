4feaa2f698066efc01ca27b24984ec52766ce929
Tim Jones <tdjones74021@yahoo.com>
2020-11-05 08:17:00 -0600

    Update YuranPad minimalist text editor to version 1.9.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25812,7 +25812,7 @@ rm -rf /tmp/${APP_NAME,,}*
 
 # Install YuranPad minimalist text editor from Debian package
 APP_NAME=YuranPad
-APP_VERSION=1.8
+APP_VERSION=1.9
 APP_EXT=deb
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=x86_64
