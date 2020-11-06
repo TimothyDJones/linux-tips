4167560182b60afd0c8ba85227191c5b566dd670
Tim Jones <tdjones74021@yahoo.com>
2020-11-06 08:07:34 -0600

    Add commands to install JavaMod minimalist Java-based audio player with support for modfiles from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25924,3 +25924,42 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/mkropat/${APP_NAME
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install JavaMod minimalist Java-based audio player with support for modfiles from package
+APP_NAME=JavaMod
+APP_GUI_NAME="Minimalist Java-based audio player with support for modfiles."
+APP_GUI_CATEGORIES="Multimedia;"
+APP_GUI_KEYWORDS="Audio;Player;"
+APP_VERSION=2.9
+APP_EXT=jar
+FILE_NAME=${APP_NAME,,}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME}.ico -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME}.ico
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}.${APP_EXT} /tmp/${APP_NAME}.ico /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT} &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/${APP_NAME}.ico
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
