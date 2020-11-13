1f181665638c0115169e1f6c95b30b11bfe5eb78
Tim Jones <tdjones74021@yahoo.com>
2020-11-13 12:19:21 -0600

    Add commands to install Passage minimalist Rust-based password manager using 'age' encryption from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -26176,3 +26176,13 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install Passage minimalist Rust-based password manager using 'age' encryption from Debian package
+APP_NAME=Passage
+APP_VERSION=0.5.0
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/stchris/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
