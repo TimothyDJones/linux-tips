206a93b9fa5689609ee9f45766e974dd2fd7e9b6
Tim Jones <tdjones74021@yahoo.com>
2020-11-13 07:52:49 -0600

    Update Belofte cross-platform Xboard-compatible chess engine to version 2.0.8.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -23818,7 +23818,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Belofte cross-platform Xboard-compatible chess engine from package
 APP_NAME=Belofte
-APP_VERSION=2.0.6
+APP_VERSION=2.0.8
 APP_EXT=N/A
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=64
