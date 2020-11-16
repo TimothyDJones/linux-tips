73288af2858c8c377ebeb32efb85f07cc068ec62
Tim Jones <tdjones74021@yahoo.com>
2020-11-16 09:18:37 -0600

    Update Gila CMS lightweight PHP/MySQL content management platform to version 2.0.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -24751,7 +24751,7 @@ rm -rf /tmp/${APP_NAME,,}*
 # https://gilacms.com/
 APP_NAME=Gila-CMS
 APP_GUI_NAME="Lightweight PHP/MySQL content management platform."
-APP_VERSION=1.15.4
+APP_VERSION=2.0.0
 APP_EXT=tar.gz
 DB_NAME=${APP_NAME,,}
 DB_NAME=${DB_NAME//-/}
