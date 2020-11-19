2b2b253c0d0d3e9e0b4890e03b5c92375a52ac55
Tim Jones <tdjones74021@yahoo.com>
2020-11-19 08:29:52 -0600

    Add commands to install yq Golang-based command-line YAML processor from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -26260,3 +26260,14 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install yq Golang-based command-line YAML processor from package
+APP_NAME=yq
+APP_VERSION=3.4.1
+APP_EXT=N/A
+FILE_NAME=${APP_NAME,,}_linux_${KERNEL_TYPE//i/}
+curl -o /tmp/${FILE_NAME} -J -L https://github.com/mikefarah/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}
+sudo cp /tmp/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
