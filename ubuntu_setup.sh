33a405e6a2ca950ae6fb81978c1e7bb8bbc5f2e3
Tim Jones <tdjones74021@yahoo.com>
2020-12-11 08:00:59 -0600

    Update Vivaldi web browser (stable version) to version 3.5.2115.8.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -517,9 +517,9 @@ sudo apt-get install -y atom
 
 # Install Vivaldi web browser (stable version) from package
 APP_NAME=Vivaldi
-APP_VERSION=3.2.1967.47-1
+APP_VERSION=3.5.2115.81
 APP_EXT=deb
-FILE_NAME=${APP_NAME,,}-stable_${APP_VERSION}_${KERNEL_TYPE}
+FILE_NAME=${APP_NAME,,}-stable_${APP_VERSION}-1_${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.vivaldi.com/stable/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
