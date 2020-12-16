b5201a031f2a68bb15122f3f4fad008fe5feaa64
Tim Jones <tdjones74021@yahoo.com>
2020-12-16 07:46:38 -0600

    Add commands to install ARITA minimalist audio player for Linux from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -26479,3 +26479,13 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install ARITA minimalist audio player for Linux from Debian package
+APP_NAME=ARITA
+APP_VERSION=3.20.10
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
