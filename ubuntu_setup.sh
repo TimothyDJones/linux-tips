a9f0190bcd21bd7a06255cdeb08acce5ce2ee16b
Tim Jones <tdjones74021@yahoo.com>
2020-12-18 16:23:56 -0600

    Update Ket cross-platform, Java-based interactive math editor with LaTeX and HTML export to version 0.7.01.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -11237,7 +11237,7 @@ sudo rm -rf /tmp/${APP_NAME}*
 # Install Ket cross-platform, Java-based interactive math editor with LaTeX and HTML export from package
 APP_NAME=Ket
 APP_GUI_NAME="Cross-platform, Java-based interactive math editor with LaTeX and HTML export."
-APP_VERSION=0.6.04
+APP_VERSION=0.7.01
 APP_EXT=jar
 FILE_NAME=${APP_NAME,,}${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
