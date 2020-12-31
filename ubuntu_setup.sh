8a8c392fcc8a774dbf97e142fa7ed97d57b64ad1
Tim Jones <tdjones74021@yahoo.com>
2020-12-31 08:50:54 -0600

    Update NSBase cross-platform database alternative to MS Access built with SQLite to version 1.9.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -17854,7 +17854,7 @@ rm -rf /tmp/*${APP_NAME}* /tmp/*${APP_NAME,,}*
 # Install NSBase cross-platform database alternative to MS Access built with SQLite from Debian package
 APP_NAME=NSBase
 APP_GUI_NAME="Cross-platform database alternative to MS Access."
-APP_VERSION=1.8.0.a
+APP_VERSION=1.9.1
 APP_EXT=deb
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=linux64
