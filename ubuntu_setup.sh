0980da4be79d7cf9d74bc529e14e63099f272e9d
Tim Jones <tdjones74021@yahoo.com>
2021-01-07 11:19:45 -0600

    Add commands to install Wayback Golang-based command-line tool to archive web pages to Archive.org, Archive.Today, and IPFS from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -26806,3 +26806,13 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/binwiederhier/${AP
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Wayback Golang-based command-line tool to archive web pages to Archive.org, Archive.Today, and IPFS from Debian package
+APP_NAME=Wayback
+APP_VERSION=0.5.3
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/wabarc/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
