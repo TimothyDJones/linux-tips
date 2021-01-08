14cc93a06e6ebe10ff9ad304e8248438d32734bb
Tim Jones <tdjones74021@yahoo.com>
2021-01-08 08:00:43 -0600

    Update Brave web browser (release channel) to version 1.18.78.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1743,7 +1743,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Brave web browser (release channel) from Debian package
 APP_NAME=Brave-Browser
-APP_VERSION=1.18.77
+APP_VERSION=1.18.78
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
