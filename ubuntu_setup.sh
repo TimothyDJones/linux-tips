8a3b3b9e471be7c404a6e613b2805638195db08f
Tim Jones <tdjones74021@yahoo.com>
2021-01-23 12:39:12 -0600

    Update Angry IP Scanner to version 3.7.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4714,7 +4714,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Angry IP Scanner from package
 APP_NAME=ipscan
-APP_VERSION=3.7.3
+APP_VERSION=3.7.4
 APP_EXT=deb
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
