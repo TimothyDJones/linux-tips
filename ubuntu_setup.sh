baf2be32ac3e5fbf08915cc1791b7156c6bfba13
Tim Jones <tdjones74021@yahoo.com>
2021-01-27 11:50:25 -0600

    Add commands to install TaskBoard minimalist PyGame-based task manager from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -27171,3 +27171,48 @@ sudo cp /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin/${APP_NAME,,}
 sudo chmod +x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install TaskBoard minimalist PyGame-based task manager from package
+APP_NAME=TaskBoard
+APP_GUI_NAME="Minimalist PyGame-based task manager."
+APP_GUI_CATEGORIES="Accessories;Office;"
+APP_GUI_KEYWORDS="Task Manager;Productivity;"
+APP_VERSION=0.2.0
+APP_EXT=zip
+FILE_NAME=${APP_NAME}-${APP_VERSION//./}-src
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/k39${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+sudo chown -R ${USER}:${USER} /opt/${APP_NAME,,}
+python3 -m venv /opt/${APP_NAME,,}/.venv
+source /opt/${APP_NAME,,}/.venv/bin/activate
+pip3 install pygame colorama
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+. /opt/${APP_NAME,,}/.venv/bin/activate
+python3 /opt/${APP_NAME,,}/__main__.py &
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
