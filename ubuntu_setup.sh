0fd5a370bb9325cbaa8001cecc5cf1abe559500e
Tim Jones <tdjones74021@yahoo.com>
2021-02-07 15:46:52 -0600

    Add commands to install Tablexplore cross-platform Qt-based spreadsheet-style data analysis and plotting tool from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -27309,3 +27309,35 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Tablexplore cross-platform Qt-based spreadsheet-style data analysis and plotting tool from AppImage
+APP_NAME=Tablexplore
+APP_GUI_NAME="Cross-platform Qt-based spreadsheet-style data analysis and plotting tool."
+APP_GUI_CATEGORIES="Development;Office;"
+APP_GUI_KEYWORDS="Analysis;Spreadsheet;"
+APP_VERSION=0.3.0
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}-x86_64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/dmnfarrell/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.ico -J -L https://raw.githubusercontent.com/dmnfarrell/tablexplore/master/img/logo.ico
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo cp /tmp/${APP_NAME,,}.ico /usr/local/share/icons
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.ico
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
