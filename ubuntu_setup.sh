60286e0df5885eb8a85f53e20ad42e655d7011e6
Tim Jones <tdjones74021@yahoo.com>
2021-02-07 09:58:40 -0600

    Add commands to install HyperKeys keyboard shortcut utility from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -27276,3 +27276,36 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install HyperKeys keyboard shortcut utility from AppImage
+# https://hyperkeys.xureilab.com/
+APP_NAME=HyperKeys
+APP_GUI_NAME="Keyboard shortcut utility."
+APP_GUI_CATEGORIES="Accessories;System;"
+APP_GUI_KEYWORDS="Keyboard;"
+APP_VERSION=1.1.11-rc1
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-${APP_VERSION//-rc1/}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/xurei/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.ico -J -L https://raw.githubusercontent.com/xurei/hyperkeys/master/src/main/icon.ico
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo cp /tmp/${APP_NAME,,}.ico /usr/local/share/icons
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.ico
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
