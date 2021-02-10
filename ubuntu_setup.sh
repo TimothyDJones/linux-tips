e61b0dee927994947c98622614e3150294d2acd3
Tim Jones <tdjones74021@yahoo.com>
2021-02-10 13:53:04 -0600

    Add commands to install Spiral Qt-based notepad that saves documents as plain HTML from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -27435,3 +27435,35 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Spiral Qt-based notepad that saves documents as plain HTML from package
+APP_NAME=Spiral
+APP_GUI_NAME="Qt-based notepad that saves documents as plain HTML."
+APP_GUI_CATEGORIES="Office;"
+APP_GUI_KEYWORDS="Notepad;"
+APP_VERSION=1.3
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME}_Release_${APP_VERSION}_Linux
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://mitchellaugustin.com/spiral/versions/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/${APP_NAME}*/* /opt/${APP_NAME,,}
+sudo ln -s -f /opt/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=env QT_QPA_PLATFORMTHEME=gtk2 /opt/${APP_NAME,,}/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/logo.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
