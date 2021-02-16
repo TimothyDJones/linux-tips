f877c3746c3f326d078dabb21a6c7a81ab9bf16d
Tim Jones <tdjones74021@yahoo.com>
2021-02-16 15:59:13 -0600

    Add 'export.sh' with enhancements for 'less'.

---
+++ export.sh
@@ -0,0 +1,13 @@
+
+# Make 'less' more powerful
+export LESS='--quit-if-one-screen --ignore-case --status-column --LONG-PROMPT --RAW-CONTROL-CHARS --HILITE-UNREAD --tabs=4 --no-init --window=-4'
+
+# Set colors and text decorations for 'less' output
+# From https://wiki.archlinux.org/index.php/Color_output_in_console#less
+export LESS_TERMCAP_mb=$'\E[1;31m'     # begin bold
+export LESS_TERMCAP_md=$'\E[1;36m'     # begin blink
+export LESS_TERMCAP_me=$'\E[0m'        # reset bold/blink
+export LESS_TERMCAP_so=$'\E[01;44;33m' # begin reverse video
+export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
+export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
+export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
