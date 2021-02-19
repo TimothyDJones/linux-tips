36d03a726a1d2c8498eb1b0b92142f499e702dbe
Tim Jones <tdjones74021@yahoo.com>
2021-02-19 13:21:51 -0600

    Add commands to install Pingnoo cross-platform combined traceroute and ping application from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -27676,3 +27676,35 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Pingnoo cross-platform combined traceroute and ping application from AppImage
+APP_NAME=Pingnoo
+APP_GUI_NAME="Cross-platform desktop GUI client for browsing Geminispace."
+APP_GUI_CATEGORIES="System;"
+APP_GUI_KEYWORDS="Networking;"
+APP_VERSION=2021.02.17-develop
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}.${APP_VERSION}.x86_64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/nedrysoft/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.ico -J -L https://github.com/nedrysoft/pingnoo/raw/develop/src/app/AppIcon.ico
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo cp /tmp/${APP_NAME,,}.ico /usr/local/share/icons
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.ico
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
