21d16eba0951e587f382b61507ceb275ad0051fc
Tim Jones <tdjones74021@yahoo.com>
2021-02-22 08:20:58 -0600

    Update TurboVNC remote desktop tool to version 2.2.6.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -14576,7 +14576,7 @@ rm -rf /tmp/*${APP_NAME}*
 # Install TurboVNC remote desktop tool from Debian package
 APP_NAME=TurboVNC
 APP_GUI_NAME="remote desktop tool."
-APP_VERSION=2.2
+APP_VERSION=2.2.6
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
