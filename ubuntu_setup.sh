5876aaf0b2692ddab1f214316b03128be90ac630
Tim Jones <tdjones74021@yahoo.com>
2021-03-15 14:11:49 -0500

    Add commands to install 7-Zip cross-platform decompression utility from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -28239,3 +28239,23 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${A
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install 7-Zip cross-platform decompression utility from package
+APP_NAME=7z
+APP_GUI_NAME="Cross-platform decompression utility."
+APP_VERSION=21.01
+APP_EXT=tar.xz
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	ARCH_TYPE=x64
+else    # Otherwise use version for 32-bit kernel
+	ARCH_TYPE=x86
+fi
+FILE_NAME=${APP_NAME,,}${APP_VERSION//./}-linux-${ARCH_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://7-zip.org/a/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+sudo ln -s -f /opt/${APP_NAME,,}/7zz /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
