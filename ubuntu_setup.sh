34a14431dbd7ce994a1123e7c62ff532f59a0c89
Tim Jones <tdjones74021@yahoo.com>
2021-04-15 10:53:15 -0500

    Add commands to install KmlShpConvert utility for converting between KML (Keyhole Markup Language) and SHP (Shapefile) geometry files from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -28678,3 +28678,46 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install KmlShpConvert utility for converting between KML (Keyhole Markup Language) and SHP (Shapefile) geometry files from package
+APP_NAME=KmlShpConvert
+APP_GUI_NAME="Utility for converting between KML (Keyhole Markup Language) and SHP (Shapefile) geometry files."
+APP_GUI_CATEGORIES="Graphics;Other;"
+APP_GUI_KEYWORDS="KML;SHP;GIS;"
+APP_VERSION=2.0
+APP_EXT=zip
+FILE_NAME=Version%20${APP_VERSION}%20-%20linux%2064%20bits%20only
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+dtrx -n /tmp/${FILE_NAME}/${APP_NAME}.tar.gz
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${APP_NAME}/${APP_NAME}/* /opt/${APP_NAME,,}
+sudo chmod -R 777 /opt/${APP_NAME,,}
+sudo chmod +rwxs /opt/${APP_NAME,,}/main
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+/opt/${APP_NAME,,}/main /opt/${APP_NAME,,}/ui/window.ui
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
