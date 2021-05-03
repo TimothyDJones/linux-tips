fb89f11bbbdf934c9b20952d5c4b6dcff73ed9dd
Tim Jones <tdjones74021@yahoo.com>
2021-05-03 14:05:13 -0500

    Add commands to install PingMe Golang-based multi-service CLI messenger from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -28943,3 +28943,15 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install PingMe Golang-based multi-service CLI messenger from package
+APP_NAME=PingMe
+APP_VERSION=0.1.7
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}_Linux_$(arch)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/kha7iq/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
