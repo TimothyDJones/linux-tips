618030b47af3d4be54b9ccb2538265f4320a00d2
Tim Jones <tdjones74021@yahoo.com>
2021-05-11 07:50:33 -0500

    Update qmmp Qt-based Multimedia Player to version 1.4.6.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5091,7 +5091,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install qmmp Qt-based Multimedia Player from source
 APP_NAME=qmmp
-APP_VERSION=1.4.5
+APP_VERSION=1.4.6
 APP_EXT=tar.bz2
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}-dev/${APP_NAME}-${APP_VERSION}.${APP_EXT}
 cd /tmp
