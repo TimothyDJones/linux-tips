4f72c33e72161f4703d88fe0c27b28734ee7ac53
Tim Jones <tdjones74021@yahoo.com>
2021-05-12 10:39:44 -0500

    Update Standard Notes Electron-based secure notepad to version 3.6.9.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8140,7 +8140,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install Standard Notes Electron-based secure notepad from App Image
 APP_NAME=Standard-Notes
 APP_GUI_NAME="Electron-based secure notepad."
-APP_VERSION=3.6.8
+APP_VERSION=3.6.9
 APP_EXT=AppImage
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=i386
