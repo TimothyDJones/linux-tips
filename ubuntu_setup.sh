a38d47ead91258af0060a17d987cf57d0b72660b
Tim Jones <tdjones74021@yahoo.com>
2021-05-12 08:31:46 -0500

    Add commands to install Sigma Studio cross-platform Java-based code editor/IDE from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -29161,3 +29161,44 @@ sudo chmod a+x /usr/local/bin/_${APP_NAME,,}
 echo 'eval "$(_kn init bash)"' >> $HOME/.bashrc && source $HOME/.bashrc
 cd $HOME
 sudo rm -rf /tmp/*${APP_NAME,,}* /tmp/${APP_NAME}
+
+# Install Sigma Studio cross-platform Java-based code editor/IDE from package
+APP_NAME="Sigma Studio"
+APP_GUI_NAME="Cross-platform Java-based code editor/IDE."
+APP_GUI_CATEGORIES="Development;Programming;"
+APP_GUI_KEYWORDS="Editor;IDE;"
+APP_VERSION=N/A
+APP_EXT=jar
+FILE_NAME=${APP_NAME// /%20}
+_APP_NAME=${APP_NAME// /-}
+_APP_NAME=${_APP_NAME,,}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_APP_NAME}/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${_APP_NAME}
+sudo cp -R /tmp/${FILE_NAME}.${APP_EXT} /opt/${_APP_NAME}
+sudo chmod -R 755 /opt/${_APP_NAME}
+cat > /tmp/${_APP_NAME} << EOF
+#! /bin/sh
+cd /opt/${_APP_NAME}
+PATH=/opt/${_APP_NAME}:\$PATH; export PATH
+java -jar /opt/${_APP_NAME}/${FILE_NAME}.${APP_EXT}
+cd \$HOME
+EOF
+sudo mv /tmp/${_APP_NAME} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${_APP_NAME}
+cat > /tmp/${_APP_NAME}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+#Path=/opt/${_APP_NAME}
+Exec=/usr/local/bin/${_APP_NAME}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME}*
