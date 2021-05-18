6f0123f654dcca98be61949965f460bd6cce0f0b
Tim Jones <tdjones74021@yahoo.com>
2021-05-18 07:50:49 -0500

    Add commands to install FinalShell Linux application launcher from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -29457,3 +29457,35 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/sezanzeb/${APP_NAM
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install FinalShell Linux application launcher from package
+APP_NAME=FinalShell
+APP_GUI_NAME="Linux application launcher."
+APP_GUI_CATEGORIES="Accessories;System;"
+APP_GUI_KEYWORDS="Launcher;"
+APP_VERSION=3.9.0
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}_linux-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -R /tmp/${FILE_NAME}/${APP_NAME}/* /usr/local
+sudo chmod +x /usr/local/bin/${APP_NAME}
+sudo ln -s -f /usr/local/bin/${APP_NAME} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/lib/${APP_NAME}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
