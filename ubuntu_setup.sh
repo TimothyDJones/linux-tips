d90f08130b036f870e5a74c36e57a9a2910b112a
Tim Jones <tdjones74021@yahoo.com>
2021-05-18 11:56:40 -0500

    Add commands to install DragIt cross-platform LAN file sharing tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -29519,3 +29519,36 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install DragIt cross-platform LAN file sharing tool from package
+APP_NAME=DragIt
+APP_GUI_NAME="Cross-platform LAN file sharing tool."
+APP_GUI_CATEGORIES="Accessories;"
+APP_GUI_KEYWORDS="File Sharing;LAN;"
+APP_VERSION=0.5.3
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}_v${APP_VERSION}_linux_amd_64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/sireliah/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/sireliah/dragit/master/static/logo.png
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp /tmp/${FILE_NAME}/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin
+sudo chmod +x /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
