da374a38bc1c7131239d8c366aac6ac83441bd6a
Tim Jones <tdjones74021@yahoo.com>
2021-05-26 07:40:12 -0500

    Add commands to install ScreenBringer Java-based minimalist remote desktop tool from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -29729,3 +29729,13 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${A
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install ScreenBringer Java-based minimalist remote desktop tool from Debian package
+APP_NAME=ScreenBringer
+APP_VERSION=0.1
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}_amd64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
