a3b8002ab6c48b76faeee09da6fe742a56802f97
Tim Jones <tdjones74021@yahoo.com>
2021-06-09 09:01:51 -0500

    Update JavAstro cross-platform Java-based astronomy tools to version 2106_03.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -29119,7 +29119,7 @@ APP_NAME=JavAstro
 APP_GUI_NAME="Cross-platform Java-based astronomy tools."
 APP_GUI_CATEGORIES="Education;Science;"
 APP_GUI_KEYWORDS="Astronomy;Science;"
-APP_VERSION=21.06_02
+APP_VERSION=2106_03
 APP_EXT=jar
 FILE_NAME=${APP_NAME,,}_${APP_VERSION//./}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
