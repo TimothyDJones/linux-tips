01700d96ee720c6a26267b48bcf83d7f5e7059ca
Tim Jones <tdjones74021@yahoo.com>
2021-06-14 08:28:24 -0500

    Update Skychart planetarium package to version 4.3-4361.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1442,7 +1442,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Skychart planetarium package from Debian package
 APP_NAME=Skychart
-APP_VERSION=4.3-4356
+APP_VERSION=4.3-4361
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
 # libpasastro (Pascal astronomical library) is dependency for Skychart.
