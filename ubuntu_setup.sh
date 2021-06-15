4f6d7f1eb73affa032c49805e63302d10d92e6fd
Tim Jones <tdjones74021@yahoo.com>
2021-06-15 09:31:50 -0500

    Add commands to install LANDrop cross-platform LAN file-sharing tool from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -30037,3 +30037,35 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/luc
 sudo sh /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install LANDrop cross-platform LAN file-sharing tool from AppImage
+APP_NAME=LANDrop
+APP_GUI_NAME="Cross-platform LAN file-sharing tool."
+APP_GUI_CATEGORIES="Network;Accessories;"
+APP_GUI_KEYWORDS="LAN;Collaboration;File Sharing;"
+APP_VERSION=0.4.0
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-${APP_VERSION}-linux
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME}/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME}.png -J -L https://raw.githubusercontent.com/${APP_NAME}/${APP_NAME}/master/${APP_NAME}/icons/app.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME}.png /usr/local/share/icons/${APP_NAME}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
