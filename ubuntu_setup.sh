7eeae338095678a18e98b407238af0b2f37efa6e
Tim Jones <tdjones74021@yahoo.com>
2021-06-24 10:27:32 -0500

    Add commands to install VidCutter media cutter/joiner built with FFmpeg and Qt from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -30206,3 +30206,33 @@ cd /tmp/${FILE_NAME}
 ./autogen.sh && ./configure && make -j$(nproc) && sudo make install
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install VidCutter media cutter/joiner built with FFmpeg and Qt from AppImage
+APP_NAME=VidCutter
+APP_GUI_NAME="Media cutter/joiner built with FFmpeg and Qt."
+APP_GUI_CATEGORIES="Multimedia;Video;"
+APP_GUI_KEYWORDS="Video;Editor;Cutter;"
+APP_VERSION=6.0.5.1
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-${APP_VERSION}-x86_64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
