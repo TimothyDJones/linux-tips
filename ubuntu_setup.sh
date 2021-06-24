3b027f5de4026b6e0bc6e09180a70584f005bfb7
Tim Jones <tdjones74021@yahoo.com>
2021-06-24 07:24:06 -0500

    Add commands to install jworktime cross-platform Java-based GUI Pomodoro timer productivity tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -30143,3 +30143,44 @@ sudo mv /tmp/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install jworktime cross-platform Java-based GUI Pomodoro timer productivity tool from package
+APP_NAME=jworktime
+APP_GUI_NAME="Cross-platform Java-based GUI Pomodoro timer productivity tool."
+APP_GUI_CATEGORIES="Accessories;"
+APP_GUI_KEYWORDS="Pomodoro;Productivity;"
+APP_VERSION=1.0.5
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}-app-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+sudo chmod -R 755 /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -cp "/opt/${APP_NAME,,}/lib/*" clock.Clock &
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
