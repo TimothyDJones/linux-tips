000c38db6ca5c9b0f5ec7a6b00e970e8ff67cec1
Tim Jones <tdjones74021@yahoo.com>
2021-06-24 10:29:43 -0500

    Update Terminus JavaScript/Electron terminal to version 1.0.141.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4959,7 +4959,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 
 # Install Terminus JavaScript/Electron terminal from Debian package
 APP_NAME=Terminus
-APP_VERSION=1.0.139
+APP_VERSION=1.0.141
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-linux
 curl -o /tmp/${FILE_NAME,,}.${APP_EXT} -J -L https://github.com/Eugeny/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
