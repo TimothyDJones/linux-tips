6c60bef1a01c521df1b4f652b1056b60844b43ae
Tim Jones <tdjones74021@yahoo.com>
2021-07-01 10:27:02 -0500

    Update Iosevka monospaced font to version 7.2.3.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -201,10 +201,10 @@ curl -o /tmp/Hack-v3.003-ttf.tar.xz -J -L https://github.com/source-foundry/Hack
 cd /tmp
 dtrx -n /tmp/Hack-v3.003-ttf.tar.xz
 sudo cp -R /tmp/Hack-v3.003-ttf/* /usr/local/share/fonts
-curl -o /tmp/ttf-iosevka-7.0.4.zip -J -L https://github.com/be5invis/Iosevka/releases/download/v7.0.4/ttf-iosevka-7.0.4.zip
+curl -o /tmp/ttf-iosevka-7.2.3.zip -J -L https://github.com/be5invis/Iosevka/releases/download/v7.2.3/ttf-iosevka-7.2.3.zip
 cd /tmp
-dtrx -n /tmp/ttf-iosevka-7.0.4.zip
-sudo cp -R /tmp/ttf-iosevka-7.0.4/* /usr/local/share/fonts
+dtrx -n /tmp/ttf-iosevka-7.2.3.zip
+sudo cp -R /tmp/ttf-iosevka-7.2.3/* /usr/local/share/fonts
 curl -o /tmp/CascadiaCode-2105.24.zip -J -L https://downloads.sourceforge.net/cascadia-code.mirror/CascadiaCode-2102.25.zip
 cd /tmp
 dtrx -n /tmp/CascadiaCode-2105.24.zip
