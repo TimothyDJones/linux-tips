e7acff3cc52558b0ac9e6113a872249c383bdcb2
Tim Jones <tdjones74021@yahoo.com>
2021-07-02 07:48:50 -0500

    Update PotatoSQL Java-based database design and learning tool to version 4.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -18320,7 +18320,7 @@ rm -rf /tmp/*${APP_NAME}* /tmp/*${APP_NAME,,}*
 # https://github.com/x-jrga/potatosql
 APP_NAME=PotatoSQL
 APP_GUI_NAME="Java-based database design and learning tool."
-APP_VERSION=3.0
+APP_VERSION=4.0
 APP_EXT=zip
 FILE_NAME=${APP_NAME}-${APP_VERSION//./}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
