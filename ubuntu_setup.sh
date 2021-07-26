3e015c7a7712f6623013c76b526f77e19bd0099d
Tim Jones <tdjones74021@yahoo.com>
2021-07-26 08:57:24 -0500

    Update Kitty cross-platform GPU-based terminal to version 0.22.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -22261,7 +22261,7 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # https://sw.kovidgoyal.net/kitty/
 APP_NAME=Kitty
 APP_GUI_NAME="Cross-platform GPU-based terminal."
-APP_VERSION=0.21.2
+APP_VERSION=0.22.0
 APP_EXT=txz
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=x86_64
