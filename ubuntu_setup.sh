dbc543f77278614edaaa32298eda9aaed16d819a
Tim Jones <tdjones74021@yahoo.com>
2021-08-02 07:28:35 -0500

    Update Hypernomicon Java-based research tracking database with built-in PDF viewer to version 1.22.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -18502,7 +18502,7 @@ sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 # Install Hypernomicon Java-based research tracking database with built-in PDF viewer from package
 APP_NAME=Hypernomicon
 APP_GUI_NAME="Java-based research tracking database with built-in PDF viewer."
-APP_VERSION=1.19.4
+APP_VERSION=1.22
 APP_EXT=sh
 FILE_NAME=${APP_NAME}_linux_${APP_VERSION//./_}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
