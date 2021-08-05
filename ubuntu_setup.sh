c03a833835013ee44cb64b46eade72000c2d6248
Tim Jones <tdjones74021@yahoo.com>
2021-08-05 12:38:12 -0500

    Add commands to install Mongomix cross-platform, free MongoDB GUI client from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -30748,3 +30748,36 @@ cd /tmp/${FILE_NAME}/${APP_NAME,,}
 ./configure && make && sudo make install
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Mongomix cross-platform, free MongoDB GUI client from package
+APP_NAME=Mongomix
+APP_GUI_NAME="Cross-platform, free MongoDB GUI client."
+APP_GUI_CATEGORIES="Development;Programming;"
+APP_GUI_KEYWORDS="Mongo;MongoDB;Database;NoSQL;"
+APP_VERSION=1.5.4
+APP_EXT=tgz
+FILE_NAME=${APP_NAME,,}-linux-x64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://mongomix.sigma-solutions.fr/release/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+sudo chmod -R 755 /opt/${APP_NAME,,}
+sudo ln -s -f /opt/${APP_NAME,,}/${APP_NAME} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/resources/app/dist/bundle/128x128.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
