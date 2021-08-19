c2c072824ac7706ed971d3a7c07569c7e47dd333
Tim Jones <tdjones74021@yahoo.com>
2021-08-19 12:07:32 -0500

    Update o minimalist Golang text editor to version 2.42.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -27677,7 +27677,7 @@ rm -rf /tmp/*${APP_NAME,,}*
 # Install o minimalist Golang text editor from package
 APP_NAME=o
 APP_GUI_NAME="Minimalist Golang text editor."
-APP_VERSION=2.41.3
+APP_VERSION=2.42.0
 APP_EXT=tar.xz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-linux
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/xyproto/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
