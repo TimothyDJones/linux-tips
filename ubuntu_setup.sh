e0af5ade1381a67233269c1db7b491e3b1ef3cf6
Tim Jones <tdjones74021@yahoo.com>
2021-08-19 10:19:11 -0500

    Update phpCollab web-based collaboration and project management tool to version v2.10.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5622,7 +5622,7 @@ rm -rf /tmp/*${APP_NAME,,}* /tmp/*${APP_NAME}*
 # Install phpCollab web-based collaboration and project management tool
 # http://www.phpcollab.com/
 APP_NAME=phpCollab
-APP_VERSION=v2.10.0
+APP_VERSION=v2.10.1
 APP_EXT=zip
 DB_NAME=${APP_NAME,,}
 DB_USER=${APP_NAME,,}
