e8a66b1b906ea3df8c06ecbe4a5f5b5a87e2195e
Tim Jones <tdjones74021@yahoo.com>
2021-08-23 07:03:33 -0500

    Update Malnotes combination to-do list/note-taking tool to version 1.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -31028,7 +31028,7 @@ rm -rf /tmp/${_APP_NAME}*
 
 # Install Malnotes combination to-do list/note-taking tool from Debian package
 APP_NAME=Malnotes
-APP_VERSION=1.0
+APP_VERSION=1.1
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_amd64
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
