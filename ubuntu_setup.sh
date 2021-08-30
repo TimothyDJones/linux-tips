5c2d8a8bab6895e901290002fc2b1c7acbf1992f
Tim Jones <tdjones74021@yahoo.com>
2021-08-30 07:59:47 -0500

    Update Kakoune console text editor with Vi keybindings to version 2021.08.28.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -30945,7 +30945,7 @@ rm -rf /tmp/${APP_NAME,,}*
 # Install Kakoune console text editor with Vi keybindings from source
 APP_NAME=Kakoune
 APP_GUI_NAME="Console text editor with Vi keybindings."
-APP_VERSION=2020.09.01
+APP_VERSION=2021.08.28
 APP_EXT=tar.bz2
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt install -yy build-essential
