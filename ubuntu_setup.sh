110389f60c2cc7dbe99af3373017184f8b77d565
Tim Jones <tdjones74021@yahoo.com>
2021-08-31 13:19:52 -0500

    Add commands to install Focus cross-platform, command-line minimalist Pomodoro timer from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -31588,3 +31588,13 @@ sudo cp /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin/${APP_NAME,,}
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Focus cross-platform, command-line minimalist Pomodoro timer from Debian package
+APP_NAME=Focus
+APP_VERSION=1.1.0
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/ayoisaiah/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
