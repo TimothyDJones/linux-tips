128e35fbc759984c1a0a85be3e9f508a8c227c53
Tim Jones <tdjones74021@yahoo.com>
2021-09-02 13:27:16 -0500

    Update Madedit-Mod text editor to version 0.4.19.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1234,7 +1234,7 @@ rm -rf /tmp/${APP_NAME,,}*
 
 # Install Madedit-Mod text editor from Debian package
 APP_NAME=Madedit-Mod
-APP_VERSION=0.4.18-1
+APP_VERSION=0.4.19-1
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}_Ubuntu19.10
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
