c1d1b0e6338cd041c2112c6b348f3edb262ac9ac
Tim Jones <tdjones74021@yahoo.com>
2021-09-06 10:17:56 -0500

    Update Open Limbas PHP database utility to version 4.3.27.1314.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4064,7 +4064,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Open Limbas PHP database utility
 APP_NAME=openlimbas
-APP_VERSION=4.3.27.1313
+APP_VERSION=4.3.27.1314
 APP_EXT=tar.gz
 DB_NAME=limbas
 DB_USER=limbas
