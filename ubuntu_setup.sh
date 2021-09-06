f4d9cb494fdcdc46b7197b38199ce409a9a874bf
Tim Jones <tdjones74021@yahoo.com>
2021-09-06 10:41:49 -0500

    Update CliFM CLI-based, ultra-lightweight, lightning fast, and written in C to version 1.2.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -29053,7 +29053,7 @@ sudo apt-get install -y cdda2wav mkisofs cdrecord
 # Install CliFM CLI-based, ultra-lightweight, lightning fast, and written in C from source
 APP_NAME=CliFM
 APP_GUI_NAME="CLI-based, ultra-lightweight, lightning fast, and written in C."
-APP_VERSION=1.2.1
+APP_VERSION=1.2.2
 APP_EXT=tar.gz
 FILE_NAME=v${APP_VERSION}
 sudo apt-get install -y libcap-dev libacl1-dev 
