1154d48245ded308c433d8749f81e5517245bd01
Tim Jones <tdjones74021@yahoo.com>
2021-09-10 11:00:11 -0500

    Update Tabby (formerly Terminus) JavaScript/Electron terminal to version 1.0.157.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4995,7 +4995,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 
 # Install Tabby (formerly Terminus) JavaScript/Electron terminal from Debian package
 APP_NAME=Tabby
-APP_VERSION=1.0.156
+APP_VERSION=1.0.157
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-linux
 curl -o /tmp/${FILE_NAME,,}.${APP_EXT} -J -L https://github.com/Eugeny/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
