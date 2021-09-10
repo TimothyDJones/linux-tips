e1eeba772c57f35e190d993c177c40aeb9b688f5
Tim Jones <tdjones74021@yahoo.com>
2021-09-10 10:41:15 -0500

    Update Hyper JS/HTML/CSS Terminal to version 3.1.3.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2223,7 +2223,7 @@ rm -rf /tmp/${APP_NAME}*
 APP_NAME=Hyper
 APP_VERSION=3.1.3
 APP_EXT=deb
-FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_$(dpkg --print-architecture)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/hyper.mirror/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
