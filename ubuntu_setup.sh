ee392491430c042274f7a996b5ba85af39b5e9b6
Tim Jones <tdjones74021@yahoo.com>
2021-09-21 13:22:15 -0500

    Update Vivaldi web browser (stable version) to version 4.2.2406.44.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -569,7 +569,7 @@ sudo apt-get install -y atom
 
 # Install Vivaldi web browser (stable version) from package
 APP_NAME=Vivaldi
-APP_VERSION=4.1.2369.18
+APP_VERSION=4.2.2406.44
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}-stable_${APP_VERSION}-1_${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.vivaldi.com/stable/${FILE_NAME}.${APP_EXT}
