422f2c8837b7b9f04a9993d2139d16288b672e4e
Tim Jones <tdjones74021@yahoo.com>
2021-09-22 07:23:45 -0500

    Update CoTerminalApps cross-platform, color terminal/console puzzle games to version 22sep21 (2.3.0).

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25445,7 +25445,7 @@ rm -rf /tmp/${APP_NAME,,}*
 # Install CoTerminalApps cross-platform, color terminal/console puzzle games from package
 APP_NAME=CoTerminalApps
 APP_GUI_NAME="Cross-platform, , color terminal/console puzzle games."
-APP_VERSION=10mar21
+APP_VERSION=22sep21
 APP_EXT=7z
 FILE_NAME=co${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
