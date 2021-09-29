ce79c2117bf8944190b35f8a018130916e47c505
Tim Jones <tdjones74021@yahoo.com>
2021-09-29 07:46:47 -0500

    Update vifm file manager to version 0.12.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1636,7 +1636,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install vifm file manager from source
 APP_NAME=vifm
-APP_VERSION=0.11
+APP_VERSION=0.12
 APP_EXT=tar.bz2
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get install -y libncursesw5-dev
