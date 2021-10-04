e856e9e31c7be7009950b289c367616412cfee51
Tim Jones <tdjones74021@yahoo.com>
2021-10-04 08:54:30 -0500

    Update PlantUML Java-based UML modeling tool to version 1.2021.11.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3446,7 +3446,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install PlantUML Java-based UML modeling tool from package
 APP_NAME=PlantUML
 APP_GUI_NAME="Java-based UML modeling tool"
-APP_VERSION=1.2021.10
+APP_VERSION=1.2021.11
 APP_EXT=jar
 FILE_NAME=${APP_NAME,,}.${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
