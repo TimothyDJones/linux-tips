034662536696778cd5d16ecc2183556736e32a1c
Tim Jones <tdjones74021@yahoo.com>
2021-10-07 15:36:09 -0500

    Update GNU nano text editor to version 5.9."

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4900,7 +4900,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install GNU nano text editor from source
 APP_NAME=nano
 APP_GUI_NAME="Minimalist console text editor."
-APP_VERSION=5.8
+APP_VERSION=5.9
 APP_EXT=tar.xz
 FILE_NAME=${APP_NAME}-${APP_VERSION}
 sudo apt-get install -y libncurses-dev
