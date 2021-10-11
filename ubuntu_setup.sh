317e7524591b395f26436a8ab8c2610eb9e9d7aa
Tim Jones <tdjones74021@yahoo.com>
2021-10-11 14:56:32 -0500

    Update pgweb cross-platform client for PostgreSQL databases to version 0.11.9.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5544,7 +5544,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install pgweb cross-platform client for PostgreSQL databases
 APP_NAME=pgweb
 APP_GUI_NAME="Cross-platform client for PostgreSQL databases"
-APP_VERSION=0.11.8
+APP_VERSION=0.11.9
 APP_EXT=zip
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=linux_amd64
