ee48246d6e32acc85d6fec0f1b226031a2883bd8
Tim Jones <tdjones74021@yahoo.com>
2021-10-15 07:34:57 -0500

    Update Microsoft PowerShell cross-platform shell and scripting environment to version 7.1.5.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8838,7 +8838,7 @@ rm -rf /tmp/${APP_NAME,,}
 # https://docs.microsoft.com/en-us/powershell/scripting/setup/installing-powershell-core-on-macos-and-linux?view=powershell-6
 APP_NAME=PowerShell
 APP_GUI_NAME="Cross-platform shell and scripting environment."
-APP_VERSION=7.1.4
+APP_VERSION=7.1.5
 APP_EXT=deb
 source /etc/lsb-release
 # PowerShell is only supported on LTS releases
