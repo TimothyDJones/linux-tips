538179e1de21e7a67b4de3b8f2ae810bcbc3467a
Tim Jones <tdjones74021@yahoo.com>
2021-10-27 13:43:07 -0500

    Update Espanso Cross-platform Text Expander written in Rust to version 2.0.4-alpha.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -24861,14 +24861,35 @@ sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd ${HOME}
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 
-# Install Espanso Cross-platform Text Expander written in Rust from Debian package
+# Install Espanso Cross-platform Text Expander written in Rust from AppImage
 APP_NAME=Espanso
 APP_GUI_NAME="Cross-platform Text Expander written in Rust."
-APP_VERSION=0.7.3
-APP_EXT=deb
-FILE_NAME=${APP_NAME,,}-debian-${KERNEL_TYPE}
+APP_GUI_CATEGORIES="Accessories;System;"
+APP_GUI_KEYWORDS="Text;Expander;"
+APP_VERSION=2.0.4-alpha
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-X11
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/federico-terzi/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
-sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://github.com/federico-terzi/espanso/blob/master/${APP_NAME,,}/src/res/logo_no_background.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 
