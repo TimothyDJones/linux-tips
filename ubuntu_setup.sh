210366605fd4e311c872468230a3f817ca26f58b
Tim Jones <tdjones74021@yahoo.com>
2021-10-27 13:28:18 -0500

    Update WebTorrent Desktop cross-platform Electron-based torrent streaming client to version 0.24.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7411,12 +7411,13 @@ rm -rf /tmp/${APP_NAME,,}
 # Install WebTorrent Desktop cross-platform Electron-based torrent streaming client from package
 APP_NAME=WebTorrent-Desktop
 APP_GUI_NAME="Cross-platform Electron-based torrent streaming client."
-APP_VERSION=0.21.0
+APP_VERSION=0.24.0
 APP_EXT=deb
-curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
-sudo gdebi -n /tmp/${APP_NAME,,}.${APP_EXT}
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
-rm -rf /tmp/${APP_NAME,,}
+rm -rf /tmp/${APP_NAME,,}*
 
 # Install Amp Rust-based command-line text editor from source
 APP_NAME=Amp
