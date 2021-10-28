058fafe79e8057ac7c89e6ac452c4bf43defaf4f
Tim Jones <tdjones74021@yahoo.com>
2021-10-28 11:34:57 -0500

    Update WackoWiki PHP-based lightweight wiki tool to version r6.1.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -6067,7 +6067,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 
 # Install WackoWiki PHP-based lightweight wiki tool
 APP_NAME=wacko
-APP_VERSION=r6.1.1
+APP_VERSION=r6.1.2
 APP_EXT=zip
 DB_NAME=${APP_NAME,,}
 DB_USER=${APP_NAME,,}
