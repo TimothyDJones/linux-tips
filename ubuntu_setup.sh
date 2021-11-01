0ad03b6fa0d674d503e13000cfd83f3346f69ae5
Tim Jones <tdjones74021@yahoo.com>
2021-11-01 07:44:33 -0500

    Update BinEd cross-platform Java-based binary/hexadecimal viewer/editor to version 0.2.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -19958,9 +19958,9 @@ rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # Install BinEd cross-platform Java-based binary/hexadecimal viewer/editor from Debian package
 APP_NAME=BinEd
 APP_GUI_NAME="Cross-platform Java-based binary/hexadecimal viewer/editor."
-APP_VERSION=0.2.0-1
+APP_VERSION=0.2.1
 APP_EXT=deb
-FILE_NAME=${APP_NAME,,}_${APP_VERSION}_all
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}-1_all
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
