38043003f78f4672deea463e667698430a179889
Tim Jones <tdjones74021@yahoo.com>
2021-11-17 11:22:03 -0600

    Update QPDF PDF utility to version 10.4.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2837,7 +2837,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install QPDF PDF utility from source
 APP_NAME=QPDF
-APP_VERSION=10.3.2
+APP_VERSION=10.4.0
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get install -y zlib1g-dev libjpeg62-dev
