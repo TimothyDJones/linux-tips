07276bfb035d143ffb48b8c8ca9ffed731cd8a6e
Tim Jones <tdjones74021@yahoo.com>
2021-11-22 08:38:49 -0600

    Add commands to install Qhfs Qt-based simple HTTP/HTTPS file server from source.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -32827,3 +32827,36 @@ cd /tmp/${FILE_NAME}/*${APP_NAME,,}*
 make && sudo make install
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Qhfs Qt-based simple HTTP/HTTPS file server from source
+APP_NAME=Qhfs
+APP_GUI_NAME="Qt-based simple HTTP/HTTPS file server."
+APP_GUI_CATEGORIES="Internet;Networking;"
+APP_GUI_KEYWORDS="File;Sharing;"
+APP_VERSION=0.40_beta
+APP_EXT=zip
+FILE_NAME=${APP_NAME}_${APP_VERSION}
+sudo apt-get install -y build-essential cmake libssl-dev
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+cd /tmp/${FILE_NAME}
+cmake CMakeLists.txt && make && make generate_translation && sudo make install
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${FILE_NAME}/Gui/Resources/Icons/${APP_NAME,,}1.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
