4c682ec93f3b5fbebc2496e3b4e92793b602f9d1
Tim Jones <tdjones74021@yahoo.com>
2021-11-30 08:18:43 -0600

    Update KSnip screenshot utility to version 1.9.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -622,7 +622,7 @@ ln -s /usr/share/applications/albert.desktop $HOME/.config/autostart/  # Create 
 
 # Install KSnip screenshot utility from Debian package
 APP_NAME=KSnip
-APP_VERSION=1.9.1
+APP_VERSION=1.9.2
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
