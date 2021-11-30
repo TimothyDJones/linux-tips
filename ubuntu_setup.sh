bf5ff5d0750db1075aca56712fda93f39b97c6cf
Tim Jones <tdjones74021@yahoo.com>
2021-11-30 15:37:07 -0600

    Add commands to install XVI ncurses-based hex editor with Vim keybindings for Linux from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -33046,3 +33046,36 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/bewue/${APP_NAME}/
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install XVI ncurses-based hex editor with Vim keybindings for Linux from AppImage
+APP_NAME=XVI
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="ncurses-based hex editor with Vim keybindings for Linux."
+APP_GUI_CATEGORIES="Programming;Development;"
+APP_GUI_KEYWORDS="Hex;Editor;"
+APP_VERSION=1.2
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}.0-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/artemsen/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/artemsen/${APP_NAME,,}/master/extra/${APP_NAME,,}.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
