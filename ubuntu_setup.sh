5f44614fbd4b3c45b0e6be8980a92ebb4e23b8d4
Tim Jones <tdjones74021@yahoo.com>
2021-12-13 15:56:40 -0600

    Add commands to install Erin Electron-based desktop wallpaper changer from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -33113,3 +33113,37 @@ EOF
 sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${_APP_NAME}*
+
+# Install Erin Electron-based desktop wallpaper changer from AppImage
+APP_NAME=Erin
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Electron-based desktop GUI for podman container manager."
+APP_GUI_CATEGORIES="Accessories;System;"
+APP_GUI_KEYWORDS="Wallpaper;"
+APP_VERSION=2.1.5
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME,,}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/losbiw/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/losbiw/${APP_NAME,,}/master/assets/icons/512x512.png
+echo "$PASSWORD" | sudo -S -k cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+echo "$PASSWORD" | sudo -S -k chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+echo "$PASSWORD" | sudo -S -k ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+echo "$PASSWORD" | sudo -S -k mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
+
