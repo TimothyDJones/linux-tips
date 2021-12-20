66423f587100d924afff83dc980225b391bbdca9
Tim Jones <tdjones74021@yahoo.com>
2021-12-20 13:34:35 -0600

    Update WezTerm cross-platform, Rust-based, GPU-accelerated terminal tool to version 20211205-192649-672c1cc1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25660,9 +25660,9 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 
 # Install WezTerm cross-platform, Rust-based, GPU-accelerated terminal tool from Debian package
 APP_NAME=WezTerm
-APP_VERSION=20210814-124438-54e29167
+APP_VERSION=20211205-192649-672c1cc1
 APP_EXT=deb
-source  /etc/lsb-release
+source /etc/lsb-release
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}.Ubuntu${DISTRIB_RELEASE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/wez/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
