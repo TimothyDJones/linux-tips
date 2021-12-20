6653f728bc2976c381e14fe6be8aeb5e1b78d6eb
Tim Jones <tdjones74021@yahoo.com>
2021-12-20 13:45:53 -0600

    Update Dayon Java-based remote access utility to version 11.0.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -26027,7 +26027,7 @@ rm -rf /tmp/${APP_NAME,,}*
 
 # Install Dayon Java-based remote access utility from Debian package
 APP_NAME=Dayon
-APP_VERSION=1.10.2
+APP_VERSION=11.0.2
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}active/${FILE_NAME}.${APP_EXT}
