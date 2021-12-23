e701c09fb0230c34b291a350c40f6bb47df36663
Tim Jones <tdjones74021@yahoo.com>
2021-12-23 07:59:17 -0600

    Add commands to install oslcrypt TCL-based GUI front-end to OpenSSL encryption methods from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -33274,3 +33274,36 @@ cd /tmp/${FILE_NAME}
 make build && sudo make install
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install oslcrypt TCL-based GUI front-end to OpenSSL encryption methods from AppImage
+APP_NAME=oslcrypt
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="TCL-based GUI front-end to OpenSSL encryption methods."
+APP_GUI_CATEGORIES="Internet;System;"
+APP_GUI_KEYWORDS="Security;Encryption;"
+APP_VERSION=0.99.9b
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}-Linux
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp /tmp/${FILE_NAME}/${APP_NAME,,}/${APP_NAME,,}-LINUX.appimage /usr/local/bin
+sudo chmod +x /usr/local/bin/${APP_NAME,,}-LINUX.appimage
+sudo ln -s -f /usr/local/bin/${APP_NAME,,}-LINUX.appimage /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+#Icon=
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
