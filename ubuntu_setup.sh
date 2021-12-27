3f1e22a391becf4a5d95faeebcf97e0dbc618a2a
Tim Jones <tdjones74021@yahoo.com>
2021-12-27 09:30:34 -0600

    Update Propeller cross-platform SDL-based side-scroller shooting game to version 2021-12-24.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -28119,7 +28119,7 @@ APP_NAME=Propeller
 APP_GUI_NAME="Cross-platform SDL-based side-scroller shooting game ."
 APP_GUI_CATEGORIES="Entertainment;Games;"
 APP_GUI_KEYWORDS="Games;Side-Scroller;"
-APP_VERSION=alpha-prerelease-20
+APP_VERSION=2021-12-24
 APP_EXT=zip
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get install -y libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev
