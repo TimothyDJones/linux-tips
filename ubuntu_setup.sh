2aa1ff33e40af80942fd3b701d7a3a5635632923
Tim Jones <tdjones74021@yahoo.com>
2021-12-28 15:57:30 -0600

    Update Lepton Electron-based Github Gist editor/viewer to version 1.10.0 and to use AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -13526,13 +13526,35 @@ sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
 
-# Install Lepton Electron-based Github Gist editor/viewer from Snap package
+# Install Lepton Electron-based Github Gist editor/viewer from AppImage
 APP_NAME=Lepton
 APP_GUI_NAME="Electron-based Github Gist editor/viewer."
-APP_VERSION=1.9.1
-APP_EXT=snap
-curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://github.com/hackjutsu/${APP_NAME,,}/releases/download/v${APP_VERSION}/${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}.${APP_EXT}
-sudo snap install --dangerous /tmp/${APP_NAME,,}.${APP_EXT}
+APP_GUI_CATEGORIES="Development;Programming;"
+APP_GUI_KEYWORDS="Snippet;Gist;"
+APP_VERSION=1.10.0
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/hackjutsu/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/hackjutsu/${APP_NAME,,}/master/docs/img/new_logo2.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
 
