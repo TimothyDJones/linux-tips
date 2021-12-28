37d9401a5d6cbf8d6e7c4804954d5a4eb8f8c810
Tim Jones <tdjones74021@yahoo.com>
2021-12-28 14:48:28 -0600

    Add commands to install LibreWolf privacy-focused fork of Firefox web browser from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -33327,3 +33327,36 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install LibreWolf privacy-focused fork of Firefox web browser from AppImage
+APP_NAME=LibreWolf
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Privacy-focused fork of Firefox web browser."
+APP_GUI_CATEGORIES="Internet;"
+APP_GUI_KEYWORDS="Web;Browser;Privacy;Firefox;"
+APP_VERSION=95.0.2
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}.$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://gitlab.com/api/v4/projects/24386000/packages/generic/${APP_NAME,,}/${APP_VERSION}-1/LibreWolf.x86_64.AppImage
+curl -o /tmp/${APP_NAME,,}.svg -J -L https://gitlab.com/librewolf-community/branding/-/raw/master/icon/icon.svg
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.svg /usr/local/share/icons/${APP_NAME,,}.svg
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.svg
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
