c855a106e177036b999252911eb7be05d99cb721
Tim Jones <tdjones74021@yahoo.com>
2022-01-02 17:18:16 -0600

    Update DK Tools system utility suite to version 4.31.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2925,7 +2925,7 @@ xdg-open http://localhost/${APP_NAME,,}/install/index.php &
 # Install DK Tools system utility suite from source
 APP_NAME=DKTools
 APP_GUI_NAME="System utility suite."
-APP_VERSION=4.31.2
+APP_VERSION=4.31.4
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 WXVERS="3.0"
