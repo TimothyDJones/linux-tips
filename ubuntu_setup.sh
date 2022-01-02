9a645937a6aa9e342f7c3a2fa246deb49ab1cfa6
Tim Jones <tdjones74021@yahoo.com>
2022-01-02 17:20:28 -0600

    Update SC Calculator cross-platform scientific calculator implemented in C++, Java, and JavaScript to version 1.19.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8480,7 +8480,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install SC Calculator cross-platform scientific calculator implemented in C++, Java, and JavaScript from package
 APP_NAME=SCCalculator
 APP_GUI_NAME="Cross-platform scientific calculator implemented in C++, Java, and JavaScript."
-APP_VERSION=1.18
+APP_VERSION=1.19
 APP_EXT=zip
 FILE_NAME=calculator${APP_VERSION}
 sudo apt-get install -y openjdk-11-jre
