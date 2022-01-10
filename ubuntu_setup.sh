2f6bb3979f1c340f88bbceb628f368f59ec4081b
Tim Jones <tdjones74021@yahoo.com>
2022-01-10 07:06:36 -0600

    Update RedDiamond programmer's text editor with EDT keypad navigation based on CopperSpice Qt library to version 0.0.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -28333,7 +28333,7 @@ APP_NAME=RedDiamond
 APP_GUI_NAME="Programmer's text editor with EDT keypad navigation based on CopperSpice Qt library."
 APP_GUI_CATEGORIES="Development;Programming;"
 APP_GUI_KEYWORDS="Editor;"
-APP_VERSION=0.0.2
+APP_VERSION=0.0.4
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
