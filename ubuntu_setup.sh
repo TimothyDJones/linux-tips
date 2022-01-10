5764e56b547672237476c8bdd72e764fe154b225
Tim Jones <tdjones74021@yahoo.com>
2022-01-10 06:32:12 -0600

    Update Ferdi cross-platform, Electron-based messaging application integration utility to version 5.6.10.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -26836,11 +26836,7 @@ rm -rf /tmp/*${APP_NAME,,}*
 # Install Ferdi cross-platform, Electron-based messaging application integration utility from Debian package
 # https://getferdi.com/
 APP_NAME=Ferdi
-<<<<<<< HEAD
-APP_VERSION=5.6.7
-=======
-APP_VERSION=5.6.8
->>>>>>> daily
+APP_VERSION=5.6.10
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
