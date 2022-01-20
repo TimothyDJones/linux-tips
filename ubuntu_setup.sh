4cf0de49525124c22ef9ad253ce1b62076a292c7
Tim Jones <tdjones74021@yahoo.com>
2022-01-20 07:00:08 -0600

    Update Shutter Encoder cross-platform audio/video encoder/converter to version 15.8.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -23864,14 +23864,15 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 
 # Install Shutter Encoder cross-platform audio/video encoder/converter from Debian package
 APP_NAME="Shutter Encoder"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross-platform audio/video encoder/converter."
 APP_VERSION=15.8
 APP_EXT=deb
-FILE_NAME=${APP_NAME// /%20}%20"("Linux%20Version%20${APP_VERSION}")"
-curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://www.shutterencoder.com/${FILE_NAME}.${APP_EXT}
+FILE_NAME=${APP_NAME// /%20}%20${APP_VERSION}%20Linux%2064bits
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_APP_NAME}/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
-sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+sudo rm -rf /tmp/${APP_NAME// /%20}*
 
 # Install Qt JSON diff GUI JSON viewer/comparer from source
 APP_NAME=QTjsonDiff
