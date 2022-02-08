bc51b18540b1d781632e0e2cc7a0008c7f8e1445
Tim Jones <tdjones74021@yahoo.com>
2022-02-08 16:41:54 -0600

    Add commands to install Gitty Golang-based command-line Git project summary tool from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -34015,3 +34015,13 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Gitty Golang-based command-line Git project summary tool from Debian package
+APP_NAME=Gitty
+APP_VERSION=0.7.0
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/muesli/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
