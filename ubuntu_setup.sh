b1801992dccc863eabb7a2adbdacd6ef5bd0cb19
Tim Jones <tdjones74021@yahoo.com>
2022-02-10 08:09:15 -0600

    Update WoTerm cross-platform Qt-based SSH client/terminal with session manager to version 2022.02.09.1812.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -20880,7 +20880,7 @@ sudo rm -rf /tmp/${APP_NAME,,} /tmp/${APP_NAME}
 # Install WoTerm cross-platform Qt-based SSH client/terminal with session manager from package
 APP_NAME=WoTerm
 APP_GUI_NAME="Cross-platform Qt-based SSH client/terminal with session manager."
-APP_VERSION=2021.12.21.0759
+APP_VERSION=2022.02.09.1812
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-linux-x86_64-${APP_VERSION//./}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
