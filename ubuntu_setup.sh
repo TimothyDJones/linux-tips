fb116bc7db62abc164841be7618c0202004fa6e0
Tim Jones <tdjones74021@yahoo.com>
2022-03-17 08:06:14 -0500

    Add commands to install Lapce Rust-based cross-platform fast text editor from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -34745,3 +34745,38 @@ sudo cp /tmp/${FILE_NAME}/build/x86_64-unknown-linux-musl/${APP_NAME,,} /usr/loc
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/${APP_NAME}*
+
+# Install Lapce Rust-based cross-platform fast text editor from package
+# https://lapce.dev/
+APP_NAME=Lapce
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Rust-based cross-platform fast text editor."
+APP_GUI_CATEGORIES="Programming;Development;Accessories;"
+APP_GUI_KEYWORDS="Editor;"
+APP_VERSION=0.0.11
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME}-linux
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME,,}/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/${APP_NAME,,}/${APP_NAME,,}/master/extra/images/logo.png
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp /tmp/${FILE_NAME}/${APP_NAME}/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME}*
