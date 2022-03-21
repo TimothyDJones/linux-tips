156d0cd75b31285ba4280638703b2606d58dd690
Tim Jones <tdjones74021@yahoo.com>
2022-03-21 14:36:58 -0500

    Add commands to install Nala front-end for Debian/Ubuntu apt package manager from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -34926,3 +34926,13 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Nala front-end for Debian/Ubuntu apt package manager from Debian package
+APP_NAME=Nala
+APP_VERSION=0.6.0
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_amd64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://gitlab.com/volian/${APP_NAME,,}/uploads/c6c528c431ce3220bcaccb48bea91f48/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
