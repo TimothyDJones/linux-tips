b46284f7d755b17e74b763edd49f9060a82122e7
Tim Jones <tdjones74021@yahoo.com>
2022-03-24 13:13:19 -0500

    Update ImHex cross-platform, multi-purpose hex editor to version 1.16.2 and to install from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -26732,25 +26732,22 @@ sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 
-# Install ImHex cross-platform, multi-purpose hex editor from package
+# Install ImHex cross-platform, multi-purpose hex editor from AppImage
 APP_NAME=ImHex
 APP_GUI_NAME="Cross-platform, multi-purpose hex editor."
 APP_GUI_CATEGORIES="Development;Programming;"
 APP_GUI_KEYWORDS="Hex Editor;Editor"
-APP_VERSION=1.6.1
+APP_VERSION=1.16.2
 APP_EXT=zip
-FILE_NAME=${APP_NAME}_${APP_VERSION}_Linux
-sudo apt-get install -y libcapstone3
-sudo ln -s -f /usr/lib/x86_64-linux-gnu/libcapstone.so.3 /usr/lib/x86_64-linux-gnu/libcapstone.so.4
+FILE_NAME=Linux.AppImage
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/WerWolv/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/WerWolv/${APP_NAME}/master/resources/icon.png
 cd /tmp
 dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
-sudo mkdir -p /opt/${APP_NAME,,}
-sudo cp -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
-sudo chmod a+x /opt/${APP_NAME,,}/${APP_NAME}
-sudo ln -s -f /opt/${APP_NAME,,}/${APP_NAME} /usr/local/bin/${APP_NAME,,}
-curl -o /tmp/${APP_NAME,,}.ico -J -L https://raw.githubusercontent.com/WerWolv/${APP_NAME}/master/icon.ico
-sudo cp -R /tmp/${APP_NAME,,}.ico /opt/${APP_NAME,,}
+sudo cp /tmp/${FILE_NAME}/${APP_NAME,,}.AppImage /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}.AppImage
+sudo ln -s -f /usr/local/bin/${APP_NAME,,}.AppImage /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
 cat > /tmp/${APP_NAME,,}.desktop << EOF
 [Desktop Entry]
 Name=${APP_NAME}
@@ -26758,7 +26755,7 @@ Comment=${APP_GUI_NAME}
 GenericName=${APP_NAME}
 Path=/usr/local/bin
 Exec=/usr/local/bin/${APP_NAME,,}
-Icon=/opt/${APP_NAME,,}/${APP_NAME,,}.ico
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
 Type=Application
 StartupNotify=true
 Terminal=false
