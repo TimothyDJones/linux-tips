0da0e6ec83c339cfa0d2db8d0df6bb449fef8815
Tim Jones <tdjones74021@yahoo.com>
2022-03-28 07:18:18 -0500

    Update GEDKeeper cross-platform personal genealogical database tool to version 2.20.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -17606,7 +17606,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 # Install GEDKeeper cross-platform personal genealogical database tool from Debian package
 APP_NAME=GEDKeeper
 APP_GUI_NAME="Cross-platform personal genealogical database tool."
-APP_VERSION=2.16.2
+APP_VERSION=2.20.0
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}-1_all
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
