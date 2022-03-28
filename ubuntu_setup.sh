9a759bfbab2c93712e8ffb08d3767fda845197b0
Tim Jones <tdjones74021@yahoo.com>
2022-03-28 07:11:54 -0500

    Update yq Golang-based command-line YAML processor to version 4.24.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -26647,7 +26647,7 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 
 # Install yq Golang-based command-line YAML processor from package
 APP_NAME=yq
-APP_VERSION=4.23.1
+APP_VERSION=4.24.2
 APP_EXT=N/A
 FILE_NAME=${APP_NAME,,}_linux_${KERNEL_TYPE//i/}
 curl -o /tmp/${FILE_NAME} -J -L https://github.com/mikefarah/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}
