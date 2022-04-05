c6cb1047ceabeb23588d43b5f686862921ca44dd
Tim Jones <tdjones74021@yahoo.com>
2022-04-05 07:31:34 -0500

    Update Franz messenger consolidator tool to version 5.9.1 and to install from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2636,38 +2636,6 @@ sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME}*
 
-APP_NAME=PsTube
-_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
-APP_GUI_NAME="Cross-platform Electron-based desktop email client."
-APP_GUI_CATEGORIES="Accessories;Internet;"
-APP_GUI_KEYWORDS="Messenger;Chat;"
-APP_VERSION=2.0.0
-APP_EXT=AppImage
-FILE_NAME=${APP_NAME,,}-linux-${APP_VERSION}-x86_64
-curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/prateekmedia/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
-curl -o /tmp/${APP_NAME,,}.png -J -L https://github.com/meetfranz/${APP_NAME,,}/blob/master/build-helpers/images/icon.png
-sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
-sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
-sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
-sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
-cat > /tmp/${APP_NAME,,}.desktop << EOF
-[Desktop Entry]
-Name=${APP_NAME}
-Comment=${APP_GUI_NAME}
-GenericName=${APP_NAME}
-Path=/usr/local/bin
-Exec=/usr/local/bin/${APP_NAME,,}
-Icon=/usr/local/share/icons/${APP_NAME,,}.png
-Type=Application
-StartupNotify=true
-Terminal=false
-Categories=${APP_GUI_CATEGORIES}
-Keywords=${APP_GUI_KEYWORDS}
-EOF
-sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
-cd $HOME
-rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
-
 # Install Sciter Notes notepad utility
 APP_NAME=sciter-notes
 APP_VERSION=N/A
