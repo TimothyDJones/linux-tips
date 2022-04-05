8646dd0edad7d19a361e35d73336bf9e4df7c5e7
Tim Jones <tdjones74021@yahoo.com>
2022-04-05 17:02:18 -0500

    Add commands to install Fhex lightweight, full-featured Qt-based hex editor from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -35085,3 +35085,39 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install Fhex lightweight, full-featured Qt-based hex editor from package
+APP_NAME=Fhex
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Lightweight, full-featured Qt-based hex editor."
+APP_GUI_CATEGORIES="Development;Programming;System;"
+APP_GUI_KEYWORDS="Hex;Editor;"
+APP_VERSION=3.0.0
+APP_EXT=bz2
+FILE_NAME=${APP_NAME}_${APP_VERSION}_Linux_x64_Qt5.9.6_static_libs
+sudo apt install -y libqt5charts5
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/echo-devim/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/echo-devim/${APP_NAME,,}/master/icon.png
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -R /tmp/${FILE_NAME} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}
+sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
