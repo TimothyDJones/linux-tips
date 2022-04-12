21bf80a0a67429dfd88851ee8e8dda9bbd4582c7
Tim Jones <tdjones74021@yahoo.com>
2022-04-12 09:08:46 -0500

    Add commands to install Geeqie GTK-based image viewer and organizer from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -35246,3 +35246,36 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Geeqie GTK-based image viewer and organizer from AppImage
+APP_NAME=Geeqie
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="GTK-based image viewer and organizer."
+APP_GUI_CATEGORIES="Accessories;Graphics;Multimedia;"
+APP_GUI_KEYWORDS="Viewer;"
+APP_VERSION=1.7.1+20220117
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-v${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://cclark.uk/${APP_NAME}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/BestImageViewer/${APP_NAME,,}/master/src/icons/gqview_icon.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME}* /tmp/${APP_NAME,,}*
