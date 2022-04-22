5a4b478d557328f8dcf8210e028251431e7c143a
Tim Jones <tdjones74021@yahoo.com>
2022-04-22 11:54:17 -0500

    Add commands to install Photo Viewer minimalist imager viewer with resizing and color effects support from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -35392,3 +35392,36 @@ cd /tmp/${FILE_NAME}
 ./autogen.sh && ./configure && make && sudo make install
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Photo Viewer minimalist imager viewer with resizing and color effects support from AppImage
+APP_NAME="Photo Viewer"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Minimalist imager viewer with resizing and color effects support."
+APP_GUI_CATEGORIES="Graphics;Accessories;"
+APP_GUI_KEYWORDS="Viewer;"
+APP_VERSION=0.1.8
+APP_EXT=AppImage
+FILE_NAME=${_APP_NAME}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/Tenpi/${_APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.png -J -L https://raw.githubusercontent.com/Tenpi/${_APP_NAME}/master/assets/icons/logo.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${_APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${_APP_NAME}.png /usr/local/share/icons/${_APP_NAME}.png
+cat > /tmp/${_APP_NAME}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${_APP_NAME,,}
+Icon=/usr/local/share/icons/${_APP_NAME}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${_APP_NAME}* /tmp/${_APP_NAME,,}*
