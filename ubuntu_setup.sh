06fedb33e4aba2c5d19e4dc98468443658dd7381
Tim Jones <tdjones74021@yahoo.com>
2022-04-29 10:47:12 -0500

    Add commands to install SQLite Micro Manager cross-platform GUI SQLite client from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -35484,3 +35484,14 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install SQLite Micro Manager cross-platform GUI SQLite client from Debian package
+APP_NAME="SQLite Micro Manager"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.6.0.84
+APP_EXT=deb
+FILE_NAME=sqlmm-qt5_${APP_VERSION}_amd64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/sqlmm/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
