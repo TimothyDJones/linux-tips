7911e2d08f3d644fea800fdf6df0e10bbafc3158
Tim Jones <tdjones74021@yahoo.com>
2022-05-11 09:34:29 -0500

    Add commands to install Converse Desktop Jabber/XMPP desktop client based on Converse.js and Electron from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -36022,3 +36022,39 @@ sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
 sudo chmod +x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Converse Desktop Jabber/XMPP desktop client based on Converse.js and Electron from package
+APP_NAME="Converse Desktop"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '_')
+APP_GUI_NAME="Jabber/XMPP desktop client based on Converse.js and Electron."
+APP_GUI_CATEGORIES="Internet;Networking;"
+APP_GUI_KEYWORDS="Messenger;Jabber;XMPP;"
+APP_VERSION=9.1.0
+APP_EXT=tar.gz
+FILE_NAME=${_APP_NAME}-${APP_VERSION}_x64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/conversejs/${_APP_NAME//_/-}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.png -J -L https://raw.githubusercontent.com/conversejs/${_APP_NAME//_/-}/master/resources/images/icon-48x48.png
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${_APP_NAME}
+sudo cp -R -a /tmp/${FILE_NAME}/*  /opt/${_APP_NAME}
+sudo cp /tmp/${_APP_NAME}.png /opt/${_APP_NAME}
+# sudo chmod -R 777 /opt/${_APP_NAME}
+sudo ln -s -f /opt/${_APP_NAME}/"@${_APP_NAME//_/}" /usr/local/bin/${_APP_NAME}
+cat > /tmp/${_APP_NAME}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${_APP_NAME}
+Exec=/usr/local/bin/${_APP_NAME}
+Icon=/opt/${_APP_NAME}/${_APP_NAME}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${_APP_NAME}*
