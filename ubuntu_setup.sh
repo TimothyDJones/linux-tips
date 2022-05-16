d34cf450d1dd7e4723cdbd984a35424837284a71
Tim Jones <tdjones74021@yahoo.com>
2022-05-16 11:18:04 -0500

    Add commands to install Basic Numerical Methods GUI tool for demonstrating common numerical methods for engineering from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -35987,3 +35987,36 @@ sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
 sudo chmod +x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Basic Numerical Methods GUI tool for demonstrating common numerical methods for engineering from AppImage
+APP_NAME=Basic-Numerical-Methods
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '-' '_')
+APP_GUI_NAME="GUI tool for demonstrating common numerical methods for engineering."
+APP_GUI_CATEGORIES="Math;Engineering;"
+APP_GUI_KEYWORDS="Math;Engineering;"
+APP_VERSION=3.0.17
+APP_EXT=AppImage
+FILE_NAME=${_APP_NAME}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://a.fsdn.com/allura/p/basic-numerical-methods/icon?062ee29e8cadddee685aa432ef8e8e31af939cd4d2982f52ed35a3fce524c443
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME}* /tmp/${APP_NAME,,}*
