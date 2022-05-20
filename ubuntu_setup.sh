28329ca0e4650a43d8a563a163dcd8fde1a01a68
Tim Jones <tdjones74021@yahoo.com>
2022-05-20 07:53:57 -0500

    Add commands to install kpcli cross-platform command-line interface/interactive shell for KeePass database files (*.kdb/*.kdbx) from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -36179,3 +36179,14 @@ sudo chmod +x /usr/local/bin/${FILE_NAME}
 sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME//./}*
+
+# Install kpcli cross-platform command-line interface/interactive shell for KeePass database files (*.kdb/*.kdbx) from Debian package
+APP_NAME=kpcli
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=3.7
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}-1
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
