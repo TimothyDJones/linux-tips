4c581470dbd3ee61b7160921bc02c0c3860e3214
Tim Jones <tdjones74021@yahoo.com>
2022-05-20 07:43:10 -0500

    Add commands to install transfer.sh Golang-based command-line simple and fast file transfer tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -36164,3 +36164,18 @@ sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
 sudo chmod +x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install transfer.sh Golang-based command-line simple and fast file transfer tool from package
+APP_NAME=transfer.sh
+APP_GUI_NAME="fq Golang-based command-line simple and fast file transfer tool."
+APP_VERSION=1.4.0
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME//./}-v${APP_VERSION}-linux-$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME//./-}.mirror/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${FILE_NAME} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}
+sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME//./}*
