aad721b1769ca255f22d9f4ad30569b9b1a43caf
Tim Jones <tdjones74021@yahoo.com>
2022-05-22 19:02:58 -0500

    Update Jack-The-Cutter Java lossless audio file cutting tool to version 0.3.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -35787,7 +35787,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
 APP_GUI_NAME="Java lossless audio file cutting tool."
 APP_GUI_CATEGORIES="Audio;Entertainment;Multimedia;"
 APP_GUI_KEYWORDS="Audio;Editor;"
-APP_VERSION=0.2.0
+APP_VERSION=0.3.0
 APP_EXT=jar
 FILE_NAME=${APP_NAME}-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
