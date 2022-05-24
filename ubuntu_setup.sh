d6046d298a4b64c057b6468323fb9ddd2d88292e
Tim Jones <tdjones74021@yahoo.com>
2022-05-24 13:05:37 -0500

    Add commands to install PassVault cross-platform Electron-based desktop password management tool from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -36244,3 +36244,14 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME}* /tmp/${APP_NAME,,}*
+
+# Install PassVault cross-platform Electron-based desktop password management tool from Debian package
+APP_NAME=PassVault
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=1.1.1
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_amd64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/SinadShan/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
