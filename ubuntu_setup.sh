a06c6245eca59f7e274ab7c893d3bd9b08a7da56
Tim Jones <tdjones74021@yahoo.com>
2022-05-31 07:33:40 -0500

    Add commands to install Radare cross-platform reverse-engineering framework from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -36331,3 +36331,14 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${A
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Radare cross-platform reverse-engineering framework from Debian package
+APP_NAME=Radare2
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=5.7.0
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
