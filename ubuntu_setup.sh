6c82be89d070e9ade0a17e81ec0ce672fca9d72e
Tim Jones <tdjones74021@yahoo.com>
2022-06-06 10:19:32 -0500

    Add commands to install hit Golang-based command-line HTTP client with history.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -36415,3 +36415,17 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME}* /tmp/${APP_NAME,,}*
+
+# Install hit Golang-based command-line HTTP client with history from package
+APP_NAME=hit
+APP_GUI_NAME="Golang-based command-line HTTP client with history."
+APP_VERSION=0.2.0
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/hbagdi/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+sudo chmod +x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME//./}*
