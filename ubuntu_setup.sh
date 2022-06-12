3440d321e66e67b497550153ae46f139ff39ec32
Tim Jones <tdjones74021@yahoo.com>
2022-06-12 18:31:07 -0500

    Update Firejail and Firetools utilities for running applications in isolated memory space to version 0.9.70.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -526,7 +526,7 @@ rm -rf /tmp/${APP_NAME,,}*
 
 # Install Firejail and Firetools utilities for running applications in isolated memory space from Debian package
 APP_NAME=Firejail
-APP_VERSION=0.9.68
+APP_VERSION=0.9.70
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}-apparmor_1_${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
