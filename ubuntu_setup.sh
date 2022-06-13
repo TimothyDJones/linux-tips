9625ea6583c084031507b79aee9f229689648b81
Tim Jones <tdjones74021@yahoo.com>
2022-06-13 07:08:00 -0500

    Add commands to install Yaka cross-platform, simple 'to do' tool with full keyboard navigation.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -36443,3 +36443,36 @@ sudo cp -a /tmp/${FILE_NAME}/bin/* /usr/local/bin
 sudo chmod +x /usr/local/bin/${APP_NAME,,} /usr/local/bin/lscp /usr/local/bin/lsftp
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Yaka cross-platform, simple 'to do' tool with full keyboard navigation from AppImage
+APP_NAME=Yaka
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Cross-platform, simple 'to do' tool with full keyboard navigation."
+APP_GUI_CATEGORIES="Accessories;Office;"
+APP_GUI_KEYWORDS="To Do;Productivity;"
+APP_VERSION=0.7.0
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/jyannick/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/jyannick/${APP_NAME,,}/main/build/icon.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
