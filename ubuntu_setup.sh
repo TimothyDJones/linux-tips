43a277cb5430fbadcbc4113acfcee7fbcbe12c2b
Tim Jones <tdjones74021@yahoo.com>
2022-06-14 08:50:01 -0500

    Add commands to install Kavita cross-platform, self-hosted web comic and ebook reader from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -36509,3 +36509,70 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install Kavita cross-platform, self-hosted web comic and ebook reader from package
+APP_NAME=Kavita
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Cross-platform, self-hosted web comic and ebook reader."
+APP_GUI_CATEGORIES="Office;Internet;"
+APP_GUI_KEYWORDS="Comics;Ebook;Reader;"
+APP_VERSION=0.5.3
+APP_EXT=tar.gz
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	ARCH_TYPE=x64
+else    # Otherwise use version for 32-bit kernel
+	ARCH_TYPE=x86
+fi
+FILE_NAME=${APP_NAME,,}-linux-${ARCH_TYPE}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.svg -J -L https://raw.githubusercontent.com/Kareadita/${APP_NAME}/develop/Logo/${APP_NAME,,}.svg
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -R /tmp/${FILE_NAME}/${APP_NAME}/* /opt/${APP_NAME,,}
+sudo cp /tmp/${APP_NAME,,}.svg /opt/${APP_NAME,,}/${APP_NAME,,}.svg
+sudo chmod -R 777 /opt/${APP_NAME,,}
+sudo ln -s -f /opt/${APP_NAME,,}/${APP_NAME} /opt/${APP_NAME,,}/${APP_NAME,,}
+sudo ln -s -f /opt/${APP_NAME,,}/${APP_NAME} /usr/local/bin/${APP_NAME,,}
+# Register Kavita application as a service to run at system start
+sudo useradd -p $(openssl passwd -6 ${APP_NAME,,}) ${APP_NAME,,}
+# sudo addgroup ${APP_NAME,,}
+# sudo usermod -a -G ${APP_NAME,,} ${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.service << EOF
+[Unit]
+Description=Kavita Server
+After=network.target
+
+[Service]
+User=${APP_NAME,,}
+Group=${APP_NAME,,}
+Type=simple
+WorkingDirectory=/opt/${APP_NAME,,}
+ExecStart=/opt/${APP_NAME,,}/${APP_NAME}
+TimeoutStopSec=20
+KillMode=process
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo cp /tmp/${APP_NAME,,}.service /etc/systemd/system
+sudo systemctl start ${APP_NAME,,}.service
+sudo systemctl enable ${APP_NAME,,}.service
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+# Path=/usr/local/bin
+Exec=xdg-open http://localhost:5000/
+Icon=/opt/${APP_NAME,,}/${APP_NAME,,}.svg
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
