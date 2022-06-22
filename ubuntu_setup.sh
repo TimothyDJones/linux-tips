e9cf56e5cb0385ea1d75bf805374d47d59dceadc
Tim Jones <tdjones74021@yahoo.com>
2022-06-22 07:06:23 -0500

    Update Radare cross-platform reverse-engineering framework to version 5.7.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -36334,7 +36334,7 @@ rm -rf /tmp/${APP_NAME,,}*
 # Install Radare cross-platform reverse-engineering framework from Debian package
 APP_NAME=Radare2
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
-APP_VERSION=5.7.0
+APP_VERSION=5.7.2
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
