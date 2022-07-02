8304c3d5b64d44199e318954cba5e70e1eeeab02
Tim Jones <tdjones74021@yahoo.com>
2022-07-02 10:51:40 -0500

    Add commands install GLogic logic gate simulator from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -36705,3 +36705,14 @@ cp /tmp/${APP_NAME,,}_config.yml $HOME/.config
 sampler -c $HOME/.config/${APP_NAME,,}_config.yml
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME//./}*
+
+# Install GLogic logic gate simulator from Debian package
+APP_NAME=GLogic
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=3.1.0
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}-1_all
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
