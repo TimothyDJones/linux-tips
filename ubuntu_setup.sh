8d425a535d3d2751785eccaa8b48a6c9e0d93e41
Tim Jones <tdjones74021@yahoo.com>
2022-07-03 17:24:06 -0500

    Update QuickRedis GUI management tool for Redis to version 2.7.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -24945,7 +24945,7 @@ APP_NAME=QuickRedis
 APP_GUI_NAME="GUI management tool for Redis."
 APP_GUI_CATEGORIES="Development;Programming;"
 APP_GUI_KEYWORDS="Redis;Database;"
-APP_VERSION=2.6.0
+APP_VERSION=2.7.1
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME}-${APP_VERSION}-linux-x86_64
 sudo apt-get install -y redis-server
