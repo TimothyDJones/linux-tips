e6203e10fbd9c3d0e71b927fe3ded0e049ce8094
Tim Jones <tdjones74021@yahoo.com>
2022-07-13 07:39:35 -0500

    Update PDF Split and Merge (PDFsam) editor to version 4.3.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3752,7 +3752,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install PDF Split and Merge (PDFsam) editor from Debian package
 APP_NAME=PDFSam
-APP_VERSION=4.3.1
+APP_VERSION=4.3.2
 APP_EXT=deb
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=amd64
