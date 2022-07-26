53d08ee3768ca6c95d1634fc6164e1223572118a
Tim Jones <tdjones74021@yahoo.com>
2022-07-26 07:01:21 -0500

    Update xsnow desktop decoration to version 3.5.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -19493,7 +19493,7 @@ rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # Install xsnow desktop decoration from Debian package
 APP_NAME=xsnow
 APP_GUI_NAME=""
-APP_VERSION=3.4.4
+APP_VERSION=3.5.1
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}-1_${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
