96ac14b5c76f5e53cf7458f78167f8cf98d0e0d7
Tim Jones <tdjones74021@yahoo.com>
2022-08-08 07:03:06 -0500

    Add commands to install bauh Python-based GUI multi-format package manager for Linux with support for AppImage, Debian and Arch Linux packages (including AUR), Flatpak, and Snap from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -37432,3 +37432,36 @@ sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
 sudo chmod +x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install bauh Python-based GUI multi-format package manager for Linux with support for AppImage, Debian and Arch Linux packages (including AUR), Flatpak, and Snap from AppImage
+APP_NAME=bauh
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Python-based GUI multi-format package manager for Linux with support for AppImage, Debian and Arch Linux packages (including AUR), Flatpak, and Snap."
+APP_GUI_CATEGORIES="System;Accessories;"
+APP_GUI_KEYWORDS="Package;Manager;"
+APP_VERSION=0.10.3
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}-x86_64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/vinifmor/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.svg -J -L https://github.com/vinifmor/bauh/blob/master/bauh/view/resources/img/logo.svg
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.svg /usr/local/share/icons/${APP_NAME,,}.svg
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.svg
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
