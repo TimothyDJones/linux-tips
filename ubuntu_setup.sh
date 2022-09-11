180c942fabbd3a073c2bb739a370dfa29454d246
Tim Jones <tdjones74021@yahoo.com>
2022-09-10 21:02:51 -0500

    Update Treeline Python/Qt-based tree-structured notepad to version 3.1.5.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3329,7 +3329,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install Treeline Python/Qt-based tree-structured notepad from package
 APP_NAME=TreeLine
 APP_GUI_NAME="Python/Qt-based tree-structured notepad"
-APP_VERSION=3.1.4
+APP_VERSION=3.1.5
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get install -y python3-pyqt5
