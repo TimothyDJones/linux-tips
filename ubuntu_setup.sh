59999569f388884bdfbcc1917a828318f39ae39b
Tim Jones <tdjones74021@yahoo.com>
2022-09-23 06:50:29 -0500

    Add commands to install Open Video Downloader cross-platform Electron-based GUI frontend for youtube-dl from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -37908,3 +37908,36 @@ sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
 sudo chmod +x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Open Video Downloader cross-platform Electron-based GUI frontend for youtube-dl from AppImage
+APP_NAME="Open Video Downloader"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform Electron-based GUI frontend for youtube-dl."
+APP_GUI_CATEGORIES="Multimedia;Entertainment;Video;"
+APP_GUI_KEYWORDS="Video;Download;"
+APP_VERSION=2.4.0
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME// /-}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_APP_NAME}.mirror/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.png -J -L https://a.fsdn.com/allura/mirror/${_APP_NAME}/icon?b5febb7f0cc048efba5c8260f2f50f82bf284b89d1414e9ff94db0c0c4e65d74
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${_APP_NAME}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${_APP_NAME}.png /usr/local/share/icons/${_APP_NAME}.png
+cat > /tmp/${_APP_NAME}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${_APP_NAME}
+Icon=/usr/local/share/icons/${_APP_NAME}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME// /-}* /tmp/${_APP_NAME}*
