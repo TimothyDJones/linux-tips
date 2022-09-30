c1515d2f0a84c5c8de9c48ce1deb70be0a1bcca5
Tim Jones <tdjones74021@yahoo.com>
2022-09-30 08:58:09 -0500

    Update Bdash cross-platform GUI (JavaScript/Electron) minimalist SQL client for data analysis to version 1.15.3.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -31434,7 +31434,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="GUI (JavaScript/Electron) minimalist SQL client for data analysis."
 APP_GUI_CATEGORIES="Development;Programming;"
 APP_GUI_KEYWORDS="Database;SQL;Editor;"
-APP_VERSION=1.11.1
+APP_VERSION=1.15.3
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME}-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME,,}-app/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
