f967dd1b82e44b72fc8f657d6ff271cc9f167c7c
Tim Jones <tdjones74021@yahoo.com>
2022-10-10 07:26:25 -0500

    Update UNA social-media community management web-based tool to version 13.0.0-B4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7714,7 +7714,7 @@ rm -rf /tmp/${APP_NAME,,}*
 
 # Install UNA social-media community management web-based tool (PHP/MySQL)
 APP_NAME=UNA
-APP_VERSION=13.0.0-B3
+APP_VERSION=13.0.0-B4
 APP_EXT=zip
 DB_NAME=${APP_NAME,,}
 DB_USER=${APP_NAME,,}
