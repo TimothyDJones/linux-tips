0cddc28b458af01deaf7da6d38d67f5e8ed41d5a
Tim Jones <tdjones74021@yahoo.com>
2022-10-12 17:08:56 -0500

    Add commands to install just project-specific command runner with make-like syntax from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -38365,3 +38365,20 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME}* /tmp/${APP_NAME,,}*
+
+# Install just project-specific command runner with make-like syntax from package
+APP_NAME=just
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Project-specific command runner with make-like syntax."
+APP_VERSION=1.5.0
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-musl
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/casey/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,}.1 /usr/local/man/man1
+sudo chmod +x /usr/local/bin/${APP_NAME,,}
+sudo mandb
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
