1d5f8a4e2e88213497ee852195a912e90fbdbd88
Tim Jones <tdjones74021@yahoo.com>
2022-10-24 07:48:22 -0500

    Update Kavita cross-platform, self-hosted web comic and ebook reader to version 0.6.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -36523,7 +36523,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
 APP_GUI_NAME="Cross-platform, self-hosted web comic and ebook reader."
 APP_GUI_CATEGORIES="Office;Internet;"
 APP_GUI_KEYWORDS="Comics;Ebook;Reader;"
-APP_VERSION=0.5.5
+APP_VERSION=0.6.0
 APP_EXT=tar.gz
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=x64
