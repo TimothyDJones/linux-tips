b18660abf42cff02d7bc4919353c5ffb05fc51c2
Tim Jones <tdjones74021@yahoo.com>
2022-10-27 07:37:07 -0500

    Update KmCaster Java-based on-screen display (OSD) for keyboard and mouse events for screencasting to version 1.1.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25006,7 +25006,7 @@ APP_NAME=KmCaster
 APP_GUI_NAME="Java-based on-screen display (OSD) for keyboard and mouse events for screencasting."
 APP_GUI_CATEGORIES="Accessories;System;"
 APP_GUI_KEYWORDS="Mouse;Keyboard;Screen;"
-APP_VERSION=1.0.3
+APP_VERSION=1.1.0
 APP_EXT=jar
 FILE_NAME=${APP_NAME,,}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/DaveJarvis/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
