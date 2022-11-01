4c3e1bac955110eed34e7c03cab6e8e271ba5945
Tim Jones <tdjones74021@yahoo.com>
2022-11-01 08:17:07 -0500

    Add commands to install StateS simple state diagram editor from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -38587,3 +38587,38 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install StateS simple state diagram editor from package
+APP_NAME=StateS
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Cross-platform simple schematic diagram editor."
+APP_GUI_CATEGORIES="Development;Programming;"
+APP_GUI_KEYWORDS="Editor;Diagramming;Visualization;"
+APP_VERSION=0.4.4
+APP_EXT=tar.xz
+FILE_NAME=${APP_NAME}_${APP_VERSION}_Linux
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/ClementFoucher/${APP_NAME}/releases/download/V.${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.svg -J -L https://raw.githubusercontent.com/ClementFoucher/${APP_NAME}/main/art/svg/${APP_NAME}.svg
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp /tmp/${FILE_NAME} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}
+sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.svg /usr/local/share/icons/${APP_NAME,,}.svg
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.svg
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
