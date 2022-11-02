1218f11741dbe68ede87c6adf523d5963a73a821
Tim Jones <tdjones74021@yahoo.com>
2022-11-02 09:38:48 -0500

    Add commands to install ddarch wrapper for dd, fdisk, parted and other tools for creating and restoring disk images from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -38665,3 +38665,14 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME}* /tmp/${APP_NAME,,}*
+
+# Install ddarch wrapper for dd, fdisk, parted and other tools for creating and restoring disk images from Debian package
+APP_NAME=ddarch
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_VERSION=0.1.9
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}-1_all
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
