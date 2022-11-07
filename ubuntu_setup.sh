7a4c8dd02abae5b30b863076a5fac538587d6c75
Tim Jones <tdjones74021@yahoo.com>
2022-11-07 07:43:07 -0600

    Update Rufas Slider puzzle game to version 7nov22.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4475,7 +4475,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install Rufas Slider puzzle game from source
 APP_NAME=RufasSlider
 APP_GUI_NAME="Klotsky-style slider puzzle game."
-APP_VERSION=22oct22
+APP_VERSION=7nov22
 APP_EXT=7z
 FILE_NAME=rs${APP_VERSION}
 sudo apt-get install -y qttools5-dev qttools5-dev-tools cmake
