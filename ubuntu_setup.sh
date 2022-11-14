7c7ef852acc194e3e5e4cf3792dab4bfd485b68e
Tim Jones <tdjones74021@yahoo.com>
2022-11-14 07:39:43 -0600

    Update dstask Golang-based command-line task manager with Git integration and Markdown support to version 0.26.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -36098,7 +36098,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 # Install dstask Golang-based command-line task manager with Git integration and Markdown support from package
 APP_NAME=dstask
 APP_GUI_NAME="Golang-based command-line task manager with Git integration and Markdown support."
-APP_VERSION=0.25
+APP_VERSION=0.26
 APP_EXT=N/A
 FILE_NAME=${APP_NAME,,}-linux-$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME} -J -L https://github.com/naggie/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}
