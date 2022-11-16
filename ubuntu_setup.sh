2b6ff29547cdb0ca3db81579b85d8acfef4ae7bb
Tim Jones <tdjones74021@yahoo.com>
2022-11-16 08:00:37 -0600

    Update Slider Puzzles cross-platform, color terminal/console puzzle games to version 16nov22.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -39002,9 +39002,9 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 APP_NAME="Slider Puzzles"
 APP_GUI_NAME="Cross-platform, color terminal/console puzzle games."
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
-APP_VERSION=15nov22
+APP_VERSION=16nov22
 APP_EXT=7z
-FILE_NAME=puz${APP_VERSION}
+FILE_NAME=asl${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_APP_NAME}/${FILE_NAME}.${APP_EXT}
 cd /tmp
 dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
