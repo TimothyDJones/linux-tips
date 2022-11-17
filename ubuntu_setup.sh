76885adab967176201f9d094484ceb223cf2cde1
Tim Jones <tdjones74021@yahoo.com>
2022-11-17 07:18:12 -0600

    Update Group-Office web-based office suite to version 6.6.129.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2744,7 +2744,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Group-Office web-based office suite (manual installation)
 APP_NAME=GroupOffice
-APP_VERSION=6.6.128
+APP_VERSION=6.6.129
 APP_EXT=tar.gz
 DB_NAME=${APP_NAME,,}
 DB_USER=${APP_NAME,,}
