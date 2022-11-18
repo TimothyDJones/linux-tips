d5774dc7e6608a7e27c2ad9f327b6aa8e7c38dbc
Tim Jones <tdjones74021@yahoo.com>
2022-11-18 16:22:31 -0600

    Add commands to install Curlie Golang-based command-line frontend to curl that offers ease-of-use of HTTPie from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -39148,3 +39148,17 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install Curlie Golang-based command-line frontend to curl that offers ease-of-use of HTTPie from package
+APP_NAME=Curlie
+APP_GUI_NAME="Golang-based command-line frontend to curl that offers ease-of-use of HTTPie."
+APP_VERSION=1.6.9
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/rs/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+sudo chmod +x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
