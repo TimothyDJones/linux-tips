06176f4def2c5fa10adcfd4045eb3f7d1be178f0
Tim Jones <tdjones74021@yahoo.com>
2022-11-19 15:09:12 -0600

    Add commands to install epick Rust-based color picker tool for creating harmonic color palettes from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -39106,3 +39106,36 @@ rm -rf /tmp/*${APP_NAME,,}*
 sudo apt install -y flatpak
 sudo flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 sudo flatpak install flathub-beta com.github.marhkb.Pods
+
+# Install epick Rust-based color picker tool for creating harmonic color palettes from package
+APP_NAME=epick
+APP_GUI_NAME="Rust-based color picker tool for creating harmonic color palettes."
+APP_GUI_CATEGORIES="Accessories;"
+APP_GUI_KEYWORDS="Color;Picker;"
+APP_VERSION=0.9.0
+APP_EXT=tar.xz
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}-$(arch)-unknown-linux
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/vv9k/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/vv9k/${APP_NAME,,}/master/assets/icon.png
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp /tmp/${FILE_NAME}/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin/${APP_NAME,,}
+sudo chmod +x /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
