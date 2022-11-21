67bb3021e8c8361fde23a200862494df1cdd0aef
Tim Jones <tdjones74021@yahoo.com>
2022-11-21 07:14:28 -0600

    Update QPDF PDF utility to version 11.2.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2879,7 +2879,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install QPDF PDF utility from source
 APP_NAME=QPDF
-APP_VERSION=11.1.1
+APP_VERSION=11.2.0
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get install -y zlib1g-dev libjpeg62-dev
