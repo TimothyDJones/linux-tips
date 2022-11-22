6d0c1aa7e4bdf4254c4c29ecaebfcbb6eb631237
Tim Jones <tdjones74021@yahoo.com>
2022-11-22 07:24:34 -0600

    Update GitUI cross-platform console GUI for Git version control to version 0.22.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -23312,7 +23312,7 @@ rm -rf /tmp/${APP_NAME}*
 # Install GitUI cross-platform console GUI for Git version control from package
 APP_NAME=GitUI
 APP_GUI_NAME="Cross-platform console GUI for Git version control."
-APP_VERSION=0.22.0
+APP_VERSION=0.22.1
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-linux-musl
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/extrawurst/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
