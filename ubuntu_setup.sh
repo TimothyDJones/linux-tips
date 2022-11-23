1ad35d0ffdfe28749b120fe1cf2c141b6de5868e
Tim Jones <tdjones74021@yahoo.com>
2022-11-23 07:22:35 -0600

    Update LosslessCut cross-platform FFmpeg GUI for video and audio editing and other operations to version 3.48.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -31303,7 +31303,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross-platform FFmpeg GUI for video and audio editing and other operations."
 APP_GUI_CATEGORIES="Multimedia;Entertainment;Audio;Video;"
 APP_GUI_KEYWORDS="Audio;Video;FFmpeg;Editing;"
-APP_VERSION=3.46.2
+APP_VERSION=3.48.2
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME}-linux
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/mifi/lossless-cut/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
