a44a624fade1d26ac4a6f9dc08ca32a6c1ee3a4e
Tim Jones <tdjones74021@yahoo.com>
2022-11-24 20:21:59 -0600

    Update Colorpicker Electron-based, cross-platform minimal GUI color identification tool to version 2.2.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -38185,7 +38185,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Electron-based, cross-platform minimal GUI color identification tool."
 APP_GUI_CATEGORIES="Accessories;Development;Programming;"
 APP_GUI_KEYWORDS="Color;Picker;"
-APP_VERSION=2.1.0
+APP_VERSION=2.2.0
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME}-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/Toinane/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
