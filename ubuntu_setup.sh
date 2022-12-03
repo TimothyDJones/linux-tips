60eb89592db163d3e8442d250fb7558cef33de84
Tim Jones <tdjones74021@yahoo.com>
2022-12-03 11:40:17 -0600

    Update Delta language-enabled syntax-highlighting diff for Git to version 0.15.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -21224,7 +21224,7 @@ sudo rm -rf /tmp/${APP_NAME,,} /tmp/${APP_NAME}
 # Install Delta language-enabled syntax-highlighting diff for Git from package
 APP_NAME=Delta
 APP_GUI_NAME="Language-enabled syntax-highlighting diff for Git."
-APP_VERSION=0.14.0
+APP_VERSION=0.15.0
 APP_EXT=tar.gz
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=x86_64
