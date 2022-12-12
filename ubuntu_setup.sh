715fbdfae9d0ce1b398ae9fa3b3ac2d198b26b24
Tim Jones <tdjones74021@yahoo.com>
2022-12-12 07:09:41 -0600

    Add commands to install cedit cross-platform minimalist console text editor from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -39546,3 +39546,22 @@ EOF
 sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/Calendar* /tmp/${APP_NAME,,}*
+
+# Install cedit cross-platform minimalist console text editor from package
+APP_NAME=cedit
+APP_GUI_NAME="Cross-platform minimalist console text editor."
+APP_VERSION=N/A
+APP_EXT=tar.gz
+if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
+	ARCH_TYPE=lin64
+else    # Otherwise use version for 32-bit kernel
+	ARCH_TYPE=lin32
+fi
+FILE_NAME=${ARCH_TYPE}_${APP_NAME,,}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/console-edit/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+sudo chmod +x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
