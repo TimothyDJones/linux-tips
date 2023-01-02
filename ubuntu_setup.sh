e314b6352a6e39495b87b05cc2c6037ea2120b23
Tim Jones <tdjones74021@yahoo.com>
2023-01-01 20:42:22 -0600

    Update Pidgin cross-platform, multi-service instant messenger (IM) utility to version 2.14.12.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8645,7 +8645,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install Pidgin cross-platform, multi-service instant messenger (IM) utility from source
 APP_NAME=Pidgin
 APP_GUI_NAME="Cross-platform , multi-service instant messenger (IM) utility."
-APP_VERSION=2.14.10
+APP_VERSION=2.14.12
 APP_EXT=tar.bz2
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 # Remove if installed from package
