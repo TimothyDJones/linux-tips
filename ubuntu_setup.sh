71ff2ca7bd924176a21a50e4fbb2d6d15f6f7d17
Tim Jones <tdjones74021@yahoo.com>
2023-01-17 16:44:34 -0600

    Add commands to install Apt-Qt Qt-based Apt GUI manager/Synaptic alternative from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -40001,3 +40001,36 @@ EOF
 sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME// /}* /tmp/${_APP_NAME}*
+
+# Install Apt-Qt Qt-based Apt GUI manager/Synaptic alternative from package
+APP_NAME=Apt-Qt
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Qt-based Apt GUI manager/Synaptic alternative."
+APP_GUI_CATEGORIES="Accessories;System;"
+APP_GUI_KEYWORDS="Apt;Synaptic;"
+APP_VERSION=0.01
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${FILE_NAME}/${APP_NAME,,}.ico /usr/local/share/icons/${APP_NAME,,}.ico
+sudo chmod +x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.ico
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
