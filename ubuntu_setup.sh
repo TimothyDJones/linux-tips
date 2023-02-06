aa0f6db72dc11699c58405dc648abbe2b33b441c
Tim Jones <tdjones74021@yahoo.com>
2023-02-06 09:40:16 -0600

    Update DirSize cross-platform Java-based directory size reporting tool to version 2.2.18.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -29553,7 +29553,7 @@ APP_NAME=DirSize
 APP_GUI_NAME="Cross-platform Java-based directory size reporting tool."
 APP_GUI_CATEGORIES="System;Accessories;"
 APP_GUI_KEYWORDS="File;System;"
-APP_VERSION=2.2.14
+APP_VERSION=2.2.18
 APP_EXT=jar
 FILE_NAME=${APP_NAME,,}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}r/${FILE_NAME}.${APP_EXT}
