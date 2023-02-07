8829cf7d70caec9873d9f53155c57263c0ea868f
Tim Jones <tdjones74021@yahoo.com>
2023-02-07 15:57:02 -0600

    Add commands to install Pulsar cross-platform Electron-based hackable text editor (fork of discontinued Atom editor) from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -40432,3 +40432,36 @@ sudo alien --scripts /tmp/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${APP_NAME,,}*.deb
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Pulsar cross-platform Electron-based hackable text editor (fork of discontinued Atom editor) from AppImage
+APP_NAME=Pulsar
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform Electron-based hackable text editor (fork of discontinued Atom editor)."
+APP_GUI_CATEGORIES="Programming;Development;Accessories;"
+APP_GUI_KEYWORDS="Editor;IDE;"
+APP_VERSION=1.101.0-beta
+APP_EXT=AppImage
+FILE_NAME=Linux.${APP_NAME}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME,,}-edit/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.png -J -L https://raw.githubusercontent.com/${APP_NAME,,}-edit/${APP_NAME,,}/master/resources/icons/128x128.png
+sudo cp -a /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${_APP_NAME}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${_APP_NAME}.png /usr/local/share/icons/${_APP_NAME}.png
+cat > /tmp/${_APP_NAME}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${_APP_NAME}
+Icon=/usr/local/share/icons/${_APP_NAME}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${_APP_NAME}*
