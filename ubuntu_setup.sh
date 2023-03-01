74575476ef74037f918207ae380d3f909da2e8b4
Tim Jones <tdjones74021@yahoo.com>
2023-03-01 14:13:31 -0600

    Add commands to install BeeRef cross-platform, PyQt-based minimalist image viewer from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -40718,3 +40718,36 @@ dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
 sudo cp -a -R /tmp/${FILE_NAME}/* /
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install BeeRef cross-platform, PyQt-based minimalist image viewer from package
+APP_NAME=BeeRef
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Cross-platform, PyQt-based minimalist image viewer."
+APP_GUI_CATEGORIES="Accessories;Graphics;"
+APP_GUI_KEYWORDS="Image;Graphics;Viewer;"
+APP_VERSION=0.2.0
+APP_EXT=N/A
+FILE_NAME=${APP_NAME}-${APP_VERSION}-linux
+curl -o /tmp/${FILE_NAME} -J -L https://github.com/rbreu/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/rbreu/${APP_NAME,,}/main/${APP_NAME,,}/assets/logo.png
+sudo cp -a /tmp/${FILE_NAME} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}
+sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME}* /tmp/${APP_NAME,,}*
