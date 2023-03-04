8deb07d74aa5256155a4374114f60d8a1c5028fd
Tim Jones <tdjones74021@yahoo.com>
2023-03-04 16:49:09 -0600

    Update Worker File Manager to version 4.12.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1839,7 +1839,7 @@ rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 
 # Install Worker File Manager (For AVFS support install AVFS above.)
 APP_NAME=worker
-APP_VERSION=4.11.0
+APP_VERSION=4.12.0
 APP_EXT=tar.zst
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get install -y liblua5.3-dev zstd
