27a4f3e35e20fa21b14a2d07bb12e3555d91020b
Tim Jones <tdjones74021@yahoo.com>
2023-03-11 10:36:29 -0600

    Update Dibuja lightweight image editor similar to MS Paint to version 0.22.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7015,7 +7015,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 # Install Dibuja lightweight image editor similar to MS Paint from source
 APP_NAME=Dibuja
 APP_GUI_NAME="Lightweight image editor similar to MS Paint."
-APP_VERSION=0.12.0
+APP_VERSION=0.22.0
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get install -y intltool libgtk2.0-dev libbabl-dev libgegl-dev libexiv2-dev
