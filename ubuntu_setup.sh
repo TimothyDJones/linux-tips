c258251756b2b62a8dcd1a8a9d04dd0f834d20c8
Tim Jones <tdjones74021@yahoo.com>
2023-03-27 17:11:52 -0500

    Add commands to install Dinosaur RSS cross-platform Electron-based simple RSS news aggregator/reader from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -40878,3 +40878,36 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install Dinosaur RSS cross-platform Electron-based simple RSS news aggregator/reader from AppImage
+APP_NAME="Dinosaur RSS"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform Electron-based simple RSS news aggregator/reader."
+APP_GUI_CATEGORIES="Internet;"
+APP_GUI_KEYWORDS="RSS;News;Aggregator;"
+APP_VERSION=1.10.1
+APP_EXT=AppImage
+FILE_NAME=Dinosaur.Rss.${APP_VERSION}.x64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/richshaw2015/dino-rss-electron/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.png -J -L https://raw.githubusercontent.com/richshaw2015/dino-rss-electron/master/build/logo.png
+sudo cp -a /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${_APP_NAME}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${_APP_NAME}.png /usr/local/share/icons/${_APP_NAME}.png
+cat > /tmp/${_APP_NAME}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${_APP_NAME}
+Icon=/usr/local/share/icons/${_APP_NAME}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${_APP_NAME}* /tmp/${APP_NAME}*
