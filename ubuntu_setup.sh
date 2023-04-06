8e97ed35c66df305a6450ad441855313826cb7d4
Tim Jones <tdjones74021@yahoo.com>
2023-04-06 15:33:36 -0500

    Add commands to Install Nuitka cross-platform optimizing Python compiler from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -41015,3 +41015,16 @@ sudo cp -R -a /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
 sudo ln -s -f /opt/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin/${APP_NAME,,}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Nuitka cross-platform optimizing Python compiler from package
+# https://nuitka.net/
+CODENAME=`egrep 'UBUNTU_CODENAME|VERSION_CODENAME' /etc/os-release | sort | head -1 | cut -d= -f2`
+if [ -z "$CODENAME" ]
+then
+   CODENAME=`lsb_release -c -s`
+fi
+wget -O - https://nuitka.net/deb/archive.key.gpg | sudo apt-key add -
+sudo apt-get install ca-certificates
+sudo echo >/etc/apt/sources.list.d/nuitka.list "deb https://nuitka.net/deb/stable/$CODENAME $CODENAME main"
+sudo apt-get update
+sudo apt-get install nuitka
