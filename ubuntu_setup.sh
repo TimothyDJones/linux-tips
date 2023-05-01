1c9d8b427124becbb7565a8b7ec3a30681380725
Tim Jones <tdjones74021@yahoo.com>
2023-05-01 08:33:58 -0500

    Update Muffon Electron/Vue.JS-based desktop music-streaming service player to version 1.5.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -36806,7 +36806,7 @@ sudo rm -rf /tmp/${APP_NAME}* /tmp/${APP_NAME,,}*
 # Install Muffon Electron/Vue.JS-based desktop music-streaming service player from Debian package
 APP_NAME=Muffon
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
-APP_VERSION=0.1.0
+APP_VERSION=1.5.0
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}.${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/staniel359/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
