7be5c9005411a1351073890b5d776db67688388b
Tim Jones <tdjones74021@yahoo.com>
2023-05-02 15:17:30 -0500

    Update Shutter Encoder cross-platform audio/video encoder/converter to version 17.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -23974,7 +23974,7 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 APP_NAME="Shutter Encoder"
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross-platform audio/video encoder/converter."
-APP_VERSION=17.0
+APP_VERSION=17.1
 APP_EXT=deb
 FILE_NAME=${APP_NAME// /%20}%20${APP_VERSION}%20Linux%2064bits
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_APP_NAME}/${FILE_NAME}.${APP_EXT}
