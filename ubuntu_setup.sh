e0449779c6af432d853d9fd93cc1110db806608a
Tim Jones <tdjones74021@yahoo.com>
2023-05-03 15:56:11 -0500

    Add commands to install Ellas War Electron-based Ancient Greek-themed role-playing game from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -41403,3 +41403,36 @@ sudo cp -a /tmp/${FILE_NAME}/completions/${_APP_NAME}.bash /usr/share/bash-compl
 sudo ln -s -f /usr/local/bin/soft /usr/local/bin/${_APP_NAME}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Ellas War Electron-based Ancient Greek-themed role-playing game from AppImage
+APP_NAME="Ellas War"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Electron-based Ancient Greek-themed role-playing game."
+APP_GUI_CATEGORIES="Games;Entertainment;"
+APP_GUI_KEYWORDS="History;Games;RPG;"
+APP_VERSION=1.3.5
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME// /}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/V-Paranoiaque/${APP_NAME// /-}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.png -J -L https://raw.githubusercontent.com/V-Paranoiaque/${APP_NAME// /-}/master/res/electron/icon.png
+sudo cp -a /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${_APP_NAME}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${_APP_NAME}.png /usr/local/share/icons/${_APP_NAME}.png
+cat > /tmp/${_APP_NAME}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${_APP_NAME}
+Icon=/usr/local/share/icons/${_APP_NAME}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME// /}* /tmp/${APP_NAME// /-}*
