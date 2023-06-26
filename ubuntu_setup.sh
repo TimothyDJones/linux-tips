eae17981831b6037ac647b6eb9d00e9ef63f230b
Tim Jones <tdjones74021@yahoo.com>
2023-06-26 11:11:58 -0500

    Add commands to install Projectable cross‐platform, Rust-based development-focused command-line file manager from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -42092,3 +42092,19 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install Projectable cross‐platform, Rust-based development-focused command-line file manager from package
+APP_NAME=Projectable
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross‐platform, Rust-based development-focused command-line file manager."
+APP_VERSION=1.0.0
+APP_EXT=tar.gz
+BASE_ARCH=
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-musl
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/dzfrias/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/prj /usr/local/bin
+sudo ln -s -f /usr/local/bin/prj /usr/local/bin/${APP_NAME,,}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
