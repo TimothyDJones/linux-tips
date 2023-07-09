0e53657de94591e52d6bb633a62892744ba3873a
Tim Jones <tdjones74021@yahoo.com>
2023-07-09 15:49:33 -0500

    Update Tea Deno-based next-generation, cross‐platform package management tool to version 0.37.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -42025,7 +42025,7 @@ rm -rf /tmp/${APP_NAME,,}*
 APP_NAME=Tea
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Deno-based next-generation, cross‐platform package management tool."
-APP_VERSION=0.35.8
+APP_VERSION=0.37.0
 APP_EXT=tar.xz
 BASE_ARCH=$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}+linux+${BASE_ARCH//_/-}
