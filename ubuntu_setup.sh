30c01d44009640c81a94248289039f71b4d0c944
Tim Jones <tdjones74021@yahoo.com>
2023-07-11 13:00:32 -0500

    Update Nyxt cross-platform, keyboard-oriented web browser with Vim and Emac keybindings to version 3.4.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -30908,7 +30908,7 @@ APP_NAME=Nyxt
 APP_GUI_NAME="Cross-platform, keyboard-oriented web browser with Vim and Emac keybindings."
 APP_GUI_CATEGORIES="Internet;"
 APP_GUI_KEYWORDS="Browser;Vim;Emacs;Lisp;"
-APP_VERSION=3.3.0
+APP_VERSION=3.4.0
 APP_EXT=tar.xz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/atlas-engineer/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
