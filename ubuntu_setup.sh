e5b7c59e830f87b014107a904e1dc5aec2bf3e60
Tim Jones <tdjones74021@yahoo.com>
2023-07-13 12:32:59 -0500

    Add commands to install Git Credential Manager (GCM) cross‐platform, C#/.NET-based tool to manage credentials for GitHub and other Git hosts from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -42377,3 +42377,20 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Git Credential Manager (GCM) cross‐platform, C#/.NET-based tool to manage credentials for GitHub and other Git hosts from package
+APP_NAME="Git Credential Manager"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross‐platform, C#/.NET-based tool to manage credentials for GitHub and other Git hosts."
+APP_VERSION=2.2.2
+APP_EXT=tar.gz
+FILE_NAME=gcm-linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU).${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_APP_NAME}.mirror/${FILE_NAME}.${APP_EXT}
+cd /tmp
+mkdir -p /tmp/${FILE_NAME}
+tar xzf /tmp/${FILE_NAME}.${APP_EXT} -p -C /tmp/${FILE_NAME}
+sudo cp -a /tmp/${FILE_NAME}/${_APP_NAME} /usr/local/bin
+sudo cp -a /tmp/${FILE_NAME}/lib*.so /usr/lib
+sudo ln -s -f /usr/local/bin/${_APP_NAME} /usr/local/bin/gcm
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
