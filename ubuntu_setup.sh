8baf2580af986ef8ed29ffe5658bd648cfeab767
Tim Jones <tdjones74021@yahoo.com>
2023-07-25 12:22:58 -0500

    Add commands to install GoldenDict++OCR cross-platform enhanced dictionary tool with built-in OCR capability from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -42687,3 +42687,45 @@ sudo cp -R /tmp/${FILE_NAME}/bin/* /usr/bin
 sudo cp -R /tmp/${FILE_NAME}/share/* /usr/share
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install GoldenDict++OCR cross-platform enhanced dictionary tool with built-in OCR capability from package
+APP_NAME=GoldenDict
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform enhanced dictionary tool with built-in OCR capability."
+APP_GUI_CATEGORIES="Accessories;Office;"
+APP_GUI_KEYWORDS="Dictionary;OCR;"
+APP_VERSION=3E1CB
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME}++OCR-${APP_VERSION}-Qt-4.8.7-p5-Debian-12.1-x64-20230725
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}-pro/${FILE_NAME}.${APP_EXT}
+cd /tmp
+sudo tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -a -R /tmp/${APP_NAME}/* /opt/${APP_NAME,,}
+sudo chmod +x /opt/${APP_NAME,,}/${APP_NAME,,}.sh /opt/${APP_NAME,,}/${APP_NAME}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+/opt/${APP_NAME,,}/${APP_NAME,,}.sh &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${APP_NAME,,}.sh
+Icon=/opt/${APP_NAME,,}/splash.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
