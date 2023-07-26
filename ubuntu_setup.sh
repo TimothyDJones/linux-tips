99122949e49bfff19ac37240d0fed255ad0ac245
Tim Jones <tdjones74021@yahoo.com>
2023-07-26 11:20:57 -0500

    Update Hexyl Rust-based hex viewer for the terminal to version 0.13.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -16789,7 +16789,7 @@ rm -rf /tmp/${APP_NAME,,}*
 # Install Hexyl Rust-based hex viewer for the terminal from Debian package
 APP_NAME=Hexyl
 APP_GUI_NAME="Rust-based hex viewer for the terminal."
-APP_VERSION=0.12.0
+APP_VERSION=0.13.1
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_amd64
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/sharkdp/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
