9e5c9ef10b5ae1129fec785ad6b85a112269365d
Tim Jones <tdjones74021@yahoo.com>
2023-07-31 09:55:55 -0500

    Update PICSimLab wxWidgets-based real-time PIC and Arduino microcontroller simulator laboratory to version 0.9.0_230729 and to install AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7967,16 +7967,38 @@ sudo chmod a+x ${WWW_HOME}/${APP_NAME,,}
 sudo chmod -R a+r ${WWW_HOME}/${APP_NAME,,}
 xdg-open http://localhost/${APP_NAME,,}/index.php &
 
-# Install PICSimLab wxWidgets-based real-time PIC and Arduino microcontroller simulator laboratory from package
+# Install PICSimLab wxWidgets-based real-time PIC and Arduino microcontroller simulator laboratory from AppImage
 APP_NAME=PICSimLab
 APP_GUI_NAME="Cross-platform real-time PIC and Arduino microcontroller simulator laboratory."
-APP_VERSION=0.8.12_230120
-APP_EXT=deb
-FILE_NAME=${APP_NAME,,}_${APP_VERSION}_Ubuntu_20.04.5_LTS_amd64
+APP_GUI_CATEGORIES="Electronics;Engineering;"
+APP_GUI_KEYWORDS="PIC;Arduino;Microcontroller;Simulation;"
+APP_VERSION=0.9.0_230729
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-${APP_VERSION}_Linux_$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+sudo apt install -y libfuse2
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/picsim/${FILE_NAME}.${APP_EXT}
-sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://a.fsdn.com/allura/p/picsim/icon?1690651145
+sudo cp -a /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
-sudo rm -rf /tmp/${APP_NAME,,}*
+rm -rf /tmp/${APP_NAME,,}*
 
 # Install x-whnb self-contained web-based hierarchical notebook (similar to Cherrytree)
 APP_NAME=x-whnb
