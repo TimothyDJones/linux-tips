6a0f44622ffdaa2a58fc5b9276569a5e91cfcd4c
Tim Jones <tdjones74021@yahoo.com>
2023-08-04 07:29:45 -0500

    Update Sommelier installer for Windows applications to run under Wine emulator to version 8.32.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25949,7 +25949,7 @@ APP_NAME=Sommelier
 APP_GUI_NAME="Installer for Windows applications to run under Wine emulator."
 APP_GUI_CATEGORIES="Accessories;System;"
 APP_GUI_KEYWORDS="Windows;"
-APP_VERSION=8.14
+APP_VERSION=8.32
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/ColumPaget/${APP_NAME}/archive/v${APP_VERSION}.${APP_EXT}
