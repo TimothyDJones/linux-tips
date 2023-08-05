58a27e98bc82d774469702b3213aecc8cf0895fa
Tim Jones <tdjones74021@yahoo.com>
2023-08-05 17:01:23 -0500

    Add commands to install IPTracker cross-platform, Electron-based PHP/Laravel GUI for tracking external IP address from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -43052,3 +43052,37 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install IPTracker cross-platform, Electron-based PHP/Laravel GUI for tracking external IP address from AppImage
+APP_NAME=IPTracker
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform, Electron-based PHP/Laravel GUI for tracking external IP address."
+APP_GUI_CATEGORIES="Networking;System;"
+APP_GUI_KEYWORDS="IP;System;"
+APP_VERSION=1.1.2
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-${APP_VERSION}
+sudo apt install -y libfuse2
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/modernben/${APP_NAME}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/modernben/iptracker/main/public/menuBarIconTemplate%402x.png
+sudo cp -a /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
