18717141c5949ce47d8064a54d4be572ca596cc9
Tim Jones <tdjones74021@yahoo.com>
2023-08-05 15:14:38 -0500

    Add commands to install Surrealist cross-platform, Electron-based GUI for SurrealDB NewSQL database from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -43018,3 +43018,37 @@ sudo cp -a /tmp/${_APP_NAME//db/} /usr/local/bin
 sudo ln -s -f /usr/local/bin/${_APP_NAME//db/} /usr/local/bin/${_APP_NAME}
 cd $HOME
 sudo rm -rf /tmp/${_APP_NAME//db/}*
+
+# Install Surrealist cross-platform, Electron-based GUI for SurrealDB NewSQL database from AppImage
+APP_NAME=Surrealist
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform, Electron-based GUI for SurrealDB NewSQL database."
+APP_GUI_CATEGORIES="Development;Programming;"
+APP_GUI_KEYWORDS="NoSQL;Database;NewSQL;"
+APP_VERSION=1.7.5
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME,,}-v${APP_VERSION}-linux
+sudo apt install -y libfuse2
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/StarlaneStudios/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.ico -J -L https://raw.githubusercontent.com/StarlaneStudios/${APP_NAME}/main/public/favicon.ico
+sudo cp -a /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.ico /usr/local/share/icons/${APP_NAME,,}.ico
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.ico
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
