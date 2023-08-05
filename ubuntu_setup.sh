4f017ef8aeb339a7cd556df9e5d30a762974521b
Tim Jones <tdjones74021@yahoo.com>
2023-08-05 07:30:29 -0500

    Update Admidio web-based (PHP/MySQL) organizational management tool to version 4.2.11.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4290,7 +4290,7 @@ xdg-open http://localhost/${APP_NAME,,}/dependent/admin/install/index.php &
 # Install Admidio web-based (PHP/MySQL) organizational management tool from package
 APP_NAME=Admidio
 APP_GUI_NAME="Web-based (PHP/MySQL) organizational management tool."
-APP_VERSION=4.2.10
+APP_VERSION=4.2.11
 APP_EXT=zip
 APP_GUI_CATEGORIES="Office;"
 APP_GUI_KEYWORDS="Organization;Management;"
