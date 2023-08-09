a94119e11887cae12b938de5da5121183347d733
Tim Jones <tdjones74021@yahoo.com>
2023-08-09 09:41:22 -0500

    Update F2 cross-platform, command-line batch file renamer to version 1.9.1 and to install from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -31042,13 +31042,15 @@ sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
 
-# Install F2 cross-platform, command-line batch file renamer from Debian package
+# Install F2 cross-platform, command-line batch file renamer from package
 APP_NAME=F2
-APP_VERSION=1.6.7
-APP_EXT=deb
+APP_VERSION=1.9.1
+APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/ayoisaiah/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
-sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
 
