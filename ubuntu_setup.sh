dd60eca1a3af0fdac3faf35e04061b92244c9c5a
Tim Jones <tdjones74021@yahoo.com>
2023-08-09 09:26:37 -0500

    Update install process for LazyGit cross-platform Golang-based shell GUI for Git.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -13273,7 +13273,7 @@ APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_Linux_$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
 cd /tmp
-dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
 sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
 sudo chmod +x /usr/local/bin/${APP_NAME,,}
 cd $HOME
