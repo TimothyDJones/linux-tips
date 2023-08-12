17942551cba8694a0c75f6d179442d119640365e
Tim Jones <tdjones74021@yahoo.com>
2023-08-12 10:57:27 -0500

    Update DeepSearch5Plus cross-platform Python/Qt-based recursive search tool that can search inside files to version 1.2.3.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -42430,7 +42430,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
 APP_GUI_NAME="Cross-platform Python/Qt-based recursive search tool that can search inside files."
 APP_GUI_CATEGORIES="Accessories;System;"
 APP_GUI_KEYWORDS="Search;File Manager;"
-APP_VERSION=1.2.0
+APP_VERSION=1.2.3
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-v${APP_VERSION}_linux_x32_64
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
