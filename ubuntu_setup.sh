8d2dae34faac9359252377701c776d6feacead09
Tim Jones <tdjones74021@yahoo.com>
2023-08-16 07:53:21 -0500

    Update Vivaldi web browser (stable version) to version 6.1.3035.302.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -556,7 +556,7 @@ sudo apt-get install -y atom
 
 # Install Vivaldi web browser (stable version) from Debian package
 APP_NAME=Vivaldi
-APP_VERSION=6.1.3035.257
+APP_VERSION=6.1.3035.302
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}-stable_${APP_VERSION}-1_${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.vivaldi.com/stable/${FILE_NAME}.${APP_EXT}
