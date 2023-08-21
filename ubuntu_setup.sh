b3024d748ecbe948aaad82e6b2cb41246f944fac
Tim Jones <tdjones74021@yahoo.com>
2023-08-21 07:17:34 -0500

    Add commands to install Oneko cross-platform, Java-based screen toy of cat that follows mouse cursor from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -43286,3 +43286,44 @@ APP_NAME=Wildcard
 sudo apt-get install -y flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.felipekinoshita.${APP_NAME}
+
+# Install Oneko cross-platform, Java-based screen toy of cat that follows mouse cursor from package
+# https://github.com/glreno/oneko
+APP_NAME=Oneko
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform, Java-based screen toy of cat that follows mouse cursor."
+APP_GUI_CATEGORIES="Accessories;System;"
+APP_GUI_KEYWORDS="Toy;Fun;Cat;"
+APP_VERSION=2.0.1
+APP_EXT=jar
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://glreno.github.io/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.gif -J -L https://raw.githubusercontent.com/glreno/oneko/master/src/main/resources/images/26.GIF
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -a -R /tmp/${APP_NAME,,}*.* /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT} &
+cd \$HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/${APP_NAME,,}.gif
+Type=Application
+StartupNotify=true
+Terminal=false
+Category=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
