79b8ddc5466af49817a4bb63a951eb1ec8ab8838
Tim Jones <tdjones74021@yahoo.com>
2023-08-24 07:57:05 -0500

    Update HTTPie Python-based human-friendly command-line HTTP client to version 3.2.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -36431,7 +36431,7 @@ sudo rm -rf /tmp/${_APP_NAME}*
 # Install HTTPie Python-based human-friendly command-line HTTP client from Debian package
 APP_NAME=HTTPie
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
-APP_VERSION=3.2.1
+APP_VERSION=3.2.2
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
