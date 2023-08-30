9ff6ab4887412ea33b86c79f1bf04b14e017da09
Tim Jones <tdjones74021@yahoo.com>
2023-08-30 07:14:15 -0500

    Update Insomnia cross-platform REST client to version 2023.5.7.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -4365,7 +4365,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross-platform REST client."
 APP_GUI_CATEGORIES="Programming;Development;"
 APP_GUI_KEYWORDS="REST;Web Services;"
-APP_VERSION=2023.5.6
+APP_VERSION=2023.5.7
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME}.Core-${APP_VERSION}
 sudo apt install -y libfuse2
