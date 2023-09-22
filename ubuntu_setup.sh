c6ed6818d8d4bdf65f71efc2f144756ddaab5c26
Tim Jones <tdjones74021@yahoo.com>
2023-09-22 11:15:17 -0500

    Update Hilbish Golang-based minimalist shell for Lua fans to version 2.1.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -40142,7 +40142,7 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # Install Hilbish Golang-based minimalist shell for Lua fans from package
 APP_NAME=Hilbish
 APP_GUI_NAME="Golang-based minimalist shell for Lua fans."
-APP_VERSION=2.0.1
+APP_VERSION=2.1.2
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-v${APP_VERSION}-linux-$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/Rosettea/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
