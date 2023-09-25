d5490e8ae7a35f249afad94ff93b76896cde09ce
Tim Jones <tdjones74021@yahoo.com>
2023-09-25 07:39:10 -0500

    Update System Monitoring Center Python/Gtk system performance and usage monitoring tool to version 2.22.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -32531,7 +32531,7 @@ rm -rf /tmp/${APP_NAME,,}*
 # Install System Monitoring Center Python/Gtk system performance and usage monitoring tool from Debian package
 # https://kod.pardus.org.tr/Hakan/system-monitoring-center
 APP_NAME="System Monitoring Center"
-APP_VERSION=1.25.0
+APP_VERSION=2.22.2
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_EXT=deb
 FILE_NAME=${_APP_NAME}_${APP_VERSION}_all
