7804c1a59caa64be94da3904fdd037045cd3380d
Tim Jones <tdjones74021@yahoo.com>
2023-10-07 10:27:00 -0500

    Update Penguin's Eggs command-line utility to create live CD ISO from current Debian/Ubuntu system to version 9.6.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -24431,7 +24431,7 @@ sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 # Install Penguin's Eggs command-line utility to create live CD ISO from current Debian/Ubuntu system from Debian package
 APP_NAME=Eggs
 APP_GUI_NAME="Command-line utility to create live CD ISO from current Debian/Ubuntu system."
-APP_VERSION=9.6.1
+APP_VERSION=9.6.2
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/penguins-eggs/${FILE_NAME}.${APP_EXT}
