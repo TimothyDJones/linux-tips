fd9cf2da6fbf0a41c6fda47b402eb2dc154ff76f
Tim Jones <tdjones74021@yahoo.com>
2023-10-19 06:26:28 -0500

    Update Jailer cross-platform Java database browser and editor to version 15.2.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1053,7 +1053,7 @@ rm -rf /tmp/${APP_NAME,,}*
 # Install Jailer cross-platform Java database browser and editor from package
 APP_NAME=Jailer
 APP_GUI_NAME="Cross-platform Java database browser and editor"
-APP_VERSION=15.2.3
+APP_VERSION=15.2.4
 APP_EXT=zip
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
