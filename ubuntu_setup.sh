68a2ae5242d9019cde9faee8a20b6868565749ff
Tim Jones <tdjones74021@yahoo.com>
2023-10-25 07:06:42 -0500

    Update Robocode Java-based tank battle programming game to version 1.9.5.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -22874,7 +22874,7 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # Install Robocode Java-based tank battle programming game from package
 APP_NAME=Robocode
 APP_GUI_NAME="Java-based tank battle programming game."
-APP_VERSION=1.9.5.1
+APP_VERSION=1.9.5.2
 APP_EXT=jar
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-setup
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
