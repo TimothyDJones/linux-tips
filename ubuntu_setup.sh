27741c394558bc4385ae84c7eeaeed068274ddda
Tim Jones <tdjones74021@yahoo.com>
2023-11-07 07:29:37 -0600

    Add commands to install Qwiktape paper-tape style calculator supporting annotations from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -44103,3 +44103,38 @@ sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
 sudo cp -R /tmp/${FILE_NAME}/completions/${APP_NAME,,}.bash /usr/share/bash-completion/completions
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Qwiktape paper-tape style calculator supporting annotations from package
+APP_NAME=Qwiktape
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Paper-tape style calculator supporting annotations."
+APP_GUI_CATEGORIES="Accessories;"
+APP_GUI_KEYWORDS="Calculator;Productivity;"
+APP_VERSION=0.6
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}-v${APP_VERSION}-linux-portable
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://a.fsdn.com/allura/p/${APP_NAME,,}/icon?b24f9a525f2712f128376f6b520fa43f26d6a4054f3599eb51d9f02954819dcd
+cd /tmp
+unzip /tmp/${FILE_NAME}.${APP_EXT} -d /tmp/${FILE_NAME}
+sudo cp -a -R /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+sudo cp -a -R /tmp/${FILE_NAME}/sciter-pdf.so /lib
+sudo chmod +x /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
