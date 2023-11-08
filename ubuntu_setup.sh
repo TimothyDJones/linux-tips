458f78056392c5fb44494a18a03282c91faf2abe
Tim Jones <tdjones74021@yahoo.com>
2023-11-08 07:04:42 -0600

    Update Spyder cross-platform Qt-based Python IDE to version 5.5.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -31058,7 +31058,7 @@ APP_NAME=Spyder
 APP_GUI_NAME="Cross-platform Qt-based Python IDE."
 APP_GUI_CATEGORIES="Programming;Development;"
 APP_GUI_KEYWORDS="Python;IDE;Editor;"
-APP_VERSION=5.4.5
+APP_VERSION=5.5.0
 mkdir -p $HOME/.local/bin/${APP_NAME,,}
 cd $HOME/.local/bin/${APP_NAME,,}
 python3 -m venv $HOME/.local/bin/${APP_NAME,,}/.venv
