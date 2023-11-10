f4f4eb403ec9305e1a91cb9a5ab02e5d833493d7
Tim Jones <tdjones74021@yahoo.com>
2023-11-10 07:11:04 -0600

    Update trdsql Golang-based command-line tool to execute SQL queries on CSV, LTSV, JSON and TBLN data to version 0.13.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -40510,7 +40510,7 @@ sudo rm -rf /tmp/${_APP_NAME}*
 APP_NAME=trdsql
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Golang-based command-line tool to execute SQL queries on CSV, LTSV, JSON and TBLN data."
-APP_VERSION=0.12.1
+APP_VERSION=0.13.0
 APP_EXT=zip
 FILE_NAME=${APP_NAME,,}_v${APP_VERSION}_linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
