4877fc761116daa8f44b18597818e4b4c6c49807
Tim Jones <tdjones74021@yahoo.com>
2023-11-15 07:56:34 -0600

    Update dateutils command-line date and time utilities, including calculation and conversion to version 0.4.10.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -31792,7 +31792,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 # Install dateutils command-line date and time utilities, including calculation and conversion, from source
 APP_NAME=dateutils
 APP_GUI_NAME="Command-line date and time utilities, including calculation and conversion."
-APP_VERSION=0.4.9
+APP_VERSION=0.4.10
 APP_EXT=tar.xz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get install -y build-essential
