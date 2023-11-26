9999fa3f713e951c262636d26d5e65473f7499aa
Tim Jones <tdjones74021@yahoo.com>
2023-11-26 16:31:09 -0600

    Update Converseen Qt-based bulk image converting/resizing tool to version 0.12.0.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -13470,7 +13470,7 @@ rm -rf /tmp/*${APP_NAME}*
 # Install Converseen Qt-based bulk image converting/resizing tool from source
 APP_NAME=Converseen
 APP_GUI_NAME="Qt-based bulk image converting/resizing tool."
-APP_VERSION=0.11.0.1
+APP_VERSION=0.12.0.0
 APP_EXT=tar.bz2
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get install -y libmagick++-dev cmake qttools5-dev-tools qttools5-dev
