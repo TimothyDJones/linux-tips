6c0e66886d2fec0e1e8818c8e38ab489a82fcb29
Tim Jones <tdjones74021@yahoo.com>
2023-11-27 07:24:52 -0600

    Update Milkman cross-platform JavaFX-based REST API client to version 5.7.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -19916,7 +19916,7 @@ rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # Install Milkman cross-platform JavaFX-based REST API client from package
 APP_NAME=Milkman
 APP_GUI_NAME="Cross-platform JavaFX-based REST API client."
-APP_VERSION=5.0.0
+APP_VERSION=5.7.1
 APP_EXT=tgz
 FILE_NAME=${APP_NAME,,}-dist-linux64-bin
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/warmuuh/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
