9f2dbc17d959b6b1a5005fa5fc1854927262e373
Tim Jones <tdjones74021@yahoo.com>
2023-11-27 06:56:22 -0600

    Add commands to install Tess cross-platform, open-source Electron-based simple rapid terminal from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -44361,3 +44361,37 @@ sudo cp -R /tmp/${FILE_NAME}/completions/${APP_NAME,,}.bash /usr/share/bash-comp
 sudo cp -R /tmp/${FILE_NAME}/manpages/${APP_NAME,,}.* /usr/share/man/man1
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Tess cross-platform, open-source Electron-based simple rapid terminal from AppImage
+APP_NAME=Tess
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Cross-platform, open-source Electron-based simple rapid terminal."
+APP_GUI_CATEGORIES="Accessories;System;"
+APP_GUI_KEYWORDS="Terminal;Shell;"
+APP_VERSION=0.6.3
+APP_EXT=AppImage
+sudo apt install -y libfuse2
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/SquitchYT/${APP_NAME}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/SquitchYT/${APP_NAME}/main/build/ressources/icon/icon.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
