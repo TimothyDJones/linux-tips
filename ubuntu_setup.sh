995b7430db0ee24a3bfcf2c3cc3eba721d64c05f
Tim Jones <tdjones74021@yahoo.com>
2023-12-15 08:16:14 -0600

    Add commands to install PikaTorrent cross-platform minimalist BitTorrent client from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -44652,3 +44652,35 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install PikaTorrent cross-platform minimalist BitTorrent client from package
+APP_NAME=PikaTorrent
+APP_GUI_NAME="Cross-platform minimalist BitTorrent client."
+APP_GUI_CATEGORIES="Internet;"
+APP_GUI_KEYWORDS="BitTorrent;"
+APP_VERSION=0.9.0
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}-linux-x64-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/G-Ray/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+unzip -q /tmp/${FILE_NAME}.${APP_EXT} -d /tmp/${FILE_NAME}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -a -R /tmp/${FILE_NAME}/${APP_NAME,,}*/* /opt/${APP_NAME,,}
+sudo ln -s -f /opt/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/opt/${APP_NAME,,}/${APP_NAME}
+Icon=/opt/${APP_NAME,,}/resources/app/assets/icon.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}
