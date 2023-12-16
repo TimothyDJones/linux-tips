eccdb6b1d8e945a701b132728da62c364ce8fc40
Tim Jones <tdjones74021@yahoo.com>
2023-12-16 11:28:35 -0600

    Update Mattermost cross-platform, Electron-based workplace messaging alternative to Slack to version 5.6.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -18390,7 +18390,7 @@ sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 # Install Mattermost cross-platform, Electron-based workplace messaging alternative to Slack from Debian package
 APP_NAME=Mattermost
 APP_GUI_NAME="Cross-platform, Electron-based workplace messaging alternative to Slack."
-APP_VERSION=5.5.0
+APP_VERSION=5.6.0
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}-desktop-${APP_VERSION}-linux-${ARCH_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://releases.mattermost.com/desktop/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
