c9b33a0a2517cbcf4d282799ba4d8ac49e0fe8f3
Tim Jones <tdjones74021@yahoo.com>
2023-12-21 08:47:55 -0600

    Update neoeedit Java-based smart, light, powerful text editor to version 384.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -37651,7 +37651,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Java-based smart, light, powerful text editor."
 APP_GUI_CATEGORIES="Programming;Development;"
 APP_GUI_KEYWORDS="Editor;"
-APP_VERSION=380rc4
+APP_VERSION=384
 APP_EXT=jar
 FILE_NAME=${APP_NAME,,}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/neoedmund/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
