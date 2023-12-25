b6d54cd37d44475eb04d476bc83336118b956ce5
Tim Jones <tdjones74021@yahoo.com>
2023-12-25 08:40:53 -0600

    Update Joplin cross-platform notepad and "To Do" list tool to version 2.13.11.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -14492,7 +14492,7 @@ rm -rf /tmp/*${APP_NAME}*
 # Install Joplin cross-platform notepad and "To Do" list tool from AppImage
 APP_NAME=Joplin
 APP_GUI_NAME="Cross-platform notepad and \"To Do\" list tool."
-APP_VERSION=2.13.9
+APP_VERSION=2.13.11
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME}-${APP_VERSION}-x86_64
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/laurent22/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
