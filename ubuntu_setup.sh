05cf5e615b43dd7b477664a2b310e91a9dd725d6
Tim Jones <tdjones74021@yahoo.com>
2023-12-30 07:50:37 -0600

    Update DrawIO Electron-based desktop diagramming and whiteboarding tool to version 22.1.16.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -35813,7 +35813,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Electron-based desktop diagramming and whiteboarding tool."
 APP_GUI_CATEGORIES="Office;Programming;Development;"
 APP_GUI_KEYWORDS="Diagramming;Drawing;"
-APP_VERSION=22.1.15
+APP_VERSION=22.1.16
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME,,}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}-desktop.mirror/${FILE_NAME}.${APP_EXT}
