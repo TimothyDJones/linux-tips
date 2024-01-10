07ddeb28bf0d2607086db4deeb6d45d455490b09
Tim Jones <tdjones74021@yahoo.com>
2024-01-10 07:29:26 -0600

    Update Nushell Rust-based minimalist shell to version 0.89.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -29131,7 +29131,7 @@ sudo rm -rf /tmp/*${APP_NAME,,}* /tmp/${APP_NAME}
 # Install Nushell Rust-based minimalist shell from package
 APP_NAME=nu
 APP_GUI_NAME="Rust-based minimalist shell."
-APP_VERSION=0.88.1
+APP_VERSION=0.89.0
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/nushell/nushell/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
