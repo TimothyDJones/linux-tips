f177094d00bb730bd33d99867622b49347a7d210
Tim Jones <tdjones74021@yahoo.com>
2024-01-22 06:26:31 -0600

    Update Pentobi Blokus-style board game to version 24.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3932,7 +3932,7 @@ xdg-open http://localhost/${APP_NAME,,}/index.php &
 
 # Install Pentobi Blokus-style board game from source
 APP_NAME=pentobi
-APP_VERSION=23.1
+APP_VERSION=24.0
 APP_EXT=tar.xz
 sudo apt-get install -y g++ make cmake qttools5-dev qttools5-dev-tools libqt5svg5-dev
 curl -o /tmp/${APP_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME}/${APP_NAME}-${APP_VERSION}.${APP_EXT}
