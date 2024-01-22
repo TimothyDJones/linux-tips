0228389fc33f81d2c841598b504330d6244cac56
Tim Jones <tdjones74021@yahoo.com>
2024-01-22 06:36:42 -0600

    Update XSchem circuit schematic editor to version 3.4.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -2875,7 +2875,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install XSchem circuit schematic editor from source
 APP_NAME=XSchem
-APP_VERSION=3.4.1
+APP_VERSION=3.4.4
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get install -y bison flex libxpm-dev libx11-dev tcl8.6-dev tk8.6-dev
