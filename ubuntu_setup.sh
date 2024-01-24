af7c31193b186e8f153b9c433f9775081adf35e3
Tim Jones <tdjones74021@yahoo.com>
2024-01-23 19:37:03 -0600

    Add commands to install IPFS Desktop cross-platform, Electron-based IPFS desktop client from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -45142,3 +45142,37 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install IPFS Desktop cross-platform, Electron-based IPFS desktop client from AppImage
+APP_NAME="IPFS Desktop"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform, Electron-based IPFS desktop client."
+APP_GUI_CATEGORIES="Internet;"
+APP_GUI_KEYWORDS="IPFS;P2P;"
+APP_VERSION=0.33.0
+APP_EXT=AppImage
+sudo apt install -y libfuse2
+FILE_NAME=${_APP_NAME}-${APP_VERSION}-linux-x86_64
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_APP_NAME}.mirror/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.png -J -L https://a.fsdn.com/allura/mirror/${_APP_NAME}/icon?5d36763ecb910114c93caa234ec4a39743445a4c61f4172f6740509e13683414
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${_APP_NAME}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${_APP_NAME}.png /usr/local/share/icons/${_APP_NAME}.png
+cat > /tmp/${_APP_NAME}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${_APP_NAME}
+Icon=/usr/local/share/icons/${_APP_NAME}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${_APP_NAME}*
