cc5ebb73b83432942c46a37e218fd5936e19dd1b
Tim Jones <tdjones74021@yahoo.com>
2024-01-27 11:11:31 -0600

    Add commands to install qView cross-platform, Qt-based minimalist image viewer from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -45285,3 +45285,37 @@ cd /tmp/${FILE_NAME}
 sudo make install /tmp/${FILE_NAME}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}
+
+# Install qView cross-platform, Qt-based minimalist image viewer from AppImage
+APP_NAME=qView
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Cross-platform, Qt-based minimalist image viewer."
+APP_GUI_CATEGORIES="Graphics;"
+APP_GUI_KEYWORDS="Viewer;"
+APP_VERSION=6.1
+APP_EXT=AppImage
+sudo apt install -y libfuse2
+FILE_NAME=${APP_NAME}-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/jurplel/${APP_NAME}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/jurplel/${APP_NAME}/master/dist/linux/hicolor/128x128/apps/com.interversehq.qView.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
