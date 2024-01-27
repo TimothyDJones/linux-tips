85f39705035493673cdafadd5080a177a7f33170
Tim Jones <tdjones74021@yahoo.com>
2024-01-27 11:42:38 -0600

    Update jDiskMark cross-platform Java-based GUI disk benchmarking tool to version 0.5-beta-3.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -45040,7 +45040,7 @@ APP_NAME=jDiskMark
 APP_GUI_NAME="Cross-platform Java-based GUI disk benchmarking tool."
 APP_GUI_CATEGORIES="Accessories;System;"
 APP_GUI_KEYWORDS="Disk;Performance;"
-APP_VERSION=0.5-beta-2
+APP_VERSION=0.5-beta-3
 APP_EXT=zip
 FILE_NAME=${APP_NAME,,}-v${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
