79e1c1dffc10906befb0649b557bf2c2a6747429
Tim Jones <tdjones74021@yahoo.com>
2024-02-02 18:33:50 -0600

    Update PET cross-platform command-line snippet manager to version 0.6.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25367,14 +25367,9 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 
 # Install PET cross-platform command-line snippet manager from Debian package
 APP_NAME=PET
-APP_VERSION=0.4.0
+APP_VERSION=0.6.0
 APP_EXT=deb
-if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
-	ARCH_TYPE=amd64
-else    # Otherwise use version for 32-bit kernel
-	ARCH_TYPE=386
-fi
-FILE_NAME=${APP_NAME,,}_${APP_VERSION}_linux_${ARCH_TYPE}
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/knqyf263/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
