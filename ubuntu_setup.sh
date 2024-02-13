dc6f10ce8e4348f9b6ee42f6efccabca6a1b39f9
Tim Jones <tdjones74021@yahoo.com>
2024-02-13 09:38:55 -0600

    Update rqlite distributed version of SQLite database to version 8.20.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -38265,7 +38265,7 @@ rm -rf /tmp/${APP_NAME}* /tmp/${APP_NAME,,}*
 APP_NAME=rqlite
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
 APP_GUI_NAME="Distributed version of SQLite database."
-APP_VERSION=8.19.0
+APP_VERSION=8.20.0
 FILE_NAME=${APP_NAME,,}-v${APP_VERSION}-linux-$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
 cd /tmp
