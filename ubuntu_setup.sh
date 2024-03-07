e2c6388f0880ed216dbf0253118d9d72c65c66e6
Tim Jones <tdjones74021@yahoo.com>
2024-03-07 15:51:31 -0600

    Update Joker cross‐platform, Golang-based simple Clojure interpreter, linter and formatter to version 1.3.5.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -42754,7 +42754,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 APP_NAME=Joker
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross‐platform, Golang-based simple Clojure interpreter, linter and formatter."
-APP_VERSION=1.3.4
+APP_VERSION=1.3.5
 APP_EXT=zip
 FILE_NAME=${APP_NAME,,}-linux-$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
