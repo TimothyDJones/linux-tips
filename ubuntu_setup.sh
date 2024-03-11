8cab2cc6ff14758b63803c44557a45eff9a1401d
Tim Jones <tdjones74021@yahoo.com>
2024-03-11 08:03:42 -0500

    Update Rukovoditel web-based (PHP/MySQL) project management tool to version 3.5.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -14633,7 +14633,7 @@ rm -rf /tmp/*${APP_NAME}*
 # Install Rukovoditel web-based (PHP/MySQL) project management tool from package
 APP_NAME=Rukovoditel
 APP_GUI_NAME="Web-based (PHP/MySQL) project management tool."
-APP_VERSION=3.5.1
+APP_VERSION=3.5.2
 APP_EXT=zip
 DB_NAME=${APP_NAME,,}
 DB_USER=${APP_NAME,,}
