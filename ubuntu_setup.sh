ed5cf791d8e8a0aedca4ba73f2c08f60a62d7698
Tim Jones <tdjones74021@yahoo.com>
2024-03-28 19:03:23 -0500

    Update xsnow desktop decoration to version 3.7.9.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -19632,7 +19632,7 @@ rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # Install xsnow desktop decoration from source
 APP_NAME=xsnow
 APP_GUI_NAME=""
-APP_VERSION=3.7.8
+APP_VERSION=3.7.9
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}-1_${KERNEL_TYPE}
 sudo apt install -y build-essential libgtk-3-dev libxt-dev libgsl-dev libxpm-dev
