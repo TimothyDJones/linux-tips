afa8c97dfe7b1094c765b18073d07e0ab1006257
Tim Jones <tdjones74021@yahoo.com>
2024-04-02 15:03:35 -0500

    Update PDFGrepGUI simple GUI for pdfgrep for text search inside PDFs to version 0.9.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -37725,9 +37725,9 @@ rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # Install PDFGrepGUI simple GUI for pdfgrep for text search inside PDFs from Debian package
 APP_NAME=PDFGrepGUI
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
-APP_VERSION=0.9.1
+APP_VERSION=0.9.2
 APP_EXT=deb
-FILE_NAME=${APP_NAME,,}V${APP_VERSION//./_}_$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}-1_$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
 sudo apt install pdfgrep -y
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
