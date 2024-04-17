7ec48d796dd886652537d9fef9eb7eb7ae3a2957
Tim Jones <tdjones74021@yahoo.com>
2024-04-17 07:27:24 -0500

    Update ASUD cross-platform Sudoku puzzle assistant to version 16apr24.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -41761,7 +41761,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross-platform Sudoku puzzle assistant."
 APP_GUI_CATEGORIES="Games;Entertainment;"
 APP_GUI_KEYWORDS="Sudoku;Puzzle;"
-APP_VERSION=8oct23
+APP_VERSION=16apr24
 APP_EXT=7z
 FILE_NAME=sud${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
