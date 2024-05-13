be02fd51ad00d95f64c441ea9e844a739ebd57ff
Tim Jones <tdjones74021@yahoo.com>
2024-05-13 07:14:04 -0500

    Add commands to install Yaak cross-platform, Electron-based desktop REST and GraphQL webservice client from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -46110,3 +46110,39 @@ sudo cp -a /tmp/dist/${FILE_NAME}/spf /usr/local/bin/spf
 sudo ln -s -f /usr/local/bin/spf /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Yaak cross-platform, Electron-based desktop REST and GraphQL webservice client from AppImage
+APP_NAME=Yaak
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform, Electron-based desktop REST and GraphQL webservice client."
+APP_GUI_CATEGORIES="Development;Programming;"
+APP_GUI_KEYWORDS="REST;API;GraphQL;"
+APP_VERSION=2024.3.10
+APP_EXT=AppImage
+sudo apt install -y libfuse2
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT}.tar.gz -J -L https://releases.yaak.app/${APP_VERSION}/${FILE_NAME}.${APP_EXT}.tar.gz
+curl -o /tmp/${APP_NAME,,}.svg -J -L https://yaak.app/logo.svg
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}.tar.gz
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${_APP_NAME}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.svg /usr/local/share/icons/${APP_NAME,,}.svg
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.svg
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
