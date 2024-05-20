0818b35430c3085c97f19768cdd6738c82242ad1
Tim Jones <tdjones74021@yahoo.com>
2024-05-20 07:39:36 -0500

    Update StatAnt Qt-based spreadsheet-style statistical calculation tool to version 3.2.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -39809,7 +39809,7 @@ APP_NAME=StatAnt
 APP_GUI_NAME="Qt-based spreadsheet-style statistical calculation tool."
 APP_GUI_CATEGORIES="Accessories;Office;Science;"
 APP_GUI_KEYWORDS="Data;Science;Spreadsheet;Statistics;"
-APP_VERSION=3.1.1
+APP_VERSION=3.2.1
 APP_EXT=tar
 FILE_NAME=${APP_NAME,,}${APP_VERSION//./}_source
 sudo apt-get install -y qt5-default qt5-qmake
