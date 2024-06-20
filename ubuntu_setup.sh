9a7503bba60066b6bc6d8b299a30c80ab6d66e98
Tim Jones <tdjones74021@yahoo.com>
2024-06-20 07:53:59 -0500

    Update Orbiton minimalist Golang text editor to version 2.65.12.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -28079,7 +28079,7 @@ rm -rf /tmp/*${APP_NAME,,}*
 # Install Orbiton minimalist Golang text editor from package
 APP_NAME=Orbiton
 APP_GUI_NAME="Minimalist Golang text editor."
-APP_VERSION=2.65.11
+APP_VERSION=2.65.12
 APP_EXT=tar.xz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-linux
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/xyproto/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
