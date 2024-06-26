02e623e4a6c8f031e139cab7fc831a34a33b4f7e
Tim Jones <tdjones74021@yahoo.com>
2024-06-26 07:11:25 -0500

    Update Git Town shell-based Git workflow enhancement tool to version 14.2.3.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7407,7 +7407,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Git Town shell-based Git workflow enhancement tool from package
 APP_NAME=Git-Town
-APP_VERSION=14.2.1
+APP_VERSION=14.2.3
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_linux_intel_64
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
