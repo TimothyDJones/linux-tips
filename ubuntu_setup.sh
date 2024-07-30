71a29c4ba39eef39ae935cbc0dd28911c5f7d595
Tim Jones <tdjones74021@yahoo.com>
2024-07-30 07:00:42 -0500

    Add commands to install LocalAI Golang-based self-hosted OpenAI-compatible API supporting multiple LLM models from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -46662,3 +46662,18 @@ sudo cp /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
 sudo chmod +x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install LocalAI Golang-based self-hosted OpenAI-compatible API supporting multiple LLM models from package
+# https://github.com/go-skynet/LocalAI
+APP_NAME="Local-AI"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '-' '[:blank:]')
+APP_GUI_NAME="Golang-based self-hosted OpenAI-compatible API supporting multiple LLM models."
+APP_VERSION=2.19.3
+APP_EXT=N/A
+FILE_NAME=${APP_NAME,,}-Linux-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME} -J -L https://downloads.sourceforge.net/${_APP_NAME}.mirror/${FILE_NAME}
+sudo cp /tmp/${FILE_NAME} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}
+sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
