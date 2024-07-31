a3b0d1cd5e5989250641f1a92f5e63de51b95564
Tim Jones <tdjones74021@yahoo.com>
2024-07-31 08:42:08 -0500

    Update YouTube Music cross-platform Electron-based desktop music player to version 3.5.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -39857,7 +39857,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross-platform Electron-based desktop music player."
 APP_GUI_CATEGORIES="Audio;Entertainment;Multimedia;"
 APP_GUI_KEYWORDS="Music;YouTube;"
-APP_VERSION=3.4.1
+APP_VERSION=3.5.0
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME// /-}-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_APP_NAME}.mirror/${FILE_NAME}.${APP_EXT}
