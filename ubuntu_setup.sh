5c798424aeaea89870e40c62cdcf6ccc45349602
Tim Jones <tdjones74021@yahoo.com>
2024-08-06 13:51:45 -0500

    Update Oh My Posh cross-platform tool for customizing command prompt with colors and themes to version 23.6.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -39719,7 +39719,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 APP_NAME="Oh My Posh"
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross-platform tool for customizing command prompt with colors and themes."
-APP_VERSION=23.3.3
+APP_VERSION=23.6.0
 APP_EXT=N/A
 FILE_NAME=posh-linux-$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME} -J -L https://downloads.sourceforge.net/ohmyposh-dev.mirror/${FILE_NAME}
