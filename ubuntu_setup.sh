e974d46f0141da897719b7f482245d4b3a9c8145
Tim Jones <tdjones74021@yahoo.com>
2024-08-17 09:02:06 -0500

    Update Java Multiprecision Calculator cross-platform calculator based on Java BigDecimal class to version 2024-08-16.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -20160,7 +20160,7 @@ rm -rf /tmp/*${APP_NAME}* /tmp/*${APP_NAME,,}*
 # Install Java Multiprecision Calculator cross-platform calculator based on Java BigDecimal class from package
 APP_NAME="MultiPrecision Calculator"
 APP_GUI_NAME="Cross-platform calculator based on Java BigDecimal class."
-APP_VERSION=2024-07-06
+APP_VERSION=2024-08-16
 APP_EXT=7z
 FILE_NAME=${APP_VERSION//-/}.${APP_NAME// /}.v1.2
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/javamultiprecis/${FILE_NAME}.${APP_EXT}
