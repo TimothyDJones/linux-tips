05aaadfc9f227af2c5557c5ec167d1f9f725c4ef
Tim Jones <tdjones74021@yahoo.com>
2024-08-23 06:16:11 -0500

    Update fd user-friendly alternative to Linux 'find' to version 10.2.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -9640,7 +9640,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install fd user-friendly alternative to Linux 'find' from package
 APP_NAME=fd
 APP_GUI_NAME="Cross-platform, user-friendly alternative to Linux 'find'."
-APP_VERSION=10.1.0
+APP_VERSION=10.2.0
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}-musl_${APP_VERSION}_${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/sharkdp/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
