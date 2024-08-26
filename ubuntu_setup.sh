5e4329caef656cb6ffc3cb3b7a8372f86a8cb9a6
Tim Jones <tdjones74021@yahoo.com>
2024-08-26 16:07:51 -0500

    Update Kitty cross-platform GPU-based terminal to version 0.36.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -22659,7 +22659,7 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # https://sw.kovidgoyal.net/kitty/
 APP_NAME=Kitten
 APP_GUI_NAME="Cross-platform GPU-based terminal."
-APP_VERSION=0.36.0
+APP_VERSION=0.36.1
 APP_EXT=txz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
 sudo apt-get install -y python3-enchant python3-pyqt5 python3-numpy python3-pyqt5.qtsvg
