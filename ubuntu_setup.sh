d19b8d4fef92aeefa7b7f0d3e441c6818e4dc293
Tim Jones <tdjones74021@yahoo.com>
2024-09-02 10:10:44 -0500

    Update croc cross-platform command-line secure file transfer tool to version 10.0.12.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25495,7 +25495,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 
 # Install croc cross-platform command-line secure file transfer tool from Debian package
 APP_NAME=croc
-APP_VERSION=10.0.11
+APP_VERSION=10.0.12
 APP_EXT=deb
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=64bit
