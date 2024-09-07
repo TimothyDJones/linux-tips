5cd7efbf0aa954d8cb3c7d387612888331dbfa36
Tim Jones <tdjones74021@yahoo.com>
2024-09-07 11:55:29 -0500

    Update GoldenDict++OCR cross-platform enhanced dictionary tool with built-in OCR capability to version 3E26-20240823.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -42866,7 +42866,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross-platform enhanced dictionary tool with built-in OCR capability."
 APP_GUI_CATEGORIES="Accessories;Office;"
 APP_GUI_KEYWORDS="Dictionary;OCR;"
-APP_VERSION=3E25-20240722
+APP_VERSION=3E26-20240823
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME}++OCR-${APP_VERSION}-Debian12_Qt-4.8.7-p5-x64
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}-pro/${FILE_NAME}.${APP_EXT}
