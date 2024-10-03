5fce9e88170543e04f96792856f9c75c9e0ca183
Tim Jones <tdjones74021@yahoo.com>
2024-10-03 07:40:44 -0500

    Update Neovim cross‐platform, next-generation Vim text editor to version 0.10.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -42384,7 +42384,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 APP_NAME=Neovim
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross‐platform, next-generation Vim text editor."
-APP_VERSION=0.9.1
+APP_VERSION=0.10.2
 APP_EXT=AppImage
 FILE_NAME=nvim
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT,,}
