43f2ef2bf509b4c4778e58e5fa4d4f1a64c25911
Tim Jones <tdjones74021@yahoo.com>
2024-10-06 20:16:26 -0500

    Update Clipboard tool to cut, copy, and paste files from the terminal to version 0.9.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -39398,7 +39398,7 @@ rm -rf /tmp/*${APP_NAME,,}*
 # Install Clipboard tool to cut, copy, and paste files from the terminal from package
 APP_NAME=Clipboard
 APP_GUI_NAME="Tool to cut, copy, and paste files from the terminal."
-APP_VERSION=0.7.1
+APP_VERSION=0.9.1
 APP_EXT=zip
 GITHUB_USER=Slackadays
 FILE_NAME=${APP_NAME,,}-linux-$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
