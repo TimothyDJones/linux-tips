cd62a0f4e0dc2408997ca34549ded1cb300ead10
Tim Jones <tdjones74021@yahoo.com>
2024-10-06 21:09:38 -0500

    Add commands to install Oculante Rust-based, GUI image viewer from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -47386,3 +47386,36 @@ sudo cp -a -R /tmp/${APP_NAME,,} /usr/local/bin
 sudo chmod +x /usr/local/bin/${APP_NAME,,}*
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Oculante Rust-based, GUI image viewer from package
+APP_NAME=Oculante
+APP_GUI_NAME="Rust-based, GUI image viewer."
+APP_GUI_CATEGORIES="Accessories;Graphics"
+APP_GUI_KEYWORDS="Image;Viewer;"
+APP_VERSION=0.9.1
+APP_EXT=zip
+FILE_NAME=${APP_NAME,,}_linux
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/woelper/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/woelper/${APP_NAME,,}/refs/heads/master/res/icons/icon.png
+cd /tmp
+unzip /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a -R /tmp/target/release/${APP_NAME,,} /usr/local/bin
+sudo chmod +x /usr/local/bin/${APP_NAME,,}*
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
