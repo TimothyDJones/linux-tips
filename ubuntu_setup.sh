867ac727178dd32d32f2e92ce4bf6e4e469bfbe4
Tim Jones <tdjones74021@yahoo.com>
2024-10-12 15:30:30 -0500

    Add commands to install Pomodorolm cross-platform, minimalist desktop Pomodoro tool from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -47426,3 +47426,37 @@ APP_NAME=Warpinator
 sudo apt-get install -y flatpak
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install flathub org.x.${APP_NAME}
+
+# Install Pomodorolm cross-platform, minimalist desktop Pomodoro tool from AppImage
+APP_NAME=Pomodorolm
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+APP_GUI_NAME="Cross-platform, minimalist desktop Pomodoro tool."
+APP_GUI_CATEGORIES="Accessories;"
+APP_GUI_KEYWORDS="Pomodoro;"
+APP_VERSION=0.2.1
+APP_EXT=AppImage
+sudo apt install -y libfuse2
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/vjousse/${APP_NAME,,}/releases/download/app-v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/vjousse/${APP_NAME,,}/refs/heads/main/src-tauri/icons/128x128%402x.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
