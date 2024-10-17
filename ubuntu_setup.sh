f93463d0165bb78cc7c7aefca3dfd4af7ef82970
Tim Jones <tdjones74021@yahoo.com>
2024-10-17 07:53:11 -0500

    Update Logisim Java-based digital logic designer and simulator to version 2.7.14.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -26560,17 +26560,21 @@ APP_NAME=Logisim
 APP_GUI_NAME="Java-based digital logic designer and simulator."
 APP_GUI_CATEGORIES="Development;Electronics;"
 APP_GUI_KEYWORDS="Logic;Simulator;Electronics;"
-APP_VERSION=2.7.12
-APP_EXT=jar
-FILE_NAME=${APP_NAME}-${APP_VERSION}
+APP_VERSION=2.7.14
+APP_EXT=zip
+FILE_NAME=${APP_NAME}-${APP_VERSION}-linux
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}-ps/${FILE_NAME}.${APP_EXT}
-sudo mkdir -p /opt/${APP_NAME,,}
-sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://a.fsdn.com/allura/p/logisim-ps/icon?1671573264
+cd /tmp
+unzip -C /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}; sudo chmod 666 /opt/${APP_NAME,,}
+sudo cp -R -a /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+sudo cp /tmp/${APP_NAME,,}.png /opt/${APP_NAME,,}
 cat > /tmp/${APP_NAME,,} << EOF
 #! /bin/sh
 cd /opt/${APP_NAME,,}
 PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
-java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+/opt/${APP_NAME,,}/${APP_NAME}-${APP_VERSION}
 cd $HOME
 EOF
 sudo mv /tmp/${APP_NAME,,} /usr/local/bin
@@ -26581,8 +26585,8 @@ Name=${APP_NAME}
 Comment=${APP_GUI_NAME}
 GenericName=${APP_NAME}
 Path=/opt/${APP_NAME,,}
-Exec=java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
-Icon=
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/${APP_NAME,,}.png
 Type=Application
 StartupNotify=true
 Terminal=false
