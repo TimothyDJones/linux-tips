1e72a17e470d8467fea1d7a1324a434ffa0774a3
Tim Jones <tdjones74021@yahoo.com>
2024-11-04 06:27:03 -0600

    Update Fastfetch C-based, cross-platform, command-line minimalist system information tool to version 2.29.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -40796,7 +40796,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 APP_NAME=Fastfetch
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="C-based, cross-platform, command-line minimalist system information tool."
-APP_VERSION=2.28.0
+APP_VERSION=2.29.0
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-Linux
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/fastfetch-cli/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
