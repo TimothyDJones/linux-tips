b807ea974c6b5e8566414b6d6730bac3177ca5b1
Tim Jones <tdjones74021@yahoo.com>
2024-11-21 14:17:17 -0600

    Update Lucas Chess cross-platform, Python-based GUI for 60+ chess engines to version 2.17b.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -30523,7 +30523,7 @@ rm -rf /tmp/${APP_NAME,,}*
 # https://lucaschess.pythonanywhere.com/
 APP_NAME="Lucas Chess"
 APP_GUI_NAME="Cross-platform, Python-based GUI for 60+ chess engines."
-APP_VERSION=2.17
+APP_VERSION=2.17b
 APP_EXT=sh
 FILE_NAME=${APP_NAME// /}R${APP_VERSION//./_}_LINUX
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/lucaschessr/${FILE_NAME}.${APP_EXT}
