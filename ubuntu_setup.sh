1f0fa218946397bf87105cc6df5a20ca7382af9a
Tim Jones <tdjones74021@yahoo.com>
2024-11-21 14:12:11 -0600

    Update Zettlr Electron-based Markdown editor with built-in preview to version 3.3.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8452,7 +8452,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install Zettlr Electron-based Markdown editor with built-in preview from Debian package
 APP_NAME=Zettlr
 APP_GUI_NAME="Electron-based Markdown editor with built-in preview."
-APP_VERSION=3.2.3
+APP_VERSION=3.3.1
 APP_EXT=deb
 FILE_NAME=${APP_NAME}-${APP_VERSION}-amd64
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME}/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
