1782d0a2a14b4973a1fe9561961f344c4c960cf3
Tim Jones <tdjones74021@yahoo.com>
2024-11-25 09:45:47 -0600

    Update Amp Rust-based command-line text editor to version 0.7.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7668,7 +7668,7 @@ rm -rf /tmp/${APP_NAME,,}*
 # Install Amp Rust-based command-line text editor from source
 APP_NAME=Amp
 APP_GUI_NAME="Cross-platform Rust-based command-line text editor."
-APP_VERSION=0.7.0
+APP_VERSION=0.7.1
 APP_EXT=N/A
 sudo apt-get install -y zlib1g-dev openssl libxcb1-dev cmake pkg-config libssl-dev
 curl https://sh.rustup.rs -sSf | sh
