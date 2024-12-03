5648064d2ea184d46e83a6251a834d8258b32d18
Tim Jones <tdjones74021@yahoo.com>
2024-12-03 11:10:23 -0600

    Update SiYuan cross-platform Electron-based notepad and personal knowledge-management tool with markdown support to version 3.1.14.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -41015,7 +41015,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross-platform Electron-based notepad and personal knowledge-management tool with markdown support."
 APP_GUI_CATEGORIES="Accessories;Office;"
 APP_GUI_KEYWORDS="Notepad;Markdown;"
-APP_VERSION=3.1.12
+APP_VERSION=3.1.14
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-linux
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
