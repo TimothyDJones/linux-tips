93ea0574bc2631951857d9a0d85ef8ef70159b77
Tim Jones <tdjones74021@yahoo.com>
2024-12-17 09:51:59 -0600

    Update Thonny minimalist Python IDE/editor for beginners to version 4.1.7.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5457,7 +5457,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install Thonny minimalist Python IDE/editor for beginners from package
 APP_NAME=Thonny
-APP_VERSION=4.1.6
+APP_VERSION=4.1.7
 APP_GUI_NAME="Minimalist Python IDE/editor for beginners."
 APP_EXT=tar.gz
 if [[ $(uname -m | grep '64') ]]; then  # Check for 64-bit Linux kernel
