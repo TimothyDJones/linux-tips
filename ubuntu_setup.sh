80ecb1596d6f9978858fc6da67371a54723c7bee
Tim Jones <tdjones74021@yahoo.com>
2024-12-20 08:09:29 -0600

    Add commands to install Keep-Alive Golang-based, simple tool to keep computer from going to sleep from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -48017,3 +48017,17 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install Keep-Alive Golang-based, simple tool to keep computer from going to sleep from package
+APP_NAME=Keep-Alive
+APP_GUI_NAME="Golang-based, simple tool to keep computer from going to sleep."
+APP_VERSION=1.0.2
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}_Linux_$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/stigoleg/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a -R /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
