0e8c7ed8a7c5caec3e9c69920d5fd50307e1451a
Tim Jones <tdjones74021@yahoo.com>
2025-01-02 10:23:29 -0600

    Add commands to install JSONly cross-platform GUI program for interacting with and manipulating JSON files from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -48047,3 +48047,37 @@ sudo make install
 sudo make install.man
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}-*
+
+# Install JSONly cross-platform GUI program for interacting with and manipulating JSON files from AppImage
+APP_NAME=JSONly
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+APP_GUI_NAME="Cross-platform GUI program for interacting with and manipulating JSON files."
+APP_GUI_CATEGORIES="Programming;Development;"
+APP_GUI_KEYWORDS="JSON;Editor;"
+APP_VERSION=1.2.0-beta
+APP_EXT=AppImage
+sudo apt install -y libfuse2
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-linux
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://a.fsdn.com/allura/p/jsonly/icon?d19bf478974d92fbf5f0d847041b21cfe0d9cd21d78a8a9113a4615e3a04b1c1
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
