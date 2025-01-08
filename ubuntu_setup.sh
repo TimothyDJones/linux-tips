886e3f878355afc54337e8657fea1d800202e944
Tim Jones <tdjones74021@yahoo.com>
2025-01-08 08:27:26 -0600

    Update bat Rust-based cat(1) clone with syntax highlighting and Git integration to version 0.25.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -37936,7 +37936,7 @@ rm -rf /tmp/${APP_NAME// /.}* /tmp/${_APP_NAME}*
 APP_NAME=bat
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
 APP_GUI_NAME="Rust-based cat(1) clone with syntax highlighting and Git integration."
-APP_VERSION=0.23.0
+APP_VERSION=0.25.0
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-v${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
