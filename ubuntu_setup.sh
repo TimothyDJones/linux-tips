156a19395e4b65745b1624c110e62d9a666c0a39
Tim Jones <tdjones74021@yahoo.com>
2025-01-15 08:02:42 -0600

    Add commands to install QroCAD cross-platform beginner-friendly desktop CAD tool from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -48166,3 +48166,37 @@ sudo chmod +x /usr/local/bin/${FILE_NAME}
 sudo ln -s /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install QroCAD cross-platform beginner-friendly desktop CAD tool from AppImage
+APP_NAME=QroCAD
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+APP_GUI_NAME="Cross-platform beginner-friendly desktop CAD tool."
+APP_GUI_CATEGORIES="Graphics;Office;"
+APP_GUI_KEYWORDS="CAD;Editor;"
+APP_VERSION=1.7.0
+APP_EXT=AppImage
+sudo apt install -y libfuse2
+FILE_NAME=${APP_NAME,,}-desktop-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://a.fsdn.com/allura/p/qrocad/icon?402c9715fd9facb1f6d5e58e6cf2ce4461408c7054ed8d0d5d73bebc0830f8af
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
