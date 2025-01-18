7f1a6cdfacb1139ddf1cfd961f8529c80633e440
Tim Jones <tdjones74021@yahoo.com>
2025-01-18 09:43:58 -0600

    Update PkgX Deno-based next-generation, cross‐platform package management tool to version 2.1.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -42171,7 +42171,7 @@ rm -rf /tmp/${APP_NAME,,}*
 APP_NAME=PkgX
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Deno-based next-generation, cross‐platform package management tool."
-APP_VERSION=1.3.1
+APP_VERSION=2.1.1
 APP_EXT=tar.xz
 BASE_ARCH=$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}+linux+${BASE_ARCH//_/-}
