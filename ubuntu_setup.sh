cb912774b0c599ad7f3d333593ea1caa0884caaf
Tim Jones <tdjones74021@yahoo.com>
2025-01-18 09:01:20 -0600

    Update Tabby (formerly Terminus) cross-platform JavaScript/Electron terminal to version 1.0.220.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5213,7 +5213,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross-platform JavaScript/Electron terminal."
 APP_GUI_CATEGORIES="System;Accessories;"
 APP_GUI_KEYWORDS="Terminal;Shell;"
-APP_VERSION=1.0.219
+APP_VERSION=1.0.220
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-linux-x64
 sudo apt install -y libfuse2
