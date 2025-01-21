c99e1db365016f198689629e7cc48eff0600fe96
Tim Jones <tdjones74021@yahoo.com>
2025-01-21 08:55:10 -0600

    Update Vieb (Vim Inspired Electron Browser) cross-platform, Chromium-based web browser with Vim keybindings to version 12.2.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -31526,7 +31526,7 @@ APP_NAME=Vieb
 APP_GUI_NAME="Cross-platform, Chromium-based web browser with Vim keybindings."
 APP_GUI_CATEGORIES="Internet;"
 APP_GUI_KEYWORDS="Vim;Browser;Chromium;Web;"
-APP_VERSION=12.1.0
+APP_VERSION=12.2.0
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/Jelmerro/${APP_NAME}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
