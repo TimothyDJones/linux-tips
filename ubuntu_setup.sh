fc804753fe3a7a4224c3ba57ce7476472b326ae9
Tim Jones <tdjones74021@yahoo.com>
2025-02-03 14:31:36 -0600

    Update Pinokio browser-based AI automation platform to version 3.6.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -46570,7 +46570,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Browser-based AI automation platform."
 APP_GUI_CATEGORIES="Internet;System;"
 APP_GUI_KEYWORDS="AI;Automation;"
-APP_VERSION=3.4.0
+APP_VERSION=3.6.0
 APP_EXT=AppImage
 sudo apt install -y libfuse2
 FILE_NAME=${APP_NAME}-${APP_VERSION}
