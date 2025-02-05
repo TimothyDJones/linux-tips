9dda1e19d8353e5773953199c68defb7d1b1151d
Tim Jones <tdjones74021@yahoo.com>
2025-02-05 08:06:07 -0600

    Update Gantt Project project management tool to version 3.3.3316.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1475,7 +1475,7 @@ sudo apt-get install -y et
 
 # Install Gantt Project project management tool from Debian package
 APP_NAME=GanttProject
-APP_VERSION=3.3.3312
+APP_VERSION=3.3.3316
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}-1_all
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
