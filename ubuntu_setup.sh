6cafd3ea3c46dd1f1d7ed0ece5ca7a2724c22563
Tim Jones <tdjones74021@yahoo.com>
2025-02-08 08:39:30 -0600

    Update TexStudio cross-platform Qt-based LaTeX editor to version 4.8.6.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3507,7 +3507,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 
 # Install TexStudio cross-platform Qt-based LaTeX editor from source
 APP_NAME=TexStudio
-APP_VERSION=4.8.5
+APP_VERSION=4.8.6
 APP_EXT=tar.gz
 FILE_NAME=${APP_VERSION}
 sudo apt-get install -y libpoppler-qt5-dev libqt5svg5-dev qttools5-dev libgs-dev qtscript5-dev texlive
