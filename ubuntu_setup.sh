21e68acc868e156f6f0f7d466f8cb69577181cb2
Tim Jones <tdjones74021@yahoo.com>
2025-02-17 16:45:56 -0600

    Update LazyGit cross-platform Golang-based shell GUI for Git to version 0.46.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -13337,7 +13337,7 @@ rm -rf /tmp/*${APP_NAME}*
 APP_NAME=LazyGit
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
 APP_GUI_NAME="Cross-platform Golang-based shell GUI for Git."
-APP_VERSION=0.45.2
+APP_VERSION=0.46.0
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_Linux_$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
