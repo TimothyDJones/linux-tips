004f80dd54cfe08ac3c5c3deb0bc588c95d78a2a
Tim Jones <tdjones74021@yahoo.com>
2025-02-17 16:24:06 -0600

    Update ImHex cross-platform, multi-purpose hex editor to version 1.37.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -26879,7 +26879,7 @@ APP_NAME=ImHex
 APP_GUI_NAME="Cross-platform, multi-purpose hex editor."
 APP_GUI_CATEGORIES="Development;Programming;"
 APP_GUI_KEYWORDS="Hex Editor;Editor"
-APP_VERSION=1.35.4
+APP_VERSION=1.37.0
 APP_EXT=zip
 FILE_NAME=Linux.AppImage
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/WerWolv/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
