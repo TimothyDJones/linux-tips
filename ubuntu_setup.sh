5d033c3327810ab184fe05efa175e3390ecf6f74
Tim Jones <tdjones74021@yahoo.com>
2025-02-19 08:36:39 -0600

    Update qBittorrent Qt-based Bittorrent client to version 5.0.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -13827,7 +13827,7 @@ rm -rf /tmp/*${APP_NAME}*
 # Install qBittorrent Qt-based Bittorrent client from source
 APP_NAME=qBittorrent
 APP_GUI_NAME="Qt-based Bittorrent client."
-APP_VERSION=5.0.3
+APP_VERSION=5.0.4
 APP_EXT=tar.xz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get build-dep -y ${APP_NAME,,}
