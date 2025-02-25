e4c9555969bd9c3deace80d8cfd834c7f6d3b686
Tim Jones <tdjones74021@yahoo.com>
2025-02-25 14:06:32 -0600

    Add commands to install SQL Studio cross‐platform, Rust-based terminal SQL client for multiple database platforms from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -48504,3 +48504,18 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install SQL Studio cross‐platform, Rust-based terminal SQL client for multiple database platforms from package
+APP_NAME="SQL Studio"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross‐platform, Rust-based terminal SQL client for multiple database platforms."
+APP_VERSION=0.1.35
+APP_EXT=tar.xz
+FILE_NAME=${_APP_NAME}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_APP_NAME}.mirror/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${_APP_NAME} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${_APP_NAME}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
