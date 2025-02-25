bc74c3ef3855c75a2bc057b1619512773556d617
Tim Jones <tdjones74021@yahoo.com>
2025-02-25 16:32:24 -0600

    Update Docker Compose command-line utility for multi-container Docker applications to version 2.33.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -15535,7 +15535,7 @@ docker run hello-world  # Confirm Docker installation.
 # Install Docker Compose command-line utility for multi-container Docker applications from package
 APP_NAME=Docker-Compose
 APP_GUI_NAME="Command-line utility for multi-container Docker applications."
-APP_VERSION=2.33.0
+APP_VERSION=2.33.1
 APP_EXT=N/A
 FILE_NAME=${APP_NAME,,}-linux-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
 curl -o /tmp/${FILE_NAME} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}
