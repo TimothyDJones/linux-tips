3b1babfbec333f29ea522d12f3db2ebecd78138a
Tim Jones <tdjones74021@yahoo.com>
2025-03-05 07:39:51 -0600

    Update Pineapple Pictures cross-platform lightweight image viewer to version 0.9.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -28846,8 +28846,10 @@ APP_NAME=Pineapple-Pictures
 APP_VERSION=0.9.2
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-Ubuntu-24.04-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
-curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
-sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${FILE_NAME}.zip -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.zip
+cd /tmp
+unzip /tmp/${FILE_NAME}.zip
+sudo gdebi -n /tmp/${APP_NAME,,}-${APP_VERSION}*.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
 
