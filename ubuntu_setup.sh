28433b1b6fde4b2cd626cdb290f3145e3798474a
Tim Jones <tdjones74021@yahoo.com>
2025-03-05 08:59:01 -0600

    Update JEditor Java-based text editor to version 1.2.26.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5281,7 +5281,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install JEditor Java-based text editor from package
 APP_NAME=jEditor
-APP_VERSION=1.2.25
+APP_VERSION=1.2.26
 APP_EXT=zip
 FILE_NAME=${APP_NAME}_GPL-bin-${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
