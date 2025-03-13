216039638e66bf2aab0a6923f37af7fe17d10f9b
Tim Jones <tdjones74021@yahoo.com>
2025-03-13 07:49:17 -0500

    Remove lfs Rust-based command-line Linux file system information utility (replaced by 'dysk').

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -34872,21 +34872,6 @@ sudo chmod a+x /usr/local/bin/${_APP_NAME}
 cd $HOME
 rm -rf /tmp/${APP_NAME}*
 
-# Install lfs Rust-based command-line Linux file system information utility from package
-APP_NAME=lfs
-_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
-APP_GUI_NAME="Rust-based command-line Linux file system information utility."
-APP_VERSION=2.5.0
-APP_EXT=zip
-FILE_NAME=${APP_NAME,,}_${APP_VERSION}
-curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/Canop/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
-cd /tmp
-dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
-sudo cp /tmp/${FILE_NAME}/build/x86_64-unknown-linux-musl/${APP_NAME,,} /usr/local/bin
-sudo chmod a+x /usr/local/bin/${APP_NAME,,}
-cd $HOME
-rm -rf /tmp/${APP_NAME}*
-
 # Install Ebook Creator Python/Qt GUI editor for creating ebooks from Markdown from Debian package
 # https://ebc.crowdware.at/
 # https://bitbucket.org/artanidos/ebookcreator/src/master/
