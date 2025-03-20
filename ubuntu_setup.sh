211e5ad744d13707d5afbbf9876cf7e892f7105b
Tim Jones <tdjones74021@yahoo.com>
2025-03-20 07:54:36 -0500

    Update Jitsi Meet cross-platform Electron-based desktop video conferencing tool to version 2025.2.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -40267,7 +40267,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross-platform Electron-based desktop video conferencing tool."
 APP_GUI_CATEGORIES="Internet;Networking;"
 APP_GUI_KEYWORDS="Video;Conferencing;"
-APP_VERSION=2024.5.0
+APP_VERSION=2025.2.0
 APP_EXT=AppImage
 FILE_NAME=${_APP_NAME}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_APP_NAME}-electron.mirror/${FILE_NAME}.${APP_EXT}
