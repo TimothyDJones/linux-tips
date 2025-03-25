29e5d63626323abaca99f26938499c6e7fa6a98d
Tim Jones <tdjones74021@yahoo.com>
2025-03-25 10:25:10 -0500

    Update Hurl Rust-based command-line HTTP scripting tool to version 6.1.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -27243,7 +27243,7 @@ rm -rf /tmp/*${APP_NAME}*
 # Install Hurl Rust-based command-line HTTP scripting tool from package
 # https://hurl.dev
 APP_NAME=Hurl
-APP_VERSION=6.1.0
+APP_VERSION=6.1.1
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/Orange-OpenSource/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
