dafc2787666c940ae6bfb544b35c629d7ef529b7
Tim Jones <tdjones74021@yahoo.com>
2025-04-01 16:09:55 -0500

    Add commands to install FireDragon customized version of Firefox web browser for Linux from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -48709,3 +48709,38 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install FireDragon customized version of Firefox web browser for Linux from AppImage
+# https://github.com/Michal-D4/fileo
+APP_NAME=FireDragon
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+APP_GUI_NAME="Customized version of Firefox web browser for Linux."
+APP_GUI_CATEGORIES="Internet;System;"
+APP_GUI_KEYWORDS="Browser;"
+APP_VERSION=11.25.0-1
+APP_EXT=AppImage
+sudo apt install -y libfuse2
+FILE_NAME=${APP_NAME,,}-v${APP_VERSION}.${APP_EXT,,}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://gitlab.com/garuda-linux/${APP_NAME,,}/builder/-/releases/v${APP_VERSION}/downloads/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://gitlab.com/garuda-linux/${APP_NAME,,}/builder/-/raw/main/source_files/browser/branding/${APP_NAME,,}/default48.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
