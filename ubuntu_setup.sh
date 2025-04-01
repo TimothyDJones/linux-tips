356e3ec36e284ee3dc76e7a3bca7b6384825e298
Tim Jones <tdjones74021@yahoo.com>
2025-04-01 13:03:29 -0500

    Update yt-dlp youtube-dl fork with additional features and fixes to version 2025.03.31.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -33658,7 +33658,7 @@ rm -rf /tmp/${APP_NAME,,}*
 # Install yt-dlp youtube-dl fork with additional features and fixes from package
 APP_NAME=yt-dlp
 APP_GUI_NAME="youtube-dl fork with additional features and fixes."
-APP_VERSION=2025.03.26
+APP_VERSION=2025.03.31
 APP_EXT=N/A
 FILE_NAME=${APP_NAME,,}
 curl -o /tmp/${FILE_NAME} -J -L https://github.com/${APP_NAME,,}/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}
