5a9faafc42eaca847ba7185bf85e00988645273f
Tim Jones <tdjones74021@yahoo.com>
2025-04-03 08:22:25 -0500

    Update GNU nano text editor to version 8.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5105,7 +5105,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install GNU nano text editor from source
 APP_NAME=nano
 APP_GUI_NAME="Minimalist console text editor."
-APP_VERSION=8.3
+APP_VERSION=8.4
 APP_EXT=tar.xz
 FILE_NAME=${APP_NAME}-${APP_VERSION}
 sudo apt-get install -y libncurses-dev
