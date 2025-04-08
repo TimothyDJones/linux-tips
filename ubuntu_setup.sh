a2d3e72243c09587df2241c1d24d8c5a4aab1124
Tim Jones <tdjones74021@yahoo.com>
2025-04-08 16:33:59 -0500

    Update DuckDB cross-platform SQLite-like database with PostgreSQL functionality to version 1.2.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25745,7 +25745,7 @@ rm -rf /tmp/${APP_NAME,,}*
 # Install DuckDB cross-platform SQLite-like database with PostgreSQL functionality from package
 APP_NAME=DuckDB
 APP_GUI_NAME="Cross-platform SQLite-like database with PostgreSQL functionality."
-APP_VERSION=1.2.1
+APP_VERSION=1.2.2
 APP_EXT=zip
 FILE_NAME=${APP_NAME,,}_cli-linux-$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/cwida/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
