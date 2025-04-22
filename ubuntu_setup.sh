43e54c5a1ff88c6284028a6726d3a778c5ba8d44
Tim Jones <tdjones74021@yahoo.com>
2025-04-22 07:06:32 -0500

    Update Notesnook cross-platform Electron-based desktop encrypted note-taking tool to version 3.1.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -37528,7 +37528,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
 APP_GUI_NAME="Cross-platform Electron-based desktop encrypted note-taking tool."
 APP_GUI_CATEGORIES="Accessories;Office;"
 APP_GUI_KEYWORDS="Notepad;Security;"
-APP_VERSION=3.0.32
+APP_VERSION=3.1.0
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME,,}_linux_x86_64
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/streetwriters/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
