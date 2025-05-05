c16aa9d5a058205ca31a6ae47d6f825937e65641
Tim Jones <tdjones74021@yahoo.com>
2025-05-05 08:18:05 -0500

    Update rpncalc cross-platform Java-based command-line RPN calculator to version 5.7.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -30610,7 +30610,7 @@ rm -rf /tmp/*${APP_NAME,,}*
 
 # Install rpncalc cross-platform Java-based command-line RPN calculator from package
 APP_NAME=rpncalc
-APP_VERSION=5.2.3
+APP_VERSION=5.7.0
 APP_EXT=jar
 FILE_NAME=${APP_NAME,,}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}-fross/${FILE_NAME}.${APP_EXT}
