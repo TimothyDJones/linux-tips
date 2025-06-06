6e8af2cb18b06c1072fe3692cb0c0392b69e700e
Tim Jones <tdjones74021@yahoo.com>
2025-06-06 08:41:28 -0500

    Add commands to install CircuiTikZ cross-platform, Qt-based GUI tool for designing electronic circuits using LaTeX from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -49224,3 +49224,38 @@ sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install CircuiTikZ cross-platform, Qt-based GUI tool for designing electronic circuits using LaTeX from AppImage
+# https://www.circuitikz-generator.xyz/
+APP_NAME=CircuiTikZ
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+APP_GUI_NAME="Cross-platform, Qt-based GUI tool for designing electronic circuits using LaTeX."
+APP_GUI_CATEGORIES="Science;Electronics;"
+APP_GUI_KEYWORDS="LaTeX;"
+APP_VERSION=0.9
+APP_EXT=AppImage
+sudo apt install -y libfuse2
+FILE_NAME=Circuitikz_Generator-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}-generator/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://a.fsdn.com/allura/p/circuitikz-generator/icon?6b8772cba240a7b71e182fd13d18fa0fc6c1d909f8d8aae2bee6a209eed670a8
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
