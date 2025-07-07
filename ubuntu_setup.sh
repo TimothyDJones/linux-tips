e314da19701b14529adbdbfa4777fa807ff4207c
Tim Jones <tdjones74021@yahoo.com>
2025-07-07 10:50:14 -0500

    Add commands to install Brisk cross-platform Flutter-based download manager from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -49525,3 +49525,42 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/durableprogramming
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install Brisk cross-platform Flutter-based download manager from package
+# https://github.com/BrisklyDev/brisk
+APP_NAME=Brisk
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform Flutter-based download manager."
+APP_GUI_CATEGORIES="Internet;"
+APP_GUI_KEYWORDS="Download;"
+APP_VERSION=2.3.4
+APP_EXT=tar.xz
+sudo apt install -yy libkeybinder-3.0-0 libayatana-appindicator3-dev
+FILE_NAME=${APP_NAME}-v${APP_VERSION}-linux-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/BrisklyDev/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/BrisklyDev/${APP_NAME,,}/refs/heads/main/assets/icons/logo-128.png
+cd /tmp
+mkdir /tmp/${FILE_NAME}
+tar -xf /tmp/${FILE_NAME}.${APP_EXT} -C /tmp/${FILE_NAME}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -a -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
+sudo cp -a -R /tmp/${APP_NAME,,}.png /opt/${APP_NAME,,}
+sudo chmod -R 755 /opt/${APP_NAME,,}
+sudo ln -s -f /opt/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
