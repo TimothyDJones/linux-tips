50f646a88a8bdcb923a1adf5f39a60c92af35e6f
Tim Jones <tdjones74021@yahoo.com>
2025-07-07 16:49:18 -0500

    Add commands to install XNEdit fast and classic X11 text editor, based on NEdit from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -49644,3 +49644,40 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/onotelli/${APP_NAM
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install XNEdit fast and classic X11 text editor, based on NEdit from AppImage
+# https://github.com/unixwork/xnedit
+APP_NAME=XNEdit
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
+APP_GUI_NAME="Fast and classic X11 text editor, based on NEdit."
+APP_GUI_CATEGORIES="Development;Programming;Accessories;System;"
+APP_GUI_KEYWORDS="Editor;"
+APP_VERSION=1.6.3
+APP_EXT=AppImage
+sudo apt install -y libfuse2
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}-${APP_EXT}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME}.tar.gz -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.tar.gz
+curl -o /tmp/${APP_NAME,,}.png -J -L https://sourceforge.net/p/${APP_NAME,,}/code/ci/master/tree/resources/desktop/${APP_NAME,,}.png?format=raw
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.tar.gz
+sudo cp /tmp/${FILE_NAME}/${APP_NAME}-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU).${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${APP_NAME}-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU).${APP_EXT}
+sudo ln -s -f /usr/local/bin/${APP_NAME}-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU).${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
