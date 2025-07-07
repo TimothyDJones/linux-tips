f8ba4c9d95dbd1d9af8dffa15cacf4add885d9e1
Tim Jones <tdjones74021@yahoo.com>
2025-07-07 16:23:50 -0500

    Add commands to install Justniffer terminal TCP packet sniffing tool from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -49633,3 +49633,14 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Justniffer terminal TCP packet sniffing tool from Debian package
+APP_NAME=Justniffer
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.6.11
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}.$(lsb_release --codename | cut -f2)_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/onotelli/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
