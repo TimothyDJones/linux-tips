39f41e713db269a6d2dbc2facb27264ff9fe313d
Tim Jones <tdjones74021@yahoo.com>
2025-07-10 14:17:31 -0500

    feat(ubuntu_setup): Add commands to install Git Basher cross‐platform, command-line script to simplify Git usage from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -49737,3 +49737,18 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Git Basher cross‐platform, command-line script to simplify Git usage from package
+APP_NAME=gitbasher
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross‐platform, command-line script to simplify Git usage."
+APP_VERSION=3.3.0
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/maxbolgarin/${APP_NAME,,}/archive/refs/tags/v${APP_VERSION}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/dist/gitb /usr/local/bin
+sudo chmod a+x /usr/local/bin/gitb
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
