ecea13bf00ee5d222d48fa31d01d0bcf08a95786
Tim Jones <tdjones74021@yahoo.com>
2025-07-28 09:25:25 -0500

    feat: Add commands to install Sip cross-platform MUD (Multi-User Dungeon) client from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -50009,3 +50009,37 @@ sudo chmod a+x /usr/local/bin/${FILE_NAME}
 sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Sip cross-platform MUD (Multi-User Dungeon) client from AppImage
+# https://github.com/bozimmerman/sip
+APP_NAME=Sip
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Cross-platform MUD (Multi-User Dungeon) client."
+APP_GUI_CATEGORIES="Entertainment;Games;"
+APP_GUI_KEYWORDS="MUD;"
+APP_VERSION=3.1.5
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-${APP_VERSION}.linux
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://a.fsdn.com/allura/p/sip/icon?a2273d9370be7e694b28a1d2db33073b86252f5640122283d00467098acc718e
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
