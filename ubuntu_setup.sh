fa1b82b58f71176b949293a8e27753ff5f3b0a6e
Tim Jones <tdjones74021@yahoo.com>
2025-08-04 08:06:33 -0500

    feat: Update croc cross-platform command-line secure file transfer tool to version 10.2.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5747,7 +5747,7 @@ rm -rf /tmp/${APP_NAME,,}
 # Install Snd open-source sound editor from source
 APP_NAME=Snd
 APP_GUI_NAME="Popular open-source audio file editor"
-APP_VERSION=25.5
+APP_VERSION=25.6
 APP_EXT=tar.gz
 sudo apt-get install -y libasound2-dev wavpack
 curl -o /tmp/${APP_NAME,,}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME,,}-${APP_VERSION}.${APP_EXT}
@@ -25488,7 +25488,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 
 # Install croc cross-platform command-line secure file transfer tool from Debian package
 APP_NAME=croc
-APP_VERSION=10.2.2
+APP_VERSION=10.2.4
 APP_EXT=deb
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=64bit
