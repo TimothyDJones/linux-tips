80e246e4550c91a2ee25049f37947706bd79f037
Tim Jones <tdjones74021@yahoo.com>
2025-08-11 07:56:15 -0500

    feat: Update Pomodorolm cross-platform, minimalist desktop Pomodoro tool to version 0.6.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -47495,7 +47495,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
 APP_GUI_NAME="Cross-platform, minimalist desktop Pomodoro tool."
 APP_GUI_CATEGORIES="Accessories;"
 APP_GUI_KEYWORDS="Pomodoro;"
-APP_VERSION=0.5.0
+APP_VERSION=0.6.0
 APP_EXT=AppImage
 sudo apt install -y libfuse2
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
