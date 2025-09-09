7bd8da8a88c06b95ec840a5caa1f2608a7e093f1
Tim Jones <tdjones74021@yahoo.com>
2025-09-09 11:29:47 -0500

    feat: Add commands to install Kerminal cross-platform terminal with SSH, tunneling, and cross-device sync from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -50547,3 +50547,37 @@ sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
 echo 'eval "$(lacy init bash)"' >> $HOME/.bashrc
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install Kerminal cross-platform terminal with SSH, tunneling, and cross-device sync from AppImage
+# https://klpod221.com/kerminal/
+APP_NAME=Kerminal
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Cross-platform terminal with SSH, tunneling, and cross-device sync."
+APP_GUI_CATEGORIES="Accessories;System"
+APP_GUI_KEYWORDS="Terminal;Shell;SSH;"
+APP_VERSION=1.0.6
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/klpod221/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/klpod221/${APP_NAME,,}/refs/heads/main/resources/icon.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
