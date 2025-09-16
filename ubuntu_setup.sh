82b93a33b80fd9031a84f07e2b8b25b436d3348f
Tim Jones <tdjones74021@yahoo.com>
2025-09-16 10:52:56 -0500

    feat: Add commands to install YoutubeGo cross-platform video/audio downloader with support for major platforms including YouTube, Vimeo, etc. from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -50663,3 +50663,37 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install YoutubeGo cross-platform video/audio downloader with support for major platforms including YouTube, Vimeo, etc. from AppImage
+# https://youtubego.org/
+APP_NAME=YoutubeGo
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Cross-platform video/audio downloader with support for major platforms including YouTube, Vimeo, etc."
+APP_GUI_CATEGORIES="Audio;Video;Multimedia"
+APP_GUI_KEYWORDS="Audio;Video;Downloader;"
+APP_VERSION=5.0.19
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/Efeckc17/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/Efeckc17/${APP_NAME,,}/refs/heads/main/assets/app.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
