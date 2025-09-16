0deb2198f3005f064fd6518a30de18c85e8a9a05
Tim Jones <tdjones74021@yahoo.com>
2025-09-16 08:37:35 -0500

    feat: Add commands to install Readest cross-platform, minimalist ebook reader from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -50629,3 +50629,37 @@ meson compile -Dsyntax_highlighting=true -C _build
 meson install -C _build
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME}*
+
+# Install Readest cross-platform, minimalist ebook reader from AppImage
+# https://readest.com/
+APP_NAME=Readest
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Cross-platform, minimalist ebook reader."
+APP_GUI_CATEGORIES="Accessories;Office"
+APP_GUI_KEYWORDS="Ebook;"
+APP_VERSION=0.9.78
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME,,}/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/${APP_NAME,,}/${APP_NAME,,}/refs/heads/main/data/icons/${APP_NAME,,}-book.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
