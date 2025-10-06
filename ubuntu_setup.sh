557f8bbe0571e4396b837d6d9f1dff4e5a9adb54
Tim Jones <tdjones74021@yahoo.com>
2025-10-06 15:25:23 -0500

    feat: Add commands to install ut Rust-based, cross-platform utility toolbox for developers from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -51271,3 +51271,18 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install ut Rust-based, cross-platform utility toolbox for developers from package
+APP_NAME=ut
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Rust-based, cross-platform utility toolbox for developers."
+APP_VERSION=0.1.0
+APP_EXT=tar.xz
+FILE_NAME=${APP_NAME,,}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/ksdme/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
