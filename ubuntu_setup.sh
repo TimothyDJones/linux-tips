1a6de7d5aae5b0a9726808494fb8fc901550d72b
Tim Jones <tdjones74021@yahoo.com>
2025-10-07 15:06:20 -0500

    feat: Add commands to install jIRCii cross-platform Java-based IRC client from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -51300,3 +51300,51 @@ sudo chmod a+x /usr/local/bin/${FILE_NAME}
 sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install jIRCii cross-platform Java-based IRC client from package
+APP_NAME=jIRCii
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform Java-based IRC client."
+APP_GUI_CATEGORIES="Internet;"
+APP_GUI_KEYWORDS="IRC;Messenger;"
+APP_VERSION=20250731
+APP_EXT=tgz
+FILE_NAME=jerk${APP_VERSION}
+if ! [ -x "$(command -v java)" ]; then
+	sudo apt install -y openjdk-17-jre
+fi
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://${APP_NAME,,}.dashnine.org/download/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://${APP_NAME,,}.dashnine.org/images/inner-logo.png
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -a -R /tmp/${APP_NAME,,}/* /opt/${APP_NAME,,}
+sudo cp -a -R /tmp/${APP_NAME,,}.png /opt/${APP_NAME,,}
+sudo chmod -R 755 /opt/${APP_NAME,,}
+sudo rm -rf /tmp/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/jerk.jar "$@"
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
