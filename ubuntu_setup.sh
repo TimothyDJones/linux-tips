5992f8f7b09031712923b2203ba4d1ec8cff54b8
Tim Jones <tdjones74021@yahoo.com>
2025-10-08 07:30:29 -0500

    feat: Update CTC (Coral Terminal Calculator) Golang-based command-line minimalist calculator to version 0.5.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -51277,7 +51277,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 APP_NAME=CTC
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Golang-based command-line minimalist calculator."
-APP_VERSION=0.4.0
+APP_VERSION=0.5.0
 APP_EXT=N/A
 FILE_NAME=${APP_NAME,,}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME} -J -L https://downloads.sourceforge.net/coral-ctc-terminal-calculator/${FILE_NAME}
