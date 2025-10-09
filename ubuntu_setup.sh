802c75ddea8864a0addd6ce16b4881cf42fcae40
Tim Jones <tdjones74021@yahoo.com>
2025-10-09 15:34:32 -0500

    feat: Add commands to install Reddix Rust-based, cross-platform Reddit client for the terminal from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -51433,3 +51433,18 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Reddix Rust-based, cross-platform Reddit client for the terminal from package
+APP_NAME=Reddix
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Rust-based, cross-platform Reddit client for the terminal."
+APP_VERSION=0.1.16
+APP_EXT=tar.xz
+FILE_NAME=${APP_NAME,,}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/ck-zhang/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
