4929ec01e32b6c40916a4e08be7958f949683857
Tim Jones <tdjones74021@yahoo.com>
2025-10-16 07:37:54 -0500

    feat: Add commands to install Flying Carpet cross-platform file transfer over ad hoc Wifi and Bluetooth from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -51559,3 +51559,36 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Flying Carpet cross-platform file transfer over ad hoc Wifi and Bluetooth from AppImage
+APP_NAME=FlyingCarpet
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Cross-platform file transfer over ad hoc Wifi and Bluetooth."
+APP_GUI_CATEGORIES="Networking;System;"
+APP_GUI_KEYWORDS="File;Transfer;"
+APP_VERSION=9.0.0
+APP_EXT=AppImage
+FILE_NAME=linux_${APP_NAME}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/spieglt/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/spieglt/${APP_NAME}/refs/heads/main/Flying%20Carpet/src/assets/icon1024.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
