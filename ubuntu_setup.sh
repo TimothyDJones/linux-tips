50c4408820514576c06f1906baf37246b2e0aa7f
Tim Jones <tdjones74021@yahoo.com>
2025-10-17 08:51:47 -0500

    feat: Add commands to install biff Rust-based, cross-platform command line tool for datetime arithmetic, parsing, formatting and more from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -51592,3 +51592,18 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install biff Rust-based, cross-platform command line tool for datetime arithmetic, parsing, formatting and more from package
+APP_NAME=biff
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Rust-based, cross-platform command line tool for datetime arithmetic, parsing, formatting and more."
+APP_VERSION=0.1.0
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-musl
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/BurntSushi/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+sudo rm -rf /tmp/${APP_NAME,,}*
