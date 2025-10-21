8342544e9475c4a12e498c5cef77de3f78f99747
Tim Jones <tdjones74021@yahoo.com>
2025-10-21 09:52:54 -0500

    feat: Add commands to install Atuin Rust-based shell history replacement tool from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -51664,3 +51664,18 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/AOSC-Dev/${APP_NAM
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install Atuin Rust-based shell history replacement tool from package
+APP_NAME=Atuin
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=18.9.0
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+echo 'eval "$(atuin init bash)"' >> ~/.bashrc
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
