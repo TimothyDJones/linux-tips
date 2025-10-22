8b6c0b9abe9d2ac62433fa70cf964159c5604db6
Tim Jones <tdjones74021@yahoo.com>
2025-10-22 07:20:57 -0500

    feat: Update Zen Browser cross-platform, minimalist, privacy-focused web browser to version 1.17b.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -47202,7 +47202,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '-' '_' )
 APP_GUI_NAME="Cross-platform, minimalist, privacy-focused web browser."
 APP_GUI_CATEGORIES="Internet;"
 APP_GUI_KEYWORDS="Web;Browser;"
-APP_VERSION=1.16.4b
+APP_VERSION=1.17b
 APP_EXT=AppImage
 sudo apt install -y libfuse2
 FILE_NAME=${APP_NAME,,}-generic
