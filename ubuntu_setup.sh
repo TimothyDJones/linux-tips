04dc1b982477a1b4ba7c9cf28144c68d51e77e7a
Tim Jones <tdjones74021@yahoo.com>
2025-11-14 10:21:05 -0600

    feat: Add commands to install dooit Python-based, cross-platform command-line comprehensive "to do" application from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -52143,3 +52143,16 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install dooit Python-based, cross-platform command-line comprehensive "to do" application from package
+APP_NAME=dooit
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=3.3.3
+APP_EXT=N/A
+FILE_NAME=linux-${APP_NAME,,}
+curl -o /tmp/${FILE_NAME} -J -L https://github.com/${APP_NAME,,}-org/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}
+sudo cp -a /tmp/${FILE_NAME} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${FILE_NAME}
+sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
