99dbee380bc4b5033a97d293a074d446c3ea775f
Tim Jones <tdjones74021@yahoo.com>
2025-11-18 16:38:24 -0600

    feat: Add commands to install treemd Rust-based, cross-platform command-line markdown viewer with tree-structured navigation from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -52170,3 +52170,19 @@ sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install treemd Rust-based, cross-platform command-line markdown viewer with tree-structured navigation from package
+APP_NAME=treemd
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.2.3
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/Epistates/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${FILE_NAME}
+sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
+/usr/local/bin/${APP_NAME,,} --setup-completions
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
