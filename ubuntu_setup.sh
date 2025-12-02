fa67a75c459b385d090c0d65d90230039c079c48
Tim Jones <tdjones74021@yahoo.com>
2025-12-02 07:25:35 -0600

    feat: Add commands to install bed Golang-based, cross-platform binary file editor from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -52297,3 +52297,17 @@ sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 sudo cp -a /tmp/${FILE_NAME}/_${APP_NAME,,} /usr/share/bash-completion/completions/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install bed Golang-based, cross-platform binary file editor from package
+APP_NAME=bed
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.2.8
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}_v${APP_VERSION}_linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/itchyny/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
