3eca079a2085d4b43a680be3cd387b5c7b1336a0
Tim Jones <tdjones74021@yahoo.com>
2025-12-16 16:49:03 -0600

    feat: Add commands to install DockMate Golang-based, cross-platform command-line Docker container manager from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -52468,3 +52468,17 @@ sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install DockMate Golang-based, cross-platform command-line Docker container manager from package
+APP_NAME=DockMate
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.0.7
+APP_EXT=N/A
+FILE_NAME=${APP_NAME,,}-linux-$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME} -J -L https://github.com/shubh-io/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}
+cd /tmp
+sudo cp -a /tmp/${FILE_NAME} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${FILE_NAME}
+sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
