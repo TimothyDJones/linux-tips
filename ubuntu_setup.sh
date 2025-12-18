e47d600b078f6e37ae94ccc8ec4720728ea50a3e
Tim Jones <tdjones74021@yahoo.com>
2025-12-18 07:10:04 -0600

    feat: Update mise cross‐platform, Rust-based, polyglot run-time package manager to version 2025.12.11.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -42388,7 +42388,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 APP_NAME=mise
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross‐platform, Rust-based, run-time package manager."
-APP_VERSION=2025.12.10
+APP_VERSION=2025.12.11
 APP_EXT=tar.xz
 BASE_ARCH=
 FILE_NAME=${APP_NAME,,}-v${APP_VERSION}-linux-x64
