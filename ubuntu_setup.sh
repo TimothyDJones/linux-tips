3362ea3911728b3d76b4be3da433161811fdabca
Tim Jones <tdjones74021@yahoo.com>
2025-12-19 13:47:04 -0600

    feat: Update tickrs Rust-based command-line stock ticker to version 0.15.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -37858,7 +37858,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 APP_NAME=tickrs
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
 APP_GUI_NAME="Rust-based command-line stock ticker."
-APP_VERSION=0.14.6
+APP_VERSION=0.15.0
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-v${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/tarkah/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
