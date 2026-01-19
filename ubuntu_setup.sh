f8ccdb4331b5d63461544be7aee842b2d2fb4a6b
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-01-19 10:22:09 -0600

    feat: Update Micro terminal-based text editor to version 2.0.15.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3427,7 +3427,7 @@ rm -rf /tmp/${APP_NAME,,}*
 
 # Install Micro terminal-based text editor from package
 APP_NAME=Micro
-APP_VERSION=2.0.14
+APP_VERSION=2.0.15
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=linux64
 else    # Otherwise use version for 32-bit kernel
