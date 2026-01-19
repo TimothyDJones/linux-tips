cc53087e5ff1f2392b62265ca2e5f5e8335e1105
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-01-19 11:26:33 -0600

    feat: Add commands to install LogoRRR cross-platform, JavaFX-based log viewer with recurring pattern search support from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -52578,3 +52578,14 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME}-app/${
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install LogoRRR cross-platform, JavaFX-based log viewer with recurring pattern search support from Debian package
+APP_NAME=LogoRRR
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=25.2.0
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/rladstaetter/${APP_NAME}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
