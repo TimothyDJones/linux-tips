993a15aaf3e705ab933a9311d1c3514a19aa0804
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-01-27 08:09:00 -0600

    feat: Update QSV cross‐platform, command-line tool for working with CSV files to version 15.0.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -49339,7 +49339,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 APP_NAME=QSV
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross‐platform, command-line tool for working with CSV files."
-APP_VERSION=7.0.0
+APP_VERSION=15.0.0
 APP_EXT=zip
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-musl
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
