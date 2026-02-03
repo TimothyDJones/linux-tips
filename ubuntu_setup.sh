a51103466d67ec7d4bbffabbf0b52c9dbb4834ab
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-02-03 13:35:38 -0600

    feat: Add commands to install sambam Golang-based command-line tool to create SAMBA file shares accessible from Windows/MacOS from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -53174,3 +53174,17 @@ sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install sambam Golang-based command-line tool to create SAMBA file shares accessible from Windows/MacOS from package
+APP_NAME=sambam
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=1.2.2
+APP_EXT=N/A
+FILE_NAME=${APP_NAME,,}-linux-$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME} -J -L https://github.com/darkpenguin23/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}
+cd /tmp
+sudo cp -a /tmp/${FILE_NAME} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${FILE_NAME}
+sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
