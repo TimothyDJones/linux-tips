cd46dddc4d2dbbd251806359b9a130820f5d9d2b
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-02-03 10:31:48 -0600

    feat: Add commands to install Pawno Golang-based, cross-platform command-line chess game from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -53160,3 +53160,17 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Pawno Golang-based, cross-platform command-line chess game from package
+APP_NAME=Pawno
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=1.1.3
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_Linux_$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://gitlab.com/philbooth/${APP_NAME,,}/-/releases/${APP_VERSION}/downloads/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
