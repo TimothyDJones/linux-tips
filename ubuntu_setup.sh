6d3905ff4c3c19dcd9188464d7e9ce8d98dfc3c7
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-02-04 08:43:23 -0600

    feat: Add commands to install Session cross-platform Electron-based decentralized, Onion-routed, private messenger from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -53256,3 +53256,37 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Session cross-platform Electron-based decentralized, Onion-routed, private messenger from AppImage
+APP_NAME=Session
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_GUI_NAME="Cross-platform Electron-based decentralized, Onion-routed, private messenger."
+APP_GUI_CATEGORIES="Internet;"
+APP_GUI_KEYWORDS="Messenger;Privacy;"
+APP_VERSION=1.17.8
+APP_EXT=AppImage
+FILE_NAME=${APP_NAME,,}-desktop-linux-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-${APP_VERSION}
+sudo apt install -y fuse
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME,,}-foundation/${APP_NAME,,}-desktop/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.png -J -L https://raw.githubusercontent.com/${APP_NAME,,}-foundation/${APP_NAME,,}-desktop/refs/heads/dev/build/icons/icon_128x128.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
