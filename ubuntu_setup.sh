6f8430ba537c1a6638af5ce995b9f5b731c1d064
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-02-05 08:08:35 -0600

    feat: Update mise cross‐platform, Rust-based, polyglot run-time package manager to version 2026.2.3.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -42430,7 +42430,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 APP_NAME=mise
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross‐platform, Rust-based, run-time package manager."
-APP_VERSION=2026.2.2
+APP_VERSION=2026.2.3
 APP_EXT=tar.xz
 BASE_ARCH=
 FILE_NAME=${APP_NAME,,}-v${APP_VERSION}-linux-x64
