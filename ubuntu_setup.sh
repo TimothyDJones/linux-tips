0b751dd9e71ce2214b6c678a4de9ecf713edea88
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-02-05 11:22:13 -0600

    feat: Add commands to install xan Rust-based, cross-platform command-line CSV viewer and data visualization from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -53290,3 +53290,17 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install xan Rust-based, cross-platform command-line CSV viewer and data visualization from package
+APP_NAME=xan
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.54.1
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/medialab/${APP_NAME}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
