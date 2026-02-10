5bc69cc9e94577731ccfb2e795b7945fc64b36de
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-02-10 16:17:51 -0600

    feat: Add commands to install godu Golang-based cross-platform command-line interactive disk usage analyzer from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -53495,3 +53495,17 @@ sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install godu Golang-based cross-platform command-line interactive disk usage analyzer from package
+APP_NAME=godu
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=1.0.0
+APP_EXT=N/A
+FILE_NAME=${APP_NAME,,}-linux-$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME} -J -L https://github.com/sadopc/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}
+cd /tmp
+sudo cp -a /tmp/${FILE_NAME} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${FILE_NAME}
+sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
