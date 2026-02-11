223bab4abb5fd7295b6c5b933d96556ea24a0fc0
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-02-11 07:40:23 -0600

    feat: Update FeedFlow Kotlin-based, cross-platform GUI RSS feed reader to version 1.10.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -50449,7 +50449,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 # Install FeedFlow Kotlin-based, cross-platform GUI RSS feed reader from Debian package
 APP_NAME=FeedFlow
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
-APP_VERSION=1.9.0
+APP_VERSION=1.10.0
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/prof18/feed-flow/releases/download/${APP_VERSION}-all/${FILE_NAME}.${APP_EXT}
