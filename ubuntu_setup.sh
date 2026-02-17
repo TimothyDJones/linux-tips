6315b0ff7c423c9191846e5458b8f71ac32de208
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-02-17 07:54:55 -0600

    feat: Update xonsh alternative, cross-platform Python-based console shell to version 0.22.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -14685,7 +14685,7 @@ rm -rf /tmp/*${APP_NAME}*
 # Install xonsh alternative, cross-platform Python-based console shell from package
 APP_NAME=xonsh
 APP_GUI_NAME="Alternative, cross-platform Python-based console shell."
-APP_VERSION=0.22.3
+APP_VERSION=0.22.4
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt install python3-pip -y
