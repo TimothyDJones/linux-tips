f274f5f8187bd05dde436ca2112624a8d075b634
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-02-18 11:35:16 -0600

    feat: Add commands to install Zerobrew Rust-based cross-platform alternative to Homebrew from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -53753,3 +53753,18 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Zerobrew Rust-based cross-platform alternative to Homebrew from package
+APP_NAME=Zerobrew
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.1.2
+APP_EXT=N/A
+FILE_NAME=zbx-linux-x64
+curl -o /tmp/${FILE_NAME} -J -L https://github.com/lucasgelfond/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}
+cd /tmp
+sudo cp -a /tmp/${FILE_NAME} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${FILE_NAME}
+sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
+sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/zbx
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
