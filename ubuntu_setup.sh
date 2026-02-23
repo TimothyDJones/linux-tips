1a7ef0db8597d17964db41742016a49d31bffe80
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-02-23 09:46:43 -0600

    feat: Update LibrePCB cross-platform EDA/PCB design tool to version 2.0.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -23565,7 +23565,7 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 # Install LibrePCB cross-platform EDA/PCB design tool from package
 APP_NAME=LibrePCB
 APP_GUI_NAME="Cross-platform EDA/PCB design tool."
-APP_VERSION=1.0.0
+APP_VERSION=2.0.1
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-linux-x86_64
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://download.${APP_NAME,,}.org/releases/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
