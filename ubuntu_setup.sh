04303eb3a50ce3327c164d3f5782536f99ee5132
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-02-26 11:31:21 -0600

    feat: Add commands to install Treekei Golang-based, cross-platform command-line file tree tool with file line counts from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -53969,3 +53969,17 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install Treekei Golang-based, cross-platform command-line file tree tool with file line counts from package
+APP_NAME=Treekei
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.2.3
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}_linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/zihao-liu-qs/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
