f62a2ad4772e47b4a1c3c83294e7a1cee241b055
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-03-03 07:46:42 -0600

    feat: Update Violet UML Editor Java-based UML modeling tool to version 2.3.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -15018,12 +15018,14 @@ rm -rf /tmp/*${APP_NAME}*
 # Install Violet UML Editor Java-based UML modeling tool from package
 APP_NAME=VioletUMLEditor
 APP_GUI_NAME="Java-based UML modeling tool."
-APP_VERSION=2.2.2
+APP_VERSION=2.3.0
 APP_EXT=jar
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-SNAPSHOT
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/violet/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.${ICON_EXT} -J -L https://a.fsdn.com/allura/p/${APP_NAME,,}/icon?1424280021
 sudo mkdir -p /opt/${APP_NAME,,}
 sudo mv /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.png /usr/local/share/icons/${APP_NAME,,}.png
 cat > /tmp/${APP_NAME,,} << EOF
 #! /bin/sh
 cd /opt/${APP_NAME,,}
@@ -15040,7 +15042,7 @@ Comment=${APP_GUI_NAME}
 GenericName=${APP_NAME}
 Path=/opt/${APP_NAME,,}
 Exec=java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
-Icon=
+Icon=/usr/local/share/icons/${APP_NAME,,}.png
 Type=Application
 StartupNotify=true
 Terminal=false
