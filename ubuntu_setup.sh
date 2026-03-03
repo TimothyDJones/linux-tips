a04eda906bf75073868c789295e4169673279f84
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-03-03 07:49:09 -0600

    feat: Update WoTerm cross-platform Qt-based SSH client/terminal with session manager to version 10.5.1-202603011108.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -21147,7 +21147,7 @@ sudo rm -rf /tmp/${APP_NAME,,} /tmp/${APP_NAME}
 # Install WoTerm cross-platform Qt-based SSH client/terminal with session manager from package
 APP_NAME=WoTerm
 APP_GUI_NAME="Cross-platform Qt-based SSH client/terminal with session manager."
-APP_VERSION=10.4.4-202601190650
+APP_VERSION=10.5.1-202603011108
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-linux-x86_64-portable-v${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
