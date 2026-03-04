27cb9e376a1b735f6a8b34e9e490ab7f1093feaa
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-03-04 16:23:17 -0600

    feat: Update Delta cross-platform, Rust-based GUI disk space analyzer with historical tracking from AppImage to version 0.1.6.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -54177,3 +54177,38 @@ sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install Delta cross-platform, Rust-based GUI disk space analyzer with historical tracking from AppImage
+APP_NAME=Delta
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform, Rust-based GUI disk space analyzer with historical tracking."
+APP_GUI_CATEGORIES="Accessories;System;"
+APP_GUI_KEYWORDS="Disk;Monitoring;"
+APP_VERSION=0.1.6
+APP_EXT=AppImage
+ICON_EXT=png
+FILE_NAME=${APP_NAME,,}-disk-analyzer_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+sudo apt install -y fuse libfuse2
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/chuunibian/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.${ICON_EXT} -J -L https://raw.githubusercontent.com/chuunibian/${APP_NAME,,}/refs/heads/main/src-tauri/icons/128x128.${ICON_EXT}
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.${ICON_EXT} /usr/local/share/icons/${APP_NAME,,}.${ICON_EXT}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.${ICON_EXT}
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
