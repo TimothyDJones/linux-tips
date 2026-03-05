fabbae1dd99266419c2ea6278c605b5a75a14566
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-03-05 07:58:31 -0600

    feat: Update Java Multiprecision Calculator cross-platform calculator based on Java BigDecimal class to version 2026-03-05.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -20237,7 +20237,7 @@ rm -rf /tmp/*${APP_NAME}* /tmp/*${APP_NAME,,}*
 # Install Java Multiprecision Calculator cross-platform calculator based on Java BigDecimal class from package
 APP_NAME="MultiPrecision Calculator"
 APP_GUI_NAME="Cross-platform calculator based on Java BigDecimal class."
-APP_VERSION=2025-10-15
+APP_VERSION=2026-03-05
 APP_EXT=7z
 FILE_NAME=${APP_VERSION//-/}.${APP_NAME// /}.v1.2
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/javamultiprecis/${FILE_NAME}.${APP_EXT}
