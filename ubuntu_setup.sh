90166f6d92c3bf713aaa782056d5c618f0948be4
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-03-09 13:22:08 -0500

    feat: Add commands to install diskard Rust-based, cross-platform command-line disk usage analyzer with delete capability from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -54429,3 +54429,17 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install diskard Rust-based, cross-platform command-line disk usage analyzer with delete capability from package
+APP_NAME=diskard
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.1.1
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/shoenot/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
