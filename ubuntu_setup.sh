240f28718ef0bd2418a9ef12119c74f3dc485422
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-03-09 08:35:02 -0500

    feat: Add commands to install Calcoo cross-platform Java-based GUI scientific calculator with RPN and algebraic modes from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -54307,3 +54307,50 @@ sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install Calcoo cross-platform Java-based GUI scientific calculator with RPN and algebraic modes from package
+APP_NAME=Calcoo
+APP_GUI_NAME="Cross-platform Java-based GUI scientific calculator with RPN and algebraic modes."
+APP_GUI_CATEGORIES="Accessories;System;"
+APP_GUI_KEYWORDS="Calculator;"
+APP_VERSION=2.1.1
+APP_EXT=jar
+FILE_NAME=${APP_NAME,,}
+if ! [ -x "$(command -v java)" ]; then
+	sudo apt install -y openjdk-25-jre
+fi
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}-src.zip -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${APP_NAME,,}-src.zip
+cd /tmp
+mkdir -p /tmp/${APP_NAME,,}-src
+unzip /tmp/${APP_NAME,,}-src.zip -d /tmp/${APP_NAME,,}-src
+sudo mkdir -p /opt/${APP_NAME,,}
+sudo cp -a -R /tmp/${FILE_NAME}.${APP_EXT} /opt/${APP_NAME,,}
+sudo cp -a -R /tmp/${APP_NAME,,}-src/src/main/resources/icons/main/${APP_NAME,,}.png /opt/${APP_NAME,,}
+sudo chmod -R 755 /opt/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,} << EOF
+#! /bin/sh
+cd /opt/${APP_NAME,,}
+PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
+java -jar /opt/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd $HOME
+EOF
+sudo mv /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/opt/${APP_NAME,,}
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/opt/${APP_NAME,,}/${APP_NAME,,}.png
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
