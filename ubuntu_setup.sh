84ad3855cdb709bef25c3d8dcc1dcb1418354a8c
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-03-09 08:00:24 -0500

    feat: Update Skychart planetarium package to version 4.3-5033.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1532,7 +1532,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install Skychart planetarium package from Debian package
 APP_NAME=Skychart
-APP_VERSION=4.3-5032
+APP_VERSION=4.3-5033
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
 # libpasastro (Pascal astronomical library) is dependency for Skychart.
