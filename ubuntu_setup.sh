9f83007970e8ad0880d227dacf59531f80b05565
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-03-12 11:39:16 -0500

    feat: Add commands to install guIDE cross-platform Electron-based local-first GUI IDE with integration with multiple LLM providers from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -54513,3 +54513,38 @@ sudo chmod a+x /usr/local/bin/fs
 sudo ln -s -f /usr/local/bin/fs /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install guIDE cross-platform Electron-based local-first GUI IDE with integration with multiple LLM providers from AppImage
+APP_NAME=guIDE
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform Electron-based local-first GUI IDE with integration with multiple LLM providers."
+APP_GUI_CATEGORIES="Development;Programming;"
+APP_GUI_KEYWORDS="IDE;Editor;AI;LLM;"
+APP_VERSION=1.8.23
+APP_EXT=AppImage
+ICON_EXT=png
+FILE_NAME=${APP_NAME}-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+sudo apt install -y fuse libfuse2
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/FileShot/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.${ICON_EXT} -J -L https://raw.githubusercontent.com/FileShot/${APP_NAME}/refs/heads/main/build/icon.png
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.${ICON_EXT} /usr/local/share/icons/${APP_NAME,,}.${ICON_EXT}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.${ICON_EXT}
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
