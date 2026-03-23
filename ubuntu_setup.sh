30b62c03e5032bc3d433d01eddd783bbb7c0e4d4
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-03-23 08:01:40 -0500

    feat: Update PDF Split and Merge (PDFsam) editor to version 6.0.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -3904,7 +3904,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install PDF Split and Merge (PDFsam) editor from Debian package
 APP_NAME=PDFSam
-APP_VERSION=5.4.5
+APP_VERSION=6.0.0
 APP_EXT=deb
 if $(uname -m | grep '64'); then  # Check for 64-bit Linux kernel
 	ARCH_TYPE=amd64
