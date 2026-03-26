8db81cf36e9962c05c2c0c580250a8274f09076b
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-03-26 13:02:38 -0500

    feat: Add commands to install Fincept Terminal cross-platform C++/Qt-based financial intelligence platform from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -54712,3 +54712,38 @@ EOF
 sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${_APP_NAME}*
+
+# Install Fincept Terminal cross-platform C++/Qt-based financial intelligence platform from AppImage
+APP_NAME="Fincept Terminal"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform C++/Qt-based financial intelligence platform."
+APP_GUI_CATEGORIES="Finance;Investing;"
+APP_GUI_KEYWORDS="Finance;Investing;"
+APP_VERSION=3.3.3
+APP_EXT=AppImage
+ICON_EXT=png
+FILE_NAME=${APP_NAME// /}-v${APP_VERSION}-Linux-x64
+sudo apt install -y fuse libfuse2
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/Fincept-Corporation/${APP_NAME// /}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.${ICON_EXT} -J -L https://a.fsdn.com/allura/mirror/fincept-terminal/icon?ba4bd9ca12979a3f7d09d7b1b22c84b0e36a6d75f68aeea8db360408ac1be6ac
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${_APP_NAME}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${_APP_NAME}.${ICON_EXT} /usr/local/share/icons/${_APP_NAME}.${ICON_EXT}
+cat > /tmp/${_APP_NAME}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${_APP_NAME}
+Icon=/usr/local/share/icons/${_APP_NAME}.${ICON_EXT}
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME// /}* /tmp/${_APP_NAME}*
