1ecc2cc3718f70a16086f60478a47a5b98386789
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-04-06 07:07:16 -0500

    feat: Update ztlgr Rust-based, cross-platform command-line notepad supporting Zettelkasten methodology to version 0.3.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -55089,17 +55089,16 @@ sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
 
-# Install ztlrg Rust-based, cross-platform command-line notepad supporting Zettelkasten methodology from package
-APP_NAME=ztlrg
+# Install ztlgr Rust-based, cross-platform command-line notepad supporting Zettelkasten methodology from package
+APP_NAME=ztlgr
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
-APP_VERSION=0.1.1
+APP_VERSION=0.3.0
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/bakudas/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
 cd /tmp
 tar -xf /tmp/${FILE_NAME}.${APP_EXT}
-sudo cp -a /tmp/${FILE_NAME}/jg /usr/local/bin
-sudo chmod a+x /usr/local/bin/jg
-sudo ln -s -f /usr/local/bin/jg /usr/local/bin/${APP_NAME,,}
+sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
