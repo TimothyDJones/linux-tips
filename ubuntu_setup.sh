f567cfe05a8ae3c0e629cc9eed992c5625f89a34
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-04-07 15:33:12 -0500

    feat: Add commands to install Slim Golang-based, cross-platform command-line tool to map localhost to clean URLs from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -55314,3 +55314,17 @@ sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install Slim Golang-based, cross-platform command-line tool to map localhost to clean URLs from package
+APP_NAME=Slim
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.9.1
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/kamranahmedse/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
