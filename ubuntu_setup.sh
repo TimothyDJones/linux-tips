49921409e6eec2078de4e20d4523477a51816150
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-04-08 14:20:14 -0500

    feat: Add commands to install Midday Commander Golang-based, cross-platform command-line file manager inspired by Midnight Commander from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -55357,3 +55357,17 @@ sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install Midday Commander Golang-based, cross-platform command-line file manager inspired by Midnight Commander from package
+APP_NAME="Midday Commander"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.3
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME// /}_${APP_VERSION}_linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/kooler/${APP_NAME// /}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/mdc /usr/local/bin
+sudo chmod a+x /usr/local/bin/mdc
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
