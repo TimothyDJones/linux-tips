e11028bfa1f27585d552cf8c0e4bca868ac8f99b
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-04-08 14:10:24 -0500

    feat: Add commands to install fast-jump (fj) Rust-based, cross-platform command-line directory jumper from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -55343,3 +55343,17 @@ sudo chmod a+x /usr/local/bin/${FILE_NAME}
 sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install fast-jump (fj) Rust-based, cross-platform command-line directory jumper from package
+APP_NAME=fj
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.1.0
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/Kandarp05/fast-jump/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
