038ba207b4a7d755b30c59e5e9ca99587d564b8d
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-04-09 16:45:20 -0500

    feat: Add commands to install tiki Golang-based, cross-platform command-line Markdown viewer focused on Git documentation from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -55429,3 +55429,17 @@ sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install tiki Golang-based, cross-platform command-line Markdown viewer focused on Git documentation from package
+APP_NAME=tiki
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.3.4
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/boolean-maybe/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
