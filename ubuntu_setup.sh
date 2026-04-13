77bd068bc886ca4f196990b7c814fbdc8c6bf327
Hyper-V User <hyperv@debian-hyperv.mshome.net>
2026-04-13 12:22:29 -0500

    feat: Update HeidiSQL cross-platform, lightweight client for multiple SQL databases to version 12.17.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -52399,7 +52399,7 @@ rm -rf /tmp/*${APP_NAME,,}*
 # Install HeidiSQL cross-platform, lightweight client for multiple SQL databases from Debian package
 APP_NAME=HeidiSQL
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
-APP_VERSION=12.16
+APP_VERSION=12.17
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
