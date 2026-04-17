374a5c0512ddd073bb0018405b80cbff8f57da42
Tim Jones <tdjones74021@yahoo.com>
2026-04-17 09:15:06 -0500

    feat: Update SpeeD ReaD cross-platform GUI speed reading tool to version 2.2.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -54619,7 +54619,7 @@ rm -rf /tmp/*${APP_NAME,,}*
 # Install SpeeD ReaD cross-platform GUI speed reading tool from Debian package
 APP_NAME="SpeeD ReaD"
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]' )
-APP_VERSION=2.2.0
+APP_VERSION=2.2.1
 APP_EXT=deb
 FILE_NAME=${_APP_NAME}.${APP_VERSION}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/speed-read/${FILE_NAME}.${APP_EXT}
