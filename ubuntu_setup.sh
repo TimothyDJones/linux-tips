7e1a6e1346ced608d0f0cafb92593d051da25d93
Tim Jones <tdjones74021@yahoo.com>
2026-04-30 16:36:33 -0500

    feat: Add commands to install PrettyMux GTK4-based terminal multiplexer built on Ghostty from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -55843,3 +55843,36 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
+
+# Install PrettyMux GTK4-based terminal multiplexer built on Ghostty from AppImage
+APP_NAME=PrettyMux
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="GTK4-based terminal multiplexer built on Ghostty."
+APP_GUI_CATEGORIES="Accessories;System;"
+APP_GUI_KEYWORDS="Terminal;Shell;"
+APP_VERSION=0.2.28
+APP_EXT=AppImage
+ICON_EXT=png
+FILE_NAME=${APP_NAME}-${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+sudo apt install -y fuse libfuse2 libadwaita-1-0 libwebkitgtk-6.0-4
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/patcito/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+# Icon=/usr/local/share/icons/${APP_NAME,,}.${ICON_EXT}
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
