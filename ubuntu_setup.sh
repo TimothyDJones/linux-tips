52f5e62470e2d7e0eb83ce4824f1e58c9dd3840d
Tim Jones <tdjones74021@yahoo.com>
2026-05-20 14:51:34 -0500

    feat: Add commands to install systemctl-tui Rust-based, cross-platform command-line tool for managing SystemD services with support for multimedia from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -56834,3 +56834,17 @@ sudo chmod a+x /usr/local/bin/${FILE_NAME}
 sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install systemctl-tui Rust-based, cross-platform command-line tool for managing SystemD services with support for multimedia from package
+APP_NAME=systemctl-tui
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.5.2
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-musl
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/rgwood/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
