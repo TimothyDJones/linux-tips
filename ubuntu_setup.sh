838496cdb847df3aa2bcad58d539057e2917c567
Tim Jones <tdjones74021@yahoo.com>
2026-05-21 08:15:24 -0500

    feat: Add commands to install fooyin cross-platform, minimalist desktop music player from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -56862,3 +56862,14 @@ sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install fooyin cross-platform, minimalist desktop music player from Debian package
+APP_NAME=fooyin
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]' )
+APP_VERSION=0.10.8
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}-$(lsb_release -c | awk '{print $2}')_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
