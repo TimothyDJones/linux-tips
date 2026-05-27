7ebec9d61bf4b1344951b80ae52ccbc2ff6f14c7
Tim Jones <tdjones74021@yahoo.com>
2026-05-27 14:10:27 -0500

    feat: Add commands to install doxx Rust-based, cross-platform command-line Word .docx file viewer with support for Markdown/CSV/JSON export from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -57252,3 +57252,17 @@ sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install doxx Rust-based, cross-platform command-line Word .docx file viewer with support for Markdown/CSV/JSON export from package
+APP_NAME=doxx
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.1.4
+APP_EXT=tar.xz
+FILE_NAME=${APP_NAME,,}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/bgreenwell/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
