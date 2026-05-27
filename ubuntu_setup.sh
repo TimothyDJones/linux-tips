7b1a0b10c332a649ba423d4deb7126d3645d0b3d
Tim Jones <tdjones74021@yahoo.com>
2026-05-27 09:25:11 -0500

    feat: Update FLTube FLTK-based minimalist Youtube client to version 2.1.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -52807,7 +52807,7 @@ rm -rf /tmp/*${APP_NAME,,}*
 # Install FLTube FLTK-based minimalist Youtube client from Debian package
 APP_NAME=FLTube
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
-APP_VERSION=2.1.0
+APP_VERSION=2.1.2
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
