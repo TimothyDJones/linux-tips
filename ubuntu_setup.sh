7a1a5a8b7f6861c7d7a7d8ab3ade81b04d1a432a
Tim Jones <tdjones74021@yahoo.com>
2026-05-27 08:05:37 -0500

    feat: Update Restish Golang-based command-line REST API client to version 2.0.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -27221,11 +27221,11 @@ rm -rf /tmp/${_APP_NAME}*
 APP_NAME=Restish
 APP_VERSION=2.0.0
 APP_EXT=tar.gz
-FILE_NAME=${APP_NAME,,}-${APP_VERSION}-linux-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}-linux-$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/rest-sh/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
 cd /tmp
-dtrx -n /tmp/${FILE_NAME}.${APP_EXT}
-sudo cp /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp /tmp/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
