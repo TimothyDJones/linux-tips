fceb5a23f12e14a6e82a620e6e8b2c95e74c9717
Tim Jones <tdjones74021@yahoo.com>
2026-06-02 08:02:15 -0500

    feat: Update vifm file manager to version 0.14.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1731,7 +1731,7 @@ rm -rf /tmp/${APP_NAME}*
 
 # Install vifm file manager from source
 APP_NAME=vifm
-APP_VERSION=0.14.3
+APP_VERSION=0.14.4
 APP_EXT=tar.bz2
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get install -y libncursesw5-dev
