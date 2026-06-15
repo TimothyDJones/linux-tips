ab6629bc4a254c637a62f9150297cfb83d5f0b15
Tim Jones <tdjones74021@yahoo.com>
2026-06-15 16:57:30 -0500

    feat: Update Yang Downloader YouTube video downloader that saves videos in VP9 format to version 3.4.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -41655,7 +41655,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="YouTube video downloader that saves videos in VP9 format."
 APP_GUI_CATEGORIES="Multimedia;Video;Internet;"
 APP_GUI_KEYWORDS="YouTube;Video;"
-APP_VERSION=3.3.1
+APP_VERSION=3.4
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME// /}-${APP_VERSION}_Linux_x64
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_APP_NAME//-/}/${FILE_NAME}.${APP_EXT}
