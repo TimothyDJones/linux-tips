d1dd4d6ea0b9fa58fa12c07eef36f1792b39bf87
Tim Jones <tdjones74021@yahoo.com>
2026-06-16 08:04:43 -0500

    feat: Add commands to install PasKnight Pascal-based, UCI-compatible chess engine from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -57627,3 +57627,19 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install PasKnight Pascal-based, UCI-compatible chess engine from package
+APP_NAME=PasKnight
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.930
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME}_v${APP_VERSION//./}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+mkdir -p /tmp/${FILE_NAME}
+tar -xf /tmp/${FILE_NAME}.${APP_EXT} -C /tmp/${FILE_NAME}
+sudo cp -a /tmp/${FILE_NAME}/${FILE_NAME} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${FILE_NAME}
+sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
