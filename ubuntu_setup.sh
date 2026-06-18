7aadf577555d4a33c85a59c05a137d31fe5ae30e
Tim Jones <tdjones74021@yahoo.com>
2026-06-18 09:41:10 -0500

    feat: Add commands to install Cardboard Circuit cross-platform, Python/Qt/PyGame-based retro slot-car racing game from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -57889,3 +57889,15 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${A
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install Cardboard Circuit cross-platform, Python/Qt/PyGame-based retro slot-car racing game from Debian package
+# https://github.com/shampuan/cardboard-circuit
+APP_NAME="Cardboard Circuit"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]' )
+APP_VERSION=1.2.0
+APP_EXT=deb
+FILE_NAME=${_APP_NAME}.${APP_VERSION}
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-' )/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
