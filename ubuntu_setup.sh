205f156bb365d3ff951d1564b3072fb52c108141
Tim Jones <tdjones74021@yahoo.com>
2026-06-25 14:24:19 -0500

    feat: Add commands to install ruclouds Rust-based, cross-platform command line animated, drifting clouds simulation from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -58319,3 +58319,17 @@ sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install ruclouds Rust-based, cross-platform command line animated, drifting clouds simulation from package
+APP_NAME=ruclouds
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.5.0
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-linux-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/AndyFerns/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp//${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
