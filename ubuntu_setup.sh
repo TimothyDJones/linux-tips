79167d9d1fff3d6a7eed817d0822bc364e5ffde7
Tim Jones <tdjones74021@yahoo.com>
2026-06-26 14:29:15 -0500

    feat: Add commands to install MdFried Rust-based, cross-platform terminal Markdown viewer with support for sixel graphics from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -58368,3 +58368,16 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install MdFried Rust-based, cross-platform terminal Markdown viewer with support for sixel graphics from package
+APP_NAME=MdFried
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.22.4
+APP_EXT=N/A
+FILE_NAME=${APP_NAME,,}
+curl -o /tmp/${FILE_NAME} -J -L https://github.com/benjajaja/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}
+cd /tmp
+sudo cp -a /tmp/${FILE_NAME} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${FILE_NAME}
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
