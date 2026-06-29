3d797e231c06334f4e4007d8e71fcbd1e3c518e5
Tim Jones <tdjones74021@yahoo.com>
2026-06-29 08:40:05 -0500

    feat: Add commands to install Markdown-Oxide Rust-based, cross-platform Personal Knowledge Management (PKM) system Language Server Protocol (LSP) tool for Markdown notes from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -58396,3 +58396,17 @@ sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/tw
 sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME}*
+
+# Install Markdown-Oxide Rust-based, cross-platform Personal Knowledge Management (PKM) system Language Server Protocol (LSP) tool for Markdown notes from package
+APP_NAME=Markdown-Oxide
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.25.12
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-v${APP_VERSION}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME}*
