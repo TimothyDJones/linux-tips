95ef0d0002f443e5284e19c4ecc9bda42c953831
Tim Jones <tdjones74021@yahoo.com>
2026-07-01 16:51:14 -0500

    feat: Add commands to install qj Rust-based, cross-platform jq-compatible command-line JSON parser/viewer from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -58640,3 +58640,17 @@ sudo chmod -R 0755 /opt/${APP_NAME,,}
 sudo ln -s -f /opt/${APP_NAME,,}/${APP_NAME,,} /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install qj Rust-based, cross-platform jq-compatible command-line JSON parser/viewer from package
+APP_NAME=qj
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]')
+APP_VERSION=0.1.4
+APP_EXT=tar.gz
+FILE_NAME=${APP_NAME,,}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/6/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
