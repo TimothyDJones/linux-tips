e1fdbd0c9be75655d2892f52df5d00bff933e665
Tim Jones <tdjones74021@yahoo.com>
2026-07-02 16:20:55 -0500

    feat: Add commands to install SimpMusic cross-platform, Kotlin/Electron-based music player based on YouTube Music from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -58726,3 +58726,38 @@ zstd -d /tmp/${FILE_NAME}.${APP_EXT} && tar -xvf /tmp/${FILE_NAME}.tar -d /tmp/$
 sudo cp -a -R /tmp/${FILE_NAME}/* /usr/local
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
+
+# Install SimpMusic cross-platform, Kotlin/Electron-based music player based on YouTube Music from AppImage
+APP_NAME=SimpMusic
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform, Kotlin/Electron-based music player based on YouTube Music."
+APP_GUI_CATEGORIES="Audio;Multimedia;"
+APP_GUI_KEYWORDS="Music;Player;"
+APP_VERSION=1.5.1
+APP_EXT=AppImage
+ICON_EXT=png
+FILE_NAME=${APP_NAME}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+sudo apt install -y fuse libfuse2
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/maxrave-dev/${APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${APP_NAME,,}.${ICON_EXT} -J -L https://raw.githubusercontent.com/maxrave-dev/${APP_NAME}/refs/heads/main/fastlane/metadata/android/en-US/images/icon.${ICON_EXT}
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.${ICON_EXT} /usr/local/share/icons/${APP_NAME,,}.${ICON_EXT}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.${ICON_EXT}
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
