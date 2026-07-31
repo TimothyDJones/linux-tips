52a72cfaf4ad0748e908bea3823549381aa454db
Tim Jones <tdjones74021@yahoo.com>
2026-07-31 08:07:26 -0500

    feat: Update ugrep cross-platform enhanced grep tool which supports searching in archives and binary files to version 7.8.3.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -24499,7 +24499,7 @@ sudo rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
 
 # Install ugrep cross-platform enhanced grep tool which supports searching in archives and binary files from source
 APP_NAME=ugrep
-APP_VERSION=7.8.2
+APP_VERSION=7.8.3
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}-${APP_VERSION}
 sudo apt-get install -y build-essential libbz2-dev libz-dev liblzma-dev libpcre2-dev libboost-regex-dev
