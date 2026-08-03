bf74fe3690ba824cebec6e33c63dec8838e6351e
Tim Jones <tdjones74021@yahoo.com>
2026-08-03 16:56:48 -0500

    feat: Add commands to install Aero P2P Chat cross-platform, minimalist P2P chat/messenger tool from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -58978,3 +58978,39 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install Aero P2P Chat cross-platform, minimalist P2P chat/messenger tool from AppImage
+# https://github.com/Zorblock/AeroP2Pchat
+APP_NAME="Aero P2P Chat"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform, minimalist P2P chat/messenger tool."
+APP_GUI_CATEGORIES="Internet;"
+APP_GUI_KEYWORDS="P2P;Chat;Messenger;"
+APP_VERSION=26.50.1
+APP_EXT=AppImage
+ICON_EXT=png
+FILE_NAME=${APP_NAME// /-}-Linux-x64
+sudo apt install -y fuse libfuse2
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_APP_NAME//-/}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.${ICON_EXT} -J -L https://a.fsdn.com/allura/p/${_APP_NAME//-/}/icon?1785786304
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${_APP_NAME}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${_APP_NAME}.${ICON_EXT} /usr/local/share/icons/${_APP_NAME}.${ICON_EXT}
+cat > /tmp/${_APP_NAME}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/	usr/local/bin
+Exec=/usr/local/bin/${_APP_NAME}
+Icon=/usr/local/share/icons/${_APP_NAME}.${ICON_EXT}
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${_APP_NAME}* /tmp/${APP_NAME// /-}*
