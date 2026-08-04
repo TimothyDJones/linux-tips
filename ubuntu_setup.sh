12a4100b8cabf5398e720127f01f38d00385cf2f
Tim Jones <tdjones74021@yahoo.com>
2026-08-04 11:02:59 -0500

    feat: Add commands to install bulletty cross-platform, Rust-based command-line RSS/Atom feed reader from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -59026,3 +59026,21 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${A
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install bulletty cross-platform, Rust-based command-line RSS/Atom feed reader from package
+APP_NAME=bulletty
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform, Rust-based command-line RSS/Atom feed reader."
+APP_GUI_CATEGORIES="Internet;"
+APP_GUI_KEYWORDS="RSS;Reader;"
+APP_VERSION=0.2.2
+APP_EXT=tar.xz
+ICON_EXT=png
+FILE_NAME=${APP_NAME,,}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-linux-gnu
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/CrociDB/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+cd /tmp
+tar -xf /tmp/${FILE_NAME}.${APP_EXT}
+sudo cp -a /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}*
