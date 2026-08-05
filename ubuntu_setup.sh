8fa2fc0bf233a1bfa00f6cac8653283eb03a62d9
Tim Jones <tdjones74021@yahoo.com>
2026-08-05 14:55:58 -0500

    feat: Add commands to install Atomic Chat cross-platform, Tauri-based self-hosted AI/LLM chatbot with built-in Llama.cpp from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -59122,3 +59122,39 @@ EOF
 sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${_APP_NAME}* /tmp/${APP_NAME// /-}*
+
+# Install Atomic Chat cross-platform, Tauri-based self-hosted AI/LLM chatbot with built-in Llama.cpp from AppImage
+# https://github.com/AtomicBot-ai/Atomic-Chat
+APP_NAME="Atomic Chat"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform,  Tauri-based self-hosted AI/LLM chatbot with built-in Llama.cpp."
+APP_GUI_CATEGORIES="Accessories;System;"
+APP_GUI_KEYWORDS="AI;LLM;Chatbot;"
+APP_VERSION=2.0.6
+APP_EXT=AppImage
+ICON_EXT=png
+FILE_NAME=${APP_NAME// /.}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+sudo apt install -y fuse libfuse2
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_APP_NAME}.mirror/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.${ICON_EXT} -J -L https://a.fsdn.com/allura/mirror/${_APP_NAME}/icon?9bfe82f26bf49f0c50fe0836fcd4dee144470b829ac18720c9ca446729035761
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${_APP_NAME}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${_APP_NAME}.${ICON_EXT} /usr/local/share/icons/${_APP_NAME}.${ICON_EXT}
+cat > /tmp/${_APP_NAME}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/	usr/local/bin
+Exec=/usr/local/bin/${_APP_NAME}
+Icon=/usr/local/share/icons/${_APP_NAME}.${ICON_EXT}
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${_APP_NAME}* /tmp/${APP_NAME// /-}*
