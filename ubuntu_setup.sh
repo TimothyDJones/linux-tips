51e286a1175036ee5b81568a3c1bd717aa15d0d1
Tim Jones <tdjones74021@yahoo.com>
2026-08-10 15:21:09 -0500

    feat: Add commands to install HTTP Toolkit cross-platform, Electron-based HTTP client with debugging proxy from AppImage."

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -59272,3 +59272,38 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install HTTP Toolkit cross-platform, Electron-based HTTP client with debugging proxy from AppImage
+APP_NAME="HTTP Toolkit"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]' )
+APP_GUI_NAME="Cross-platform, Electron-based HTTP client with debugging proxy."
+APP_GUI_CATEGORIES="Programming;Development;"
+APP_GUI_KEYWORDS="HTTP;Web;Services;"
+APP_VERSION=1.27.0
+APP_EXT=AppImage
+ICON_EXT=png
+FILE_NAME=HttpToolkit-${APP_VERSION}-x64
+sudo apt install -y fuse libfuse2
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${_APP_NAME}/${_APP_NAME}-desktop/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.${ICON_EXT} -J -L https://raw.githubusercontent.com/${_APP_NAME}/${_APP_NAME}-desktop/refs/heads/main/src/icons/icon.${ICON_EXT}
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${_APP_NAME}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${_APP_NAME}.${ICON_EXT} /usr/local/share/icons/${_APP_NAME}.${ICON_EXT}
+cat > /tmp/${_APP_NAME}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/	usr/local/bin
+Exec=/usr/local/bin/${_APP_NAME}
+Icon=/usr/local/share/icons/${_APP_NAME}.${ICON_EXT}
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${_APP_NAME}* /tmp/${APP_NAME// /-}*
