11a50bed3c143db71d80c9e9a27d2483d23f2c20
Tim Jones <tdjones74021@yahoo.com>
2026-08-14 07:45:39 -0500

    feat: Update Nuclear cross-platform Electron-based desktop music player focused on streaming from free sources to version 1.46.3.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -17217,7 +17217,7 @@ rm -rf /tmp/*${APP_NAME// /}*
 # Install Nuclear cross-platform Electron-based desktop music player focused on streaming from free sources from Debian package
 APP_NAME=Nuclear
 APP_GUI_NAME="Cross-platform Electron-based desktop music player focused on streaming from free sources."
-APP_VERSION=1.46.2
+APP_VERSION=1.46.3
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/nukeop/${APP_NAME,,}/releases/download/player@${APP_VERSION}/${FILE_NAME}.${APP_EXT}
