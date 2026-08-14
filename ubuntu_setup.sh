b7171bd25f157aecea3277e10c517f5cc86bce39
Tim Jones <tdjones74021@yahoo.com>
2026-08-14 08:17:48 -0500

    feat: Update Penguin's Eggs command-line utility to create live CD ISO from current Debian/Ubuntu system to version 26.8.14-1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -24685,7 +24685,7 @@ sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 # Install Penguin's Eggs command-line utility to create live CD ISO from current Debian/Ubuntu system from Debian package
 APP_NAME=Eggs
 APP_GUI_NAME="Command-line utility to create live CD ISO from current Debian/Ubuntu system."
-APP_VERSION=26.7.31-2
+APP_VERSION=26.8.14-1
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_${KERNEL_TYPE}
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/penguins-eggs/${FILE_NAME}.${APP_EXT}
@@ -59406,7 +59406,7 @@ cat > /tmp/${_APP_NAME}.desktop << EOF
 Name=${APP_NAME}
 Comment=${APP_GUI_NAME}
 GenericName=${APP_NAME}
-Path=/	usr/local/bin
+Path=/usr/local/bin
 Exec=/usr/local/bin/${_APP_NAME}
 Icon=/usr/local/share/icons/${_APP_NAME}.${ICON_EXT}
 Type=Application
@@ -59418,3 +59418,38 @@ EOF
 sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${_APP_NAME}* /tmp/${APP_NAME// /-}*
+
+# Install LineSolv cross-platform, Golang/Typescript-based desktop natural-language calculator from package
+# https://github.com/rkriad585/LineSolv
+APP_NAME=LineSolv
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform, Golang/Typescript-based desktop natural-language calculator."
+APP_GUI_CATEGORIES="Education;Science;Math;"
+APP_GUI_KEYWORDS="Calculator;"
+APP_VERSION=0.17.0
+APP_EXT=N/A
+ICON_EXT=png
+FILE_NAME=${APP_NAME}-${APP_VERSION}-linux-$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}
+curl -o /tmp/${APP_NAME,,}.${ICON_EXT} -J -L https://a.fsdn.com/allura/p/${APP_NAME,,}/icon?42aa6758a6389ab6f1dd53f74d17962e90adab60e8a5e09ca7e43c4cf9f421fa
+sudo cp -a /tmp/${FILE_NAME} /usr/local/bin
+sudo chmod a+x /usr/local/bin/${FILE_NAME}
+sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${APP_NAME,,}.${ICON_EXT} /usr/local/share/icons/${APP_NAME,,}.${ICON_EXT}
+cat > /tmp/${APP_NAME,,}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${APP_NAME,,}
+Icon=/usr/local/share/icons/${APP_NAME,,}.${ICON_EXT}
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${APP_NAME,,}* /tmp/${FILE_NAME}*
