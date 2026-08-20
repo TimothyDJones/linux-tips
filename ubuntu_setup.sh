de97770699ab6c68b7919d51b66242b432be50a3
Tim Jones <tdjones74021@yahoo.com>
2026-08-20 07:56:00 -0500

    feat: Add commands to install PDF Bunny cross-platform, Python/Qt-based PDF viewer from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -59622,3 +59622,38 @@ tar -xf /tmp/${FILE_NAME}.${APP_EXT} -C /tmp/${FILE_NAME}
 sudo cp -a -R /tmp/${FILE_NAME}/* /usr/local
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${FILE_NAME}*
+
+# Install PDF Bunny cross-platform, Python/Qt-based PDF viewer from AppImage
+APP_NAME="PDF Bunny"
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-' )
+APP_GUI_NAME="Cross-platform, Python/Qt-based PDF viewer."
+APP_GUI_CATEGORIES="Office;Accessories;"
+APP_GUI_KEYWORDS="PDF;"
+APP_VERSION=3.4.1
+APP_EXT=AppImage
+ICON_EXT=png
+FILE_NAME=${APP_NAME// /_}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)
+sudo apt install -y fuse libfuse2
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/ksharindam/${_APP_NAME}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.${ICON_EXT} -J -L https://raw.githubusercontent.com/ksharindam/${_APP_NAME}/refs/heads/main/data/icons/${_APP_NAME}.${ICON_EXT}
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${_APP_NAME}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${_APP_NAME}.${ICON_EXT} /usr/local/share/icons/${_APP_NAME}.${ICON_EXT}
+cat > /tmp/${_APP_NAME}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${_APP_NAME}
+Icon=/usr/local/share/icons/${_APP_NAME}.${ICON_EXT}
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${_APP_NAME}* /tmp/${APP_NAME// /-}*
