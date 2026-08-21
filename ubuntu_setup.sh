a1cf39ff3976805c56641e9673fadaa4eee36684
Tim Jones <tdjones74021@yahoo.com>
2026-08-21 16:27:33 -0500

    feat: Update Rotki Electron-based cryptocurrency portfolio tracking tool to version 1.44.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -27760,7 +27760,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Electron-based cryptocurrency portfolio tracking tool."
 APP_GUI_CATEGORIES="Internet;"
 APP_GUI_KEYWORDS="Blockchain;Cryptocurrency;Bitcoin;"
-APP_VERSION=1.43.2
+APP_VERSION=1.44.0
 APP_EXT=AppImage
 FILE_NAME=${APP_NAME,,}-linux_$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-v${APP_VERSION}
 sudo apt install -y libfuse2
