8b14e455ffaccc4a6e21cab2fdfa76fc842aca59
Tim Jones <tdjones74021@yahoo.com>
2026-08-21 10:58:56 -0500

    feat: Update Gitnuro cross-platform Java-based GUI Git client to version 1.5.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -48239,7 +48239,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross-platform Java-based GUI Git client."
 APP_GUI_CATEGORIES="Programming;Development;"
 APP_GUI_KEYWORDS="Git;"
-APP_VERSION=1.4.3
+APP_VERSION=1.5.0
 APP_EXT=jar
 FILE_NAME=${APP_NAME}-linux-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-${APP_VERSION}
 if ! [ -x "$(command -v java)" ]; then
