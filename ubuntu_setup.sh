14bd902823d817590d9ffb54381edb9438f95a4b
Tim Jones <tdjones74021@yahoo.com>
2026-08-21 17:35:56 -0500

    feat: Add commands to install BoolDownload cross-platform, C#/.NET-based GUI download manager from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -59705,3 +59705,14 @@ curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/$(e
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/*${APP_NAME,,}*
+
+# Install BoolDownload cross-platform, C#/.NET-based GUI download manager from Debian package
+APP_NAME=BoolDownload
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]' )
+APP_VERSION=1.0.0
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}-1.el7_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
