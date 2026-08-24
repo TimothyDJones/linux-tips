aebf4f945b5668ecc5524766c598aee939004ac0
Tim Jones <tdjones74021@yahoo.com>
2026-08-24 08:37:35 -0500

    feat: Add commands to install BoolPlayer cross-platform, C#/.NET-based GUI media player from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -59751,3 +59751,14 @@ EOF
 sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${APP_NAME}*
+
+# Install BoolPlayer cross-platform, C#/.NET-based GUI media player from Debian package
+APP_NAME=BoolPlayer
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]' )
+APP_VERSION=1.0.0
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}-1.el7_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
