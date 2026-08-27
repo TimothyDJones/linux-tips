b2941d34141c5d5894a1550c7fae6e45048c98bf
Tim Jones <tdjones74021@yahoo.com>
2026-08-27 09:54:31 -0500

    feat: Update Vivaldi web browser (stable version) to version 8.1.4087.75.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -568,7 +568,7 @@ sudo apt-get install -y atom
 
 # Install Vivaldi web browser (stable version) from Debian package
 APP_NAME=Vivaldi
-APP_VERSION=8.1.4087.70
+APP_VERSION=8.1.4087.75
 APP_EXT=deb
 FILE_NAME=${APP_NAME,,}-stable_${APP_VERSION}-1_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.vivaldi.com/stable/${FILE_NAME}.${APP_EXT}
@@ -59899,7 +59899,6 @@ sudo chmod a+x /usr/local/bin/${FILE_NAME}
 sudo ln -s -f /usr/local/bin/${FILE_NAME} /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${FILE_NAME}*
-
 
 # Install Xfetch cross-platform, Rust-based command-line system information tool from package
 APP_NAME=Xfetch
