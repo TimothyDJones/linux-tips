b5d74ea9c26421af4b0ce8f538acad1ac3d4698a
Tim Jones <tdjones74021@yahoo.com>
2026-08-31 12:40:26 -0500

    feat: Update F2 cross-platform, command-line batch file renamer to version 2.2.3.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -31397,7 +31397,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install F2 cross-platform, command-line batch file renamer from package
 APP_NAME=F2
-APP_VERSION=1.9.1
+APP_VERSION=2.2.3
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/ayoisaiah/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
