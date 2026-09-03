03474fc249a4c50040264791b9c4e29993f31ea8
Tim Jones <tdjones74021@yahoo.com>
2026-09-03 08:19:04 -0500

    feat: Update Git Interactive Rebase Tool command-line tool for performing interactive rebase in Git to version 2.4.1.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -33515,19 +33515,23 @@ rm -rf /tmp/${APP_NAME,,}*
 
 # Install Git Interactive Rebase Tool command-line tool for performing interactive rebase in Git from Debian package
 APP_NAME=git-interactive-rebase-tool
-APP_VERSION=2.3.0
+APP_VERSION=2.4.1
 APP_EXT=deb
 source /etc/lsb-release
-if [[ "${DISTRIB_CODENAME:0:2}" =~ ^(xe|ya|ze|ar)$ ]]; then   # 16.04 - 17.10
-    DISTRIB_RELEASE=16.04
-elif [[ "${DISTRIB_CODENAME:0:2}" =~ ^(bi|co|di|eo)$ ]]; then   # 18.04 - 19.10
-	DISTRIB_RELEASE=18.04
-elif [[ "${DISTRIB_CODENAME:0:2}" =~ ^(fo|gr|hi|im)$ ]]; then
-	DISTRIB_RELEASE=20.04
+if [[ "${DISTRIB_CODENAME:0:2}" =~ ^(fo|gr|hi|im|bi|co|di|eo|xe|ya|ze|ar)$ ]]; then
+	DISTRIB_RELEASE=ubuntu-20.04
 elif [[ "${DISTRIB_CODENAME:0:2}" =~ ^(ja|ki|lu|ma)$ ]]; then
-	DISTRIB_RELEASE=22.10
+	DISTRIB_RELEASE=ubuntu-22.04
+elif [[ "${DISTRIB_CODENAME:0:2}" =~ ^(no|or|pl|qu|re|st)$ ]]; then
+	DISTRIB_RELEASE=ubuntu-24.04
+elif [[ "${DISTRIB_CODENAME:0:3}" =~ ^(Bus)$ ]]; then
+	DISTRIB_RELEASE=debian-10
+elif [[ "${DISTRIB_CODENAME:0:3}" =~ ^(Bu1)$ ]]; then
+	DISTRIB_RELEASE=debian-11
+elif [[ "${DISTRIB_CODENAME:0:3}" =~ ^(Boo|Tri)$ ]]; then
+	DISTRIB_RELEASE=debian-12
 fi
-FILE_NAME=${APP_NAME,,}-${APP_VERSION}-ubuntu-${DISTRIB_RELEASE}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+FILE_NAME=${APP_NAME,,}-${APP_VERSION}-${DISTRIB_RELEASE}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/MitMaro/${APP_NAME,,}/releases/download/${APP_VERSION}/${FILE_NAME}.${APP_EXT}
 sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
