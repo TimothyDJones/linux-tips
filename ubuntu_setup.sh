ef53789de9352dfd1ce337ce5a26274e28b44636
Tim Jones <tdjones74021@yahoo.com>
2026-09-03 07:24:01 -0500

    feat: Add commands to install wmux Rust-based, cross-platform customizable terminal multiplexer from package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -60143,7 +60143,7 @@ FILE_NAME=${APP_NAME,,}-$(dpkg-architecture --query DEB_BUILD_GNU_CPU)-unknown-l
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/shreshthkapai/${APP_NAME,,}/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
 cd /tmp
 tar -xf /tmp/${FILE_NAME}.${APP_EXT}
-sudo cp -a -R /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
-sudo chmod a+x /usr/local/bin/${APP_NAME,,}
+sudo cp -a -R /tmp/${FILE_NAME}/${APP_NAME,,}* /usr/local/bin
+sudo chmod a+x /usr/local/bin/${APP_NAME,,}*
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${FILE_NAME}*
