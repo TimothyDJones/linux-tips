eb72164f08bca3962857eb1ad3819140f5b5deda
Tim Jones <tdjones74021@yahoo.com>
2026-09-08 07:40:45 -0500

    feat: Update mise en place cross‐platform, Rust-based, run-time package manager to version 2026.9.2.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -42581,7 +42581,7 @@ sudo rm -rf /tmp/${APP_NAME,,}*
 APP_NAME=mise
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross‐platform, Rust-based, run-time package manager."
-APP_VERSION=2026.9.1
+APP_VERSION=2026.9.2
 APP_EXT=tar.xz
 BASE_ARCH=
 FILE_NAME=${APP_NAME,,}-v${APP_VERSION}-linux-x64
