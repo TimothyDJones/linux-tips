c98e2966439900d890ba1587276b84eb6d22458f
Tim Jones <tdjones74021@yahoo.com>
2026-09-08 06:41:44 -0500

    feat: Update phpSysInfo web-based Linux system information utility to version 3.4.7.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -8212,7 +8212,7 @@ rm -rf /tmp/${APP_NAME,,}
 
 # Install phpSysInfo web-based Linux system information utility
 APP_NAME=phpSysInfo
-APP_VERSION=3.4.6
+APP_VERSION=3.4.7
 APP_EXT=tar.gz
 DB_NAME=${APP_NAME,,}
 DB_USER=${APP_NAME,,}
