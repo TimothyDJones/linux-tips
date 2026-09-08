ba561aeebc1d598f6031309fd969687746d32e76
Tim Jones <tdjones74021@yahoo.com>
2026-09-08 16:38:08 -0500

    feat: Add commands to install ImageGlass cross-platform, C#/.NET-based multi-format minimalist image viewer from AppImage.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -60198,3 +60198,39 @@ EOF
 sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
 cd $HOME
 rm -rf /tmp/${_APP_NAME}* /tmp/${APP_NAME// /}*
+
+# Install ImageGlass cross-platform, C#/.NET-based multi-format minimalist image viewer from AppImage
+# https://github.com/d2phap/ImageGlass
+APP_NAME=ImageGlass
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
+APP_GUI_NAME="Cross-platform, Java-based traditional roguelike game with pixel-art graphics."
+APP_GUI_CATEGORIES="Accessories;Graphics;"
+APP_GUI_KEYWORDS="Image;Viewer;"
+APP_VERSION=10.0.6.906
+APP_EXT=AppImage
+ICON_EXT=png
+FILE_NAME=${APP_NAME}_${APP_VERSION}_linux-x64
+sudo apt install -y fuse libfuse2
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${_APP_NAME}.mirror/${FILE_NAME}.${APP_EXT}
+curl -o /tmp/${_APP_NAME}.${ICON_EXT} -J -L https://a.fsdn.com/allura/mirror/${_APP_NAME}/icon?8f060a7979d712de2de567ecfb570c220fa2379dd31cfa662fe6a7a20e55c4ac
+sudo cp /tmp/${FILE_NAME}.${APP_EXT} /usr/local/bin
+sudo chmod +x /usr/local/bin/${FILE_NAME}.${APP_EXT}
+sudo ln -s -f /usr/local/bin/${FILE_NAME}.${APP_EXT} /usr/local/bin/${_APP_NAME}
+sudo mkdir -p /usr/local/share/icons && sudo cp /tmp/${_APP_NAME}.${ICON_EXT} /usr/local/share/icons/${_APP_NAME}.${ICON_EXT}
+cat > /tmp/${_APP_NAME}.desktop << EOF
+[Desktop Entry]
+Name=${APP_NAME}
+Comment=${APP_GUI_NAME}
+GenericName=${APP_NAME}
+Path=/usr/local/bin
+Exec=/usr/local/bin/${_APP_NAME}
+Icon=/usr/local/share/icons/${_APP_NAME}.${ICON_EXT}
+Type=Application
+StartupNotify=true
+Terminal=false
+Categories=${APP_GUI_CATEGORIES}
+Keywords=${APP_GUI_KEYWORDS}
+EOF
+sudo mv /tmp/${_APP_NAME}.desktop /usr/share/applications/
+cd $HOME
+rm -rf /tmp/${_APP_NAME}* /tmp/${APP_NAME// /}*
