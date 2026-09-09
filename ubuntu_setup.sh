bb46340d73fcbc5a21f9d629eec5a886534bd1e0
Tim Jones <tdjones74021@yahoo.com>
2026-09-09 16:53:49 -0500

    feat: Update DBX cross-platform, Rust/Tauri-based lightweight database client to version 0.6.8.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -48376,7 +48376,7 @@ rm -rf /tmp/*${APP_NAME,,}*
 APP_NAME=dblab
 _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Golang-based command-line database client for multiple databases."
-APP_VERSION=0.49.0
+APP_VERSION=0.50.0
 APP_EXT=tar.gz
 FILE_NAME=${APP_NAME,,}_${APP_VERSION}_linux_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
 curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}.mirror/${FILE_NAME}.${APP_EXT}
@@ -56980,7 +56980,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross-platform, Rust/Tauri-based lightweight database client."
 APP_GUI_CATEGORIES="Development;Programming;"
 APP_GUI_KEYWORDS="Database;Editor;"
-APP_VERSION=0.6.6
+APP_VERSION=0.6.8
 APP_EXT=AppImage
 ICON_EXT=png
 FILE_NAME=${APP_NAME}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
