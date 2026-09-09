6f702b415084512edc715805c7a4659f07e89d7b
Tim Jones <tdjones74021@yahoo.com>
2026-09-09 16:35:34 -0500

    feat: Update jDiskMark cross-platform Java-based GUI disk benchmarking tool to version 0.8.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -45335,49 +45335,14 @@ make && sudo make install
 cd $HOME
 sudo rm -rf /tmp/${APP_NAME,,}*
 
-# Install jDiskMark cross-platform Java-based GUI disk benchmarking tool from package
+# Install jDiskMark cross-platform Java-based GUI disk benchmarking tool from Debian package
 APP_NAME=jDiskMark
 APP_GUI_NAME="Cross-platform Java-based GUI disk benchmarking tool."
-APP_GUI_CATEGORIES="Accessories;System;"
-APP_GUI_KEYWORDS="Disk;Performance;"
-APP_VERSION=0.7.1
-APP_EXT=zip
-FILE_NAME=${APP_NAME,,}-${APP_VERSION}
-if ! [ -x "$(command -v java)" ]; then
-	echo 'Error. Java is not installed. ' >&2
-	echo 'Installing Java...'
-	sudo apt-get install -y openjdk-21-jre
-fi
-curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://github.com/${APP_NAME}/jdm-java/releases/download/v${APP_VERSION}/${FILE_NAME}.${APP_EXT}
-cd /tmp
-unzip /tmp/${FILE_NAME}.${APP_EXT}
-sudo mkdir -p /opt/${APP_NAME,,}
-sudo cp -a -R /tmp/${FILE_NAME}/* /opt/${APP_NAME,,}
-sudo chmod -R 755 /opt/${APP_NAME,,}
-cat > /tmp/${APP_NAME,,} << EOF
-#! /bin/sh
-cd /opt/${APP_NAME,,}
-PATH=/opt/${APP_NAME,,}:\$PATH; export PATH
-java -jar /opt/${APP_NAME,,}/${APP_NAME,,}.jar
-cd $HOME
-EOF
-sudo mv /tmp/${APP_NAME,,} /usr/local/bin
-sudo chmod a+x /usr/local/bin/${APP_NAME,,}
-cat > /tmp/${APP_NAME,,}.desktop << EOF
-[Desktop Entry]
-Name=${APP_NAME}
-Comment=${APP_GUI_NAME}
-GenericName=${APP_NAME}
-Path=/opt/${APP_NAME,,}
-Exec=/usr/local/bin/${APP_NAME,,}
-# Icon=
-Type=Application
-StartupNotify=true
-Terminal=false
-Categories=${APP_GUI_CATEGORIES}
-Keywords=${APP_GUI_KEYWORDS}
-EOF
-sudo mv /tmp/${APP_NAME,,}.desktop /usr/share/applications/
+APP_VERSION=0.8.0
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}*
 
