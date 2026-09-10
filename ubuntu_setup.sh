4ee41b5b94be7b1fe3d49057386f74340d67c30a
Tim Jones <tdjones74021@yahoo.com>
2026-09-10 09:19:50 -0500

    feat: Add commands to install BoolPdf cross-platform, C#/.NET-based PDF reader from Debian package.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -60213,3 +60213,14 @@ sudo cp -a -R /tmp/${FILE_NAME}/${APP_NAME,,} /usr/local/bin
 sudo chmod a+x /usr/local/bin/${APP_NAME,,}
 cd $HOME
 rm -rf /tmp/${APP_NAME,,}* /tmp/${FILE_NAME}*
+
+# Install BoolPdf cross-platform, C#/.NET-based PDF reader from Debian package
+APP_NAME=BoolPdf
+_APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr -d '[:blank:]' )
+APP_VERSION=1.0.0
+APP_EXT=deb
+FILE_NAME=${APP_NAME,,}_${APP_VERSION}-1mint22.3_$(dpkg-architecture --query DEB_BUILD_ARCH_CPU)
+curl -o /tmp/${FILE_NAME}.${APP_EXT} -J -L https://downloads.sourceforge.net/${APP_NAME,,}/${FILE_NAME}.${APP_EXT}
+sudo gdebi -n /tmp/${FILE_NAME}.${APP_EXT}
+cd $HOME
+rm -rf /tmp/*${APP_NAME,,}*
