19bd79a8931da31801cb0df495bca93401c95695
Tim Jones <tdjones74021@yahoo.com>
2026-09-14 09:52:26 -0500

    feat: Update Electorrent cross-platform GUI front-end for multiple BitTorrent clients to version 2.17.0.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -49346,7 +49346,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr ' ' '-')
 APP_GUI_NAME="Cross-platform GUI front-end for multiple BitTorrent clients."
 APP_GUI_CATEGORIES="Multimedia;Internet;"
 APP_GUI_KEYWORDS="BitTorrent;"
-APP_VERSION=2.16.1
+APP_VERSION=2.17.0
 APP_EXT=AppImage
 sudo apt install -y libfuse2
 FILE_NAME=${APP_NAME}-${APP_VERSION}
