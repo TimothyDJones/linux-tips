e017c5e7a0d19898160ad4740e4607538d72936a
Tim Jones <tdjones74021@yahoo.com>
2026-09-14 14:57:55 -0500

    feat: Update Mindolph cross-platform personal knowledge base with mind mapping, UML, and more to version 1.14.7.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -49083,7 +49083,7 @@ _APP_NAME=$(echo ${APP_NAME} | tr '[:upper:]' '[:lower:]' | tr '[:blank:]' '-')
 APP_GUI_NAME="Cross-platform Java-based CPU info tool."
 APP_GUI_CATEGORIES="Office;System;Accessories;"
 APP_GUI_KEYWORDS="Knowledge;PIM;UML;"
-APP_VERSION=1.13.6
+APP_VERSION=1.14.7
 APP_EXT=jar
 FILE_NAME=${APP_NAME}-${APP_VERSION}
 if ! [ -x "$(command -v java)" ]; then
